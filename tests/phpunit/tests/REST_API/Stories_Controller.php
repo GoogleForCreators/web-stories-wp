0214bd49cfa02ca62427732963919c2c622791fc
Pascal Birchler <pascalb@google.com>
2020-01-30 16:27:18 -0800

    Add test class for Stories_Controller

---
+++ tests/phpunit/tests/REST_API/Stories_Controller.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Web_Stories\Tests\REST_API;
+
+use Spy_REST_Server;
+use WP_REST_Request;
+
+class Stories_Controller extends \WP_Test_REST_TestCase {
+	protected $server;
+
+	public function setUp() {
+		parent::setUp();
+
+		/** @var \WP_REST_Server $wp_rest_server */
+		global $wp_rest_server;
+		$wp_rest_server = new Spy_REST_Server;
+		do_action( 'rest_api_init', $wp_rest_server );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		/** @var \WP_REST_Server $wp_rest_server */
+		global $wp_rest_server;
+		$wp_rest_server = null;
+	}
+
+	public function test_register_routes() {
+		$routes = rest_get_server()->get_routes();
+
+		$this->assertArrayHasKey( '/wp/v2/web-story', $routes );
+		$this->assertCount( 2, $routes['/wp/v2/web-story'] );
+	}
+
+	public function test_get_item_schema() {
+		$request    = new WP_REST_Request( 'OPTIONS', '/wp/v2/web-story' );
+		$response   = rest_get_server()->dispatch( $request );
+		$data       = $response->get_data();
+		$properties = $data['schema']['properties'];
+		$this->assertArrayHasKey( 'story_data', $properties );
+		$this->assertArrayHasKey( 'featured_media_url', $properties );
+	}
+}
