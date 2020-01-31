6ed9f22466bf86dfb241e6160ff20aa6054348cf
Pascal Birchler <pascalb@google.com>
2020-01-30 16:18:56 -0800

    Begin with tests for fonts controller

---
+++ tests/phpunit/tests/REST_API/Fonts_Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Google\Web_Stories\Tests\REST_API;
+
+use Spy_REST_Server;
+use WP_REST_Request;
+
+class Fonts_Controller extends \WP_Test_REST_TestCase {
+	protected $server;
+
+	public function setUp() {
+		parent::setUp();
+
+		/** @var \WP_REST_Server $wp_rest_server */
+		global $wp_rest_server;
+		$wp_rest_server = new Spy_REST_Server;
+		do_action( 'rest_api_init', $wp_rest_server );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+
+		/** @var \WP_REST_Server $wp_rest_server */
+		global $wp_rest_server;
+		$wp_rest_server = null;
+	}
+
+	public function test_register_routes() {
+		$routes = rest_get_server()->get_routes();
+
+		$this->assertArrayHasKey( '/amp/v1/fonts', $routes );
+		$this->assertCount( 1, $routes['/amp/v1/fonts'] );
+	}
+
+	public function test_get_item_schema() {
+		$request    = new WP_REST_Request( 'OPTIONS', '/amp/v1/fonts' );
+		$response   = rest_get_server()->dispatch( $request );
+		$data       = $response->get_data();
+		$properties = $data['schema']['properties'];
+		$this->assertCount( 6, $properties );
+		$this->assertArrayHasKey( 'name', $properties );
+		$this->assertArrayHasKey( 'slug', $properties );
+		$this->assertArrayHasKey( 'handle', $properties );
+		$this->assertArrayHasKey( 'fallbacks', $properties );
+		$this->assertArrayHasKey( 'weights', $properties );
+		$this->assertArrayHasKey( 'src', $properties );
+	}
+}
