656eee7d35fa117e564659f39a90639f2c03346a
Pascal Birchler <pascalb@google.com>
2020-02-17 20:08:56 +0100

    Format JS files in tests folder

--- tests/js/jest.config.js
+++ tests/js/jest.config.js
@@ -15,26 +15,25 @@
  */
 
 module.exports = {
-	rootDir: '../../',
-	...require( '@wordpress/scripts/config/jest-unit.config' ),
-	transform: {
-		'^.+\\.[jt]sx?$': '<rootDir>/node_modules/@wordpress/scripts/config/babel-transform',
-	},
-	setupFiles: [
-		'<rootDir>/tests/js/setup-globals',
-	],
-	testPathIgnorePatterns: [
-		'<rootDir>/.git',
-		'<rootDir>/node_modules',
-		'<rootDir>/build',
-		'_utils',
-	],
-	coveragePathIgnorePatterns: [ '/node_modules/', '<rootDir>/build/' ],
-	coverageReporters: [ 'lcov' ],
-	coverageDirectory: '<rootDir>/build/logs',
-	collectCoverageFrom: [
-		'<rootDir>/assets/src/edit-story/**/*.js',
-		'!**/test/**',
-	],
-	reporters: [ [ 'jest-silent-reporter', { useDots: true } ] ],
+  rootDir: '../../',
+  ...require('@wordpress/scripts/config/jest-unit.config'),
+  transform: {
+    '^.+\\.[jt]sx?$':
+      '<rootDir>/node_modules/@wordpress/scripts/config/babel-transform',
+  },
+  setupFiles: ['<rootDir>/tests/js/setup-globals'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.git',
+    '<rootDir>/node_modules',
+    '<rootDir>/build',
+    '_utils',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/build/'],
+  coverageReporters: ['lcov'],
+  coverageDirectory: '<rootDir>/build/logs',
+  collectCoverageFrom: [
+    '<rootDir>/assets/src/edit-story/**/*.js',
+    '!**/test/**',
+  ],
+  reporters: [['jest-silent-reporter', { useDots: true }]],
 };
--- tests/js/setup-globals.js
+++ tests/js/setup-globals.js
@@ -14,6 +14,4 @@
  * limitations under the License.
  */
 
-window.ampStoriesEditSettings = {
-
-};
+window.ampStoriesEditSettings = {};
