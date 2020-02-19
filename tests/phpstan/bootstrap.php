fedd65902ad1855268faafd65c9a16bbf9100864
Pascal Birchler <pascalb@google.com>
2020-02-19 13:04:33 +0100

    Add new constants to phpstan config

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -2,3 +2,6 @@
 
 define( 'WEBSTORIES_VERSION', '0.0.0' );
 define( 'WEBSTORIES_PLUGIN_FILE', dirname( __DIR__, 2 ) . '/web-stories.php' );
+define( 'WEBSTORIES_PLUGIN_DIR_PATH', dirname( WEBSTORIES_PLUGIN_FILE ) );
+define( 'WEBSTORIES_PLUGIN_DIR_URL', 'https://example.com/wp-content/plugins/web-stories/' );
+define( 'WEBSTORIES_MINIMUM_PHP_VERSION', '5.6.0' );
