1c8488bdc812c2983aa6a6dce7a69a6d7aa549a2
Pascal Birchler <pascalb@google.com>
2020-02-28 14:48:41 +0100

    Lint fixes

--- assets/src/edit-story/components/panels/videoPoster.js
+++ assets/src/edit-story/components/panels/videoPoster.js
@@ -18,7 +18,7 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled  from 'styled-components';
 import { useEffect, useState } from 'react';
 
 /**
