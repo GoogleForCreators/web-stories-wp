0baa54f10ac20622e0814e9493538ec0c242c53d
Pascal Birchler <pascalb@google.com>
2020-02-28 16:18:59 +0100

    Add foundation for Dashboard page (#344)
    Adds a simple Dashboard React app that can be used as the foundation for the upcoming work on this.
    
    Refactors the PHP code a little bit to more easily accommodate for this change, but it's still far from perfect.

--- assets/src/dashboard/index.js
+++ assets/src/dashboard/index.js
@@ -15,38 +15,27 @@
  */
 
 /**
- * External dependencies
- */
-import Modal from 'react-modal';
-
-/**
  * WordPress dependencies
  */
 import { render } from '@wordpress/element';
-import domReady from '@wordpress/dom-ready';
 
 /**
  * Internal dependencies
  */
 import App from './app';
-import './style.css'; // This way the general editor styles are loaded before all the component styles.
+import './style.css'; // This way the general dashboard styles are loaded before all the component styles.
 
 /**
- * Initializes the web stories editor.
- *
- * @param {string} id       ID of the root element to render the screen in.
- * @param {Object} config   Story editor settings.
+ * Initializes the dashboard screen.
  */
-const initialize = (id, config) => {
-  const appElement = document.getElementById(id);
+const initialize = () => {
+  const appElement = document.getElementById('web-stories-dashboard');
 
-  // see http://reactcommunity.org/react-modal/accessibility/
-  Modal.setAppElement(appElement);
-
-  render(<App config={config} />, appElement);
+  render(<App />, appElement);
 };
 
-domReady(() => {
-  const { id, config } = window.ampStoriesEditSettings;
-  initialize(id, config);
-});
+if ('loading' === document.readyState) {
+  document.addEventListener('DOMContentLoaded', initialize);
+} else {
+  initialize();
+}
