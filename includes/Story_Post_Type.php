b345d4af12390e71e1f68a1603544d308d2c02c3
Pascal Birchler <pascalb@google.com>
2020-03-06 12:16:16 +0100

    Rename REST routes

--- includes/Story_Post_Type.php
+++ includes/Story_Post_Type.php
@@ -316,7 +316,7 @@ class Story_Post_Type {
 						'media'    => '/wp/v2/media',
 						'users'    => '/wp/v2/users',
 						'statuses' => '/wp/v2/statuses',
-						'fonts'    => '/amp/v1/fonts',
+						'fonts'    => '/web-stories/v1/fonts',
 						'link'     => '/web-stories/v1/link',
 					],
 				],
