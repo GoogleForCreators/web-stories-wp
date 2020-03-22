2fe69f7668082c0837f97741c03d0e8bc00c7280
Wassim Gharbi <wassgha@gmail.com>
2020-03-22 14:22:14 -0700

    Fixed integration test (a11y issue)

--- assets/src/edit-story/masks/output.js
+++ assets/src/edit-story/masks/output.js
@@ -74,7 +74,7 @@ export default function WithMask({
   // @todo: Chrome cannot do inline clip-path using data: URLs.
   // See https://bugs.chromium.org/p/chromium/issues/detail?id=1041024.
 
-  const maskId = `mask-${mask.type}-${element.id}`;
+  const maskId = `mask-${mask.type}-${element.id}-output`;
 
   return (
     <div
