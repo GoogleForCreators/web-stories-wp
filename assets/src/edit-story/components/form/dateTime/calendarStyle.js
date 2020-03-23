9266e22bfae2b90b43b0865e5c4ce884b6502f12
Miina Sikk <miina.sikk@gmail.com>
2020-03-23 09:40:49 +0100

    Remove unnecessary colors from CSS.

--- assets/src/edit-story/components/form/dateTime/calendarStyle.js
+++ assets/src/edit-story/components/form/dateTime/calendarStyle.js
@@ -26,48 +26,6 @@ export const GlobalStyle = createGlobalStyle`
 .CalendarDay__selected:hover {
   background: rgb(0, 113, 158);
 }
-body.admin-color-sunrise .CalendarDay__selected {
-  background: #d1864a;
-}
-body.admin-color-ocean .CalendarDay__selected {
-  background: #a3b9a2;
-}
-body.admin-color-midnight .CalendarDay__selected {
-  background: #e14d43;
-}
-body.admin-color-ectoplasm .CalendarDay__selected {
-  background: #a7b656;
-}
-body.admin-color-coffee .CalendarDay__selected {
-  background: #c2a68c;
-}
-body.admin-color-blue .CalendarDay__selected {
-  background: #82b4cb;
-}
-body.admin-color-light .CalendarDay__selected {
-  background: #0085ba;
-}
-body.admin-color-sunrise .CalendarDay__selected:hover {
-  background: rgb(178, 114, 63);
-}
-body.admin-color-ocean .CalendarDay__selected:hover {
-  background: rgb(139, 157, 138);
-}
-body.admin-color-midnight .CalendarDay__selected:hover {
-  background: rgb(191, 65, 57);
-}
-body.admin-color-ectoplasm .CalendarDay__selected:hover {
-  background: rgb(142, 155, 73);
-}
-body.admin-color-coffee .CalendarDay__selected:hover {
-  background: rgb(165, 141, 119);
-}
-body.admin-color-blue .CalendarDay__selected:hover {
-  background: rgb(111, 153, 173);
-}
-body.admin-color-light .CalendarDay__selected:hover {
-  background: rgb(0, 113, 158);
-}
 .DayPickerNavigation_button__horizontalDefault {
   padding: 2px 8px;
   top: 20px; }
