80deb80bfbd2415f2c1edc3a6cf85ad10a24a19e
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-04-02 10:17:38 -0700

    query cleanup

--- assets/src/dashboard/app/api/apiProvider.js
+++ assets/src/dashboard/app/api/apiProvider.js
@@ -58,13 +58,12 @@ export default function ApiProvider({ children }) {
         return [];
       }
       const perPage = '100'; // TODO set up pagination
-      const query = searchTerm
-        ? {
-            status,
-            search: `${searchTerm}`,
-            per_page: perPage,
-          }
-        : { status, per_page: perPage, context: 'edit' };
+      const query = {
+        status,
+        per_page: perPage,
+        context: 'edit',
+        search: searchTerm || undefined,
+      };
 
       try {
         const path = queryString.stringifyUrl({
