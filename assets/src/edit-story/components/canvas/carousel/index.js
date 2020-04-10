d073489c334844f0cdcae9268c8ef3023957f26c
Wassim Gharbi <wassgha@gmail.com>
2020-04-10 12:10:43 -0700

    Hide scrollbar on gridview

--- assets/src/edit-story/components/canvas/carousel/index.js
+++ assets/src/edit-story/components/canvas/carousel/index.js
@@ -122,7 +122,7 @@ const PageList = styled(Reorderable).attrs({
   align-items: center;
   justify-content: ${({ hasHorizontalOverflow }) =>
     hasHorizontalOverflow ? 'flex-start' : 'center'};
-  overflow-x: scroll;
+  overflow-x: auto;
   overflow-y: hidden;
   margin: 0 0 ${CAROUSEL_BOTTOM_SCROLL_MARGIN}px 0;
   padding: 0px 10px;
