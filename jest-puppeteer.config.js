421c344cb2ffbd021622007d0b09a8cd9975203b
Pascal Birchler <pascalb@google.com>
2020-04-24 06:37:26 +0200

    Enable E2E Tests on Firefox (#1317)
    * Update Puppeteer to version 3
    
    * Env: Remove dummy favicon
    
    * E2E: change browser depending on `PUPPETEER_PRODUCT` env var
    
    * E2E: ignore warning emitted by Firefox
    
    * E2E: display browser version at beginning
    
    * Fix jest puppeteer config
    
    * Ignore warning in Firefox
    
    * Add npm scripts for firefox e2e testing
    
    * Run firefox e2e tests on Travis
    
    * Lint fix
    
    * Try setting `PUPPETEER_SKIP_CHROMIUM_DOWNLOAD`
    
    * Ignore another message
    
    * Remove debug cruft

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -18,5 +18,7 @@ module.exports = {
   launch: {
     headless: process.env.PUPPETEER_HEADLESS !== 'false',
     slowMo: parseInt(process.env.PUPPETEER_SLOWMO) || 0,
+    dumpio: true,
+    product: process.env.PUPPETEER_PRODUCT || 'chrome',
   },
 };
