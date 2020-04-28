566f51381234f4a16d9b6852500a3fc4eed3f451
Diego Varese <diegovar@google.com>
2020-04-28 11:22:18 +1000

    Add image changes

--- assets/src/edit-story/elements/image/test/output.js
+++ assets/src/edit-story/elements/image/test/output.js
@@ -15,35 +15,63 @@
  */
 
 /**
+ * External dependencies
+ */
+import { renderToStaticMarkup } from 'react-dom/server';
+
+/**
  * Internal dependencies
  */
 import ImageOutput from '../output';
 
 describe('Image output', () => {
-  it('should produce valid AMP output', async () => {
-    const props = {
-      element: {
-        id: '123',
+  const baseProps = {
+    element: {
+      id: '123',
+      type: 'image',
+      mimeType: 'image/png',
+      scale: 1,
+      origRatio: 9 / 16,
+      x: 50,
+      y: 100,
+      height: 1920,
+      width: 1080,
+      rotationAngle: 0,
+      resource: {
+        id: 123,
         type: 'image',
         mimeType: 'image/png',
-        scale: 1,
-        x: 50,
-        y: 100,
+        src: 'https://example.com/image.png',
+        alt: 'alt text',
         height: 1920,
         width: 1080,
-        rotationAngle: 0,
-        resource: {
-          id: 123,
-          type: 'image',
-          mimeType: 'image/png',
-          src: 'https://example.com/image.png',
-          height: 1920,
-          width: 1080,
-        },
       },
-      box: { width: 1080, height: 1920, x: 50, y: 100, rotationAngle: 0 },
-    };
+    },
+    box: { width: 1080, height: 1920, x: 50, y: 100, rotationAngle: 0 },
+  };
 
-    await expect(<ImageOutput {...props} />).toBeValidAMPStoryElement();
+  it('should produce valid AMP output', async () => {
+    await expect(<ImageOutput {...baseProps} />).toBeValidAMPStoryElement();
+  });
+
+  it('an undefined alt tag in the element should fall back to the resource', async () => {
+    const props = {
+      ...baseProps,
+      element: { ...baseProps.element, alt: undefined },
+    };
+    const output = <ImageOutput {...props} />;
+    await expect(output).toBeValidAMPStoryElement();
+    const outputStr = renderToStaticMarkup(output);
+    await expect(outputStr).toStrictEqual(expect.stringMatching('alt text'));
+  });
+
+  it('an empty string alt tag in the element should not fall back to the resource', async () => {
+    const props = { ...baseProps, element: { ...baseProps.element, alt: '' } };
+    const output = <ImageOutput {...props} />;
+    await expect(output).toBeValidAMPStoryElement();
+    const outputStr = renderToStaticMarkup(output);
+    await expect(outputStr).not.toStrictEqual(
+      expect.stringMatching('alt text')
+    );
   });
 });
