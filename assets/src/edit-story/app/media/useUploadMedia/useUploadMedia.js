de333b4502ad093f1670c537588229c8850be11e
Jon Htin <jhtin@google.com>
2020-04-30 21:29:33 +1000

    Update useUploadMedia.js

--- assets/src/edit-story/app/media/useUploadMedia/useUploadMedia.js
+++ assets/src/edit-story/app/media/useUploadMedia/useUploadMedia.js
@@ -62,7 +62,7 @@ function useUploadMedia({ media, pagingNum, mediaType, fetchMedia, setMedia }) {
             localResource: await getResourceFromLocalFile(file),
             file,
           }))
-        );
+        ).then((files) => files.filter((f) => f.localResource != null));
 
         if (onLocalFile) {
           localFiles = localFiles.map(({ localResource, file }) => {
