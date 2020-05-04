0081a79c9cf9a0c7a02b59038e63d741f396aaee
Morten Barklund <morten.barklund@xwp.co>
2020-05-04 15:26:32 -0400

    Added jsdocs for complex style manipulation functions

--- assets/src/edit-story/components/richText/styleManipulation.js
+++ assets/src/edit-story/components/richText/styleManipulation.js
@@ -25,6 +25,14 @@ import { Modifier, EditorState } from 'draft-js';
 import { NONE } from './customConstants';
 import { getAllStyleSetsInSelection } from './draftUtils';
 
+/**
+ * Get a first style in the given set that match the given prefix,
+ * or NONE if there's no match.
+ *
+ * @param {Set.<string>} styles  Set (ImmutableSet even) of styles to check
+ * @param {string} prefix  Prefix to test styles for
+ * @return {string} First match or NONE
+ */
 export function getPrefixStyleForCharacter(styles, prefix) {
   const list = styles.toArray().map((style) => style.style ?? style);
   const matcher = (style) => style && style.startsWith(prefix);
@@ -34,6 +42,54 @@ export function getPrefixStyleForCharacter(styles, prefix) {
   return list.find(matcher);
 }
 
+/**
+ * Get a deduped list of all matching styles for every character in the
+ * current selection for the current content.
+ *
+ * A style is matching, if it matches the current prefix. If for any given
+ * character in the current selection, there's no prefix match, NONE will
+ * be part of the returned set.
+ *
+ * If selection is collapsed, it'll get the style matching at the current
+ * insertion point (or NONE).
+ *
+ * <example>
+ * // In all examples below,
+ * // - first character is WEIGHT-700, ITALIC
+ * // - second character is WEIGHT-900, ITALIC
+ * // - third character is ITALIC
+ * // - all other characters have no styling
+ *
+ * // This is the text: Hello
+ * // Editor state with selection "[He]llo":
+ * const editorStateEl = {};
+ * // Editor state with selection "[Hel]lo":
+ * const editorStateHel = {};
+ * // Editor state with selection "[Hello]":
+ * const editorStateHello = {};
+ * // Editor state with selection "Hel[lo]":
+ * const editorStateLo = {};
+ *
+ * const styles = getPrefixStylesInSelection(editorStateEl, 'ITALIC');
+ * // styles are now: ['ITALIC']
+ *
+ * const styles = getPrefixStylesInSelection(editorStateHello, 'ITALIC');
+ * // styles are now: ['ITALIC', 'NONE']
+ *
+ * const styles = getPrefixStylesInSelection(editorStateLo, 'ITALIC');
+ * // styles are now: ['NONE']
+ *
+ * const styles = getPrefixStylesInSelection(editorStateHel, 'WEIGHT');
+ * // styles are now: ['WEIGHT-700', 'WEIGHT-900', 'NONE']
+ *
+ * const styles = getPrefixStylesInSelection(editorStateLo, 'WEIGHT');
+ * // styles are now: ['NONE']
+ * </example>
+ *
+ * @param {Object} editorState  Current editor state
+ * @param {string} prefix  Prefix to test styles for
+ * @return {Array.<string>} Deduped array of all matching styles
+ */
 export function getPrefixStylesInSelection(editorState, prefix) {
   const selection = editorState.getSelection();
   if (selection.isCollapsed()) {
@@ -55,6 +111,21 @@ function applyContent(editorState, contentState) {
   return EditorState.push(editorState, contentState, 'change-inline-style');
 }
 
+/**
+ * Toggle prefix style in selection. This is a pretty complex function and
+ * it's probably easiest to understand how it works by following the inline
+ * comments and reading through the corresponding exhaustive unit tests.
+ *
+ * @param {Object} editorState  Current editor state
+ * @param {string} prefix  Style (prefix) to remove from state and potentially
+ * replace with different style
+ * @param {Function} shouldSetStyle  Optional function to get if new style
+ * should be added or not
+ * @param {Function} getStyleToSet  Optional function to get what new style
+ * should be added
+ *
+ * @return {Object} New editor state
+ */
 export function togglePrefixStyle(
   editorState,
   prefix,
