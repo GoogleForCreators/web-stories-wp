c2b0340fdc14e5153b74e031e5ac02e9d319f0b0
Miina <miina.sikk@gmail.com>
2020-05-04 08:38:09 +0200

    Auto-Save (for Draft) (#1361)
    * Start adding auto-save logic.
    
    * Add regular auto-save
    
    * Filter revision fields.
    
    * Exclude auto-save from influencing unload prevention.
    
    * Disable for a published post.
    
    * Disable Save Draft button when nothing to save.
    
    * Check if autosave_interval is defined.
    
    * Use cleanup function in hook.
    
    Allow settings new changes to false only.
    
    * Remove debugging values.
    
    * Remove all the logic related to published posts.
    
    * Always use save instead of auto-save for now.
    
    * Add test for auto-saving
    
    * Add missin test.
    
    * Return undefined.
    
    * Change version number when undoing and redoing.
    
    * Use version number properly for tracking changes.
    
    * Set timeout to null after executing.
    
    * Simplify logic
    
    Use callback

---
+++ assets/src/edit-story/components/autoSaveHandler/test/autoSaveHandler.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { render } from '@testing-library/react';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import HistoryContext from '../../../app/history/context';
+import StoryContext from '../../../app/story/context';
+import ConfigContext from '../../../app/config/context';
+import AutoSaveHandler from '../index';
+
+function setup({
+  hasNewChanges = true,
+  status = 'draft',
+  autoSaveInterval = 0.1,
+}) {
+  const saveStory = jest.fn();
+  const historyContextValue = { state: { hasNewChanges } };
+  const configValue = {
+    autoSaveInterval,
+  };
+  const storyContextValue = {
+    state: {
+      story: { status },
+    },
+    actions: { saveStory },
+  };
+  render(
+    <ConfigContext.Provider value={configValue}>
+      <HistoryContext.Provider value={historyContextValue}>
+        <StoryContext.Provider value={storyContextValue}>
+          <AutoSaveHandler />
+        </StoryContext.Provider>
+      </HistoryContext.Provider>
+    </ConfigContext.Provider>
+  );
+  return {
+    saveStory,
+  };
+}
+
+jest.useFakeTimers();
+
+describe('AutoSaveHandler', () => {
+  it('should trigger saving in case of a draft', () => {
+    const { saveStory } = setup({});
+    jest.runAllTimers();
+    expect(saveStory).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not trigger saving in case of not having new changes', () => {
+    const { saveStory } = setup({ hasNewChanges: false });
+    jest.runAllTimers();
+    expect(saveStory).toHaveBeenCalledTimes(0);
+  });
+
+  it('should not trigger saving in case of a non-draft post', () => {
+    const { saveStory } = setup({ hasNewChanges: true, status: 'publish' });
+    jest.runAllTimers();
+    expect(saveStory).toHaveBeenCalledTimes(0);
+  });
+
+  it('should not trigger saving when interval is 0', () => {
+    const { saveStory } = setup({
+      autoSaveInterval: 0,
+    });
+    jest.runAllTimers();
+    expect(saveStory).toHaveBeenCalledTimes(0);
+  });
+});
