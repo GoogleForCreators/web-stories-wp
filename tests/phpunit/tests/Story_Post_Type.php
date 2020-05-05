0b2df12780939c9f1cc007364674336e0b077732
spacedmonkey <jon@spacedmonkey.co.uk>
2020-05-05 13:21:50 +0100

    Add some basic php unit tests

---
+++ tests/phpunit/tests/Story_Post_Type.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Web_Stories\Tests;
+
+class Story_Post_Type extends \WP_UnitTestCase {
+	/**
+	 * Admin user for test.
+	 *
+	 * @var int
+	 */
+	protected static $admin_id;
+
+	/**
+	 * Subscriber user for test.
+	 *
+	 * @var int
+	 */
+	protected static $subscriber_id;
+
+	public static function wpSetUpBeforeClass( $factory ) {
+		self::$admin_id      = $factory->user->create(
+			[ 'role' => 'administrator' ]
+		);
+		self::$subscriber_id = $factory->user->create(
+			[ 'role' => 'subscriber' ]
+		);
+	}
+
+	public function test_get_editor_settings_admin() {
+		wp_set_current_user( self::$admin_id );
+		$post_type = new \Google\Web_Stories\Story_Post_Type();
+		$results   = $post_type::get_editor_settings();
+		$this->assertTrue( $results['config']['capabilities']['hasUploadMediaAction'] );
+	}
+
+	public function test_get_editor_settings_sub() {
+		wp_set_current_user( self::$subscriber_id );
+		$post_type = new \Google\Web_Stories\Story_Post_Type();
+		$results   = $post_type::get_editor_settings();
+		$this->assertTrue( $results['config']['capabilities']['hasUploadMediaAction'] );
+	}
+
+
+}
