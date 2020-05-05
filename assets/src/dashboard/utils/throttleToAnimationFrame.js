7157f473a0e59fabf14b29c0d996bbc7e048803f
Maxwell Yinger <max.yinger@formidable.com>
2020-05-05 14:55:04 -0600

    remove external comment

--- assets/src/dashboard/utils/throttleToAnimationFrame.js
+++ assets/src/dashboard/utils/throttleToAnimationFrame.js
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * External dependencies
- */
 
 export default (callback) => {
   let frameId = null;
