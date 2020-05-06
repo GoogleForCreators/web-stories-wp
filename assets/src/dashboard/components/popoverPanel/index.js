20349ea460abbb1d1d65562b1e7e3106d195aead
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-05-06 10:09:38 -0700

    update panel spacing and eliminate header area in component. WIP remove header props from storybook and usecases

--- assets/src/dashboard/components/popoverPanel/index.js
+++ assets/src/dashboard/components/popoverPanel/index.js
@@ -23,68 +23,42 @@ import styled from 'styled-components';
 /**
  * Internal dependencies
  */
-import { ReactComponent as CloseIcon } from '../../icons/close.svg';
 import { Z_INDEX } from '../../constants';
 import { Pill } from '../pill';
 import { DROPDOWN_ITEM_PROP_TYPE } from '../types';
 import { ColorDot } from '../colorDot';
 
 export const Panel = styled.div`
-  align-items: flex-start;
-  background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 8px;
-  box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.25);
-  display: flex;
-  flex-direction: column;
-  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
-  overflow: hidden;
-  padding: 20px 20px 8px;
-  margin: 24px 0 0;
-  position: absolute;
-  pointer-events: ${({ isOpen }) => (isOpen ? 'auto' : 'none')};
-  transform: ${({ isOpen }) =>
-    isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, -1rem, 0)'};
-  z-index: ${Z_INDEX.POPOVER_PANEL};
-  width: ${({ theme }) => theme.popoverPanel.desktopWidth}px;
+  ${({ isOpen, theme }) => `
+    align-items: flex-start;
+    background-color: ${theme.colors.white};
+    border-radius: 8px;
+    box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.25);
+    display: flex;
+    flex-direction: column;
+    opacity: ${isOpen ? 1 : 0};
+    overflow: hidden;
+    padding: 10px 20px;
+    margin: 10px 0 0;
+    position: absolute;
+    pointer-events: ${isOpen ? 'auto' : 'none'};
+    transform: ${isOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, -1rem, 0)'};
+    z-index: ${Z_INDEX.POPOVER_PANEL};
+    width: ${theme.popoverPanel.desktopWidth}px;
 
-  @media ${({ theme }) => theme.breakpoint.tablet} {
-    width: ${({ theme }) => theme.popoverPanel.tabletWidth}px;
-  }
+    @media ${theme.breakpoint.tablet} {
+      width: ${theme.popoverPanel.tabletWidth}px;
+    }
 
-  @media ${({ theme }) => theme.breakpoint.desktop} {
-    width: ${({ theme }) => theme.popoverPanel.desktopWidth}px;
-  }
+    @media ${theme.breakpoint.desktop} {
+      width: ${theme.popoverPanel.desktopWidth}px;
+    }
+  `}
 `;
 
 Panel.propTypes = {
   isOpen: PropTypes.bool,
 };
-
-const CloseButton = styled.button`
-  border: none;
-  background-color: transparent;
-  color: ${({ theme }) => theme.colors.gray400};
-  margin: 0;
-  padding: 0;
-`;
-
-const TitleBar = styled.div`
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  display: flex;
-`;
-
-const PanelHeader = styled.h3`
-  color: ${({ theme }) => theme.colors.gray700};
-  font-family: ${({ theme }) => theme.fonts.heading3.family};
-  font-size: ${({ theme }) => theme.fonts.heading3.size}px;
-  font-weight: ${({ theme }) => theme.fonts.heading3.weight};
-  line-height: ${({ theme }) => theme.fonts.heading3.lineHeight}px;
-  letter-spacing: ${({ theme }) => theme.fonts.heading3.letterSpacing}em;
-  margin: 0;
-  padding-left: 20px;
-`;
 
 const PillFieldset = styled.fieldset`
   width: 100%;
@@ -96,21 +70,14 @@ const PillFieldset = styled.fieldset`
   }
 `;
 
-const PopoverPanel = ({ isOpen, onClose, title, items, onSelect }) => {
+const PopoverPanel = ({ isOpen, title, items, onSelect }) => {
   return (
     <Panel isOpen={isOpen}>
-      <TitleBar>
-        <CloseButton
-          data-testid="popover-close-btn"
-          aria-label="Close Button"
-          onClick={onClose}
-        >
-          <CloseIcon width={13} height={13} />
-        </CloseButton>
-        <PanelHeader>{title}</PanelHeader>
-      </TitleBar>
       {isOpen && (
-        <PillFieldset data-testid={'pill-fieldset'}>
+        <PillFieldset
+          data-testid={'pill-fieldset'}
+          aria-label={`options for ${title}`}
+        >
           {items.map(
             ({ label, selected, value, hex, disabled = false }, index) => {
               return (
@@ -136,7 +103,6 @@ const PopoverPanel = ({ isOpen, onClose, title, items, onSelect }) => {
 
 PopoverPanel.propTypes = {
   title: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
   onSelect: PropTypes.func.isRequired,
   isOpen: PropTypes.bool,
   items: PropTypes.arrayOf(DROPDOWN_ITEM_PROP_TYPE),
