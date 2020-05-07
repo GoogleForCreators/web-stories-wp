4dff7295eda826c91abf679e48a2bb6987c9cb26
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-05-07 13:19:09 -0700

    change ariaDescription to ariaLabel on keyboard close button

--- assets/src/dashboard/components/popoverPanel/index.js
+++ assets/src/dashboard/components/popoverPanel/index.js
@@ -112,7 +112,7 @@ const PopoverPanel = ({ onClose, isOpen, title, items, onSelect }) => {
           <KeyboardCloseOnly
             onClick={onClose}
             data-testid={'popover-close-btn'}
-            ariaDescription={__('close menu', 'web-stories')}
+            ariaLabel={__('close menu', 'web-stories')}
           >
             <CloseIcon width={14} height="14" />
           </KeyboardCloseOnly>
