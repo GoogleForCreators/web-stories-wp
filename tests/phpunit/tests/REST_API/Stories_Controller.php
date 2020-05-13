4c3deda76424139569ed1dae0b2d3c2989dfcda8
spacedmonkey <jon@spacedmonkey.co.uk>
2020-05-13 17:44:35 +0100

    Add statuses.

--- tests/phpunit/tests/REST_API/Stories_Controller.php
+++ tests/phpunit/tests/REST_API/Stories_Controller.php
@@ -86,6 +86,7 @@ class Stories_Controller extends \WP_Test_REST_TestCase {
 		wp_set_current_user( self::$user_id );
 		$request = new WP_REST_Request( 'GET', '/wp/v2/web-story' );
 		$request->set_param( 'author', self::$user_id );
+		$request->set_param( 'status', [ 'publish', 'draft' ] );
 		$request->set_param( 'context', 'edit' );
 		$response = rest_get_server()->dispatch( $request );
 		$headers  = $response->get_headers();
