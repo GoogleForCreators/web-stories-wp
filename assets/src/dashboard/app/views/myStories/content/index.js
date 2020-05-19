df48cffb501fd7e022aab6171450017645d8569b
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-05-19 16:42:24 -0700

    pull my stories index apart into Header and Content as subdirectories. Alphabetize imports. Move providers needed for story preview to Content level, no need to wrap the header contents in it. Storybook for myStories/Content and Header

---
+++ assets/src/dashboard/app/views/myStories/content/index.js
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * External dependencies
+ */
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { useMemo } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { UnitsProvider } from '../../../../../edit-story/units';
+import { TransformProvider } from '../../../../../edit-story/components/transform';
+import {
+  InfiniteScroller,
+  Layout,
+  StandardViewContentGutter,
+} from '../../../../components';
+import {
+  VIEW_STYLE,
+  STORY_ITEM_CENTER_ACTION_LABELS,
+} from '../../../../constants';
+import FontProvider from '../../../font/fontProvider';
+import { NoResults, StoryGridView, StoryListView } from '../../shared';
+import {
+  UsersPropType,
+  TagsPropType,
+  CategoriesPropType,
+  StoriesPropType,
+} from '../../../../types';
+import {
+  FilterPropTypes,
+  ViewPropTypes,
+  PagePropTypes,
+  SortPropTypes,
+} from '../../../../utils/useStoryView';
+
+const DefaultBodyText = styled.p`
+  font-family: ${({ theme }) => theme.fonts.body1.family};
+  font-weight: ${({ theme }) => theme.fonts.body1.weight};
+  font-size: ${({ theme }) => theme.fonts.body1.size}px;
+  line-height: ${({ theme }) => theme.fonts.body1.lineHeight}px;
+  letter-spacing: ${({ theme }) => theme.fonts.body1.letterSpacing}em;
+  color: ${({ theme }) => theme.colors.gray200};
+  margin: 40px 20px;
+`;
+
+function Content({
+  allPagesFetched,
+  categories,
+  filter,
+  isLoading,
+  page,
+  search,
+  sort,
+  stories,
+  storyActions = {},
+  tags,
+  users,
+  view,
+}) {
+  const storiesView = useMemo(() => {
+    switch (view.style) {
+      case VIEW_STYLE.GRID:
+        return (
+          <StoryGridView
+            trashStory={storyActions.trashStory}
+            updateStory={storyActions.updateStory}
+            createTemplateFromStory={storyActions.createTemplateFromStory}
+            duplicateStory={storyActions.duplicateStory}
+            stories={stories}
+            users={users}
+            centerActionLabelByStatus={STORY_ITEM_CENTER_ACTION_LABELS}
+            bottomActionLabel={__('Open in editor', 'web-stories')}
+          />
+        );
+      case VIEW_STYLE.LIST:
+        return (
+          <StoryListView
+            stories={stories}
+            storySort={sort.value}
+            storyStatus={filter.value}
+            sortDirection={sort.direction}
+            handleSortChange={sort.set}
+            handleSortDirectionChange={sort.setDirection}
+            tags={tags}
+            categories={categories}
+            users={users}
+          />
+        );
+      default:
+        return null;
+    }
+  }, [
+    categories,
+    filter.value,
+    sort,
+    storyActions,
+    stories,
+    tags,
+    users,
+    view.style,
+  ]);
+
+  const BodyContent = useMemo(() => {
+    if (stories.length > 0) {
+      return (
+        <StandardViewContentGutter>
+          {storiesView}
+          <InfiniteScroller
+            canLoadMore={!allPagesFetched}
+            isLoading={isLoading}
+            allDataLoadedMessage={__('No more stories', 'web-stories')}
+            onLoadMore={page.requestNextPage}
+          />
+        </StandardViewContentGutter>
+      );
+    } else if (search.keyword.length > 0) {
+      return <NoResults typeaheadValue={search.keyword} />;
+    }
+
+    return (
+      <DefaultBodyText>
+        {__('Create a story to get started!', 'web-stories')}
+      </DefaultBodyText>
+    );
+  }, [
+    stories.length,
+    isLoading,
+    allPagesFetched,
+    page.requestNextPage,
+    search.keyword,
+    storiesView,
+  ]);
+
+  return (
+    <Layout.Scrollable>
+      <FontProvider>
+        <TransformProvider>
+          <UnitsProvider pageSize={view.pageSize}>{BodyContent}</UnitsProvider>
+        </TransformProvider>
+      </FontProvider>
+    </Layout.Scrollable>
+  );
+}
+Content.propTypes = {
+  allPagesFetched: PropTypes.bool,
+  categories: CategoriesPropType,
+  filter: FilterPropTypes,
+  isLoading: PropTypes.bool,
+  page: PagePropTypes,
+  search: PropTypes.object,
+  sort: SortPropTypes,
+  stories: StoriesPropType,
+  storyActions: PropTypes.shape({
+    createTemplateFromStory: PropTypes.func,
+    duplicateStory: PropTypes.func,
+    trashStory: PropTypes.func,
+    updateStory: PropTypes.func,
+  }),
+  tags: TagsPropType,
+  users: UsersPropType,
+  view: ViewPropTypes,
+};
+
+export default Content;
---
+++ assets/src/dashboard/app/views/myStories/content/stories/index.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { action } from '@storybook/addon-actions';
+
+/**
+ * Internal dependencies
+ */
+import Content from '../';
+import {
+  STORY_SORT_OPTIONS,
+  SORT_DIRECTION,
+  VIEW_STYLE,
+  STORY_STATUS,
+} from '../../../../../constants';
+import formattedStoriesArray from '../../../../../storybookUtils/formattedStoriesArray';
+import formattedUsersObject from '../../../../../storybookUtils/formattedUsersObject';
+
+import { Layout } from '../../../../../components';
+
+export default {
+  title: 'Dashboard/Components/myStories/Content',
+  component: Content,
+};
+
+const filter = {
+  status: 'all',
+  value: STORY_STATUS.ALL,
+  set: action('set filter'),
+};
+const sort = {
+  value: STORY_SORT_OPTIONS.NAME,
+  set: action('set sort'),
+  direction: SORT_DIRECTION[0],
+  setDirection: action('set sort direction'),
+};
+const search = {
+  keyword: '',
+  setKeyword: action('set search'),
+};
+const view = {
+  style: VIEW_STYLE.GRID,
+  toggleStyle: action('toggle view style'),
+  pageSize: { width: 300, height: 255 },
+};
+const page = {
+  value: 1,
+  set: action('set page number'),
+  requestNextPage: action('request next page clicked'),
+};
+const tags = {};
+const categories = {};
+
+const defaultProps = {
+  allPagesFetched: false,
+  categories: categories,
+  filter: filter,
+  isLoading: false,
+  page: page,
+  search: search,
+  sort: sort,
+  stories: formattedStoriesArray,
+  storyActions: {
+    createTemplateFromStory: action('create template from story clicked'),
+    duplicateStory: action('duplicate story clicked'),
+    trashStory: action('trash story clicked'),
+    updateStory: action('update story clicked'),
+  },
+  tags: tags,
+  users: formattedUsersObject,
+  view: view,
+};
+export const _default = () => (
+  <Layout.Provider>
+    <Content {...defaultProps} />
+  </Layout.Provider>
+);
+
+export const NoStories = () => (
+  <Layout.Provider>
+    <Content {...defaultProps} stories={[]} />
+  </Layout.Provider>
+);
+
+export const AllDataFetched = () => (
+  <Layout.Provider>
+    <Content {...defaultProps} allPagesFetched={true} />
+  </Layout.Provider>
+);
+
+export const AllDataFetchedAsList = () => (
+  <Layout.Provider>
+    <Content
+      {...defaultProps}
+      allPagesFetched={true}
+      view={{
+        ...view,
+        style: VIEW_STYLE.LIST,
+      }}
+    />
+  </Layout.Provider>
+);
