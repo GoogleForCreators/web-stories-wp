5e98afac118fbc11cd029a7dac61302a744700ac
Jon Htin <jhtin@google.com>
2020-05-20 15:18:40 +1000

    Updating test to remove unnecessary wrapper.

--- assets/src/edit-story/components/library/test/shapes/shapePreview.js
+++ assets/src/edit-story/components/library/test/shapes/shapePreview.js
@@ -22,23 +22,21 @@ import { act } from '@testing-library/react';
 /**
  * Internal dependencies
  */
-import { FontProvider } from '../../../../app/font';
 import ShapePreview from '../../panes/shapes/shapePreview';
-import APIContext from '../../../../app/api/context';
 import { renderWithTheme } from '../../../../testUtils/index';
 
 jest.mock('../../useLibrary');
 import useLibrary from '../../useLibrary';
 
-const rectangleMask = {
-  type: 'rectangle',
-  name: 'Rectangle',
-  path: 'M 0,0 1,0 1,1 0,1 0,0 Z',
-  ratio: 1,
-};
-
 describe('ShapePreview', () => {
   const insertElement = jest.fn();
+
+  const rectangleMask = {
+    type: 'rectangle',
+    name: 'Rectangle',
+    path: 'M 0,0 1,0 1,1 0,1 0,0 Z',
+    ratio: 1,
+  };
 
   beforeAll(() => {
     useLibrary.mockImplementation(() => ({
@@ -49,24 +47,12 @@ describe('ShapePreview', () => {
   });
 
   it('should be draggable', async () => {
-    const getAllFontsPromise = Promise.resolve([]);
-    const apiContextValue = {
-      actions: {
-        getAllFonts: () => getAllFontsPromise,
-      },
-    };
-
     let shapePreviewElement;
-    await act(async () => {
+    act(() => {
       const { getByTestId } = renderWithTheme(
-        <APIContext.Provider value={apiContextValue}>
-          <FontProvider apiContextValue>
-            <ShapePreview mask={rectangleMask} />
-          </FontProvider>
-        </APIContext.Provider>
+        <ShapePreview mask={rectangleMask} />
       );
 
-      await getAllFontsPromise;
       // Test that the drag event has the right data transfer object...
       shapePreviewElement = getByTestId('preview');
     });
