d288ca6c48cb0ded03e19fd72044b2660ca6344f
Pascal Birchler <pascalb@google.com>
2020-05-20 16:50:13 +0200

    Web Story Embed block for Gutenberg (#1249)

--- tests/phpunit/tests/REST_API/Embed_Controller.php
+++ tests/phpunit/tests/REST_API/Embed_Controller.php
@@ -1,11 +1,26 @@
 <?php
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace Google\Web_Stories\Tests\REST_API;
 
 use Spy_REST_Server;
 use WP_REST_Request;
 
-class Link_Controller extends \WP_Test_REST_TestCase {
+class Embed_Controller extends \WP_Test_REST_TestCase {
 	/**
 	 * @var \WP_REST_Server
 	 */
@@ -14,10 +29,9 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 	protected static $editor;
 	protected static $subscriber;
 
-	const INVALID_URL = 'https://www.notreallyawebsite.com/foobar.html';
-	const EMPTY_URL   = 'https://empty.example.com';
-	const EXAMPLE_URL = 'https://example.com';
-	const VALID_URL   = 'https://amp.dev';
+	const INVALID_URL              = 'https://www.notreallyawebsite.com/foobar.html';
+	const VALID_URL_EMPTY_DOCUMENT = 'https://empty.example.com';
+	const VALID_URL                = 'https://preview.amp.dev/documentation/examples/introduction/stories_in_amp';
 
 	/**
 	 * Count of the number of requests attempted.
@@ -79,7 +93,7 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 	public function mock_http_request( $preempt, $r, $url ) {
 		$this->request_count += 1;
 
-		if ( false !== strpos( $url, self::EMPTY_URL ) ) {
+		if ( false !== strpos( $url, self::VALID_URL_EMPTY_DOCUMENT ) ) {
 			return [
 				'response' => [
 					'code' => 200,
@@ -88,21 +102,12 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 			];
 		}
 
-		if ( false !== strpos( $url, self::EXAMPLE_URL ) ) {
-			return [
-				'response' => [
-					'code' => 200,
-				],
-				'body'     => file_get_contents( __DIR__ . '/../../data/example.com.html' ),
-			];
-		}
-
 		if ( false !== strpos( $url, self::VALID_URL ) ) {
 			return [
 				'response' => [
 					'code' => 200,
 				],
-				'body'     => file_get_contents( __DIR__ . '/../../data/amp.dev.html' ),
+				'body'     => file_get_contents( __DIR__ . '/../../data/stories_in_amp.html' ),
 			];
 		}
 
@@ -116,9 +121,9 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 	public function test_register_routes() {
 		$routes = rest_get_server()->get_routes();
 
-		$this->assertArrayHasKey( '/web-stories/v1/link', $routes );
+		$this->assertArrayHasKey( '/web-stories/v1/embed', $routes );
 
-		$route = $routes['/web-stories/v1/link'];
+		$route = $routes['/web-stories/v1/embed'];
 		$this->assertCount( 1, $route );
 		$this->assertArrayHasKey( 'callback', $route[0] );
 		$this->assertArrayHasKey( 'permission_callback', $route[0] );
@@ -128,14 +133,14 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 
 	public function test_without_permission() {
 		// Test without a login.
-		$request  = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
+		$request  = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
 		$response = rest_get_server()->dispatch( $request );
 
 		$this->assertEquals( 400, $response->get_status() );
 
 		// Test with a user that does not have edit_posts capability.
 		wp_set_current_user( self::$subscriber );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
 		$request->set_param( 'url', self::VALID_URL );
 		$response = rest_get_server()->dispatch( $request );
 
@@ -144,9 +149,21 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 		$this->assertEquals( $data['code'], 'rest_forbidden' );
 	}
 
+	public function test_url_empty_string() {
+		wp_set_current_user( self::$editor );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
+		$request->set_param( 'url', '' );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 0, $this->request_count );
+		$this->assertEquals( 404, $response->get_status() );
+		$this->assertEquals( $data['code'], 'rest_invalid_url' );
+	}
+
 	public function test_invalid_url() {
 		wp_set_current_user( self::$editor );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
 		$request->set_param( 'url', self::INVALID_URL );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
@@ -157,60 +174,28 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 
 	public function test_empty_url() {
 		wp_set_current_user( self::$editor );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
-		$request->set_param( 'url', self::EMPTY_URL );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
+		$request->set_param( 'url', self::VALID_URL_EMPTY_DOCUMENT );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 
-		$expected = [
-			'title'       => '',
-			'image'       => '',
-			'description' => '',
-		];
-
-		// Subsequent requests is cached and so it should not cause a request.
-		rest_get_server()->dispatch( $request );
-		$this->assertEquals( 1, $this->request_count );
-
-		$this->assertNotEmpty( $data );
-		$this->assertEqualSetsWithIndex( $expected, $data );
-	}
-
-	public function test_example_url() {
-		wp_set_current_user( self::$editor );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
-		$request->set_param( 'url', self::EXAMPLE_URL );
-		$response = rest_get_server()->dispatch( $request );
-		$data     = $response->get_data();
-
-		$expected = [
-			'title'       => 'Example Domain',
-			'image'       => '',
-			'description' => '',
-		];
-
-		// Subsequent requests is cached and so it should not cause a request.
-		rest_get_server()->dispatch( $request );
-		$this->assertEquals( 1, $this->request_count );
-
-		$this->assertNotEmpty( $data );
-		$this->assertEqualSetsWithIndex( $expected, $data );
+		$this->assertEquals( 404, $response->get_status() );
+		$this->assertEquals( $data['code'], 'rest_invalid_story' );
 	}
 
 	public function test_valid_url() {
 		wp_set_current_user( self::$editor );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
 		$request->set_param( 'url', self::VALID_URL );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 
 		$expected = [
-			'title'       => 'AMP - a web component framework to easily create user-first web experiences - amp.dev',
-			'image'       => 'https://amp.dev/static/img/sharing/default-600x314.png',
-			'description' => 'Whether you are a publisher, e-commerce company, storyteller, advertiser or email sender, AMP makes it easy to create great experiences on the web. Use AMP to build websites, stories, ads and emails.',
+			'title'  => 'Stories in AMP - Hello World',
+			'poster' => 'https://amp.dev/static/samples/img/story_dog2_portrait.jpg',
 		];
 
-		// Subsequent requests is cached and so it should not cause a request.
+		// Subsequent requests are cached, so it should not perform another HTTP request.
 		rest_get_server()->dispatch( $request );
 		$this->assertEquals( 1, $this->request_count );
 
@@ -220,19 +205,18 @@ class Link_Controller extends \WP_Test_REST_TestCase {
 
 	public function test_removes_trailing_slashes() {
 		wp_set_current_user( self::$editor );
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
-		$request->set_param( 'url', self::EXAMPLE_URL );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
+		$request->set_param( 'url', self::VALID_URL );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
 
 		$expected = [
-			'title'       => 'Example Domain',
-			'image'       => '',
-			'description' => '',
+			'title'  => 'Stories in AMP - Hello World',
+			'poster' => 'https://amp.dev/static/samples/img/story_dog2_portrait.jpg',
 		];
 
-		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/link' );
-		$request->set_param( 'url', self::EXAMPLE_URL . '/' );
+		$request = new WP_REST_Request( \WP_REST_Server::READABLE, '/web-stories/v1/embed' );
+		$request->set_param( 'url', self::VALID_URL . '/' );
 		rest_get_server()->dispatch( $request );
 		$this->assertEquals( 1, $this->request_count );
 
