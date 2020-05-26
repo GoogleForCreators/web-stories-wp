7e729b81d288a780548c8b2cab7f138b7a54b502
Morten Barklund <morten.barklund@xwp.co>
2020-05-26 18:47:49 -0400

    Change font-display to swap

--- assets/src/edit-story/app/font/fontProvider.js
+++ assets/src/edit-story/app/font/fontProvider.js
@@ -112,7 +112,7 @@ function FontProvider({ children }) {
 
     // Create new <link> in head with ref to new font families
     const families = encodeURIComponent(newMenuFonts.join('|'));
-    const url = `${GOOGLE_MENU_FONT_URL}?family=${families}&subset=menu`;
+    const url = `${GOOGLE_MENU_FONT_URL}?family=${families}&subset=menu&display=swap`;
     loadStylesheet(url).catch(() => {
       // If they failed to load, remove from array again!
       menuFonts.current = menuFonts.current.filter(
