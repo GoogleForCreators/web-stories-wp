376d03a0adae648d03811010994db39d4607e0e4
Pascal Birchler <pascalb@google.com>
2020-05-26 20:42:37 +0200

    phpstan fixes

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -31,6 +31,7 @@ namespace Google\Web_Stories;
 use Google\Web_Stories\REST_API\Embed_Controller;
 use Google\Web_Stories\REST_API\Fonts_Controller;
 use Google\Web_Stories\REST_API\Link_Controller;
+use WP_Screen;
 
 /**
  * Plugin class.
@@ -77,6 +78,9 @@ class Plugin {
 	/**
 	 * Filter the list of admin classes.
 	 *
+	 * Makes sure the admin menu is collapsed when accessing
+	 * the dashboard and the editor.
+	 *
 	 * @param string $class Current classes.
 	 *
 	 * @return string $class List of Classes.
@@ -88,7 +92,12 @@ class Plugin {
 			return $class;
 		}
 
-		if ( self::POST_TYPE_SLUG !== $screen->post_type ) {
+		if ( Story_Post_Type::POST_TYPE_SLUG !== $screen->post_type ) {
+			return $class;
+		}
+
+		// Default WordPress posts list table screen.
+		if ( 'edit' === $screen->base ) {
 			return $class;
 		}
 
