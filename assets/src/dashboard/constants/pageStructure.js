eaa7afad4f2b3f0845ef6dad022a8cf6ecace373
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-05-26 16:10:56 -0700

    it is critical that we use an ID that will exist as the dashboard is created to track resize, so wpbody not an id within the dashboard.

--- assets/src/dashboard/constants/pageStructure.js
+++ assets/src/dashboard/constants/pageStructure.js
@@ -18,8 +18,7 @@
  * Internal dependencies
  */
 import { PAGE_HEIGHT, PAGE_WIDTH } from '../../edit-story/constants';
-
-export const TOP_LEVEL_DASHBOARD_APP_ID = 'dashboardAppFrame';
+export const WPBODY_ID = 'wpbody';
 export const PAGE_RATIO = PAGE_WIDTH / PAGE_HEIGHT;
 
 export const DASHBOARD_LEFT_NAV_WIDTH = 190;
