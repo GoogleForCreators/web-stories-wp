af5b0be4878c44920ce96c060ead8c64b3e2835b
spacedmonkey <jon@spacedmonkey.co.uk>
2020-05-28 11:09:34 +0100

    Remove taxonomies from stories.

--- includes/Dashboard.php
+++ includes/Dashboard.php
@@ -168,8 +168,6 @@ class Dashboard {
 					'api'          => [
 						'stories'    => sprintf( '/wp/v2/%s', $rest_base ),
 						'users'      => '/wp/v2/users',
-						'tags'       => '/wp/v2/tags',
-						'categories' => '/wp/v2/categories',
 						'fonts'      => '/web-stories/v1/fonts',
 					],
 				],
@@ -182,7 +180,7 @@ class Dashboard {
 			[],
 			WEBSTORIES_VERSION
 		);
-		
+
 		wp_enqueue_style(
 			self::SCRIPT_HANDLE,
 			WEBSTORIES_PLUGIN_DIR_URL . 'assets/css/' . self::SCRIPT_HANDLE . '.css',
