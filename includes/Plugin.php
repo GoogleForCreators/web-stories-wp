6a0d1f094be8aa4dec52b0670adf1e8379f8e5cc
Pascal Birchler <pascalb@google.com>
2020-06-02 15:44:54 +0200

    Improve Site Kit Google Analytics integration (#2075)

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -31,6 +31,7 @@ namespace Google\Web_Stories;
 use Google\Web_Stories\REST_API\Embed_Controller;
 use Google\Web_Stories\REST_API\Fonts_Controller;
 use Google\Web_Stories\REST_API\Link_Controller;
+use WP_Post;
 
 /**
  * Plugin class.
@@ -69,5 +70,66 @@ class Plugin {
 		// Gutenberg Blocks.
 		$embed_block = new Embed_Block();
 		add_action( 'init', [ $embed_block, 'init' ] );
+
+		// Integrations.
+		add_filter( 'googlesitekit_amp_gtag_opt', [ $this, 'filter_site_kit_gtag_opt' ] );
+	}
+
+	/**
+	 * Filters the gtag configuration options for the amp-analytics tag.
+	 *
+	 * @see https://blog.amp.dev/2019/08/28/analytics-for-your-amp-stories/
+	 * @see https://github.com/ampproject/amphtml/blob/master/extensions/amp-story/amp-story-analytics.md
+	 *
+	 * @param array $gtag_opt Array of gtag configuration options.
+	 * @return array Modified configuration options.
+	 */
+	public function filter_site_kit_gtag_opt( $gtag_opt ) {
+		if ( ! is_singular( Story_Post_Type::POST_TYPE_SLUG ) ) {
+			return $gtag_opt;
+		}
+
+		$post = get_post();
+
+		if ( ! $post instanceof WP_Post ) {
+			return $gtag_opt;
+		}
+
+		$title       = get_the_title( $post );
+		$story_id    = $post->ID;
+		$tracking_id = $gtag_opt['vars']['gtag_id'];
+
+		$gtag_opt['triggers'] = $gtag_opt['triggers'] ?: [];
+
+		if ( ! isset( $gtag_opt['triggers']['storyProgress'] ) ) {
+			$gtag_opt['triggers']['storyProgress'] = [
+				'on'   => 'story-page-visible',
+				'vars' => [
+					'event_name'     => 'custom',
+					'event_action'   => 'story_progress',
+					'event_category' => $title,
+					'event_label'    => $story_id,
+					'send_to'        => [
+						$tracking_id,
+					],
+				],
+			];
+		}
+
+		if ( ! isset( $gtag_opt['triggers']['storyEnd'] ) ) {
+			$gtag_opt['triggers']['storyEnd'] = [
+				'on'   => 'story-last-page-visible',
+				'vars' => [
+					'event_name'     => 'custom',
+					'event_action'   => 'story_complete',
+					'event_category' => $title,
+					'send_to'        => [
+						$tracking_id,
+					],
+				],
+			];
+		}
+
+		return $gtag_opt;
 	}
 }
