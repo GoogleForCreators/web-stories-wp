4586f10f8c7b2aac5354d05d1d674a7c822a8a9e
Jon Htin <jhtin@google.com>
2020-06-03 13:43:59 +1000

    Create shapes.karma.js

---
+++ assets/src/edit-story/components/library/karma/shapes/shapes.karma.js
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import { Fixture } from '../../../../karma/fixture';
+import { act } from '@testing-library/react';
+
+describe('TextEdit integration', () => {
+  let fixture;
+  const path = 'M 0.5 0 L 1 1 L 0 1 Z';
+  const label = 'Triangle';
+  const shapeInPageQuery = `[data-testid="safezone"] [d="${path}"`;
+
+  beforeEach(async () => {
+    fixture = new Fixture();
+
+    await fixture.render();
+  });
+
+  afterEach(() => {
+    fixture.restore();
+  });
+  //d="M 0.5 0 L 1 1 L 0 1 Z" triangle.
+
+  it('should render ok', () => {
+    expect(
+      fixture.container.querySelector('[data-testid="fullbleed"]')
+    ).toBeTruthy();
+  });
+
+  it('via clicking on shape preview', async () => {
+    let shapes = await fixture.container.querySelectorAll(
+      '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+    );
+    expect(shapes.length).toBe(0);
+
+    //await karmaPause();
+    await act(async () => {
+      await fixture.events.click(
+        fixture.querySelector(`[id="library-tab-shapes"]`)
+      );
+      await fixture.events.click(
+        fixture.querySelector('[aria-label="Triangle"]')
+      );
+
+      shapes = await fixture.container.querySelectorAll(
+        '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+      );
+    });
+    expect(shapes.length).toBeGreaterThan(0);
+  });
+
+  it('via dragging from shape preview', async () => {
+    let shapes = await fixture.container.querySelectorAll(
+      '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+    );
+    expect(shapes.length).toBe(0);
+
+    await act(async () => {
+      await fixture.events.click(
+        fixture.querySelector(`[id="library-tab-shapes"]`)
+      );
+      // Move the mouse to the middel of the shape preview and start mouse press
+      // down.
+      const shapePreview = await fixture.querySelector(
+        '[aria-label="Triangle"]'
+      );
+      const shapeBoundingBox = await shapePreview.getBoundingClientRect();
+      await fixture.events.mouse.move(
+        shapeBoundingBox.x + shapeBoundingBox.width / 2,
+        shapeBoundingBox.y + shapeBoundingBox.height / 2
+      );
+      await fixture.events.mouse.down();
+
+      // Move the mouse to the middle of the safe area and then mouse press up.
+      const safezone = await fixture.querySelector('[data-testid="safezone"]');
+      const safezoneBox = await safezone.getBoundingClientRect();
+      await fixture.events.mouse.move(
+        safezoneBox.x + safezoneBox.width / 2,
+        safezoneBox.y + safezoneBox.height / 2
+      );
+      await karmaPause();
+
+      await fixture.events.mouse.up();
+    });
+
+    await karmaPause();
+
+    shapes = await fixture.container.querySelectorAll(
+      '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+    );
+    expect(shapes.length).toBeGreaterThan(0);
+  });
+});
