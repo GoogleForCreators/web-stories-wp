973c856b418a2e3f3483b7b5a63dca64850cad95
Morten Barklund <morten.barklund@xwp.co>
2020-06-02 23:41:11 -0400

    Added a lot of drop target tests for bg replacement

--- assets/src/edit-story/components/dropTargets/karma/background.karma.js
+++ assets/src/edit-story/components/dropTargets/karma/background.karma.js
@@ -19,7 +19,6 @@
  */
 import { Fixture } from '../../../karma';
 import { useStory } from '../../../app/story';
-import { useInsertElement } from '../../../components/canvas';
 
 describe('Background Drop-Target integration', () => {
   let fixture;
@@ -34,20 +33,63 @@ describe('Background Drop-Target integration', () => {
     fixture.restore();
   });
 
-  it('should by default have white background', async () => {
-    const bgElement = await getCanvasBackgroundElement(fixture);
-    // Verify that it's empty
-    expect(bgElement).toBeEmpty();
-    // And that background color is white:
-    expect(bgElement).toHaveStyle('backgroundColor', 'rgb(255, 255, 255)');
+  describe('when there is nothing on the canvas', () => {
+    it('should by default have white background', async () => {
+      const bgElement = await getCanvasBackgroundElement(fixture);
+      // Verify that it's empty
+      expect(bgElement).toBeEmpty();
+      // And that background color is white:
+      expect(bgElement).toHaveStyle('backgroundColor', 'rgb(255, 255, 255)');
+    });
+
+    // Disable reason: For unknown reasons this (dragging from library) doesn't
+    // work in the regular runner, only in debug runner.
+    xit('should correctly handle image dragged from library straight to edge', async () => {
+      const backgroundId = await getBackgroundElementId(fixture);
+
+      // Verify that bg replacement is empty
+      const rep1 = await getCanvasBackgroundReplacement(fixture);
+      expect(rep1).toBeEmpty();
+
+      // Get library element reference
+      const libraryElement = getMediaLibraryElementByIndex(fixture, 0);
+      const libraryImage = libraryElement.querySelector('img');
+
+      // Drag the element to the background
+      await dragToDropTarget(fixture, libraryElement, backgroundId);
+
+      // Verify that bg replacement is no longer empty
+      expect(rep1).not.toBeEmpty();
+
+      // Verify that replacement img has correct source
+      const replaceImg = rep1.querySelector('img');
+      expect(replaceImg).toHaveProperty('src', libraryImage.src);
+
+      // Now drop the element
+      await fixture.events.mouse.up();
+
+      // Verify new background element has the correct image
+      const bg = await getCanvasBackgroundElement(fixture);
+      const bgImg = bg.querySelector('img');
+      expect(bgImg).toHaveProperty('src', libraryImage.src);
+
+      // And verify that we no longer have a replacement element
+      const rep2 = await getCanvasBackgroundReplacement(fixture);
+      expect(rep2).toBeEmpty();
+    });
   });
 
   describe('when there is an image on the canvas', () => {
     let imageData;
 
     beforeEach(async () => {
-      imageData = await addDummyImage(fixture);
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000000;
+      await addDummyImage(fixture, 0);
+      const {
+        state: {
+          currentPage: { elements },
+        },
+      } = await fixture.renderHook(() => useStory());
+      imageData = elements[1];
     });
 
     it('should correctly handle image dropped on edge', async () => {
@@ -58,7 +100,7 @@ describe('Background Drop-Target integration', () => {
       expect(rep1).toBeEmpty();
 
       // Drag the image element to the background
-      await dragToDropTarget(fixture, imageData.id, backgroundId);
+      await dragCanvasElementToDropTarget(fixture, imageData.id, backgroundId);
 
       // Verify that bg replacement is no longer empty
       expect(rep1).not.toBeEmpty();
@@ -101,33 +143,316 @@ describe('Background Drop-Target integration', () => {
       expect(bgImg).toHaveProperty('src', imageData.resource.src);
     });
   });
+
+  describe('when there is a background image and nothing else', () => {
+    let bgImageData;
+
+    beforeEach(async () => {
+      await addDummyImage(fixture, 0);
+      const setAsBackground = getButtonByText(fixture, 'Set as background');
+      await fixture.events.click(setAsBackground);
+      const {
+        state: {
+          currentPage: { elements },
+        },
+      } = await fixture.renderHook(() => useStory());
+      bgImageData = elements[0];
+    });
+
+    // Disable reason: For unknown reasons this (dragging from library) doesn't
+    // work in the regular runner, only in debug runner.
+    xit('should correctly handle image dragged from library straight to edge replacing old image', async () => {
+      const backgroundId = await getBackgroundElementId(fixture);
+
+      // Verify that background element has the correct image before doing anything
+      const bg1 = await getCanvasBackgroundElement(fixture);
+      const bgImg1 = bg1.querySelector('img');
+      expect(bgImg1).toHaveProperty('src', bgImageData.resource.src);
+
+      // Verify that bg replacement is empty
+      const rep1 = await getCanvasBackgroundReplacement(fixture);
+      expect(rep1).toBeEmpty();
+
+      // Get library element reference
+      const libraryElement = getMediaLibraryElementByIndex(fixture, 0);
+      const libraryImage = libraryElement.querySelector('img');
+
+      // Drag the element to the background
+      await dragToDropTarget(fixture, libraryElement, backgroundId);
+
+      // Verify that bg replacement is no longer empty
+      expect(rep1).not.toBeEmpty();
+
+      // Verify that replacement img has correct source
+      const replaceImg = rep1.querySelector('img');
+      expect(replaceImg).toHaveProperty('src', libraryImage.src);
+
+      // Now drop the element
+      await fixture.events.mouse.up();
+
+      // Verify new background element has the correct image
+      const bg2 = await getCanvasBackgroundElement(fixture);
+      const bgImg2 = bg2.querySelector('img');
+      expect(bgImg2).toHaveProperty('src', libraryImage.src);
+
+      // And verify that we no longer have a replacement element
+      const rep2 = await getCanvasBackgroundReplacement(fixture);
+      expect(rep2).toBeEmpty();
+    });
+
+    describe('when there is second image on canvas', () => {
+      let imageData;
+
+      beforeEach(async () => {
+        await addDummyImage(fixture, 1);
+        const {
+          state: {
+            currentPage: { elements },
+          },
+        } = await fixture.renderHook(() => useStory());
+        imageData = elements[1];
+      });
+
+      it('should correctly handle image dropped on edge replacing old image', async () => {
+        // Verify that background element has the correct image before doing anything
+        const bg1 = await getCanvasBackgroundElement(fixture);
+        const bgImg1 = bg1.querySelector('img');
+        expect(bgImg1).toHaveProperty('src', bgImageData.resource.src);
+
+        // Verify that bg replacement is empty
+        const rep1 = await getCanvasBackgroundReplacement(fixture);
+        expect(rep1).toBeEmpty();
+
+        // Drag the image element to the background
+        await dragCanvasElementToDropTarget(
+          fixture,
+          imageData.id,
+          bgImageData.id
+        );
+
+        // Verify that bg replacement is no longer empty
+        expect(rep1).not.toBeEmpty();
+
+        // Verify that replacement img has correct source
+        const replaceImg = rep1.querySelector('img');
+        expect(replaceImg).toHaveProperty('src', imageData.resource.src);
+
+        // Now drop the element
+        await fixture.events.mouse.up();
+
+        // Verify new background element has the correct image
+        const bg2 = await getCanvasBackgroundElement(fixture);
+        const bgImg2 = bg2.querySelector('img');
+        expect(bgImg2).toHaveProperty('src', imageData.resource.src);
+
+        // And verify that we no longer have a replacement element
+        const rep2 = await getCanvasBackgroundReplacement(fixture);
+        expect(rep2).toBeEmpty();
+      });
+
+      it('should correctly handle pressing "set as background" replacing old image', async () => {
+        // Verify that background element has the correct image before doing anything
+        const bg1 = await getCanvasBackgroundElement(fixture);
+        const bgImg1 = bg1.querySelector('img');
+        expect(bgImg1).toHaveProperty('src', bgImageData.resource.src);
+
+        // Find element
+        const el = getCanvasElementWrapperById(fixture, imageData.id);
+        const rect = el.getBoundingClientRect();
+
+        // Click the element center
+        await fixture.events.mouse.click(
+          rect.x + rect.width / 2,
+          rect.y + rect.height / 2
+        );
+
+        // Click the "set as background" button
+        const setAsBackground = getButtonByText(fixture, 'Set as background');
+        await fixture.events.click(setAsBackground);
+
+        // Verify new background element has the correct image
+        const bg2 = await getCanvasBackgroundElement(fixture);
+        const bgImg2 = bg2.querySelector('img');
+        expect(bgImg2).toHaveProperty('src', imageData.resource.src);
+      });
+
+      describe('when the background is flipped', () => {
+        beforeEach(async () => {
+          // Click the bg in the top left corner
+          const bgElement = getCanvasElementWrapperById(
+            fixture,
+            bgImageData.id
+          );
+          const bgRect = bgElement.getBoundingClientRect();
+          await fixture.events.mouse.click(bgRect.left + 1, bgRect.top + 1);
+          // And flip it
+          const flip = getInputByAriaLabel(fixture, 'Flip horizontally');
+          const flipLabel = flip.parentElement;
+          await fixture.events.click(flipLabel);
+        });
+
+        it('should correctly handle image dropped on edge without flip', async () => {
+          // Verify that background element has the correct transform (flip) before doing anything
+          const bg1 = await getCanvasBackgroundElementWrapper(fixture);
+          const rep = bg1.querySelector(
+            `[class^="displayElement__ReplacementContainer-"]`
+          );
+          const bgImg1 = bg1.querySelector('img');
+          const combinedBgTransform1 = getAllTransformsBetween(bgImg1, bg1);
+          expect(combinedBgTransform1).toBe('matrix(-1, 0, 0, 1, 0, 0)');
+
+          // Drag the image element to the background
+          await dragCanvasElementToDropTarget(
+            fixture,
+            imageData.id,
+            bgImageData.id
+          );
+
+          // Verify that replacement img has correct transform
+          const replaceImg = rep.querySelector('img');
+          const combinedRepTransform = getAllTransformsBetween(replaceImg, bg1);
+          expect(combinedRepTransform).toBe('');
+
+          // Now drop the element
+          await fixture.events.mouse.up();
+
+          //await karmaPause();
+
+          // Verify that new background is not flipped
+          const bg2 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg2 = bg2.querySelector('img');
+          const combinedBgTransform2 = getAllTransformsBetween(bgImg2, bg2);
+          expect(combinedBgTransform2).toBe('');
+        });
+
+        it('should correctly handle pressing "set as background" without flip', async () => {
+          // Verify that background element has the correct transform (flip) before doing anything
+          const bg1 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg1 = bg1.querySelector('img');
+          const combinedBgTransform1 = getAllTransformsBetween(bgImg1, bg1);
+          expect(combinedBgTransform1).toBe('matrix(-1, 0, 0, 1, 0, 0)');
+
+          // Find element
+          const el = getCanvasElementWrapperById(fixture, imageData.id);
+          const rect = el.getBoundingClientRect();
+
+          // Click the element center
+          await fixture.events.mouse.click(
+            rect.x + rect.width / 2,
+            rect.y + rect.height / 2
+          );
+
+          // Click the "set as background" button
+          const setAsBackground = getButtonByText(fixture, 'Set as background');
+          await fixture.events.click(setAsBackground);
+
+          // Verify that new background is not flipped
+          const bg2 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg2 = bg2.querySelector('img');
+          const combinedBgTransform2 = getAllTransformsBetween(bgImg2, bg2);
+          expect(combinedBgTransform2).toBe('');
+        });
+      });
+
+      describe('when the canvas element is flipped', () => {
+        beforeEach(async () => {
+          // Click the corner of the on-canvas element
+          const element = getCanvasElementWrapperById(fixture, imageData.id);
+          const rect = element.getBoundingClientRect();
+          await fixture.events.mouse.click(rect.left + 1, rect.top + 1);
+          // And flip it
+          const flip = getInputByAriaLabel(fixture, 'Flip horizontally');
+          const flipLabel = flip.parentElement;
+          await fixture.events.click(flipLabel);
+        });
+
+        it('should correctly handle image dropped on edge with flip', async () => {
+          // Verify that background element has the correct transform (none) before doing anything
+          const bg1 = await getCanvasBackgroundElementWrapper(fixture);
+          const rep = bg1.querySelector(
+            `[class^="displayElement__ReplacementContainer-"]`
+          );
+          const bgImg1 = bg1.querySelector('img');
+          const combinedBgTransform1 = getAllTransformsBetween(bgImg1, bg1);
+          expect(combinedBgTransform1).toBe('');
+
+          // Drag the image element to the background
+          await dragCanvasElementToDropTarget(
+            fixture,
+            imageData.id,
+            bgImageData.id
+          );
+
+          // Verify that replacement img is flipped
+          const replaceImg = rep.querySelector('img');
+          const combinedRepTransform = getAllTransformsBetween(replaceImg, bg1);
+          expect(combinedRepTransform).toBe('matrix(-1, 0, 0, 1, 0, 0)');
+
+          // Now drop the element
+          await fixture.events.mouse.up();
+
+          // Verify that new background is flipped
+          const bg2 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg2 = bg2.querySelector('img');
+          const combinedBgTransform2 = getAllTransformsBetween(bgImg2, bg2);
+          expect(combinedBgTransform2).toBe('matrix(-1, 0, 0, 1, 0, 0)');
+        });
+
+        it('should correctly handle pressing "set as background" with flip', async () => {
+          // Verify that background element has the correct transform (none) before doing anything
+          const bg1 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg1 = bg1.querySelector('img');
+          const combinedBgTransform1 = getAllTransformsBetween(bgImg1, bg1);
+          expect(combinedBgTransform1).toBe('');
+
+          // Find element
+          const el = getCanvasElementWrapperById(fixture, imageData.id);
+          const rect = el.getBoundingClientRect();
+
+          // Click the element center
+          await fixture.events.mouse.click(
+            rect.x + rect.width / 2,
+            rect.y + rect.height / 2
+          );
+
+          // Click the "set as background" button
+          const setAsBackground = getButtonByText(fixture, 'Set as background');
+          await fixture.events.click(setAsBackground);
+
+          // Verify that new background is flipped
+          const bg2 = await getCanvasBackgroundElementWrapper(fixture);
+          const bgImg2 = bg2.querySelector('img');
+          const combinedBgTransform2 = getAllTransformsBetween(bgImg2, bg2);
+          expect(combinedBgTransform2).toBe('matrix(-1, 0, 0, 1, 0, 0)');
+        });
+      });
+    });
+  });
 });
 
-async function addDummyImage(fixture) {
-  const insertElement = await fixture.renderHook(() => useInsertElement());
-  return fixture.act(() =>
-    insertElement('image', {
-      resource: {
-        type: 'image',
-        src: 'http://localhost:9876/__static__/blue-marble.jpg',
-        width: 600,
-        height: 600,
-      },
-      x: 40,
-      y: 40,
-      width: 250,
-      height: 250,
-    })
-  );
+function getMediaLibraryElementByIndex(fixture, index) {
+  return fixture.querySelectorAll('[data-testid=mediaElement]')[index];
 }
 
-async function dragToDropTarget(fixture, fromId, toId) {
-  // Find from element dimensions
-  const from = await getCanvasElementWrapperById(fixture, fromId);
-  const fromRect = from.getBoundingClientRect();
+async function addDummyImage(fixture, index) {
+  await fixture.events.click(getMediaLibraryElementByIndex(fixture, index));
+}
 
-  // Find to element dimensions
+async function dragCanvasElementToDropTarget(
+  fixture,
+  canvasElementId,
+  targetId
+) {
+  // Find from element
+  const from = await getCanvasElementWrapperById(fixture, canvasElementId);
+  return dragToDropTarget(fixture, from, targetId);
+}
+
+async function dragToDropTarget(fixture, from, toId) {
   const to = getCanvasElementWrapperById(fixture, toId);
+
+  // Find element dimensions
+  const fromRect = from.getBoundingClientRect();
   const toRect = to.getBoundingClientRect();
 
   // Schedule a sequence of events by dragging from center of image to edge of bg
@@ -197,6 +522,10 @@ function getButtonByText(fixture, buttonText) {
   );
 }
 
+function getInputByAriaLabel(fixture, ariaLabel) {
+  return fixture.querySelector(`input[aria-label="${ariaLabel}"]`);
+}
+
 function getCanvasElementWrapperById(fixture, id) {
   return fixture.querySelector(`[data-element-id="${id}"]`);
 }
@@ -229,6 +558,16 @@ async function getCanvasBackgroundReplacement(fixture) {
   return wrapper.querySelector(
     `[class^="displayElement__ReplacementContainer-"]`
   );
+}
+
+function getAllTransformsBetween(startElement, endElement) {
+  let e = startElement;
+  const transforms = [];
+  while (e && e != endElement) {
+    transforms.unshift(getComputedStyle(e).transform);
+    e = e.parentNode;
+  }
+  return transforms.filter((t) => t !== 'none').join(' ');
 }
 
 function getComputedStyle(element) {
