c0720446f49e198ee7b3fead9ce5b8e97a3470f4
Jon Htin <jhtin@google.com>
2020-06-04 17:38:55 +1000

    Changing deleted test to xit instead.

--- assets/src/edit-story/components/library/karma/shapes/shapes.karma.js
+++ assets/src/edit-story/components/library/karma/shapes/shapes.karma.js
@@ -62,4 +62,47 @@ describe('TextEdit integration', () => {
     });
     expect(shapes.length).toBeGreaterThan(0);
   });
+
+  xit('add shape via dragging from shape preview', async () => {
+    // Skipping this test because drag and drop doesn't work properly in
+    // puppeteer for now.
+    let shapes = await fixture.container.querySelectorAll(
+      '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+    );
+    expect(shapes.length).toBe(0);
+
+    await act(async () => {
+      await fixture.events.click(
+        fixture.querySelector(`[id="library-tab-shapes"]`)
+      );
+      // Move the mouse to the middel of the shape preview and start mouse press
+      // down.
+      const shapePreview = await fixture.querySelector(
+        '[aria-label="Triangle"]'
+      );
+      const shapeBoundingBox = await shapePreview.getBoundingClientRect();
+      await fixture.events.mouse.move(
+        shapeBoundingBox.x + shapeBoundingBox.width / 2,
+        shapeBoundingBox.y + shapeBoundingBox.height / 2
+      );
+      await fixture.events.mouse.down();
+
+      // Move the mouse to the middle of the safe area and then mouse press up.
+      const safezone = await fixture.querySelector('[data-testid="safezone"]');
+      const safezoneBox = await safezone.getBoundingClientRect();
+
+      await fixture.events.mouse.move(
+        safezoneBox.x + safezoneBox.width / 2,
+        safezoneBox.y + safezoneBox.height / 2,
+        { steps: 100 }
+      );
+
+      await fixture.events.mouse.up();
+    });
+
+    shapes = await fixture.container.querySelectorAll(
+      '[data-testid="safezone"] [d="M 0.5 0 L 1 1 L 0 1 Z"]'
+    );
+    expect(shapes.length).toBeGreaterThan(0);
+  });
 });
