2c5d21c5c47055e091d0274bacf5a92623f75d0a
Dima Voytenko <dvoytenko@gmail.com>
2020-06-04 17:39:40 -0700

    tests

--- assets/src/edit-story/output/test/page.js
+++ assets/src/edit-story/output/test/page.js
@@ -24,177 +24,268 @@ import { render } from '@testing-library/react';
  */
 import PageOutput from '../page';
 import { queryByAutoAdvanceAfter, queryById } from '../../testUtils';
+import { PAGE_WIDTH, PAGE_HEIGHT } from '../../constants';
 
 describe('Page output', () => {
-  it('should use default value for auto-advance-after', async () => {
-    const props = {
-      id: '123',
-      backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
-      page: {
+  describe('aspect-ratio markup', () => {
+    it('should render a single layer with no background', () => {
+      const props = {
         id: '123',
-        elements: [],
-      },
-      autoAdvance: false,
-      defaultPageDuration: 7,
-    };
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: '123',
+          elements: [],
+        },
+        autoAdvance: false,
+        defaultPageDuration: 7,
+      };
 
-    const { container } = render(<PageOutput {...props} />);
-    await expect(
-      queryByAutoAdvanceAfter(container, '7s')
-    ).not.toBeInTheDocument();
-  });
+      const { container } = render(<PageOutput {...props} />);
+      const layers = container.querySelectorAll('amp-story-grid-layer');
+      expect(layers).toHaveLength(1);
+      const layer = layers[0];
+      expect(layer).toHaveAttribute(
+        'aspect-ratio',
+        `${PAGE_WIDTH}:${PAGE_HEIGHT}`
+      );
+      expect(layer.firstElementChild.className).toStrictEqual(
+        'page-fullbleed-area'
+      );
+      expect(layer.firstElementChild.firstElementChild.className).toStrictEqual(
+        'page-safe-area'
+      );
+    });
 
-  it('should use default duration for auto-advance-after', async () => {
-    const props = {
-      id: '123',
-      backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
-      page: {
+    it('should render the layer for background', () => {
+      const props = {
         id: '123',
-        elements: [],
-      },
-      autoAdvance: true,
-      defaultPageDuration: 7,
-    };
-
-    const { container } = render(<PageOutput {...props} />);
-    await expect(queryByAutoAdvanceAfter(container, '7s')).toBeInTheDocument();
-  });
-
-  it('should use default duration for images', async () => {
-    const props = {
-      id: '123',
-      backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
-      page: {
-        id: '123',
-        elements: [
-          {
-            id: 'baz',
-            type: 'image',
-            mimeType: 'image/png',
-            scale: 1,
-            origRatio: 9 / 16,
-            x: 50,
-            y: 100,
-            height: 1920,
-            width: 1080,
-            rotationAngle: 0,
-            loop: true,
-            resource: {
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: '123',
+          elements: [
+            {
+              isBackground: true,
+              id: 'baz',
               type: 'image',
               mimeType: 'image/png',
-              id: 123,
-              src: 'https://example.com/image.png',
-              poster: 'https://example.com/poster.png',
+              scale: 1,
+              origRatio: 9 / 16,
+              x: 50,
+              y: 100,
               height: 1920,
               width: 1080,
+              rotationAngle: 0,
+              loop: true,
+              resource: {
+                type: 'image',
+                mimeType: 'image/png',
+                id: 123,
+                src: 'https://example.com/image.png',
+                poster: 'https://example.com/poster.png',
+                height: 1920,
+                width: 1080,
+              },
             },
-          },
-        ],
-      },
-      autoAdvance: true,
-      defaultPageDuration: 7,
-    };
+          ],
+        },
+        autoAdvance: false,
+        defaultPageDuration: 7,
+      };
 
-    const { container } = render(<PageOutput {...props} />);
-    await expect(queryByAutoAdvanceAfter(container, '7s')).toBeInTheDocument();
+      const { container } = render(<PageOutput {...props} />);
+      const layers = container.querySelectorAll('amp-story-grid-layer');
+      expect(layers).toHaveLength(2);
+      const bgLayer = layers[0];
+      expect(bgLayer).toHaveAttribute(
+        'aspect-ratio',
+        `${PAGE_WIDTH}:${PAGE_HEIGHT}`
+      );
+      expect(bgLayer.firstElementChild.className).toStrictEqual(
+        'page-fullbleed-area'
+      );
+      expect(
+        bgLayer.firstElementChild.firstElementChild.className
+      ).toStrictEqual('page-safe-area');
+    });
   });
 
-  it('should use video element ID for auto-advance-after', async () => {
-    const props = {
-      id: 'foo',
-      backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
-      page: {
-        id: 'bar',
-        elements: [
-          {
-            id: 'baz',
-            type: 'video',
-            mimeType: 'video/mp4',
-            scale: 1,
-            origRatio: 9 / 16,
-            x: 50,
-            y: 100,
-            height: 1920,
-            width: 1080,
-            rotationAngle: 0,
-            loop: false,
-            resource: {
-              type: 'video',
-              mimeType: 'video/mp4',
-              id: 123,
-              src: 'https://example.com/video.mp4',
-              poster: 'https://example.com/poster.png',
+  describe('autoAdvance', () => {
+    it('should use default value for auto-advance-after', async () => {
+      const props = {
+        id: '123',
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: '123',
+          elements: [],
+        },
+        autoAdvance: false,
+        defaultPageDuration: 7,
+      };
+
+      const { container } = render(<PageOutput {...props} />);
+      await expect(
+        queryByAutoAdvanceAfter(container, '7s')
+      ).not.toBeInTheDocument();
+    });
+
+    it('should use default duration for auto-advance-after', async () => {
+      const props = {
+        id: '123',
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: '123',
+          elements: [],
+        },
+        autoAdvance: true,
+        defaultPageDuration: 7,
+      };
+
+      const { container } = render(<PageOutput {...props} />);
+      await expect(
+        queryByAutoAdvanceAfter(container, '7s')
+      ).toBeInTheDocument();
+    });
+
+    it('should use default duration for images', async () => {
+      const props = {
+        id: '123',
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: '123',
+          elements: [
+            {
+              id: 'baz',
+              type: 'image',
+              mimeType: 'image/png',
+              scale: 1,
+              origRatio: 9 / 16,
+              x: 50,
+              y: 100,
               height: 1920,
               width: 1080,
-              length: 99,
+              rotationAngle: 0,
+              loop: true,
+              resource: {
+                type: 'image',
+                mimeType: 'image/png',
+                id: 123,
+                src: 'https://example.com/image.png',
+                poster: 'https://example.com/poster.png',
+                height: 1920,
+                width: 1080,
+              },
             },
-          },
-        ],
-      },
-      autoAdvance: true,
-      defaultPageDuration: 7,
-    };
+          ],
+        },
+        autoAdvance: true,
+        defaultPageDuration: 7,
+      };
 
-    const { container } = render(<PageOutput {...props} />);
-    const video = queryById(container, 'el-baz-media');
-    await expect(video).toBeInTheDocument();
-    expect(video).toMatchInlineSnapshot(`
-      <amp-video
-        artwork="https://example.com/poster.png"
-        autoplay="autoplay"
-        id="el-baz-media"
-        layout="fill"
-        poster="https://example.com/poster.png"
-      >
-        <source
-          src="https://example.com/video.mp4"
-          type="video/mp4"
-        />
-      </amp-video>
-    `);
-    await expect(
-      queryByAutoAdvanceAfter(container, 'el-baz-media')
-    ).toBeInTheDocument();
-  });
+      const { container } = render(<PageOutput {...props} />);
+      await expect(
+        queryByAutoAdvanceAfter(container, '7s')
+      ).toBeInTheDocument();
+    });
 
-  it('should ignore looping video for auto-advance-after and set default instead', async () => {
-    const props = {
-      id: 'foo',
-      backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
-      page: {
-        id: 'bar',
-        elements: [
-          {
-            id: 'baz',
-            type: 'video',
-            mimeType: 'video/mp4',
-            scale: 1,
-            origRatio: 9 / 16,
-            x: 50,
-            y: 100,
-            height: 1920,
-            width: 1080,
-            rotationAngle: 0,
-            loop: true,
-            resource: {
+    it('should use video element ID for auto-advance-after', async () => {
+      const props = {
+        id: 'foo',
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: 'bar',
+          elements: [
+            {
+              id: 'baz',
               type: 'video',
               mimeType: 'video/mp4',
-              id: 123,
-              src: 'https://example.com/video.mp4',
-              poster: 'https://example.com/poster.png',
+              scale: 1,
+              origRatio: 9 / 16,
+              x: 50,
+              y: 100,
               height: 1920,
               width: 1080,
-              length: 99,
+              rotationAngle: 0,
+              loop: false,
+              resource: {
+                type: 'video',
+                mimeType: 'video/mp4',
+                id: 123,
+                src: 'https://example.com/video.mp4',
+                poster: 'https://example.com/poster.png',
+                height: 1920,
+                width: 1080,
+                length: 99,
+              },
             },
-          },
-        ],
-      },
-      autoAdvance: true,
-      defaultPageDuration: 7,
-    };
+          ],
+        },
+        autoAdvance: true,
+        defaultPageDuration: 7,
+      };
 
-    const { container } = render(<PageOutput {...props} />);
-    await expect(queryByAutoAdvanceAfter(container, '7s')).toBeInTheDocument();
+      const { container } = render(<PageOutput {...props} />);
+      const video = queryById(container, 'el-baz-media');
+      await expect(video).toBeInTheDocument();
+      expect(video).toMatchInlineSnapshot(`
+        <amp-video
+          artwork="https://example.com/poster.png"
+          autoplay="autoplay"
+          id="el-baz-media"
+          layout="fill"
+          poster="https://example.com/poster.png"
+        >
+          <source
+            src="https://example.com/video.mp4"
+            type="video/mp4"
+          />
+        </amp-video>
+      `);
+      await expect(
+        queryByAutoAdvanceAfter(container, 'el-baz-media')
+      ).toBeInTheDocument();
+    });
+
+    it('should ignore looping video for auto-advance-after and set default instead', async () => {
+      const props = {
+        id: 'foo',
+        backgroundColor: { type: 'solid', color: { r: 255, g: 255, b: 255 } },
+        page: {
+          id: 'bar',
+          elements: [
+            {
+              id: 'baz',
+              type: 'video',
+              mimeType: 'video/mp4',
+              scale: 1,
+              origRatio: 9 / 16,
+              x: 50,
+              y: 100,
+              height: 1920,
+              width: 1080,
+              rotationAngle: 0,
+              loop: true,
+              resource: {
+                type: 'video',
+                mimeType: 'video/mp4',
+                id: 123,
+                src: 'https://example.com/video.mp4',
+                poster: 'https://example.com/poster.png',
+                height: 1920,
+                width: 1080,
+                length: 99,
+              },
+            },
+          ],
+        },
+        autoAdvance: true,
+        defaultPageDuration: 7,
+      };
+
+      const { container } = render(<PageOutput {...props} />);
+      await expect(
+        queryByAutoAdvanceAfter(container, '7s')
+      ).toBeInTheDocument();
+    });
   });
 
   describe('AMP validation', () => {
