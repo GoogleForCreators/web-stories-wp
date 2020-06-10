8161896792b57925decea84ba744e1fd4ae5a823
spacedmonkey <jon@spacedmonkey.co.uk>
2020-06-10 12:43:19 +0100

    Feedback.

--- includes/Discovery.php
+++ includes/Discovery.php
@@ -41,7 +41,7 @@ class Discovery {
 	 * @return void
 	 */
 	public function init() {
-		add_action( 'web_stories_story_head', [ $this, 'print_title_metadata' ], 1 );
+		add_action( 'web_stories_story_head', [ $this, 'print_metadata' ], 1 );
 		add_action( 'web_stories_story_head', [ $this, 'print_schemaorg_metadata' ] );
 		add_action( 'web_stories_story_head', [ $this, 'print_open_graph_metadata' ] );
 		add_action( 'web_stories_story_head', [ $this, 'print_twitter_metadata' ] );
@@ -63,11 +63,15 @@ class Discovery {
 	}
 
 	/**
-	 * Theme support for title-tag is implied for stories. See _wp_render_title_tag().
+	 * Prints general metadata on the single story template.
+	 *
+	 * Theme support for title tag is implied for stories.
+	 *
+	 * @see _wp_render_title_tag().
 	 *
 	 * @return void
 	 */
-	public function print_title_metadata() {
+	public function print_metadata() {
 		?>
 		<title><?php echo esc_html( wp_get_document_title() ); ?></title>
 		<?php
