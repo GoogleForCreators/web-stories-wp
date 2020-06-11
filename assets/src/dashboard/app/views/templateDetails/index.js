bf4d3ae172717a62a858e0b3f2d250667bbcd029
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-06-11 15:28:31 -0600

    add a flag for bookmarking

--- assets/src/dashboard/app/views/templateDetails/index.js
+++ assets/src/dashboard/app/views/templateDetails/index.js
@@ -22,6 +22,8 @@ import { sprintf, __ } from '@wordpress/i18n';
  * External dependencies
  */
 import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import { useFeature } from 'flagged';
+
 /**
  * Internal dependencies
  */
@@ -63,6 +65,7 @@ function TemplateDetails() {
   const [orderedTemplates, setOrderedTemplates] = useState([]);
   const { pageSize } = usePagePreviewSize({ isGrid: true });
   const { isRTL } = useConfig();
+  const enableBookmarks = useFeature('enableBookmarkActions');
 
   const {
     state: {
@@ -202,6 +205,7 @@ function TemplateDetails() {
             <Layout.Fixed>
               <TemplateNavBar
                 handleCta={() => createStoryFromTemplate(template)}
+                handleBookmarkClick={enableBookmarks}
               />
             </Layout.Fixed>
             <Layout.Scrollable>
