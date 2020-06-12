94212b47ac84bb1482f61739987064d558ac9018
Morten Barklund <morten.barklund@xwp.co>
2020-06-12 15:02:14 -0400

    Make background overlay editable (#1925)
    * Added migration to add specific background overlay to element
    
    * Use new overlay data structure when rendering display and output
    
    * Updated migration to use new CSS radial definition from #1653
    
    * Add conversion functions for background overlay
    
    * Reorganized background overlay to separate panel and added color picker
    
    * Always show "transparent" shading behind color swatch
    
    * Removed unused conversion function
    
    * Fix background overlay rules as per agreement
    
    * Fix tests and some cleanup
    
    * Added unit test for `convertOverlay`
    
    * Added karma test for background overlay
    
    * Fixed linear overlay migration
    
    * Fixed unit test

--- assets/src/edit-story/migration/migrate.js
+++ assets/src/edit-story/migration/migrate.js
@@ -40,6 +40,7 @@ import conicToLinear from './migrations/v0019_conicToLinear';
 import isFillDeprecate from './migrations/v0020_isFillDeprecate';
 import backgroundColorToPage from './migrations/v0021_backgroundColorToPage';
 import dataPixelTo412 from './migrations/v0022_dataPixelTo412';
+import convertOverlayPattern from './migrations/v0023_convertOverlayPattern';
 
 const MIGRATIONS = {
   1: [storyDataArrayToObject],
@@ -64,6 +65,7 @@ const MIGRATIONS = {
   20: [isFillDeprecate],
   21: [backgroundColorToPage],
   22: [dataPixelTo412],
+  23: [convertOverlayPattern],
 };
 
 export const DATA_VERSION = Math.max.apply(
