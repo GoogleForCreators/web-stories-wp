6b31ff8c8106bba558409d4855907f761b76eb6f
Morten Barklund <morten.barklund@xwp.co>
2020-06-19 18:00:28 -0400

    Removed negative option for letter spacing and line height

--- assets/src/edit-story/components/panels/textStyle/textStyle.js
+++ assets/src/edit-story/components/panels/textStyle/textStyle.js
@@ -118,7 +118,6 @@ function StylePanel({ selectedElements, pushUpdate }) {
           suffix={<OffsetVertical />}
           onChange={(value) => pushUpdate({ lineHeight: value })}
           canBeEmpty
-          canBeNegative
         />
         <Space />
         <ExpandedNumeric
@@ -130,7 +129,6 @@ function StylePanel({ selectedElements, pushUpdate }) {
           symbol="%"
           onChange={setLetterSpacingMinMax}
           canBeEmpty
-          canBeNegative
         />
       </Row>
       <Row>
