6013f9e196662e6073414e35c3855219bc512230
Dima Voytenko <dvoytenko@google.com>
2020-06-24 10:52:18 -0700

    Re-enable carousel integration tests

--- assets/src/edit-story/karma/fixture/containers/carousel.js
+++ assets/src/edit-story/karma/fixture/containers/carousel.js
@@ -18,44 +18,47 @@
  * Internal dependencies
  */
 import { Container } from './container';
-import { Canvas } from './canvas';
-import { Carousel } from './carousel';
-import { Library } from './library';
 
 /**
- * The complete editor container, including library, canvas, inspector, etc.
+ * The page carousel.
  */
-export class Editor extends Container {
+export class Carousel extends Container {
   constructor(node, path) {
     super(node, path);
   }
 
-  get canvas() {
-    return this._get(
-      this.getByRole('region', { name: 'Canvas' }),
-      'canvas',
-      Canvas
+  get pages() {
+    const pageList = this.getByRole('listbox', { name: 'Pages List' });
+    if (!pageList) {
+      return null;
+    }
+    return this._getAll(
+      // @todo: improve query.
+      pageList.querySelectorAll('button[role="option"]'),
+      (node) => `pages[${node.getAttribute('data-page-id')}]`,
+      PageThumb
     );
   }
 
-  get titleBar() {
-    // @todo: title bar container.
-    return null;
-  }
-
-  get library() {
+  page(pageId) {
+    const pageList = this.getByRole('listbox', { name: 'Pages List' });
+    if (!pageList) {
+      return null;
+    }
     return this._get(
-      this.getByRole('region', { name: 'Library' }),
-      'library',
-      Library
+      // @todo: improve query.
+      pageList.querySelector(`button[data-page-id="${pageId}"]`),
+      `pages[${pageId}]`,
+      PageThumb
     );
   }
+}
 
-  get carousel() {
-    return this._get(
-      this.getByRole('region', { name: 'Page Carousel' }),
-      'carousel',
-      Carousel
-    );
+/**
+ * A page thumbnail.
+ */
+class PageThumb extends Container {
+  constructor(node, path) {
+    super(node, path);
   }
 }
