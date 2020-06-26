76bbf043b0a5d6caf27c0cbdd2de32b5b6539458
Pascal Birchler <pascalb@google.com>
2020-06-26 22:46:55 +0200

    Use svg file for icon

--- assets/src/story-embed-block/block/index.js
+++ assets/src/story-embed-block/block/index.js
@@ -22,7 +22,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import icon from './icon';
+import { ReactComponent as icon } from './icon.svg';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
