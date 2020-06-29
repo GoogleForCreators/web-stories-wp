15646734934fd5074e132338cf4fbaa23102fbaa
merapi <marcin@webskill.pl>
2020-06-29 23:29:56 +0200

    Just to trigger new build

--- assets/src/edit-story/components/canvas/useUploadWithPreview.js
+++ assets/src/edit-story/components/canvas/useUploadWithPreview.js
@@ -13,10 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * External dependencies
  */
 import { useCallback } from 'react';
+
 /**
  * Internal dependencies
  */
