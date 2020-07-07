8cbf74e7f9f15f3a88f8c7d42b5a3cabb6a2f3a7
Miina Sikk <miina.sikk@gmail.com>
2020-07-07 17:31:11 +0300

    Start adding Karma tests.

--- assets/src/edit-story/karma/pageAttachment.cuj.karma.js
+++ assets/src/edit-story/karma/pageAttachment.cuj.karma.js
@@ -14,11 +14,53 @@
  * limitations under the License.
  */
 
+/**
+ * Internal dependencies
+ */
+import { Fixture } from './fixture';
+
 describe('CUJ: Creator can Add a Page Attachment', () => {
   describe('Action: Add Page Attachment', () => {
-    // Disable reason: Not implemented yet
-    // eslint-disable-next-line jasmine/no-disabled-tests
-    xit('it should allow adding Page Attachment with custom CTA Text');
+    let fixture;
+    const setPageAttachmentLink = async (link) => {
+      const input = fixture.screen.getByLabelText('Edit: Page Attachment link');
+      await fixture.events.click(input, { clickCount: 3 });
+      await fixture.events.keyboard.type(link);
+      await input.dispatchEvent(new window.Event('blur'));
+    };
+
+    const setCtaText = async (text) => {
+      const input = fixture.screen.getByLabelText(
+        'Edit: Page Attachment CTA text'
+      );
+      await fixture.events.click(input, { clickCount: 3 });
+      await fixture.events.keyboard.type(text);
+      await input.dispatchEvent(new window.Event('blur'));
+    };
+
+    const clickOnTarget = async (target) => {
+      const { x, y, width, height } = target.getBoundingClientRect();
+      await fixture.events.mouse.click(x + width / 2, y + height / 2);
+    };
+
+    beforeEach(async () => {
+      fixture = new Fixture();
+      await fixture.render();
+      // Select Page by default.
+      const safezone = fixture.querySelector('[data-testid="safezone"]');
+      await clickOnTarget(safezone);
+    });
+
+    afterEach(() => {
+      fixture.restore();
+    });
+
+    it('it should allow adding Page Attachment with custom CTA Text', async () => {
+      await setPageAttachmentLink('http://example.com');
+      await setCtaText('Click me!');
+      const ctaText = fixture.screen.getByText('Click me!');
+      expect(ctaText).toBeDefined();
+    });
 
     // Disable reason: Not implemented yet
     // eslint-disable-next-line jasmine/no-disabled-tests
