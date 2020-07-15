ec9d491260ccf71e0fdc2f2b1487de8f795fd03f
Miina Sikk <miina.sikk@gmail.com>
2020-07-15 16:41:38 +0300

    Add placeholders for Karma tests.

---
+++ assets/src/edit-story/karma/textStyle.cuj.karma.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('CUJ: Creator Can Style Text', () => {
+  describe('Action: Use font picker', () => {
+    // Disable reason: Not implemented yet
+    // eslint-disable-next-line jasmine/no-disabled-tests
+    xit('it should display as expected when open', () => {
+      // @todo Add snapshot.
+    });
+
+    // Disable reason: Not implemented yet
+    // eslint-disable-next-line jasmine/no-disabled-tests
+    xit('it should apply the selected font on the element', () => {});
+
+    describe('when searching fonts', () => {
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should search fonts if at least 2 characters added', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should restore default view with less than 2 characters', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should display the correct fonts when searching', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should show empty list in case of no results', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should clear search when clicking on `X`', () => {});
+    });
+
+    describe('with recent fonts', () => {
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should not display any recent fonts by default', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should add up to 5 recent fonts, displaying the most recent first', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should display the selected recent font with a tick', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should include recent fonts to search', () => {});
+    });
+
+    describe('using keyboard only', () => {
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should allow selecting a font with arrow keys and Enter', () => {});
+
+      // Disable reason: Not implemented yet
+      // eslint-disable-next-line jasmine/no-disabled-tests
+      xit('should close the font picker with Esc', () => {});
+    });
+  });
+});
