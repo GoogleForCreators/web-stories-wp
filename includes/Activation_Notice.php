8111948e685a46680bc024a01bb3def023effca3
Pascal Birchler <pascalb@google.com>
2020-07-15 11:26:43 +0200

    Only load Roboto variants that are actually needed

--- includes/Activation_Notice.php
+++ includes/Activation_Notice.php
@@ -87,8 +87,8 @@ class Activation_Notice {
 		unset( $_GET['activate'] ); // phpcs:ignore WordPress.Security.NonceVerification, WordPress.VIP.SuperGlobalInputUsage
 
 		wp_enqueue_style(
-			'roboto',
-			'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+			'web-stories-activation-notice-roboto',
+			'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900&display=swap',
 			[],
 			WEBSTORIES_VERSION
 		);
