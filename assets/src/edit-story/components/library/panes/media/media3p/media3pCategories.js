0fe8eddec2bccc6ca657686a7bf0b6b759540c66
Marc Prengemann <mprengemann@google.com>
2020-07-27 15:14:55 +1000

    (De-) Select category UI

--- assets/src/edit-story/components/library/panes/media/media3p/media3pCategories.js
+++ assets/src/edit-story/components/library/panes/media/media3p/media3pCategories.js
@@ -26,14 +26,14 @@ import PropTypes from 'prop-types';
  */
 import { ArrowDown } from '../../../../button/index';
 import CategoryPill from './categoryPill';
-// TODO(#2360) Category should be collapsible and expandable.
+
 const CategorySection = styled.div`
   background-color: ${({ theme }) => theme.colors.bg.v3};
   min-height: 94px;
   padding: 30px 24px 10px;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
 `;
 
 // This hides the category pills unless expanded
@@ -47,6 +47,8 @@ const CategoryPillContainer = styled.div`
 // Flips the button upside down when expanded;
 const ExpandButton = styled(ArrowDown)`
   ${(props) => props.isExpanded && 'transform: matrix(1, 0, 0, -1, 0, 0);'};
+  visibility: ${(props) => (props.hidden ? 'hidden' : 'visible')};
+  align-self: center;
 `;
 
 const Media3pCategories = ({
@@ -56,26 +58,44 @@ const Media3pCategories = ({
   deselectCategory,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+
+  function renderCategories() {
+    return categories.map((e) => {
+      return (
+        <CategoryPill
+          isSelected={false}
+          key={e.id}
+          title={e.displayName}
+          onClick={() => {
+            setIsExpanded(false);
+            selectCategory(e.id);
+          }}
+        />
+      );
+    });
+  }
+
+  function renderSelectedCategory() {
+    const selected = categories.find((e) => e.id === selectedCategoryId);
+    return (
+      <CategoryPill
+        isSelected={true}
+        key={selected.id}
+        title={selected.displayName}
+        onClick={() => deselectCategory()}
+      />
+    );
+  }
+
   return categories.length ? (
     <CategorySection aria-expanded={isExpanded}>
       <CategoryPillContainer isExpanded={isExpanded} role="tablist">
-        {categories.map((e) => {
-          const selected = e.id === selectedCategoryId;
-          return (
-            <CategoryPill
-              isSelected={selected}
-              key={e.id}
-              title={e.displayName}
-              onClick={() =>
-                selected ? deselectCategory() : selectCategory(e.id)
-              }
-            />
-          );
-        })}
+        {selectedCategoryId ? renderSelectedCategory() : renderCategories()}
       </CategoryPillContainer>
       <ExpandButton
         onClick={() => setIsExpanded(!isExpanded)}
         isExpanded={isExpanded}
+        hidden={selectedCategoryId}
       />
     </CategorySection>
   ) : null;
