226357a3ae2112c8811affb348dd87d58b8fea20
Pascal Birchler <pascalb@google.com>
2020-07-30 14:35:04 +0200

    Add missing demo story URL

--- includes/Activation_Notice.php
+++ includes/Activation_Notice.php
@@ -127,13 +127,15 @@ class Activation_Notice {
 			)
 		);
 
+		$demo_story_url = 'https://google.github.io/web-stories-wp/beta/tips.html';
+
 		// @todo Implement Get Started story - see https://github.com/google/web-stories-wp/pull/2845
 		return [
 			'id'     => 'web-stories-plugin-activation-notice',
 			'config' => [
 				'isRTL'        => is_rtl(),
 				'assetsURL'    => trailingslashit( WEBSTORIES_ASSETS_URL ),
-				'demoStoryURL' => '#',
+				'demoStoryURL' => $demo_story_url,
 				'newStoryURL'  => $new_story_url,
 				'dashboardURL' => $dashboard_url,
 			],
