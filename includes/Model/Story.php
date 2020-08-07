14a045154d718bf5755ccff4bfc7e994916fa025
spacedmonkey <jon@spacedmonkey.co.uk>
2020-08-07 18:01:44 +0100

    Remove space.

--- includes/Model/Story.php
+++ includes/Model/Story.php
@@ -107,7 +107,7 @@ class Story {
 		$thumbnail_id = (int) get_post_thumbnail_id( $post );
 
 		if ( 0 === $thumbnail_id ) {
-			$this->poster_portrait  = plugins_url( 'assets/images/fallback-poster.jpg', WEBSTORIES_PLUGIN_FILE );
+			$this->poster_portrait = plugins_url( 'assets/images/fallback-poster.jpg', WEBSTORIES_PLUGIN_FILE );
 		} else {
 			$this->poster_portrait  = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_PORTRAIT_IMAGE_SIZE );
 			$this->poster_square    = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_SQUARE_IMAGE_SIZE );
