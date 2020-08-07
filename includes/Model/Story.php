94fac77150ba2596a58e193b1e66bfec17793a3f
spacedmonkey <jon@spacedmonkey.co.uk>
2020-08-07 17:42:53 +0100

    Wrap in else.

--- includes/Model/Story.php
+++ includes/Model/Story.php
@@ -111,12 +111,12 @@ class Story {
 			$this->poster_portrait  = $poster;
 			$this->poster_square    = $poster;
 			$this->poster_landscape = $poster;
+		} else {
+			$this->poster_portrait  = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_PORTRAIT_IMAGE_SIZE );
+			$this->poster_square    = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_SQUARE_IMAGE_SIZE );
+			$this->poster_landscape = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_LANDSCAPE_IMAGE_SIZE );
 		}
-
-		$this->poster_portrait  = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_PORTRAIT_IMAGE_SIZE );
-		$this->poster_square    = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_SQUARE_IMAGE_SIZE );
-		$this->poster_landscape = (string) wp_get_attachment_image_url( $thumbnail_id, Media::POSTER_LANDSCAPE_IMAGE_SIZE );
-
+		
 		return true;
 	}
 
