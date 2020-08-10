b822a893307e1f1b9a2a854e728dcdc9db9ec3d7
Clinton Volzke <cvolzke@google.com>
2020-08-11 07:48:37 +1000

    Update useRovingTabIndex to focus outer element instead of firstChild

--- assets/src/edit-story/components/library/panes/media/common/mediaElement.js
+++ assets/src/edit-story/components/library/panes/media/common/mediaElement.js
@@ -54,11 +54,20 @@ const Video = styled.video`
   object-fit: cover;
 `;
 
-const Container = styled.div`
+const Container = styled.div.attrs((props) => ({
+  style: {
+    width: props.width + 'px',
+    height: props.height + 'px',
+    margin: props.margin,
+  },
+}))``;
+
+const InnerContainer = styled.div`
   position: relative;
   display: flex;
   margin-bottom: 10px;
-  body${KEYBOARD_USER_SELECTOR} &:focus {
+
+  body${KEYBOARD_USER_SELECTOR} .mediaElement:focus > & {
     outline: solid 2px #fff;
   }
 `;
@@ -121,6 +130,7 @@ const HiddenPosterImage = styled.img`
  * @param {Object} param.resource Resource object
  * @param {number} param.width Width that element is inserted into editor.
  * @param {number} param.height Height that element is inserted into editor.
+ * @param {number?} param.margin The margin in pixels around the element
  * @param {Function} param.onInsert Insertion callback.
  * @param {ProviderType} param.providerType Which provider the element is from.
  * @return {null|*} Element or null if does not map to video/image.
@@ -130,6 +140,7 @@ const MediaElement = ({
   resource,
   width: requestedWidth,
   height: requestedHeight,
+  margin,
   onInsert,
   providerType,
 }) => {
@@ -302,34 +313,39 @@ const MediaElement = ({
       data-testid="mediaElement"
       data-id={resourceId}
       className={'mediaElement'}
+      width={width}
+      height={height}
+      margin={margin}
       onPointerEnter={makeActive}
       onFocus={makeActive}
       onPointerLeave={makeInactive}
       onBlur={makeInactive}
       tabIndex={index === 0 ? 0 : -1}
     >
-      {innerElement}
-      {attribution}
-      {local && (
-        <CSSTransition
-          in
-          appear={true}
-          timeout={0}
-          className="uploading-indicator"
-        >
-          <UploadingIndicator />
-        </CSSTransition>
-      )}
-      {hasDropdownMenu && providerType === ProviderType.LOCAL && (
-        <DropDownMenu
-          resource={resource}
-          display={active}
-          isMenuOpen={isMenuOpen}
-          onMenuOpen={onMenuOpen}
-          onMenuCancelled={onMenuCancelled}
-          onMenuSelected={onMenuSelected}
-        />
-      )}
+      <InnerContainer>
+        {innerElement}
+        {attribution}
+        {local && (
+          <CSSTransition
+            in
+            appear={true}
+            timeout={0}
+            className="uploading-indicator"
+          >
+            <UploadingIndicator />
+          </CSSTransition>
+        )}
+        {hasDropdownMenu && providerType === ProviderType.LOCAL && (
+          <DropDownMenu
+            resource={resource}
+            display={active}
+            isMenuOpen={isMenuOpen}
+            onMenuOpen={onMenuOpen}
+            onMenuCancelled={onMenuCancelled}
+            onMenuSelected={onMenuSelected}
+          />
+        )}
+      </InnerContainer>
     </Container>
   );
 };
@@ -400,6 +416,7 @@ MediaElement.propTypes = {
   resource: PropTypes.object,
   width: PropTypes.number,
   height: PropTypes.number,
+  margin: PropTypes.number,
   onInsert: PropTypes.func,
   providerType: PropTypes.string,
 };
--- assets/src/edit-story/components/library/panes/media/common/mediaGallery.js
+++ assets/src/edit-story/components/library/panes/media/common/mediaGallery.js
@@ -19,7 +19,6 @@
  */
 import React, { useCallback, memo } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 /**
  * Internal dependencies
  */
@@ -28,15 +27,6 @@ import { ProviderType } from '../../../../../app/media/providerType';
 import MediaElement from './mediaElement';
 
 const PHOTO_MARGIN = 4;
-
-const PhotoContainer = styled.div.attrs((props) => ({
-  style: {
-    width: props.photo.width + 'px',
-    height: props.photo.height + 'px',
-  },
-}))`
-  margin: ${PHOTO_MARGIN}px;
-`;
 
 /**
  * Displays a gallery of media elements arranged in a row-based format.
@@ -56,16 +46,15 @@ function MediaGallery({ resources, onInsert, providerType }) {
 
   const imageRenderer = useCallback(
     ({ index, photo }) => (
-      <PhotoContainer photo={photo} key={index}>
-        <MediaElement
-          index={index}
-          resource={resources[index]}
-          width={photo.width}
-          height={photo.height}
-          onInsert={onInsert}
-          providerType={providerType}
-        />
-      </PhotoContainer>
+      <MediaElement
+        index={index}
+        margin={PHOTO_MARGIN + 'px'}
+        resource={resources[index]}
+        width={photo.width}
+        height={photo.height}
+        onInsert={onInsert}
+        providerType={providerType}
+      />
     ),
     [providerType, onInsert, resources]
   );
