4e834f6fecb859be8026179cfdba5edbcc96ffc2
spacedmonkey <jon@spacedmonkey.co.uk>
2020-08-13 13:14:59 +0100

    Enable embedded player for search or custom post type archives.

--- includes/Story_Post_Type.php
+++ includes/Story_Post_Type.php
@@ -26,7 +26,9 @@
 
 namespace Google\Web_Stories;
 
+use Google\Web_Stories\Model\Story;
 use Google\Web_Stories\REST_API\Stories_Controller;
+use Google\Web_Stories\Story_Renderer\Embed;
 use Google\Web_Stories\Traits\Assets;
 use Google\Web_Stories\Traits\Publisher;
 use Google\Web_Stories\Traits\Types;
@@ -172,6 +174,9 @@ class Story_Post_Type {
 		add_filter( 'amp_supportable_post_types', [ $this, 'filter_supportable_post_types' ] );
 
 		add_filter( '_wp_post_revision_fields', [ $this, 'filter_revision_fields' ], 10, 2 );
+
+		add_filter( 'the_content', [ $this, 'embed_player' ], PHP_INT_MAX );
+		add_filter( 'the_excerpt', [ $this, 'embed_player' ], PHP_INT_MAX );
 
 		// See https://github.com/Automattic/jetpack/blob/4b85be883b3c584c64eeb2fb0f3fcc15dabe2d30/modules/custom-post-types/portfolios.php#L80.
 		if ( defined( 'IS_WPCOM' ) && IS_WPCOM ) {
@@ -613,5 +618,34 @@ class Story_Post_Type {
 		$post_types[] = self::POST_TYPE_SLUG;
 
 		return $post_types;
+	}
+
+	/**
+	 * Change the content to an embedded player
+	 *
+	 * @param string $content Current content of filter.
+	 *
+	 * @return string
+	 */
+	public function embed_player( $content ) {
+		$post = get_post();
+
+		if ( is_feed() ) {
+			return $content;
+		}
+
+		if ( ! is_search() && ! is_post_type_archive() ) {
+			return $content;
+		}
+
+		if ( $post instanceof WP_Post && self::POST_TYPE_SLUG === $post->post_type ) {
+			$story = new Story();
+			$story->load_from_post( $post );
+
+			$embed   = new Embed( $story );
+			$content = $embed->render();
+		}
+
+		return $content;
 	}
 }
