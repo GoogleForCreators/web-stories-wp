59edd75502188864778d140113f6142a81b42e9e
Dillon Mulroy <dillon.mulroy@formidable.com>
2020-08-20 16:15:11 -0400

    refactor config context

--- assets/src/dashboard/utils/index.js
+++ assets/src/dashboard/utils/index.js
@@ -37,3 +37,4 @@ export { default as useResizeEffect } from '../../edit-story/utils/useResizeEffe
 // TODO use these shared utils to structure image resources mirroring editor
 export { default as getTypeFromMime } from '../../edit-story/app/media/utils/getTypeFromMime';
 export { default as getResourceFromLocalFile } from '../../edit-story/app/media/utils/getResourceFromLocalFile';
+export * from '../../edit-story/utils/context';
