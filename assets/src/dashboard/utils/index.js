303bd581946a5584fec4787a45547a373f59c384
Dillon Mulroy <dillon.mulroy@formidable.com>
2020-08-24 15:57:10 -0400

    update import/export

--- assets/src/dashboard/utils/index.js
+++ assets/src/dashboard/utils/index.js
@@ -37,4 +37,8 @@ export { default as useResizeEffect } from '../../edit-story/utils/useResizeEffe
 // TODO use these shared utils to structure image resources mirroring editor
 export { default as getTypeFromMime } from '../../edit-story/app/media/utils/getTypeFromMime';
 export { default as getResourceFromLocalFile } from '../../edit-story/app/media/utils/getResourceFromLocalFile';
-export * from '../../edit-story/utils/context';
+export {
+  createContext,
+  identity,
+  useContextSelector,
+} from '../../edit-story/utils/context';
