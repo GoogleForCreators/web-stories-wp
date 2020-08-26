f4e9d91ef35c3bcca4cfd274db44c41754766614
Morten Barklund <morten.barklund@xwp.co>
2020-08-26 19:30:51 -0400

    Implement reset poster image (#4112)
    * Generalized drop down menu component and used it in media form input
    
    * Added show/hide to media dropdown and full keyboard interactivity
    
    * Fixed some edge case issues with events and handling and fixed actual reset function
    
    * Fixed a11y tags for menu and menuitems
    
    * Removed a few superfluous aria attributes
    
    * Fixed tests (and added role of media picker input)
    
    * Allowed dropdown list to be either listbox or menu
    
    * Added karma tests for video accessibility panel
    
    * Added some snapshots
    
    * Removed cursor:pointer from media image container
    
    * Only set poster if different from old value

---
+++ assets/src/edit-story/components/dropDownMenu/index.js
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { useCallback, useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { More } from '../button';
+import DropDownList from '../form/dropDown/list';
+import Popup from '../popup';
+import { useKeyDownEffect } from '../keyboard';
+
+const MoreButton = styled(More)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  width: 28px;
+  height: 28px;
+  padding: 0;
+  background: ${({ theme }) => theme.colors.bg.panel};
+  color: ${({ theme }) => theme.colors.fg.white};
+  border-radius: 100%;
+  border: 0;
+`;
+
+const DropDownContainer = styled.div`
+  margin-top: 10px;
+`;
+
+function DropDownMenu({ options, onOption }) {
+  const dropDownId = useRef(`dropdown-${uuidv4()}`);
+  const ref = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOption = useCallback(
+    (...args) => {
+      setIsOpen(false);
+      onOption(...args);
+    },
+    [onOption]
+  );
+
+  const handleClose = useCallback(() => {
+    setIsOpen(false);
+    ref.current.focus();
+  }, []);
+
+  const handleOpen = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  useKeyDownEffect(ref, ['enter', 'down', 'up'], handleOpen, [handleOpen]);
+
+  return (
+    <>
+      <MoreButton
+        ref={ref}
+        onClick={handleOpen}
+        aria-haspopup="menu"
+        aria-expanded={isOpen}
+        aria-controls={dropDownId.current}
+        width="28"
+        height="28"
+        aria-label={__('More', 'web-stories')}
+      />
+      <Popup
+        placement="bottom-end"
+        anchor={ref}
+        isOpen={isOpen}
+        width={160}
+        id={dropDownId.current}
+      >
+        <DropDownContainer>
+          <DropDownList
+            handleCurrentValue={handleOption}
+            options={options}
+            value={options[0].value}
+            toggleOptions={handleClose}
+            hasMenuRole
+            aria-label={__('More', 'web-stories')}
+          />
+        </DropDownContainer>
+      </Popup>
+    </>
+  );
+}
+
+DropDownMenu.propTypes = {
+  options: PropTypes.array.isRequired,
+  onOption: PropTypes.func.isRequired,
+};
+
+export default DropDownMenu;
