be51fad21a35826db23ff4b629fcf3a8a1433921
Miina Sikk <miina.sikk@gmail.com>
2020-08-26 11:10:18 +0300

    Add Docs for degree helper.

--- assets/src/edit-story/components/canvas/utils/normalizeRotationDegrees.js
+++ assets/src/edit-story/components/canvas/utils/normalizeRotationDegrees.js
@@ -13,6 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * Converts degrees above 360 or below 0 to an equivalent between 0 and 360.
+ * Note that using just degrees % 360 would leave in negative degrees.
+ *
+ * @param {number} degrees Initial rotation angle.
+ * @return {number} Degrees between 0 and 360.
+ */
 function normalizeRotationDegrees(degrees) {
   return ((degrees % 360) + 360) % 360;
 }
