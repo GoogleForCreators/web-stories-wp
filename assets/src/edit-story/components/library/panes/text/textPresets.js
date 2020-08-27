f2f3dfdd394d6bef44606fa7b1b80055a4122c87
Mariano Martinez III <mariano.martinez@formidable.com>
2020-08-27 10:32:35 -0700

    Added fontweight to text presets

--- assets/src/edit-story/components/library/panes/text/textPresets.js
+++ assets/src/edit-story/components/library/panes/text/textPresets.js
@@ -22,7 +22,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { PAGE_HEIGHT, PAGE_WIDTH } from '../../../../constants';
+import { PAGE_HEIGHT, PAGE_WIDTH, FONT_WEIGHT } from '../../../../constants';
 import { dataFontEm } from '../../../../units';
 import { TEXT_ELEMENT_DEFAULT_FONT } from '../../../../app/font/defaultFonts';
 
@@ -33,6 +33,7 @@ const DEFAULT_LEFT_MARGIN = 40;
 
 const DEFAULT_PRESET = {
   content: __('Fill in some text', 'web-stories'),
+  fontWeight: FONT_WEIGHT.NORMAL,
   fontSize: dataFontEm(1.2),
   lineHeight: 1.5,
   x: DEFAULT_LEFT_MARGIN,
@@ -46,10 +47,11 @@ const PRESETS = [
   {
     title: __('Heading 1', 'web-stories'),
     element: {
-      content: `<span style="font-weight: 700">${__(
+      content: `<span style="font-weight: ${FONT_WEIGHT.BOLD}">${__(
         'Heading 1',
         'web-stories'
       )}</span>`,
+      fontWeight: FONT_WEIGHT.BOLD,
       fontSize: dataFontEm(2.7),
       lineHeight: 1.1,
       x: DEFAULT_LEFT_MARGIN,
@@ -61,10 +63,11 @@ const PRESETS = [
   {
     title: __('Heading 2', 'web-stories'),
     element: {
-      content: `<span style="font-weight: 700">${__(
+      content: `<span style="font-weight: ${FONT_WEIGHT.BOLD}">${__(
         'Heading 2',
         'web-stories'
       )}</span>`,
+      fontWeight: FONT_WEIGHT.BOLD,
       fontSize: dataFontEm(2),
       lineHeight: 1.2,
       x: DEFAULT_LEFT_MARGIN,
@@ -76,10 +79,11 @@ const PRESETS = [
   {
     title: __('Heading 3', 'web-stories'),
     element: {
-      content: `<span style="font-weight: 700">${__(
+      content: `<span style="font-weight: ${FONT_WEIGHT.BOLD}">${__(
         'Heading 3',
         'web-stories'
       )}</span>`,
+      fontWeight: FONT_WEIGHT.BOLD,
       fontSize: dataFontEm(1.6),
       lineHeight: 1.3,
       x: DEFAULT_LEFT_MARGIN,
@@ -95,6 +99,7 @@ const PRESETS = [
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         'web-stories'
       ),
+      fontWeight: FONT_WEIGHT.NORMAL,
       fontSize: dataFontEm(1.3),
       lineHeight: 1.5,
       x: DEFAULT_LEFT_MARGIN,
@@ -107,6 +112,7 @@ const PRESETS = [
     title: __('Caption', 'web-stories'),
     element: {
       content: __('Caption', 'web-stories'),
+      fontWeight: FONT_WEIGHT.NORMAL,
       fontSize: dataFontEm(1),
       lineHeight: 1.5,
       x: DEFAULT_LEFT_MARGIN,
@@ -119,6 +125,7 @@ const PRESETS = [
     title: __('OVERLINE', 'web-stories'),
     element: {
       content: __('OVERLINE', 'web-stories'),
+      fontWeight: FONT_WEIGHT.NORMAL,
       fontSize: dataFontEm(0.9),
       lineHeight: 1.5,
       x: DEFAULT_LEFT_MARGIN,
