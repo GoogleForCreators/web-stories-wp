d7c0ca259736daf4a82037ddd805e21dd1f402a3
Mariano Martinez III <mariano.martinez@formidable.com>
2020-08-31 15:03:57 -0700

    Small update based on feedback

--- assets/src/edit-story/components/library/panes/text/textSets/getTextSets.js
+++ assets/src/edit-story/components/library/panes/text/textSets/getTextSets.js
@@ -19,30 +19,33 @@
  */
 import { migrate } from '../../../../../migration/migrate';
 
+function updateMinMax(minMax, element) {
+  // Purposely mutating object so passed
+  // in minMax is modified
+  minMax.minX = Math.min(minMax.minX, element.x);
+  minMax.maxX = Math.max(minMax.maxX, element.x + element.width);
+
+  minMax.minY = Math.min(minMax.minY, element.y);
+  minMax.maxY = Math.max(minMax.maxY, element.y + element.height);
+
+  return minMax;
+}
+
 async function loadTextSet(name) {
   const data = await import(`./raw/${name}.json`);
   const migrated = migrate(data, data.version);
 
   const textSets = migrated.pages.reduce((sets, page) => {
-    let minX = Infinity;
-    let maxX = 0;
+    const minMax = {
+      minX: Infinity,
+      maxX: 0,
+      minY: Infinity,
+      maxY: 0,
+    };
 
-    let minY = Infinity;
-    let maxY = 0;
-
-    const textElements = page.elements.reduce((acc, element) => {
-      if (!element.isBackground) {
-        minX = Math.min(minX, element.x);
-        maxX = Math.max(maxX, element.x + element.width);
-
-        minY = Math.min(minY, element.y);
-        maxY = Math.max(maxY, element.y + element.height);
-
-        return [...acc, element];
-      }
-
-      return acc;
-    }, []);
+    const textElements = page.elements.filter((element) => {
+      return !element.isBackground && Boolean(updateMinMax(minMax, element));
+    });
 
     return [
       ...sets,
@@ -50,12 +53,12 @@ async function loadTextSet(name) {
         ...e,
         // Offset elements so the text set's
         // default position is (0,0)
-        x: e.x - minX,
-        y: e.y - minY,
+        x: e.x - minMax.minX,
+        y: e.y - minMax.minY,
         // The overall text set width & height
         // is the delta between the max/mins
-        textSetWidth: maxX - minX,
-        textSetHeight: maxY - minY,
+        textSetWidth: minMax.maxX - minMax.minX,
+        textSetHeight: minMax.maxY - minMax.minY,
       })),
     ];
   }, []);
