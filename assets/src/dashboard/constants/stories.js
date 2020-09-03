2a133bf39c7f447cc3a37c6a716c1c63381599e6
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-09-03 10:26:06 -0700

    stories should use same button text to open preview dialog - preview

--- assets/src/dashboard/constants/stories.js
+++ assets/src/dashboard/constants/stories.js
@@ -118,8 +118,8 @@ export const STORY_STATUS = {
 };
 
 export const STORY_ITEM_CENTER_ACTION_LABELS = {
-  [STORY_STATUS.PUBLISH]: __('View', 'web-stories'),
-  [STORY_STATUS.FUTURE]: __('View', 'web-stories'),
+  [STORY_STATUS.PUBLISH]: __('Preview', 'web-stories'),
+  [STORY_STATUS.FUTURE]: __('Preview', 'web-stories'),
   [STORY_STATUS.DRAFT]: __('Preview', 'web-stories'),
 };
 
