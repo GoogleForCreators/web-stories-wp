cb1b9e68dc5c3cca43a30830f950425be59d5bbc
Pascal Birchler <pascalb@google.com>
2020-09-10 12:09:41 +0200

    Tracking: allow any user to update their own opt-in preferences

--- tests/phpunit/tests/Tracking.php
+++ tests/phpunit/tests/Tracking.php
@@ -26,16 +26,30 @@ use WP_REST_Request;
 class Tracking extends \WP_UnitTestCase {
 	protected static $user_id;
 
+	protected static $author_id;
+
 	public static function wpSetUpBeforeClass( $factory ) {
 		self::$user_id = $factory->user->create(
 			[
 				'role' => 'administrator',
 			]
 		);
+
+		self::$author_id = $factory->user->create(
+			[
+				'role' => 'author',
+			]
+		);
 	}
 
 	public static function wpTearDownAfterClass() {
 		self::delete_user( self::$user_id );
+		self::delete_user( self::$author_id );
+	}
+
+	public function tearDown() {
+		unregister_meta_key( 'user', \Google\Web_Stories\Tracking::OPTIN_META_KEY );
+		parent::tearDown();
 	}
 
 	/**
@@ -49,9 +63,53 @@ class Tracking extends \WP_UnitTestCase {
 		$request  = new WP_REST_Request( \WP_REST_Server::READABLE, sprintf( '/wp/v2/users/%d', self::$user_id ) );
 		$response = rest_get_server()->dispatch( $request );
 		$data     = $response->get_data();
+
+		$this->assertArrayHasKey( 'meta', $data );
 		$this->assertArrayHasKey( \Google\Web_Stories\Tracking::OPTIN_META_KEY, $data['meta'] );
 		$this->assertTrue( $data['meta'][ \Google\Web_Stories\Tracking::OPTIN_META_KEY ] );
 	}
+
+	/**
+	 * @covers ::init
+	 */
+	public function test_add_optin_field_to_rest_api_for_author_user() {
+		wp_set_current_user( self::$author_id );
+		( new \Google\Web_Stories\Tracking() )->init();
+		add_user_meta( get_current_user_id(), \Google\Web_Stories\Tracking::OPTIN_META_KEY, true );
+
+		$request  = new WP_REST_Request( \WP_REST_Server::READABLE, '/wp/v2/users/me' );
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertArrayHasKey( 'meta', $data );
+		$this->assertArrayHasKey( \Google\Web_Stories\Tracking::OPTIN_META_KEY, $data['meta'] );
+		$this->assertTrue( $data['meta'][ \Google\Web_Stories\Tracking::OPTIN_META_KEY ] );
+	}
+
+	/**
+	 * @covers ::init
+	 */
+	public function test_enables_author_user_to_update_meta_field() {
+		wp_set_current_user( self::$author_id );
+		( new \Google\Web_Stories\Tracking() )->init();
+		add_user_meta( get_current_user_id(), \Google\Web_Stories\Tracking::OPTIN_META_KEY, false );
+
+		$request = new WP_REST_Request( \WP_REST_Server::CREATABLE, '/wp/v2/users/me' );
+		$request->set_body_params(
+			[
+				'meta' => [
+					\Google\Web_Stories\Tracking::OPTIN_META_KEY => true,
+				],
+			]
+		);
+		$response = rest_get_server()->dispatch( $request );
+		$data     = $response->get_data();
+
+		$this->assertArrayHasKey( 'meta', $data );
+		$this->assertArrayHasKey( \Google\Web_Stories\Tracking::OPTIN_META_KEY, $data['meta'] );
+		$this->assertTrue( $data['meta'][ \Google\Web_Stories\Tracking::OPTIN_META_KEY ] );
+	}
+
 
 	/**
 	 * @covers ::init
