4b19f3513bdd1ec65b14d8d57948ae3c6ff5c0ad
Pascal Birchler <pascalb@google.com>
2020-09-21 14:21:36 +0200

    Update scraper script for better use with real content

--- web-stories-scraper/README.md
+++ web-stories-scraper/README.md
@@ -4,14 +4,10 @@ Downloads web stories to a given directory.
 
 ## Usage
 
-`stories.txt` contains the list of story URLs to be downloaded.
-
-You can add new URLs to that file, separated by newline.
-
-Then, execute the script and provide the target directory. Example:
+Pass the story URL and target directory to the script like so:
 
 ```bash
-$ node index.js ../public/stories
-Downloading 1 story and saving to '../public/stories'
-Downloaded https://example.com/story to ../public/stories/example.com/story/index.html
+$ node index.js https://example.com/stories/intro-to-web-stories-storytime/ ../public/stories
+Downloading story and saving to '../public/stories'
+Downloaded https://example.com/stories/intro-to-web-stories-storytime/ to ../public/stories/intro-to-web-stories-storytime/index.html
 ```
--- web-stories-scraper/index.js
+++ web-stories-scraper/index.js
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 
-import { fileURLToPath } from 'url';
-import { dirname, join, relative } from 'path';
-import { existsSync, readFileSync } from 'fs';
+import { fileURLToPath, URL } from 'url';
+import { dirname, join, relative, extname, basename } from 'path';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+
 import scrape from 'website-scraper';
 import SaveToExistingDirectoryPlugin from 'website-scraper-existing-directory';
 
@@ -25,48 +26,118 @@ const __dirname = dirname(__filename);
 
 const args = process.argv.slice(2);
 
-const directory = args[0] ? join(__dirname, args[0]) : undefined;
+const url = args[0] ? args[0] : undefined;
+const directory = args[1] ? join(__dirname, args[1]) : undefined;
 
-if (!directory || !existsSync(directory)) {
+if (!url) {
+  console.log('Story URL was not provided');
+  process.exit(1);
+}
+
+if (!directory) {
   console.log('Target directory was not provided.');
   process.exit(1);
 }
 
-const urls = readFileSync('./stories.txt', 'utf8').split('\n').filter(Boolean);
+if (!existsSync(directory)) {
+  mkdirSync(directory);
+}
 
-const noun = urls.length === 1 ? 'story' : 'stories';
 console.log(
-  `Downloading ${urls.length} ${noun} and saving to '${relative(
-    __dirname,
-    directory
-  )}'`
+  `Downloading story and saving to '${relative(__dirname, directory)}'`
 );
 
+class WebStoriesScraperPlugin {
+  apply(registerAction) {
+    let defaultFilename;
+    let subdirectories;
+    let storyPath = '';
+
+    registerAction('beforeStart', ({ options }) => {
+      defaultFilename = options.defaultFilename;
+      subdirectories = options.subdirectories;
+    });
+
+    registerAction('generateFilename', ({ resource }) => {
+      const parsedURL = new URL(resource.getUrl());
+      const urlPath = basename(parsedURL.pathname);
+      let filename = resource.getFilename() || urlPath || defaultFilename;
+      const fileExtension = extname(filename);
+
+      if (resource.isHtml()) {
+        storyPath = urlPath;
+      }
+
+      if (resource.isHtml()) {
+        filename = join(urlPath, filename);
+      } else {
+        const directoryByExtension =
+          subdirectories
+            .filter((dir) => dir.extensions.includes(fileExtension))
+            .map((dir) => dir.directory)
+            .shift() || '';
+
+        filename = join(storyPath, directoryByExtension, filename);
+      }
+
+      return {
+        filename,
+      };
+    });
+
+    // Clean up resulting HTML file.
+    registerAction('onResourceSaved', ({ resource }) => {
+      if (resource.isHtml()) {
+        const filePath = join(directory, resource.getFilename());
+
+        writeFileSync(
+          filePath,
+          readFileSync(filePath, 'utf-8')
+            // Remove some clutter.
+            .replace(
+              /<link rel="(EditURI|wlwmanifest|prev|next|shortlink|alternate)"[^>]+>\s?/gm,
+              ''
+            )
+            .replace(/<link rel="https:\/\/api\.w\.org\/"[^>]+>/gm, '')
+            // Fix for https://github.com/website-scraper/node-website-scraper/issues/355.
+            .replace(/font-family:"([^,]+)"/gm, `font-family:'$1'`)
+        );
+      }
+    });
+  }
+}
+
 const options = {
-  urls,
+  urls: [url],
   directory,
   sources: [
     { selector: 'amp-story', attr: 'publisher-logo-src' },
     { selector: 'amp-story', attr: 'poster-portrait-src' },
+    { selector: 'amp-story', attr: 'poster-landscape-src' },
+    { selector: 'amp-story', attr: 'poster-square-src' },
     { selector: 'amp-img', attr: 'src' },
     { selector: 'amp-img', attr: 'srcset' },
     { selector: 'amp-video', attr: 'poster' },
+    { selector: 'amp-video', attr: 'artwork' },
     { selector: 'amp-video > source', attr: 'src' },
+    { selector: 'meta[property="og:url"]', attr: 'content' },
+    { selector: 'meta[property="og:image"]', attr: 'content' },
+    { selector: 'meta[property="twitter:image"]', attr: 'content' },
+    { selector: 'link[rel="canonical"]', attr: 'href' },
   ],
   subdirectories: [
     {
       directory: 'assets',
-      extensions: ['.jpg', '.png', '.svg', '.webp', '.mp4'],
+      extensions: ['.jpg', '.jpeg', '.png', '.svg', '.webp', '.gif', '.mp4'],
     },
   ],
-  filenameGenerator: 'bySiteStructure',
-  plugins: [new SaveToExistingDirectoryPlugin()],
+  plugins: [new SaveToExistingDirectoryPlugin(), new WebStoriesScraperPlugin()],
 };
 
 const result = await scrape(options);
 
-for (const { url, filename } of result) {
+for (const { url: _url, filename } of result) {
   console.log(
-    `Downloaded ${url} to ${relative(__dirname, join(directory, filename))}`
+    `Downloaded ${_url} to ${relative(__dirname, join(directory, filename))}`
   );
 }
