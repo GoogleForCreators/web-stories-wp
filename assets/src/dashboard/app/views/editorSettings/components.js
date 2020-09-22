d06c939e0d72865799aae36e43a40befd9490af2
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-09-22 13:09:09 -0700

    restores label to google analytics input on editor settings view. Adds test to check that label is present for screen readers. fixes some spelling errors that snuck by and moves save button text to be with object holding other text for view

--- assets/src/dashboard/app/views/editorSettings/components.js
+++ assets/src/dashboard/app/views/editorSettings/components.js
@@ -214,7 +214,7 @@ export const ErrorText = styled.p`
 export const InlineForm = styled.div`
   display: flex;
 `;
-
+export const VisuallyHiddenLabel = styled.label(visuallyHiddenStyles);
 export const GoogleAnalyticsTextInput = styled(TextInput)`
   flex: 3;
   width: auto;
