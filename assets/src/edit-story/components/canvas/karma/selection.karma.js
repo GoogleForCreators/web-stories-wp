8f33bec94be670928306203d5259db091fa6c9d5
Miina Sikk <miina.sikk@gmail.com>
2020-09-23 22:22:22 +0300

    Add CUJ description.

--- assets/src/edit-story/components/canvas/karma/selection.karma.js
+++ assets/src/edit-story/components/canvas/karma/selection.karma.js
@@ -20,7 +20,7 @@
 import { Fixture } from '../../../karma';
 import { useStory } from '../../../app/story';
 
-describe('Selection integration', () => {
+describe('CUJ: Creator can Transform an Element: Selection integration', () => {
   let fixture;
   let fullbleed;
 
