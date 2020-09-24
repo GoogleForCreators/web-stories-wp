e093bf4cb535c1f8a34dd8a1777e35bd9a7747ff
Pascal Birchler <pascalb@google.com>
2020-09-24 13:03:00 +0200

    Bump version to 1.1.0-alpha

--- web-stories.php
+++ web-stories.php
@@ -12,7 +12,7 @@
  * Plugin URI: https://wp.stories.google/
  * Author: Google
  * Author URI: https://opensource.google.com/
- * Version: 1.0.0
+ * Version: 1.1.0-alpha
  * Requires at least: 5.3
  * Requires PHP: 5.6
  * Text Domain: web-stories
@@ -40,7 +40,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WEBSTORIES_VERSION', '1.0.0' );
+define( 'WEBSTORIES_VERSION', '1.1.0-alpha' );
 define( 'WEBSTORIES_DB_VERSION', '3.0.2' );
 define( 'WEBSTORIES_PLUGIN_FILE', __FILE__ );
 define( 'WEBSTORIES_PLUGIN_DIR_PATH', plugin_dir_path( WEBSTORIES_PLUGIN_FILE ) );
