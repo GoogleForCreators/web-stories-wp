350d7124d7561978ef3c948ace7d01fc8003c00c
Pascal Birchler <pascalb@google.com>
2020-10-01 22:12:31 +0200

    Undo autoWatch change

--- karma-dashboard.config.cjs
+++ karma-dashboard.config.cjs
@@ -118,7 +118,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
