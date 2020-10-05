f30ce6758f134c652186459874d03cb0d582591d
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-10-05 16:51:35 -0700

    initial refactor of saved templates to catch up to updates made in templates and stories. gave saved templates its own grid instance - it will be great to consolidate these three variations of functionality but for now they are different enough that it makes sense to have them be separate while the UX is figured out. Gave fetchMyTemplates a serializer. Gave saved templates its own source for context menu, disabled most of the actions. Connected use template button and preview - neither of these work right now because of the bad data format but the functionality underlying it is present

---
+++ assets/src/dashboard/app/serializers/savedTemplates.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+/**
+ * Internal dependencies
+ */
+import { migrate, DATA_VERSION } from '../../../edit-story/migration/migrate';
+import { toUTCDate } from '../../../date';
+
+// TODO: this is mostly placeholder to generally lend shape to saved templates that matches stories. We do no have working template APIs yet
+export default function reshapeSavedTemplateObject(originalSavedTemplateData) {
+  const {
+    id,
+    title,
+    modified_gmt,
+    status,
+    date_gmt,
+    link,
+    story_data: storyData = {},
+  } = originalSavedTemplateData;
+  if (!Array.isArray(storyData?.pages) || !id || storyData.pages.length === 0) {
+    storyData.pages = [];
+    // return null;
+  }
+
+  const updatedStoryData = {
+    ...migrate(storyData, storyData.version),
+    version: DATA_VERSION,
+  };
+
+  return {
+    id,
+    status,
+    title: title,
+    modified: toUTCDate(modified_gmt || new Date()),
+    created: toUTCDate(date_gmt || new Date()),
+    pages: updatedStoryData?.pages,
+    author: __('Google', 'web-stories'), // TODO update after users refactor is merged
+    link,
+    originalSavedTemplateData,
+  };
+}
---
+++ assets/src/dashboard/app/views/savedTemplates/content/index.js
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { useFeature } from 'flagged';
+import { useCallback, useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { TransformProvider } from '../../../../../edit-story/components/transform';
+import { UnitsProvider } from '../../../../../edit-story/units';
+import {
+  Layout,
+  StandardViewContentGutter,
+  InfiniteScroller,
+} from '../../../../components';
+import {
+  SAVED_TEMPLATE_CONTEXT_MENU_ITEMS,
+  TEMPLATES_GALLERY_ITEM_CENTER_ACTION_LABELS,
+} from '../../../../constants';
+import { TemplateActionsPropType, TemplatesPropType } from '../../../../types';
+import { PagePropTypes, ViewPropTypes } from '../../../../utils/useStoryView';
+import FontProvider from '../../../font/fontProvider';
+import { SavedTemplateGridView } from '../../shared';
+
+function Content({
+  allPagesFetched,
+  initialFocusId,
+  isLoading,
+  page,
+  search,
+  templates,
+  actions,
+  view,
+}) {
+  const [contextMenuId, setContextMenuId] = useState(-1);
+  const enableInProgressStoryActions = useFeature(
+    'enableInProgressStoryActions'
+  );
+
+  const handleMenuItemSelected = useCallback(() => {
+    setContextMenuId(-1);
+    // TODO add context menu actions
+  }, []);
+
+  const enabledMenuItems = useMemo(() => {
+    if (enableInProgressStoryActions) {
+      return SAVED_TEMPLATE_CONTEXT_MENU_ITEMS;
+    }
+    return SAVED_TEMPLATE_CONTEXT_MENU_ITEMS.filter((item) => !item.inProgress);
+  }, [enableInProgressStoryActions]);
+
+  const templateMenu = useMemo(() => {
+    return {
+      handleMenuToggle: setContextMenuId,
+      contextMenuId,
+      handleMenuItemSelected,
+      menuItems: enabledMenuItems,
+    };
+  }, [
+    setContextMenuId,
+    contextMenuId,
+    handleMenuItemSelected,
+    enabledMenuItems,
+  ]);
+
+  return (
+    <Layout.Scrollable>
+      <FontProvider>
+        <TransformProvider>
+          <UnitsProvider
+            pageSize={{
+              width: view.pageSize.width,
+              height: view.pageSize.height,
+            }}
+          >
+            <StandardViewContentGutter>
+              {/* Add empty view from task/clean-up-repeat-dashboard-text-components */}
+              <SavedTemplateGridView
+                bottomActionLabel={__('Use template', 'web-stories')}
+                centerActionLabelByStatus={
+                  TEMPLATES_GALLERY_ITEM_CENTER_ACTION_LABELS
+                }
+                actions={actions}
+                pageSize={view.pageSize}
+                templateMenu={templateMenu}
+                templates={templates}
+                returnFocusId={initialFocusId}
+              />
+              <InfiniteScroller
+                allDataLoadedMessage={__('No more templates', 'web-stories')}
+                isLoading={isLoading}
+                canLoadMore={!allPagesFetched}
+                onLoadMore={page.requestNextPage}
+              />
+            </StandardViewContentGutter>
+          </UnitsProvider>
+        </TransformProvider>
+      </FontProvider>
+    </Layout.Scrollable>
+  );
+}
+
+export default Content;
+
+Content.propTypes = {
+  view: ViewPropTypes.isRequired,
+  page: PagePropTypes.isRequired,
+  templates: TemplatesPropType,
+  initialFocusId: PropTypes.number,
+  isLoading: PropTypes.bool,
+  allPagesFetched: PropTypes.bool,
+  actions: TemplateActionsPropType,
+  search: PropTypes.object,
+};
