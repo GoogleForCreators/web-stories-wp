0f74b9cb95200a1034064100c6b4abf2af3b8dfe
Emily Bartman <emily.bartman@formidable.com>
2020-10-12 15:41:16 -0700

    lint fix

--- assets/src/edit-story/app/media/utils/createResource.js
+++ assets/src/edit-story/app/media/utils/createResource.js
@@ -99,7 +99,7 @@ import getTypeFromMime from './getTypeFromMime';
  * @property {Object.<string, ResourceSize>} sizes Object of image sizes.
  * @property {Attribution|null} attribution An optional attribution for the
  * resource.
- * @property {Object|null} output An optional object of video sizes for rendering gifs as videos
+ * @property {?Object} output An optional object of video sizes for rendering gifs as videos
  */
 
 /**
