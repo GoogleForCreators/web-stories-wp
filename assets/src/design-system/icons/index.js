b81518c4fc39f49ec6893adaca7f042e6c2b12e0
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-10-16 08:08:08 -0700

    WIP icons

---
+++ assets/src/design-system/icons/index.js
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+export { ReactComponent as AddFilled } from './addFilled.svg';
+export { ReactComponent as AddOutlined } from './addOutlined.svg';
+export { ReactComponent as AlignHorizontalCenter } from './alignHorizontalCenter.svg';
+export { ReactComponent as AlignLeft } from './alignLeft.svg';
+export { ReactComponent as AlignRight } from './alignHorizontalCenter.svg';
+export { ReactComponent as Animation } from './animation.svg';
+export { ReactComponent as ArrowLeft } from './arrowLeft.svg';
+export { ReactComponent as ArrowRight } from './arrowRight.svg';
+export { ReactComponent as Bold } from './bold.svg';
+export { ReactComponent as Bookmark } from './bookmark.svg';
+export { ReactComponent as Checkmark } from './checkmark.svg';
+export { ReactComponent as ChevronDown } from './chevronDown.svg';
+export { ReactComponent as Close } from './close.svg';
+export { ReactComponent as DistributeDropdown } from './distributeDropdown.svg';
+export { ReactComponent as DistributeVertical } from './distributeVertical.svg';
+export { ReactComponent as Drag } from './drag.svg';
+export { ReactComponent as DragVertical } from './dragVertical.svg';
+export { ReactComponent as Duplicate } from './duplicate.svg';
+export { ReactComponent as Edit } from './edit.svg';
+export { ReactComponent as FlipVertical } from './flipVertical.svg';
+export { ReactComponent as Grid } from './grid.svg';
+export { ReactComponent as Grid2 } from './grid2.svg';
+export { ReactComponent as Info } from './info.svg';
+export { ReactComponent as Italic } from './italic.svg';
+export { ReactComponent as KeyboardShortcut } from './keyboardShortcut.svg';
+export { ReactComponent as Layout } from './layout.svg';
+export { ReactComponent as LetterSpacing } from './letterSpacing.svg';
+export { ReactComponent as LightModeNegative } from './lightModeNegative.svg';
+export { ReactComponent as LightModeWarning } from './lightModeWarning.svg';
+export { ReactComponent as LineHeight } from './lineHeight.svg';
+export { ReactComponent as Lock } from './lock.svg';
+export { ReactComponent as Media } from './media.svg';
+export { ReactComponent as More } from './more.svg';
+export { ReactComponent as MoreVertical } from './moreVertical.svg';
+export { ReactComponent as Percent } from './percent.svg';
+export { ReactComponent as Play } from './play.svg';
+export { ReactComponent as PlayAnimation } from './playAnimation.svg';
+export { ReactComponent as Plus } from './plus.svg';
+export { ReactComponent as Preview } from './preview.svg';
+export { ReactComponent as Redo } from './redo.svg';
+export { ReactComponent as Rotate } from './rotate.svg';
+export { ReactComponent as SafeZone } from './safeZone.svg';
+export { ReactComponent as Search } from './search.svg';
+export { ReactComponent as Settings } from './settings.svg';
+export { ReactComponent as Shapes } from './shapes.svg';
+export { ReactComponent as Share } from './share.svg';
+export { ReactComponent as StopAnimation } from './stopAnimation.svg';
+export { ReactComponent as StrikeThrough } from './strikeThrough.svg';
+export { ReactComponent as Styles } from './styles.svg';
+export { ReactComponent as Text } from './text.svg';
+export { ReactComponent as TextAdd } from './textAdd.svg';
+export { ReactComponent as TextAlignCenter } from './textAlignCenter.svg';
+export { ReactComponent as TextAlignJustified } from './textAlignJustified.svg';
+export { ReactComponent as TextAlignLeft } from './textAlignLeft.svg';
+export { ReactComponent as TextAlignRight } from './textAlignRight.svg';
+export { ReactComponent as TextSize } from './textSize.svg';
+export { ReactComponent as Timeline } from './timeline.svg';
+export { ReactComponent as Trash } from './trash.svg';
+export { ReactComponent as Underline } from './underline.svg';
+export { ReactComponent as Undo } from './undo.svg';
+export { ReactComponent as Unlock } from './unlock.svg';
+export { ReactComponent as Upload } from './upload.svg';
+export { ReactComponent as Video } from './video.svg';
+export { ReactComponent as ViewList } from './viewList.svg';
+export { ReactComponent as ZoomMinus } from './zoomMinus.svg';
+export { ReactComponent as ZoomPlus } from './zoomPlus.svg';
+
+export { ReactComponent as ExploreTemplatesActive } from './dashboard/exploreTemplatesActive.svg';
+export { ReactComponent as ExploreTemplatesNormal } from './dashboard/exploreTemplatesNormal.svg';
+export { ReactComponent as SavedTemplatesActive } from './dashboard/savedTemplatesActive.svg';
+export { ReactComponent as SavedTemplatesNormal } from './dashboard/savedTemplatesNormal.svg';
+export { ReactComponent as StoriesActive } from './dashboard/storiesActive.svg';
+export { ReactComponent as StoriesNormal } from './dashboard/storiesNormal.svg';
