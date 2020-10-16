e177e2c93caebd692cbbd0554af0c8768b330c49
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-10-16 13:10:03 -0700

    try out subdirectories to group icons

---
+++ assets/src/design-system/icons/general/index.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+export { ReactComponent as Bookmark } from './bookmark.svg';
+export { ReactComponent as Checkmark } from './checkmark.svg';
+export { ReactComponent as Chevron } from './chevron.svg';
+export { ReactComponent as Edit } from './edit.svg';
+export { ReactComponent as Info } from './info.svg';
+export { ReactComponent as Search } from './search.svg';
+export { ReactComponent as Settings } from './settings.svg';
+export { ReactComponent as Trash } from './trash.svg';
