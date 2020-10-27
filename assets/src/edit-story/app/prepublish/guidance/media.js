0e9399fa59e6aa28a1df1b85850483348ca3e00c
Emily Bartman <emily.bartman@formidable.com>
2020-10-27 16:37:04 -0700

    prepublish/guidance/media: checklist with some todos

--- assets/src/edit-story/app/prepublish/guidance/media.js
+++ assets/src/edit-story/app/prepublish/guidance/media.js
@@ -14,10 +14,106 @@
  * limitations under the License.
  */
 
-export function mediaElementSizeOnPage(/*element*/) {}
+/**
+ * Internal dependencies
+ */
+import { PAGE_HEIGHT, PAGE_WIDTH } from '../../../constants';
 
-export function mediaElementResolution(/*element*/) {}
+const SAFE_ZONE_AREA = PAGE_HEIGHT * PAGE_WIDTH;
 
-export function videoElementLength(/*element*/) {}
+const MIN_PIXEL_DENSITY = 2;
 
-export function videoElementFps(/*element*/) {}
+const MAX_VIDEO_LENGTH_SECONDS = 60;
+const MAX_VIDEO_WIDTH = 3840;
+const MAX_VIDEO_HEIGHT = 2160;
+const MIN_VIDEO_HEIGHT = 480;
+const MIN_VIDEO_WIDTH = 852;
+
+// todo: get video frames per second
+// MIN_VIDEO_FPS = 24;
+// export function videoElementFps(element) {}
+
+const getChecklistMessage = (elementId, message) => ({
+  type: 'guidance',
+  elementId,
+  message,
+});
+
+export function mediaElementSizeOnPage(element) {
+  // triggered if only one video is on the page and it takes less than 50% of the safe zone area
+  // encourage users to consider a more immersive media sizing and cropping.
+  const elementArea = element.height * element.width;
+  const isTooSmallOnPage = SAFE_ZONE_AREA / 2 > elementArea;
+
+  if (isTooSmallOnPage) {
+    const media = element.type === 'video' ? 'Video' : 'Image';
+    return getChecklistMessage(element.id, `${media} is too small on the page`);
+  }
+
+  return undefined;
+}
+
+export function videoElementSizeOnPage(page) {
+  const videoElementsOnPage = page.elements.filter(
+    ({ type }) => type === 'video'
+  );
+  if (videoElementsOnPage.length === 1) {
+    const [videoElement] = videoElementsOnPage;
+    return mediaElementSizeOnPage(videoElement);
+  }
+  return undefined;
+}
+
+export function mediaElementResolution(element) {
+  if (element.type === 'video') {
+    return videoElementResolution(element);
+  }
+
+  if (element.type === 'image' || element.type === 'gif') {
+    return imageElementResolution(element);
+  }
+
+  return undefined;
+}
+
+function videoElementResolution(element) {
+  const videoResolutionLow =
+    element.resource.full.height <= MIN_VIDEO_HEIGHT &&
+    element.resource.full.width <= MIN_VIDEO_WIDTH;
+  const videoResolutionHigh =
+    element.resource.full.height >= MAX_VIDEO_HEIGHT &&
+    element.resource.full.width >= MAX_VIDEO_WIDTH;
+
+  if (videoResolutionHigh) {
+    return getChecklistMessage(
+      element.id,
+      "Video's resolution is too high to display on most mobile devices (>4k)"
+    );
+  }
+
+  if (videoResolutionLow) {
+    return getChecklistMessage(element.id, 'Video has low resolution');
+  }
+
+  return undefined;
+}
+
+function imageElementResolution(element) {
+  const resourceDims = element.resource.height * element.resource.width;
+  const elementDims = element.height * element.width;
+  const minDensity = resourceDims / MIN_PIXEL_DENSITY;
+  if (elementDims > minDensity) {
+    return getChecklistMessage(element.id, 'Image has low resolution');
+  }
+  return undefined;
+}
+
+export function videoElementLength(element) {
+  if (element.resource.length > MAX_VIDEO_LENGTH_SECONDS) {
+    return getChecklistMessage(
+      element.id,
+      'Video is longer than 1 minute (suggest breaking video up into multiple segments)'
+    );
+  }
+  return undefined;
+}
--- assets/src/edit-story/app/prepublish/guidance/test/media.js
+++ assets/src/edit-story/app/prepublish/guidance/test/media.js
@@ -17,24 +17,102 @@
 /**
  * Internal dependencies
  */
-// import * as mediaGuidance from '../media';
+import * as mediaGuidance from '../media';
 
 describe('Pre-publish checklist - media guidelines (guidance)', () => {
-  // triggered if only one video is on the page and it takes less than 50% of the safe zone area
-  // encourage users to consider a more immersive media sizing and cropping.
-  it.todo(
-    'should return a message if a video or image element is too small on the page'
-  );
-  // actual image asset on screen, at the current zoom, offers <2x pixel density
-  // guideline is to strive for >828 x 1792
+  it('should return a message if an image element takes up <50% of the safe zone area', () => {
+    const tooSmallImageElement = {
+      id: 123,
+      type: 'image',
+      height: 50,
+      width: 50,
+    };
+    const result = mediaGuidance.mediaElementSizeOnPage(tooSmallImageElement);
+    expect(result).not.toBeUndefined();
+    expect(result.type).toStrictEqual('guidance');
+    expect(result.elementId).toStrictEqual(tooSmallImageElement.id);
+  });
+
+  it('should return a message if a video element takes up <50% of the safe zone area', () => {
+    const tooSmallVideoElement = {
+      id: 456,
+      type: 'video',
+      height: 100,
+      width: 400,
+    };
+    const result = mediaGuidance.mediaElementSizeOnPage(tooSmallVideoElement);
+    expect(result).not.toBeUndefined();
+    expect(result.message).toMatchInlineSnapshot(
+      `"Video is too small on the page"`
+    );
+    expect(result.type).toStrictEqual('guidance');
+    expect(result.elementId).toStrictEqual(tooSmallVideoElement.id);
+  });
+
+  it('should return a message if a video element is less than 480p', () => {
+    const tooLowResolutionVideoElement = {
+      id: 789,
+      type: 'video',
+      resource: {
+        full: {
+          height: 480,
+          width: 852,
+        },
+      },
+    };
+
+    const result = mediaGuidance.mediaElementResolution(
+      tooLowResolutionVideoElement
+    );
+    expect(result).not.toBeUndefined();
+    expect(result.message).toMatchInlineSnapshot(`"Video has low resolution"`);
+    expect(result.type).toStrictEqual('guidance');
+    expect(result.elementId).toStrictEqual(tooLowResolutionVideoElement.id);
+  });
+
+  it('should return a message if any video resolution is too high to display on most mobile devices (>4k)', () => {
+    const tooHighVideoResolution = {
+      id: 101,
+      type: 'video',
+      resource: {
+        full: {
+          height: 2160,
+          width: 3840,
+        },
+      },
+    };
+
+    const result = mediaGuidance.mediaElementResolution(tooHighVideoResolution);
+    expect(result).not.toBeUndefined();
+    expect(result.message).toMatchInlineSnapshot(
+      `"Video's resolution is too high to display on most mobile devices (>4k)"`
+    );
+    expect(result.type).toStrictEqual('guidance');
+    expect(result.elementId).toStrictEqual(tooHighVideoResolution.id);
+  });
+
+  it('should return a message if the video element is longer than 1 minute', () => {
+    const tooLongVideo = {
+      id: 202,
+      type: 'video',
+      resource: {
+        length: 75,
+        full: {
+          height: 2160,
+          width: 3840,
+        },
+      },
+    };
+
+    const result = mediaGuidance.videoElementLength(tooLongVideo);
+    expect(result).not.toBeUndefined();
+    expect(result.message).toMatchInlineSnapshot(
+      `"Video is longer than 1 minute (suggest breaking video up into multiple segments)"`
+    );
+    expect(result.type).toStrictEqual('guidance');
+    expect(result.elementId).toStrictEqual(tooLongVideo.id);
+  });
+
   it.todo('should return a message if an image element has low resolution');
-  // suggest breaking video up into multiple segments
-  it.todo(
-    'should return a message if the video element is longer than 1 minute'
-  );
   it.todo('should return a message if the video element is less than 24fps');
-  it.todo('should return a message if any videos are less than 480p');
-  it.todo(
-    'should return a message if any video resolution is too high to display on most mobile devices (>4k)'
-  );
 });
