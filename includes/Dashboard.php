77bd4e83e28b398fa04e05a2fd8177b5416f60ed
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-10-29 10:35:48 -0700

    remove comment that we dont need

--- includes/Dashboard.php
+++ includes/Dashboard.php
@@ -241,8 +241,6 @@ class Dashboard {
 	 * @return boolean 
 	 */
 	public function is_site_kit_plugin_installed() {
-
-		// Get all plugins
 		$all_plugins = get_plugins();
 
 		return array_key_exists( 'google-site-kit/google-site-kit.php', $all_plugins );
