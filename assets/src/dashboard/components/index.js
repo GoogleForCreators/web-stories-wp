0614464725ad5ffe98be9c34962351411e7b1a2b
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-11-06 17:08:56 -0700

    gut dialog and modal from dashboard components

--- assets/src/dashboard/components/index.js
+++ assets/src/dashboard/components/index.js
@@ -31,14 +31,12 @@ export {
   StandardViewContentGutter,
 } from './contentGutter';
 export { DetailViewNavBar } from './detailViewNavBar';
-export { default as Dialog } from './dialog';
 export { default as Dropdown } from './dropdown';
 export { default as FileUpload } from './fileUpload';
 export { default as InfiniteScroller } from './infiniteScroller';
 export { default as InlineInputForm } from './inlineInputForm';
 export { TextInput } from './input';
 export { default as Layout, useLayoutContext } from './layout';
-export { default as Modal } from './modal';
 export { default as MultiPartPill } from './multiPartPill';
 export { default as NavProvider, useNavContext } from './navProvider';
 export {
