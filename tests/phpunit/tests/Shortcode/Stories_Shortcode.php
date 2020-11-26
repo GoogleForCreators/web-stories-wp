279d9818ce181ae9ac6c650868d341043ce0d64d
Ankit Gade <ankit.gade@rtcamp.com>
2020-11-26 21:43:42 +0530

    Tests - Max number stories

--- tests/phpunit/tests/Shortcode/Stories_Shortcode.php
+++ tests/phpunit/tests/Shortcode/Stories_Shortcode.php
@@ -28,11 +28,14 @@ namespace Google\Web_Stories\Tests\Shortcode;
 
 use Google\Web_Stories\Shortcode\Stories_Shortcode as Testee;
 use Google\Web_Stories\Story_Post_Type;
+use Google\Web_Stories\Tests\Private_Access;
 
 /**
  * @coversDefaultClass \Google\Web_Stories\Shortcode\Stories_Shortcode
  */
 class Stories_Shortcode extends \WP_UnitTestCase {
+
+	use Private_Access;
 
 	/**
 	 * Story ID.
@@ -77,5 +80,58 @@ class Stories_Shortcode extends \WP_UnitTestCase {
 		);
 
 		$this->assertTrue( false !== strpos( $actual, '<amp-carousel' ) );
+	}
+
+	/**
+	 * @covers ::render_stories
+	 * @covers ::prepare_story_attrs
+	 * @covers ::prepare_story_args
+	 */
+	public function test_render_circles_view_in_shortcode() {
+		$stories_Shortcode = new Testee();
+		$actual = $stories_Shortcode->render_stories(
+			[
+				'view' => 'circles',
+			]
+		);
+
+		$this->assertTrue( false !== strpos( $actual, '<div class="web-stories-list__story-wrapper has-poster">' ) );
+	}
+
+	/**
+	 * Test story player while using shortcode.
+	 *
+	 * @covers ::render_stories
+	 */
+	public function test_render_story_player_in_shortcode() {
+		$stories_shortcode = new Testee();
+		$actual            = $stories_shortcode->render_stories(
+			[
+				'story_poster' => 'false',
+				'view'         => 'grid',
+			]
+		);
+
+		$this->assertTrue( false !== strpos( $actual, '<amp-story-player' ) );
+	}
+
+	/**
+	 * Stories should not be greater than 100.
+	 *
+	 * @covers ::prepare_story_args
+	 */
+	public function test_max_number_for_stories() {
+		$stories_Shortcode = new Testee();
+		$this->set_private_property(
+			$stories_Shortcode,
+			'attributes',
+			[
+				'number' => 1000000,
+				'order'  => 'DESC'
+			]
+		);
+
+		$args = $this->call_private_method( $stories_Shortcode, 'prepare_story_args' );
+		$this->assertSame( 100, $args['posts_per_page'] );
 	}
 }
