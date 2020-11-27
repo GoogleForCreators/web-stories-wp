3d47587cce74f06d394f49b515bf64b2ca8a11f3
spacedmonkey <jon@spacedmonkey.co.uk>
2020-11-27 16:25:15 +0000

    Add more tests.

--- assets/src/edit-story/app/api/test/base64Encode.js
+++ assets/src/edit-story/app/api/test/base64Encode.js
@@ -20,7 +20,7 @@
 import base64Encode from '../base64Encode';
 
 function base64Decode(str) {
-  return decodeURIComponent(atob(str)).replace('__WEB_STORIES_ENCODED__', '');
+  return decodeURIComponent(atob(str.replace('__WEB_STORIES_ENCODED__', '')));
 }
 
 describe('base64Encode', () => {
@@ -29,12 +29,38 @@ describe('base64Encode', () => {
   });
 
   it('converts Unicode characters', () => {
-    const actual = base64Encode('Hello 🌍 - これはサンプルです。');
+    const string = 'Hello 🌍 - これはサンプルです。';
+    const actual = base64Encode(string);
     expect(actual).toStrictEqual(
       '__WEB_STORIES_ENCODED__SGVsbG8lMjAlRjAlOUYlOEMlOEQlMjAtJTIwJUUzJTgxJTkzJUUzJTgyJThDJUUzJTgxJUFGJUUzJTgyJUI1JUUzJTgzJUIzJUUzJTgzJTk3JUUzJTgzJUFCJUUzJTgxJUE3JUUzJTgxJTk5JUUzJTgwJTgy'
     ); // Hello 🌍 - これはサンプルです。
-    expect(base64Decode(actual)).toStrictEqual(
-      'Hello 🌍 - これはサンプルです。'
-    );
+    expect(base64Decode(actual)).toStrictEqual(string);
+  });
+
+  it('converts html', () => {
+    const string = 'Hello <a href="#">world</a>';
+    const actual = base64Encode(string);
+    expect(actual).toStrictEqual(
+      '__WEB_STORIES_ENCODED__SGVsbG8lMjAlM0NhJTIwaHJlZiUzRCUyMiUyMyUyMiUzRXdvcmxkJTNDJTJGYSUzRQ=='
+    ); // Hello <a href="#">world</a>
+    expect(base64Decode(actual)).toStrictEqual(string);
+  });
+
+  it('converts html characters', () => {
+    const string = 'Hello &copy;&dollar;&pound;';
+    const actual = base64Encode(string);
+    expect(actual).toStrictEqual(
+      '__WEB_STORIES_ENCODED__SGVsbG8lMjAlMjZjb3B5JTNCJTI2ZG9sbGFyJTNCJTI2cG91bmQlM0I='
+    ); // Hello &copy;&dollar;&pound;
+    expect(base64Decode(actual)).toStrictEqual(string);
+  });
+
+  it('converts UTF 16', () => {
+    const string = 'Hello world - ØÙßæĄŒƕƜǄǆɷ';
+    const actual = base64Encode(string);
+    expect(actual).toStrictEqual(
+      '__WEB_STORIES_ENCODED__SGVsbG8lMjB3b3JsZCUyMC0lMjAlQzMlOTglQzMlOTklQzMlOUYlQzMlQTYlQzQlODQlQzUlOTIlQzYlOTUlQzYlOUMlQzclODQlQzclODYlQzklQjc='
+    ); // Hello world - ØÙßæĄŒƕƜǄǆɷ
+    expect(base64Decode(actual)).toStrictEqual(string);
   });
 });
