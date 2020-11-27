d4b87292c8286ff3a05d3e10384a0a59ac7e13e7
Miina <miina.sikk@gmail.com>
2020-11-27 17:22:53 +0100

    Change the relevant color instantly when changing colors in the color picker (#5327)
    * Start adding instant color change.
    
    * Use generatePatternStyles instead.
    
    * Pass style to ColorPicker to know which param to transform.
    
    * Add transform for text background.
    
    * Add instant color change for page element.
    
    * Add instant color change for text.
    
    * Add color change to background overlay.
    
    * Remove debug comment.
    
    * Fix loop.
    
    * Add default for selectedElementIds
    
    * Add transformHandler for outside border.
    
    * Handle transforming for border color.
    
    * Fix changing color of multi-line text.
    
    * Add instant color change for text edit selection.
    
    * Add instant color change to edit mode background, too.
    
    * Improve faux selection color changing.
    
    * Fix highlight color for edit mode.
    
    * Text edit mode border color.
    
    * Fix unwanted content update.

--- assets/src/edit-story/elements/text/edit.js
+++ assets/src/edit-story/elements/text/edit.js
@@ -54,6 +54,8 @@ import {
   isOutsideBorder,
   getBorderPositionCSS,
 } from '../../components/elementBorder/utils';
+import useCSSVarColorTransformHandler from '../shared/useCSSVarColorTransformHandler';
+import useColorTransformHandler from '../shared/useColorTransformHandler';
 import {
   calcFontMetrics,
   generateParagraphTextStyle,
@@ -67,6 +69,8 @@ const Wrapper = styled.div`
   ${elementFillContent}
   ${elementWithBackgroundColor}
   ${elementWithBorderRadius}
+
+  --faux-selection-color: inherit;
 
   span {
     box-decoration-break: clone;
@@ -210,11 +214,13 @@ function TextEdit({
 
   const wrapperRef = useRef(null);
   const highlightRef = useRef(null);
+  const highlightBgRef = useRef(null);
   const textBoxRef = useRef(null);
   const editorRef = useRef(null);
   const boxRef = useRef();
   const contentRef = useRef();
   const editorHeightRef = useRef(0);
+  const outsideBorderRef = useRef(null);
 
   // x, y, height, rotationAngle changes should not update the content while in edit mode.
   // updateContent should be only called on unmount.
@@ -343,6 +349,33 @@ function TextEdit({
     }
   });
 
+  const hasHighlightBackgroundTextMode =
+    backgroundTextMode === BACKGROUND_TEXT_MODE.HIGHLIGHT;
+
+  // For instant color change on selection.
+  useCSSVarColorTransformHandler({
+    id,
+    targetRef: wrapperRef,
+    cssVar: '--faux-selection-color',
+    expectedStyle: 'color',
+  });
+
+  const backgroundRef = hasHighlightBackgroundTextMode
+    ? highlightBgRef
+    : wrapperRef;
+  useColorTransformHandler({
+    id,
+    targetRef: backgroundRef,
+    expectedStyle: 'background',
+  });
+
+  // Inner and center color are handled separately, add transform for outside border if relevant.
+  useColorTransformHandler({
+    id,
+    targetRef: isOutsideBorder(border) ? outsideBorderRef : null,
+    expectedStyle: 'border-color',
+  });
+
   const {
     state: { editorState },
     actions: { getContentFromState },
@@ -352,15 +385,16 @@ function TextEdit({
   const wrapperBackgroundColor =
     backgroundTextMode === BACKGROUND_TEXT_MODE.FILL && backgroundColor;
 
-  const hasHighlightBackgroundTextMode =
-    backgroundTextMode === BACKGROUND_TEXT_MODE.HIGHLIGHT;
-
   const wrapperProps = {
     backgroundColor: wrapperBackgroundColor,
   };
 
   return (
-    <OutsideBorder border={border} borderRadius={borderRadius}>
+    <OutsideBorder
+      ref={outsideBorderRef}
+      border={border}
+      borderRadius={borderRadius}
+    >
       <Wrapper
         ref={wrapperRef}
         onClick={onClick}
@@ -371,6 +405,7 @@ function TextEdit({
         {editorContent && hasHighlightBackgroundTextMode && (
           <TextBoxPadded ref={highlightRef} {...highlightTextProps}>
             <Highlight
+              ref={highlightBgRef}
               dangerouslySetInnerHTML={{ __html: editorContent }}
               {...textProps}
             />
