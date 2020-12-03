ea6ccb66794c750b895cb88ef0a7d39e6fbbd7ec
spacedmonkey <jon@spacedmonkey.co.uk>
2020-12-03 15:38:44 +0000

    Wait 1 seconds.

--- tests/e2e/specs/wordpress/embedBlock.js
+++ tests/e2e/specs/wordpress/embedBlock.js
@@ -85,7 +85,7 @@ describe('Embed Block', () => {
     );
     await expect(page).toClick('button', { text: 'Embed' });
 
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(1000);
     await expect(page).not.toMatch(
       'Sorry, this content could not be embedded.'
     );
