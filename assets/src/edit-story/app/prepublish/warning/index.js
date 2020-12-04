705c378bcb81b7ff0935c2258b2cf945fe53b7b2
Emily Bartman <emily.bartman@formidable.com>
2020-12-03 17:20:48 -0700

    prepublish(inc): get the part of the background image behind the text

--- assets/src/edit-story/app/prepublish/warning/index.js
+++ assets/src/edit-story/app/prepublish/warning/index.js
@@ -21,7 +21,10 @@ import * as distributionWarnings from './distribution';
 
 export default {
   story: [distributionWarnings.storyMissingExcerpt],
-  page: [accessibilityWarnings.pageTooManyLinks],
+  page: [
+    accessibilityWarnings.pageTooManyLinks,
+    accessibilityWarnings.pageBackgroundWithTextLowContrast,
+  ],
   text: [
     accessibilityWarnings.textElementFontSizeTooSmall,
     accessibilityWarnings.elementLinkTappableRegionTooSmall,
