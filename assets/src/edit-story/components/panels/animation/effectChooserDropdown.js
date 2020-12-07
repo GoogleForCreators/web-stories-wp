d4d83cc9c159cff49c403b7fe2a928290e0f4625
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-12-07 08:56:21 -0700

    when new animation is selected from effect chooser check for keyboard user again after initial close of menu and return focus.

--- assets/src/edit-story/components/panels/animation/effectChooserDropdown.js
+++ assets/src/edit-story/components/panels/animation/effectChooserDropdown.js
@@ -66,6 +66,17 @@ export default function EffectChooserDropdown({
     }
   }, []);
 
+  const handleAnimationSelected = useCallback(
+    (animation) => {
+      if (isKeyboardUser()) {
+        // if keyboard is in use, we want to re-return focus back to dropdown after update is made
+        selectRef.current.focus();
+      }
+      onAnimationSelected(animation);
+    },
+    [onAnimationSelected]
+  );
+
   return (
     <DropDownSelect
       aria-label={__('Animation: Effect Chooser', 'web-stories')}
@@ -84,7 +95,7 @@ export default function EffectChooserDropdown({
         <Container ref={dropdownRef}>
           <EffectChooser
             onNoEffectSelected={onNoEffectSelected}
-            onAnimationSelected={onAnimationSelected}
+            onAnimationSelected={handleAnimationSelected}
             onDismiss={closeDropDown}
             isBackgroundEffects={isBackgroundEffects}
             disabledTypeOptionsMap={disabledTypeOptionsMap}
