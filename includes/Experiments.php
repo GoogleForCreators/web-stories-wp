fd967a29f3dfcb8fa875ce0cc4c3a6e535ef382b
Pascal Birchler <pascalb@google.com>
2020-12-22 13:41:02 +0100

    Experiments: fix checks for experiments enabled by default

--- includes/Experiments.php
+++ includes/Experiments.php
@@ -397,6 +397,19 @@ class Experiments {
 	}
 
 	/**
+	 * Returns an experiment by name.
+	 *
+	 * @since 1.3.0
+	 *
+	 * @param string $name Experiment name.
+	 * @return array|null Experiment if found, null otherwise.
+	 */
+	protected function get_experiment( $name ) {
+		$experiment = wp_list_filter( $this->get_experiments(), [ 'name' => $name ] );
+		return ! empty( $experiment ) ? array_shift( $experiment ) : null;
+	}
+
+	/**
 	 * Checks whether an experiment is enabled.
 	 *
 	 * @since 1.0.0
@@ -406,6 +419,16 @@ class Experiments {
 	 * @return bool Whether the experiment is enabled.
 	 */
 	public function is_experiment_enabled( $name ) {
+		$experiment = $this->get_experiment( $name );
+
+		if ( ! $experiment ) {
+			return false;
+		}
+
+		if ( array_key_exists( 'default', $experiment ) ) {
+			return (bool) $experiment['default'];
+		}
+
 		$experiments = get_option( Settings::SETTING_NAME_EXPERIMENTS );
 		return ! empty( $experiments[ $name ] );
 	}
