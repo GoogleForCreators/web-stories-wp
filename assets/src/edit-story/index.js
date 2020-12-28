696c02968991596ca55362609eff650becce5d1a
Maxwell Yinger <max.yinger@formidable.com>
2020-12-28 07:47:52 -0500

    waapi polyfill addded to editor

--- assets/src/edit-story/index.js
+++ assets/src/edit-story/index.js
@@ -21,6 +21,7 @@ import Modal from 'react-modal';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { FlagsProvider } from 'flagged';
+import 'web-animations-js/web-animations-next-lite.min.js';
 
 /**
  * Internal dependencies
