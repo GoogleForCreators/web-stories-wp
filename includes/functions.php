ab8cd7697ae1265dd88042b1dbcc4a1a0d209ceb
Ankit Gade <ankit.gade@rtcamp.com>
2020-12-30 19:05:06 +0530

    Add carousel icon

--- includes/functions.php
+++ includes/functions.php
@@ -150,7 +150,7 @@ function web_stories_script_data( $hook ) {
 
 	$data = [
 		'orderlist' => $order_list,
-		'icon'      => WEBSTORIES_ASSETS_URL . '/src/tinymce/images/carousel.svg',
+		'icon'      => WEBSTORIES_PLUGIN_DIR_URL . '/__static__/carousel-icon.png',
 		'tag'       => 'stories',
 		'views'     => $view_types,
 		'fields'    => $field_states,
