c2c26dfe9816d65f41142c9278c17295e9d183ed
Zach Hale <zachhale@gmail.com>
2021-01-11 15:55:09 -0800

    Change page layouts to filter by page layout type, fix titles to include page layout type, and categorize DIY template pages

--- assets/src/edit-story/components/library/panes/pageLayouts/pageLayoutsPane.js
+++ assets/src/edit-story/components/library/panes/pageLayouts/pageLayoutsPane.js
@@ -23,7 +23,7 @@ import styled from 'styled-components';
 /**
  * WordPress dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -33,6 +33,7 @@ import { Pane } from '../shared';
 import PillGroup from '../shared/pillGroup';
 import paneId from './paneId';
 import PageLayouts from './pageLayouts';
+import { PAGE_LAYOUT_TYPES } from './constants';
 
 export const StyledPane = styled(Pane)`
   height: 100%;
@@ -60,51 +61,54 @@ function PageLayoutsPane(props) {
     actions: { getTemplates },
   } = useAPI();
   const [templates, setTemplates] = useState([]);
-  const [selectedTemplateId, setSelectedTemplateId] = useState(null);
+  const [selectedPageLayoutType, setSelectedPageLayoutType] = useState(null);
 
   const pageLayoutsParentRef = useRef();
 
+  // load and process templates
   useEffect(() => {
     getTemplates().then((result) => setTemplates(result));
-  }, [getTemplates]);
+  }, [getTemplates, setTemplates]);
 
   const pills = useMemo(
     () =>
-      templates.map((template) => ({
-        id: template.id,
-        label: template.title,
+      Object.entries(PAGE_LAYOUT_TYPES).map(([key, { title }]) => ({
+        id: key,
+        label: title,
       })),
-    [templates]
+    []
   );
 
-  const filteredPages = useMemo(() => {
-    if (selectedTemplateId) {
-      const template = templates.find(
-        (template) => template.id === selectedTemplateId
-      );
-      if (template) {
-        return template.pages;
-      }
-    }
-    return templates.reduce(
-      (pages, template) => [
-        ...pages,
-        ...template.pages.map((page, index) => ({
-          ...page,
-          title: sprintf(
-            /* translators: 1: template name. 2: page number. */
-            __(`%1$s Page %2$s`, 'web-stories'),
-            template.title,
-            index + 1
-          ),
-        })),
-      ],
-      []
-    );
-  }, [templates, selectedTemplateId]);
+  const filteredPages = useMemo(
+    () =>
+      templates.reduce((pages, template) => {
+        const templatePages = template.pages.reduce((acc, page) => {
+          // skip unselected page layout types if not matching
+          if (
+            selectedPageLayoutType &&
+            page.pageLayoutType !== selectedPageLayoutType
+          ) {
+            return acc;
+          }
 
-  const handleSelectTemplate = useCallback((templateId) => {
-    setSelectedTemplateId(templateId);
+          // translation not required because page layout title is already translated
+          const pageLayoutTitle = PAGE_LAYOUT_TYPES[page.pageLayoutType]?.title;
+          return [
+            ...acc,
+            {
+              ...page,
+              title: `${template.title} ${pageLayoutTitle}`,
+            },
+          ];
+        }, []);
+
+        return [...pages, ...templatePages];
+      }, []),
+    [templates, selectedPageLayoutType]
+  );
+
+  const handleSelectPageLayoutType = useCallback((key) => {
+    setSelectedPageLayoutType(key);
   }, []);
 
   return (
@@ -112,13 +116,13 @@ function PageLayoutsPane(props) {
       <PaneInner>
         <PillGroup
           items={pills}
-          selectedItemId={selectedTemplateId}
-          selectItem={handleSelectTemplate}
-          deselectItem={() => handleSelectTemplate(null)}
+          selectedItemId={selectedPageLayoutType}
+          selectItem={handleSelectPageLayoutType}
+          deselectItem={() => handleSelectPageLayoutType(null)}
         />
         <PageLayoutsParentContainer
           ref={pageLayoutsParentRef}
-          title={__('Page layouts', 'web-stories')}
+          title={__('Page Layouts', 'web-stories')}
         >
           {pageLayoutsParentRef.current && (
             <PageLayouts
