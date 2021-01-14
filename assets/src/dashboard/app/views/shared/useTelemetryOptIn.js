b911ae0468a8861b7da01e25e5abe436d6c55efa
Maxwell Yinger <max.yinger@formidable.com>
2021-01-14 13:01:59 -0700

    use localStore util

--- assets/src/dashboard/app/views/shared/useTelemetryOptIn.js
+++ assets/src/dashboard/app/views/shared/useTelemetryOptIn.js
@@ -25,6 +25,7 @@ import { useCallback, useState, useEffect, useRef } from 'react';
 import useApi from '../../api/useApi';
 import { useRouteHistory } from '../../router';
 import { APP_ROUTES } from '../../../constants';
+import localStore from '../../../../edit-story/utils/localStore';
 
 // The value associated with this key indicates if the user has interacted with
 // the banner previously. If they have, we do not show the banner again.
@@ -73,21 +74,13 @@ export default function useTelemetryOptIn() {
 
   const _toggleWebStoriesTrackingOptIn = useCallback(() => {
     toggleWebStoriesTrackingOptIn();
-    try {
-      localStorage.setItem(LOCAL_STORAGE_KEY, true);
-    } catch {
-      // Do something
-    }
+    localStore.setItemByKey(LOCAL_STORAGE_KEY, true);
     setOptInCheckboxClicked(true);
   }, [toggleWebStoriesTrackingOptIn]);
 
   const closeBanner = useCallback(() => {
     setBannerPreviouslyClosed(true);
-    try {
-      localStorage.setItem(LOCAL_STORAGE_KEY, true);
-    } catch {
-      // Do something
-    }
+    localStore.setItemByKey(LOCAL_STORAGE_KEY, true);
   }, []);
 
   let bannerVisible = true;
