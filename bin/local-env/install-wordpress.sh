e629c1137bf1fec299a532d30ede37a32e798a57
Jonathan Harris <jon@spacedmonkey.co.uk>
2021-01-20 10:12:26 +0000

    Return missing video on import.

--- bin/local-env/install-wordpress.sh
+++ bin/local-env/install-wordpress.sh
@@ -167,10 +167,17 @@ fi
 # Let's make sure we have some media in the media library to work with.
 echo -e $(status_message "Import default set of media assets...")
 # TODO: use glob pattern to import items. See https://developer.wordpress.org/cli/commands/media/import/.
+
+WEBM_VIDEO_ID=$(wp media import /var/www/html/wp-content/e2e-assets/small-video.webm --porcelain)
+WEBM_VIDEO_POSTER_ID=$(wp media import /var/www/html/wp-content/e2e-assets/small-video-poster.jpg --post_id=$WEBM_VIDEO_ID --featured_image --porcelain)
+
+# So the poster is marked as such and hidden in the editor.
+wp post term add $WEBM_VIDEO_POSTER_ID web_story_media_source "poster-generation" --quiet
+wp post meta add $WEBM_VIDEO_ID web_stories_poster_id $WEBM_VIDEO_POSTER_ID --quiet
+
 wp media import /var/www/html/wp-content/e2e-assets/example-1.jpg --quiet
 wp media import /var/www/html/wp-content/e2e-assets/example-2.jpg --quiet
 wp media import /var/www/html/wp-content/e2e-assets/example-3.png --quiet
-
 
 wp option patch insert web_stories_experiments enableSVG 1
 wp media import /var/www/html/wp-content/e2e-assets/video-play.svg
