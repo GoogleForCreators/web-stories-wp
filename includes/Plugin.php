6f0c494421f64d1ed6013a87aec567483316fca7
Ankit Gade <ankit.gade@rtcamp.com>
2021-01-21 16:43:53 +0530

    Restore: Loading of amp compatibility functions

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -215,7 +215,7 @@ class Plugin {
 	 */
 	public function register() {
 		// Plugin compatibility / polyfills.
-		add_action( 'init', [ $this, 'load_amp_plugin_compat' ] );
+		add_action( 'wp', [ $this, 'load_amp_plugin_compat' ] );
 		add_action( 'init', [ $this, 'includes' ] );
 
 		// Settings.
