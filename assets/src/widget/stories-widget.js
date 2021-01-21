1f55f9a52ee59892a76eddb8a5822affc34e16e5
ankitrox <ankit.gade@rtcamp.com>
2021-01-21 21:55:37 +0530

    Import domReady from package
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- assets/src/widget/stories-widget.js
+++ assets/src/widget/stories-widget.js
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const { domReady } = wp;
+/**
+ * WordPress dependencies
+ */
+import domReady from '@wordpress/dom-ready';
 
 /**
  * Store field states in local variable.
