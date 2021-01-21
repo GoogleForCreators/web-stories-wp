9218a3531a91056f3029f7789db17f58fb701e96
Maxwell Yinger <max.yinger@formidable.com>
2021-01-21 13:30:58 -0700

    added help center to application behind feature flag

--- assets/src/edit-story/components/helpCenter/index.js
+++ assets/src/edit-story/components/helpCenter/index.js
@@ -13,4 +13,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const HelpCenter = () => null;
+/**
+ * External dependencies
+ */
+import { useState } from 'react';
+import { useFeatures } from 'flagged';
+import styled, { ThemeProvider, css } from 'styled-components';
+/**
+ * Internal dependencies
+ */
+import { theme, themeHelpers } from '../../../design-system';
+import { Toggle } from './toggle';
+
+const Wrapper = styled.div`
+  position: absolute;
+  bottom: 16px;
+  left: 11px;
+  z-index: 10;
+
+  ${themeHelpers.mq.tablet(
+    css`
+      bottom: 24px;
+      left: 23px;
+    `
+  )}
+
+  ${themeHelpers.mq.desktop(
+    css`
+      bottom: 16px;
+      left: 31px;
+    `
+  )}
+`;
+
+export const HelpCenter = () => {
+  const { enableQuickTips } = useFeatures();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    enableQuickTips && (
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <Toggle
+            isOpen={isOpen}
+            onClick={() => setIsOpen((v) => !v)}
+            notificationCount={1}
+          />
+        </Wrapper>
+      </ThemeProvider>
+    )
+  );
+};
