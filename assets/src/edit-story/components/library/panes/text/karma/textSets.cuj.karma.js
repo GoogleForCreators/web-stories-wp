41d55c93c8ceca265ed7ef8542e84e8539ae8863
Miina Sikk <miina.sikk@gmail.com>
2021-02-03 18:44:12 +0200

    Another try.

--- assets/src/edit-story/components/library/panes/text/karma/textSets.cuj.karma.js
+++ assets/src/edit-story/components/library/panes/text/karma/textSets.cuj.karma.js
@@ -76,9 +76,11 @@ fdescribe('CUJ: Text Sets (Text and Shape Combinations): Using Text Sets', () =>
     await fixture.events.mouse.moveRel(textSet, 25, 25);
     await fixture.events.mouse.down();
 
-    // @todo The following few lines of code are for giving the text set more time to start dragging.
-    // These can (likely) be removed once https://github.com/google/web-stories-wp/issues/6027 is fixed.
-    await fixture.events.mouse.move(5, 5);
+    // The following few lines of code are for giving the text set more time to start dragging.
+    // @todo These can (likely) be removed once https://github.com/google/web-stories-wp/issues/6027 is fixed.
+    // await fixture.events.mouse.move(5, 5);
+
+    await fixture.events.mouse.moveRel(page, 50, 100, { steps: 50 });
     await waitFor(
       () =>
         expect(
@@ -86,8 +88,6 @@ fdescribe('CUJ: Text Sets (Text and Shape Combinations): Using Text Sets', () =>
         ).toBeTruthy(),
       { timeout: 2000 }
     );
-
-    await fixture.events.mouse.moveRel(page, 50, 100, { steps: 20 });
     await fixture.snapshot('Text set dragged');
     await fixture.events.mouse.up();
 
