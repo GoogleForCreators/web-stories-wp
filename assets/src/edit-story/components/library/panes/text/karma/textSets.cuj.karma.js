552f5b1b2f21af5968281878c6a7362e40367be5
Miina Sikk <miina.sikk@gmail.com>
2021-02-03 19:04:04 +0200

    Try without sleep.

--- assets/src/edit-story/components/library/panes/text/karma/textSets.cuj.karma.js
+++ assets/src/edit-story/components/library/panes/text/karma/textSets.cuj.karma.js
@@ -78,8 +78,7 @@ fdescribe('CUJ: Text Sets (Text and Shape Combinations): Using Text Sets', () =>
 
     // The following few lines of code are for giving the text set more time to start dragging.
     // @todo These can (likely) be removed once https://github.com/google/web-stories-wp/issues/6027 is fixed.
-    await fixture.events.mouse.move(5, 5);
-    await fixture.events.sleep(1000);
+    await fixture.events.mouse.move(15, 15, { steps: 50 });
 
     await fixture.events.mouse.moveRel(page, 50, 100, { steps: 50 });
     await waitFor(
