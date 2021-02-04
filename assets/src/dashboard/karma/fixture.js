dc79de6c6b3572cfe63972a5f4af2987baf3c62f
Pascal Birchler <pascalb@google.com>
2021-02-04 11:11:39 +0100

    Update URL in fixture

--- assets/src/dashboard/karma/fixture.js
+++ assets/src/dashboard/karma/fixture.js
@@ -61,7 +61,7 @@ const defaultConfig = {
   editStoryURL: 'http://localhost:8899/wp-admin/post.php?action=edit',
   wpListURL: 'http://localhost:8899/wp-admin/edit.php?post_type=web-story',
   assetsURL: 'http://localhost:8899/wp-content/plugins/web-stories/assets',
-  cdnURL: '__WEB_STORIES_TEMPLATE_BASE_URL__',
+  cdnURL: 'https://cdn.example.com/',
   version: '1.0.0-alpha.9',
   api: {
     stories: '/web-stories/v1/web-story',
