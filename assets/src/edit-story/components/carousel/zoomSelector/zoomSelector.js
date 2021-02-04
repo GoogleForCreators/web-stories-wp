29bc993d9a3b980046edd5f20fa7f88fa42cbfb0
Morten Barklund <morten.barklund@xwp.co>
2021-02-04 18:06:16 -0500

    Fixed imports

--- assets/src/edit-story/components/carousel/zoomSelector/zoomSelector.js
+++ assets/src/edit-story/components/carousel/zoomSelector/zoomSelector.js
@@ -22,7 +22,7 @@ import { useCallback, useMemo } from 'react';
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _x } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
