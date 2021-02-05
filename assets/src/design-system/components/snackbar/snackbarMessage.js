5e1467f115ea29cc1d36bcfb533bc81261c4eddf
samwhale <samuelestrella226@gmail.com>
2021-02-05 11:50:51 -0700

    Match design border radius

--- assets/src/design-system/components/snackbar/snackbarMessage.js
+++ assets/src/design-system/components/snackbar/snackbarMessage.js
@@ -57,7 +57,7 @@ const MessageContainer = styled.div`
   color: ${({ theme }) => theme.colors.inverted.fg.primary};
   border: ${({ theme }) =>
     `1px solid ${rgba(theme.colors.standard.white, 0.24)}`};
-  border-radius: ${({ theme }) => theme.borders.radius.small};
+  border-radius: ${({ theme }) => theme.borders.radius.medium};
 
   animation: 0.5s ${slideIn} ease-out;
 `;
