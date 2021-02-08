79cac18677ec9bc3c054c8936d2ab6a814b3e431
Jonathan Harris <jon@spacedmonkey.co.uk>
2021-02-08 08:54:06 +0000

    Fix conflict.

--- assets/src/edit-story/components/helpCenter/navigator/bottomNavigation.js
+++ assets/src/edit-story/components/helpCenter/navigator/bottomNavigation.js
@@ -18,11 +18,8 @@
  */
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-/**
- * WordPress dependencies
- */
+import { __ } from '@web-stories-wp/i18n';
 import { useEffect, useState, useRef } from 'react';
-import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
