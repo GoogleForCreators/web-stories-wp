c016a41dc45065f3c04c34057d7d3f47bddd00af
Pascal Birchler <pascalb@google.com>
2021-02-09 18:50:03 +0100

    Fix ESLint errors after merge

--- assets/src/edit-story/components/form/media.js
+++ assets/src/edit-story/components/form/media.js
@@ -21,11 +21,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { rgba } from 'polished';
 import { useCallback, useState, useRef, forwardRef } from 'react';
-
-/**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
+import { __ } from '@web-stories-wp/i18n';
 
 /**
  * Internal dependencies
