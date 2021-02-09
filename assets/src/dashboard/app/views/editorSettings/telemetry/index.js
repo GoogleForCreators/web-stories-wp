8f3d3001ec8f99621602e95f203f2c912432e422
Samuel Estrella <samuelestrella226@gmail.com>
2021-02-09 16:42:39 -0700

    Dashboard: Implement redesign of the settings page (#6178)
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- assets/src/dashboard/app/views/editorSettings/telemetry/index.js
+++ assets/src/dashboard/app/views/editorSettings/telemetry/index.js
@@ -17,46 +17,32 @@
 /**
  * External dependencies
  */
-import { useEffect, useRef } from 'react';
 import propTypes from 'prop-types';
-import styled from 'styled-components';
+import { useMemo } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * WordPress dependencies
+ */
 import { __, TranslateWithMarkup } from '@web-stories-wp/i18n';
 
 /**
  * Internal dependencies
  */
-import { SettingForm, SettingHeading, FormLabel } from '../components';
-
-const CheckBox = styled.input.attrs({
-  type: 'checkbox',
-  id: 'telemetry-opt-in',
-})`
-  height: 18px;
-  width: 18px;
-  margin: 0 12px 0 0;
-  flex: 1 0 18px;
-`;
-
-const Label = styled.label.attrs({ htmlFor: 'telemetry-opt-in' })`
-  display: flex;
-  justify-content: flex-start;
-`;
+import {
+  SettingForm,
+  SettingHeading,
+  CheckboxLabel,
+  CheckboxLabelText,
+} from '../components';
+import { Checkbox, THEME_CONSTANTS } from '../../../../../design-system';
 
 export default function TelemetrySettings({
   selected,
   onCheckboxSelected,
   disabled,
 }) {
-  const checkboxRef = useRef();
-  const focusOnCheckbox = useRef(false);
-
-  const checked = Boolean(selected);
-
-  useEffect(() => {
-    if (focusOnCheckbox.current) {
-      checkboxRef.current.focus();
-    }
-  });
+  const telemetryOptInId = useMemo(() => `telemetry-opt-in-${uuidv4()}`, []);
 
   return (
     <SettingForm>
@@ -66,21 +52,19 @@ export default function TelemetrySettings({
         </SettingHeading>
       </div>
       <div>
-        <Label>
-          <CheckBox
-            ref={checkboxRef}
+        <CheckboxLabel forwardedAs="label" htmlFor={telemetryOptInId}>
+          <Checkbox
+            id={telemetryOptInId}
             data-testid="telemetry-settings-checkbox"
             disabled={disabled}
-            onChange={() => {
-              onCheckboxSelected();
-              focusOnCheckbox.current = true;
-            }}
-            onBlur={() => {
-              focusOnCheckbox.current = false;
-            }}
-            checked={checked}
+            onChange={onCheckboxSelected}
+            checked={Boolean(selected)}
           />
-          <FormLabel aria-checked={checked}>
+          <CheckboxLabelText
+            size={THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.X_SMALL}
+            aria-checked={Boolean(selected)}
+            forwardedAs="span"
+          >
             <TranslateWithMarkup
               mapping={{
                 a: (
@@ -105,8 +89,8 @@ export default function TelemetrySettings({
                 'web-stories'
               )}
             </TranslateWithMarkup>
-          </FormLabel>
-        </Label>
+          </CheckboxLabelText>
+        </CheckboxLabel>
       </div>
     </SettingForm>
   );
