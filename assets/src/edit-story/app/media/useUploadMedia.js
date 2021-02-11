3c58ea13caacfb3ea63811f051e0e3cc5da38117
Pascal Birchler <pascalb@google.com>
2021-02-11 11:32:17 +0100

    Improve and document useUploadMedia + tests

--- assets/src/edit-story/app/media/useUploadMedia.js
+++ assets/src/edit-story/app/media/useUploadMedia.js
@@ -17,7 +17,7 @@
 /**
  * External dependencies
  */
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { trackError } from '@web-stories-wp/tracking';
 import { __ } from '@web-stories-wp/i18n';
 
@@ -29,30 +29,62 @@ import { useUploader } from '../uploader';
 import { useSnackbar } from '../snackbar';
 import { getResourceFromLocalFile, getResourceFromAttachment } from './utils';
 
+/**
+ * Upload media items to the app while displaying the local files in the library.
+ *
+ * @todo Revisit local resource handling.
+ * See https://github.com/google/web-stories-wp/issues/6053 and https://github.com/google/web-stories-wp/issues/6088
+ *
+ * @param {Object} props Props.
+ * @param {Array<Object<*>>} props.media Media items.
+ * @param {Function} props.setMedia Actions to set media items.
+ * @return {{uploadMedia: Function, isUploading: boolean}} Upload status, and function to upload media.
+ */
 function useUploadMedia({ media, setMedia }) {
   const { uploadFile } = useUploader();
   const { showSnackbar } = useSnackbar();
   const [isUploading, setIsUploading] = useState(false);
   const setPreventUnload = usePreventWindowUnload();
 
+  /**
+   * @type {import('react').MutableRefObject<Array<Object<*>>>} mediaRef Ref for current media items.
+   */
   const mediaRef = useRef();
   mediaRef.current = media;
 
+  // Prevent closing the window/tab while upload is in progress.
+  useEffect(() => {
+    setPreventUnload('upload', isUploading);
+  }, [isUploading, setPreventUnload]);
+
   const uploadMedia = useCallback(
+    /**
+     *
+     * @param {Array<File>} files Files to upload.
+     * @param {Object} args Additional arguments.
+     * @param {Function} args.onLocalFile Callback to act on local files.
+     * @param {Function} args.onUploadedFile Callback for successful uploads.
+     * @param {Function} args.onUploadFailure Callback for uploadfailures.
+     * @return {Promise<void>}
+     */
     async (files, { onLocalFile, onUploadedFile, onUploadFailure } = {}) => {
-      // eslint-disable-next-line no-shadow
-      const media = mediaRef.current;
+      const mediaItems = mediaRef.current;
 
       // If there are no files passed, don't try to upload.
-      if (!files || files.length === 0) {
+      if (!files?.length) {
         return;
       }
 
+      /**
+       * @type {Array<Object<{localResource:Object<*>, file:File, fileUploaded?:Object<*>}>>} localFiles.
+       */
       let localFiles;
       let updatedMedia;
+
+      // First, prepare all files for uploading by getting a resource object
+      // and adding the files to the media library.
       try {
         setIsUploading(true);
-        setPreventUnload('upload', true);
 
         localFiles = await Promise.all(
           files.reverse().map(async (file) => ({
@@ -60,7 +92,9 @@ function useUploadMedia({ media, setMedia }) {
             file,
           }))
         );
-        localFiles = localFiles.filter((f) => f.localResource != null);
+
+        // localResource can be null for files with invalid or missing type.
+        localFiles = localFiles.filter((f) => f.localResource);
 
         if (onLocalFile) {
           localFiles = localFiles.map(({ localResource, file }) => {
@@ -70,15 +104,22 @@ function useUploadMedia({ media, setMedia }) {
             return { localResource, file, element };
           });
         }
-        updatedMedia = [
-          ...localFiles.map(({ localResource }) => localResource),
-          ...media,
-        ];
-        setMedia({ media: updatedMedia });
+
+        // If there are any valid local files remaining, add their resources to the library.
+        if (localFiles.length) {
+          updatedMedia = [
+            ...localFiles.map(({ localResource }) => localResource),
+            ...mediaItems,
+          ];
+          setMedia({ media: updatedMedia });
+        }
       } catch (e) {
+        // Catching errors from getResourceFromLocalFile() above.
+
         trackError('upload media', e.message);
-        setMedia({ media });
+
         setIsUploading(false);
+
         showSnackbar({
           message:
             e.message ||
@@ -87,11 +128,10 @@ function useUploadMedia({ media, setMedia }) {
               'web-stories'
             ),
         });
+
         return;
       }
 
-      // TODO: Revisit local resource handling.
-      // See https://github.com/google/web-stories-wp/issues/6053 and https://github.com/google/web-stories-wp/issues/6088
       if (localFiles.length !== files.length) {
         showSnackbar({
           message: __(
@@ -106,6 +146,8 @@ function useUploadMedia({ media, setMedia }) {
       }
 
       try {
+        // Upload all the files one by one and get the new resource
+        // from the uploaded attachment returned by the server.
         const uploadedFiles = await Promise.all(
           localFiles.map(async (localFile) => ({
             ...localFile,
@@ -117,6 +159,7 @@ function useUploadMedia({ media, setMedia }) {
 
         setIsUploading(false);
 
+        // So callers can replace local resource with newly uploaded resource (e.g. on the canvas).
         if (onUploadedFile) {
           uploadedFiles.forEach(({ element, fileUploaded }) => {
             onUploadedFile({
@@ -126,6 +169,7 @@ function useUploadMedia({ media, setMedia }) {
           });
         }
 
+        // Add newly uploaded files to the media library.
         const uploadedFilesMap = new Map(
           uploadedFiles.map(({ localResource, fileUploaded }) => [
             localResource,
@@ -139,24 +183,26 @@ function useUploadMedia({ media, setMedia }) {
         });
       } catch (e) {
         trackError('upload media', e.message);
+
         showSnackbar({
           message: e.message,
         });
+
+        // Remove local files from library again upon failure
+        // and inform callers so they can remove the file again (e.g. on the canvas).
         localFiles.forEach(({ localResource, element }) => {
           if (onUploadFailure) {
             onUploadFailure({ element });
           }
           setMedia({
-            media: media.filter((resource) => resource !== localResource),
+            media: mediaItems.filter((resource) => resource !== localResource),
           });
         });
 
         setIsUploading(false);
-      } finally {
-        setPreventUnload('upload', false);
       }
     },
-    [setMedia, showSnackbar, uploadFile, setPreventUnload]
+    [setMedia, showSnackbar, uploadFile]
   );
 
   return {
