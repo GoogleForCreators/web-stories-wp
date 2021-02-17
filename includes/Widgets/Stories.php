3eb3af09f26f73d0697d2f5eed2cccba32e094b7
ankitrox <ankit.gade@rtcamp.com>
2021-02-17 12:27:30 +0530

    esc_attr for escaping
    Co-authored-by: Jonny Harris <spacedmonkey@users.noreply.github.com>

--- includes/Widgets/Stories.php
+++ includes/Widgets/Stories.php
@@ -401,7 +401,7 @@ class Stories extends WP_Widget {
 
 			if ( ! empty( $args['attributes'] ) && is_array( $args['attributes'] ) ) {
 				foreach ( $args['attributes'] as $attr_key => $attr_val ) {
-					$extra_attrs .= sprintf( ' %1s=%2s', $attr_key, $attr_val );
+					$extra_attrs .= sprintf( ' %1s=%2s', $attr_key, esc_attr( $attr_val ) );
 				}
 			}
 			?>
