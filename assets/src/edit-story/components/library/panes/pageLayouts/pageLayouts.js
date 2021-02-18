cd3d1e8a710d902668c81316e181eec5ec1656aa
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-02-18 09:00:43 -0700

    Page Layouts: Keyboard Navigation (#5989)
    * use useRovingIndex to handle keyboard arrow nav. Watch for tab on page layouts to manually set focus to canvas (if there is a better way to do this, we should, this was the best I could find since nothing else in the panel after the layouts are focusable). Restructure the virtualized list to also handle columns and eliminate some nesting so that the rovingIndex works.
    
    * clean up sending focus to canvas on tab by using useFocusCanvas hook now that I know it exists. Remove excess keys and fragments. Tighten up mapping on virtualized columns.
    
    * catch page layout update for a11y key nav to using new theme. remove lint warning about forwardedRefs
    
    * catch karma pageLayouts fixture container up to change in DOM - not listitems anymore - they are buttons
    
    Co-authored-by: Zach Hale <zachhale@gmail.com>

--- assets/src/edit-story/components/library/panes/pageLayouts/pageLayouts.js
+++ assets/src/edit-story/components/library/panes/pageLayouts/pageLayouts.js
@@ -17,7 +17,7 @@
 /**
  * External dependencies
  */
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useVirtual } from 'react-virtual';
@@ -26,13 +26,18 @@ import { trackEvent } from '@web-stories-wp/tracking';
 /**
  * Internal dependencies
  */
-import { PANE_PADDING } from '../shared';
-import { PAGE_RATIO, FULLBLEED_RATIO } from '../../../../constants';
-import { UnitsProvider } from '../../../../units';
+import { useFocusOut, useKeyDownEffect } from '../../../../../design-system';
 import { useStory } from '../../../../app';
+import { PAGE_RATIO, FULLBLEED_RATIO } from '../../../../constants';
 import { duplicatePage } from '../../../../elements';
+import useRovingTabIndex from '../../../../utils/useRovingTabIndex';
+
+import { UnitsProvider } from '../../../../units';
 import isDefaultPage from '../../../../utils/isDefaultPage';
+import useFocusCanvas from '../../../canvas/useFocusCanvas';
+import { PANE_PADDING } from '../shared';
 import PageLayout from './pageLayout';
+import ConfirmPageLayoutDialog from './confirmPageLayoutDialog';
 
 const PAGE_LAYOUT_PANE_WIDTH = 158;
 const PAGE_LAYOUT_ROW_GAP = 12;
@@ -43,20 +48,23 @@ const PageLayoutsContainer = styled.div`
   position: relative;
 `;
 
-const PageLayoutsRow = styled.div`
+const PageLayoutsVirtualizedContainer = styled.div`
   position: absolute;
   top: 0;
   left: ${PANE_PADDING};
-  height: ${({ height }) => `${height}px`};
-  transform: ${({ translateY }) => `translateY(${translateY}px)`};
   display: grid;
   grid-template-columns: 1fr 1fr;
-  column-gap: 12px;
+  grid-template-columns: ${({ pageSize }) => `
+    repeat(auto-fill, ${pageSize.width}px)`};
+  grid-template-rows: ${({ pageSize }) =>
+    `minmax(${pageSize.containerHeight}px, auto)`};
+  gap: ${PAGE_LAYOUT_ROW_GAP}px;
+  width: 100%;
+  height: 100%;
+  margin-top: 4px;
 `;
 
-function PageLayouts(props) {
-  const { pages, parentRef } = props;
-
+function PageLayouts({ pages, parentRef }) {
   const { replaceCurrentPage, currentPage } = useStory(
     ({ actions: { replaceCurrentPage }, state: { currentPage } }) => ({
       replaceCurrentPage,
@@ -64,15 +72,19 @@ function PageLayouts(props) {
     })
   );
 
-  const handleApplyPageLayout = useCallback(
-    (page) => {
-      const duplicatedPage = duplicatePage(page);
-      replaceCurrentPage({ page: duplicatedPage });
-      trackEvent('insert_page_layout', {
-        name: page.title,
-      });
-    },
-    [replaceCurrentPage]
+  const containerRef = useRef();
+  const pageRefs = useRef({});
+
+  const [selectedPage, setSelectedPage] = useState();
+  const [activePageId, setActivePageId] = useState();
+  const [isConfirming, setIsConfirming] = useState();
+  const [isPageLayoutsFocused, setIsPageLayoutsFocused] = useState(false);
+
+  const pageIds = useMemo(() => pages.map((page) => page.id), [pages]);
+
+  const requiresConfirmation = useMemo(
+    () => currentPage && !isDefaultPage(currentPage),
+    [currentPage]
   );
 
   const pageSize = useMemo(() => {
@@ -82,19 +94,138 @@ function PageLayouts(props) {
     return { width, height, containerHeight };
   }, []);
 
+  const handleApplyPageLayout = useCallback(
+    (page) => {
+      const duplicatedPage = duplicatePage(page);
+      replaceCurrentPage({ page: duplicatedPage });
+      trackEvent('insert_page_layout', {
+        name: page.title,
+      });
+
+      setSelectedPage(null);
+    },
+    [replaceCurrentPage]
+  );
+
+  const handlePageClick = useCallback(
+    (page) => {
+      if (requiresConfirmation) {
+        setIsConfirming(true);
+        setSelectedPage(page);
+      } else {
+        handleApplyPageLayout(page);
+        setSelectedPage(null);
+      }
+    },
+    [requiresConfirmation, handleApplyPageLayout]
+  );
+
+  const handleKeyboardPageClick = useCallback(
+    ({ key }, page) => {
+      if (key === 'Enter') {
+        if (isPageLayoutsFocused) {
+          handlePageClick(page);
+        }
+      }
+    },
+    [isPageLayoutsFocused, handlePageClick]
+  );
+
+  const handleCloseDialog = useCallback(() => {
+    setSelectedPage(null);
+    setIsConfirming(false);
+  }, [setIsConfirming]);
+
+  const handleConfirmDialog = useCallback(() => {
+    handleApplyPageLayout(selectedPage);
+    setIsConfirming(false);
+  }, [selectedPage, handleApplyPageLayout]);
+
+  const handlePageFocus = useCallback(
+    (pageId) => {
+      if (activePageId !== pageId) {
+        setActivePageId(pageId);
+      }
+    },
+    [activePageId]
+  );
+
   const rowVirtualizer = useVirtual({
-    size: Math.ceil(pages.length / 2),
+    size: Math.ceil((pages || []).length / 2),
     parentRef,
     estimateSize: useCallback(
       () => pageSize.containerHeight + PAGE_LAYOUT_ROW_GAP,
       [pageSize.containerHeight]
     ),
-    overscan: 2,
+    overscan: 4,
   });
 
-  const requiresConfirmation = useMemo(
-    () => currentPage && !isDefaultPage(currentPage),
-    [currentPage]
+  const columnVirtualizer = useVirtual({
+    horizontal: true,
+    size: 2,
+    parentRef,
+    estimateSize: useCallback(() => pageSize.width + PAGE_LAYOUT_ROW_GAP, [
+      pageSize.width,
+    ]),
+    overscan: 0,
+  });
+
+  /**
+   * useVirtual and useRovingTabIndex do not play well together but we need both!
+   * The Below useEffect is key to maintaining the proper focus for keyboard users.
+   * What happens is on "scroll" of the virtualized container useVirtual is grabbing more rows
+   * and these rows are fed through the rendered map to create page layouts that are visible to users.
+   * While it doesn't look like the DOM is updating, it is in fact updating.
+   * So the old refs get lost and the list gets new ones! By manually forcing focus and maintaining an object
+   * of pageRefs that are organized by page id (those don't update) we can make sure the focus stays where it's supposed to be.
+   * Checking for a difference in currentAvailableRows and assigning the matching ref to any change is just so that the effect hook will run when the virtualized list
+   * updates and reattach focus to the new instance of an already selected layout.
+   */
+  const currentAvailableRows = useMemo(() => rowVirtualizer.virtualItems, [
+    rowVirtualizer,
+  ]);
+  const currentAvailableRowsRef = useRef();
+
+  useEffect(() => {
+    if (currentAvailableRows !== currentAvailableRowsRef?.current) {
+      currentAvailableRowsRef.current = currentAvailableRows;
+    }
+
+    if (activePageId && isPageLayoutsFocused) {
+      pageRefs.current?.[activePageId]?.focus();
+    }
+  }, [activePageId, currentAvailableRows, isPageLayoutsFocused]);
+
+  const handlePageLayoutFocus = useCallback(() => {
+    if (!isPageLayoutsFocused) {
+      const newPageId = pageRefs.current?.[activePageId]
+        ? activePageId
+        : pageIds[0];
+
+      setActivePageId(newPageId);
+      setIsPageLayoutsFocused(true);
+      pageRefs.current?.[newPageId]?.focus();
+    }
+  }, [activePageId, isPageLayoutsFocused, pageIds]);
+
+  useRovingTabIndex({ ref: containerRef });
+
+  // Exit page layouts
+  const focusCanvas = useFocusCanvas();
+
+  const onTabKeyDown = useCallback(() => {
+    focusCanvas();
+    setIsPageLayoutsFocused(false);
+  }, [focusCanvas]);
+
+  useKeyDownEffect(containerRef, 'tab', onTabKeyDown, [onTabKeyDown]);
+
+  useFocusOut(
+    containerRef,
+    () => {
+      setIsPageLayoutsFocused(false);
+    },
+    [isConfirming]
   );
 
   return (
@@ -105,33 +236,49 @@ function PageLayouts(props) {
       }}
     >
       <PageLayoutsContainer height={rowVirtualizer.totalSize}>
-        {rowVirtualizer.virtualItems.map((virtualRow) => {
-          const firstColumnIndex = virtualRow.index * 2;
-          const indexes = [firstColumnIndex, firstColumnIndex + 1];
-          return (
-            <PageLayoutsRow
-              key={virtualRow.index}
-              height={virtualRow.size}
-              translateY={virtualRow.start}
-            >
-              {indexes.map((index) => {
-                const page = pages[index];
-                if (!page) {
-                  return null;
-                }
-                return (
-                  <PageLayout
-                    key={page.id}
-                    page={page}
-                    pageSize={pageSize}
-                    onConfirm={() => handleApplyPageLayout(page)}
-                    requiresConfirmation={requiresConfirmation}
-                  />
-                );
-              })}
-            </PageLayoutsRow>
-          );
-        })}
+        <PageLayoutsVirtualizedContainer
+          height={rowVirtualizer.totalSize}
+          ref={containerRef}
+          pageSize={pageSize}
+          tab={0}
+          onFocus={handlePageLayoutFocus}
+        >
+          {rowVirtualizer.virtualItems.map((virtualRow) =>
+            columnVirtualizer.virtualItems.map((virtualColumn) => {
+              const pageIndex =
+                virtualColumn.index === 0
+                  ? virtualRow.index * 2
+                  : virtualRow.index * 2 + 1;
+              const page = pages[pageIndex];
+
+              if (!page) {
+                return null;
+              }
+              const isActive = activePageId === page.id && isPageLayoutsFocused;
+
+              return (
+                <PageLayout
+                  key={pageIndex}
+                  ref={(el) => (pageRefs.current[page.id] = el)}
+                  translateY={virtualRow.start}
+                  translateX={virtualColumn.start}
+                  page={page}
+                  pageSize={pageSize}
+                  isActive={isActive}
+                  onFocus={() => handlePageFocus(page.id)}
+                  onClick={() => handlePageClick(page)}
+                  onKeyUp={(event) => handleKeyboardPageClick(event, page)}
+                />
+              );
+            })
+          )}
+        </PageLayoutsVirtualizedContainer>
+        {isConfirming && (
+          <ConfirmPageLayoutDialog
+            onConfirm={handleConfirmDialog}
+            onClose={handleCloseDialog}
+          />
+        )}
       </PageLayoutsContainer>
     </UnitsProvider>
   );
