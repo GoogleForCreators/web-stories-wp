b851f3ca8c24d50ec327b4c5121d338d64ef7cbc
Emily Bartman <emily@emwaves.org>
2021-02-22 01:17:56 -0700

    3P Media: Use videos instead of gifs in library (#6402)
    Co-authored-by: Emily Bartman <emily.bartman@formidable.com>

--- assets/src/edit-story/app/media/media3p/constants.js
+++ assets/src/edit-story/app/media/media3p/constants.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,14 @@
  * limitations under the License.
  */
 
-export { default as MediaProvider } from './mediaProvider';
-export { default as useLocalMedia } from './local/useLocalMedia';
-export { default as useMedia } from './useMedia';
-export * from './media3p/constants';
+export const ContentType = {
+  IMAGE: 'image',
+  VIDEO: 'video',
+  GIF: 'gif',
+};
+
+export const ProviderType = {
+  UNSPLASH: 'unsplash',
+  COVERR: 'coverr',
+  TENOR: 'tenor',
+};
