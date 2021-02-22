e92abd409162d5b6dd34c38eac40083cac654e62
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-02-22 15:43:23 -0700

    capture sams latest updates

--- assets/src/design-system/theme/typography.js
+++ assets/src/design-system/theme/typography.js
@@ -129,7 +129,7 @@ export const typography = {
     },
     link: {
       [PRESET_SIZES.X_LARGE]: {
-        weight: 500,
+        weight: 400,
         size: 24,
         lineHeight: 32,
         letterSpacing: -0.02,
@@ -161,25 +161,25 @@ export const typography = {
     },
     label: {
       [PRESET_SIZES.LARGE]: {
-        weight: 500,
+        weight: 400,
         size: 18,
         lineHeight: 24,
         letterSpacing: 0,
       },
       [PRESET_SIZES.MEDIUM]: {
-        weight: 500,
+        weight: 400,
         size: 16,
         lineHeight: 20,
         letterSpacing: 0,
       },
       [PRESET_SIZES.SMALL]: {
-        weight: 500,
+        weight: 400,
         size: 14,
-        lineHeight: 20,
+        lineHeight: 12,
         letterSpacing: 0,
       },
       [PRESET_SIZES.X_SMALL]: {
-        weight: 500,
+        weight: 400,
         size: 12,
         lineHeight: 20,
         letterSpacing: 0,
