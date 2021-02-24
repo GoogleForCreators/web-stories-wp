30dcefb083026a8c496cf94fbd0165f950f4e2c1
Maxwell Yinger <max.yinger@formidable.com>
2021-02-24 09:24:35 -0700

    fix warnings on icon and fix some e2e tests

--- assets/src/design-system/components/dropDown/select/index.js
+++ assets/src/design-system/components/dropDown/select/index.js
@@ -26,6 +26,7 @@ import PropTypes from 'prop-types';
 import { THEME_CONSTANTS } from '../../../theme';
 import {
   SelectButton,
+  ChevronWrap,
   StyledChevron,
   Value,
   LabelText,
@@ -65,7 +66,9 @@ const DropDownSelect = (
         </LabelText>
       )}
 
-      <StyledChevron isOpen={isOpen} />
+      <ChevronWrap isOpen={isOpen}>
+        <StyledChevron />
+      </ChevronWrap>
     </Label>
   </SelectButton>
 );
