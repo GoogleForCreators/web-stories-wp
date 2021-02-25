24c42716180e9bb844f0bf09d6f5f676dcae6a1c
Morten Barklund <morten.barklund@xwp.co>
2021-02-24 21:10:29 -0500

    Updated story for pill component to reflect usage

--- assets/src/design-system/components/pill/stories/index.js
+++ assets/src/design-system/components/pill/stories/index.js
@@ -17,8 +17,7 @@
 /**
  * External dependencies
  */
-import { action } from '@storybook/addon-actions';
-import { boolean, text } from '@storybook/addon-knobs';
+import { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 /**
  * Internal dependencies
@@ -32,33 +31,30 @@ export default {
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.colors.bg.primary};
-  width: 600px;
-  height: 400px;
   padding: 30px;
+  display: flex;
+  gap: 12px;
 `;
-// Override light theme because this component is only set up for dark theme right now given fg and bg coloring
-export const _default = () => (
-  <ThemeProvider theme={theme}>
-    <Container>
-      <Pill
-        isActive={boolean('isActive', false)}
-        onClick={(e) => action('click on pill')(e)}
-      >
-        {text('children', 'I am pill text')}
-      </Pill>
-    </Container>
-  </ThemeProvider>
-);
 
-export const LightTheme = () => {
+function Pills() {
+  const [active, setActive] = useState(1);
   return (
     <Container>
-      <Pill
-        isActive={boolean('isActive', false)}
-        onClick={(e) => action('click on pill')(e)}
-      >
-        {text('children', 'I am pill text')}
-      </Pill>
+      {[1, 2, 3].map((i) => (
+        <Pill key={i} isActive={active === i} onClick={() => setActive(i)}>
+          {`Pill ${i}`}
+        </Pill>
+      ))}
     </Container>
   );
-};
+}
+
+// Override light theme because this component is only set up for dark theme right now given fg and bg coloring
+export const _default = () => (
+  <>
+    <Pills />
+    <ThemeProvider theme={theme}>
+      <Pills />
+    </ThemeProvider>
+  </>
+);
