32c7ffe49fa3af37019ee029f92f5b00bc05a5b8
Jonathan Harris <jon@spacedmonkey.co.uk>
2021-03-05 18:11:46 +0000

    Add storybook integration.

---
+++ assets/src/edit-story/components/postLock/stories/postTakeOverDialog.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { action } from '@storybook/addon-actions';
+
+/**
+ * Internal dependencies
+ */
+import PostTakeOverDialog from '../postTakeOverDialog';
+
+export default {
+  title: 'Stories Editor/Components/Dialog/Post Take Over Dialog',
+  component: PostTakeOverDialog,
+};
+
+export const _default = () => {
+  const user = {
+    name: 'Matt Mullenweg',
+    avatar_urls: {
+      48: 'http://1.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60?size=48',
+    },
+  };
+  const dashboardLink = 'http://www.example.com/dashboard';
+  return (
+    <PostTakeOverDialog
+      user={user}
+      open
+      dashboardLink={dashboardLink}
+      onClose={action('closed')}
+    />
+  );
+};
