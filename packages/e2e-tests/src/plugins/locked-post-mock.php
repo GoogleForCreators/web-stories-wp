c9216093a9999cf36cc06718a6be50496ffc5903
Jonathan Harris <jon@spacedmonkey.co.uk>
2021-03-08 20:42:27 +0000

    Create new user.

--- packages/e2e-tests/src/plugins/locked-post-mock.php
+++ packages/e2e-tests/src/plugins/locked-post-mock.php
@@ -20,13 +20,38 @@ function filter_meta( $value, $object_id, $meta_key ) {
 		return $value;
 	}
 
-	$user    = get_user_by( 'login', 'author' );
+	$user    = get_user_by( 'login', 'test_locker' );
+	if ( ! $user ) {
+		return $value;
+	}
 	$user_id = $user->ID;
 	$now     = time();
 	$lock    = "$now:$user_id";
 
 	return $lock;
 }
-
 add_filter( 'get_post_metadata', __NAMESPACE__ . '\filter_meta', 10, 3 );
+
+
+function activate() {
+	$user = get_user_by( 'login', 'test_locker' );
+	if ( ! $user ) {
+		wp_insert_user( [
+			'user_login' => 'test_locker',
+			'role'       => 'author',
+			'user_email' => 'lock@example.com',
+			'user_pass'  => 'fsfdsfds'
+		] );
+	}
+}
+
+function deactivate() {
+	$user = get_user_by( 'login', 'test_locker' );
+	if ( $user ) {
+		wp_delete_user( $user->ID );
+	}
+}
+
+register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\deactivate' );
 
