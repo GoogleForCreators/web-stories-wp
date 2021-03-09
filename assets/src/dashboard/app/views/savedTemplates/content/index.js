381acb925ab554a015c936b5b2b18acbaf92163b
samwhale <samuelestrella226@gmail.com>
2021-03-09 14:27:14 -0700

    Show loading spinner any time templates load

--- assets/src/dashboard/app/views/savedTemplates/content/index.js
+++ assets/src/dashboard/app/views/savedTemplates/content/index.js
@@ -112,38 +112,66 @@ function Content({
     enabledMenuItems,
   ]);
 
-  const showLoadingAnimation = isLoading && templates.length === 0;
-
-  const emptyContent = useMemo(
-    () =>
-      showLoadingAnimation ? (
+  const pageContent = useMemo(() => {
+    if (isLoading) {
+      return (
         <LoadingContainer>
           <LoadingSpinner />
         </LoadingContainer>
-      ) : (
-        <EmptyContentMessage>
-          <Headline
-            size={THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.SMALL}
-            as="h3"
-          >
-            {search?.keyword
-              ? sprintf(
-                  /* translators: %s: search term. */
-                  __(
-                    'Sorry, we couldn\'t find any results matching "%s"',
-                    'web-stories'
-                  ),
-                  search.keyword
-                )
-              : __(
-                  'Bookmark a story or template to get started!',
+      );
+    }
+
+    return templates.length > 0 ? (
+      <>
+        <SavedTemplateGridView
+          bottomActionLabel={__('Use template', 'web-stories')}
+          centerActionLabelByStatus={
+            TEMPLATES_GALLERY_ITEM_CENTER_ACTION_LABELS
+          }
+          actions={actions}
+          pageSize={view.pageSize}
+          templateMenu={templateMenu}
+          templates={templates}
+          returnFocusId={initialFocusId}
+        />
+        <InfiniteScroller
+          allDataLoadedAriaMessage={__(
+            'All templates are loaded',
+            'web-stories'
+          )}
+          loadingAriaMessage={__('Loading more templates', 'web-stories')}
+          isLoading={isLoading}
+          canLoadMore={!allPagesFetched}
+          onLoadMore={page.requestNextPage}
+        />
+      </>
+    ) : (
+      <EmptyContentMessage>
+        <Headline size={THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.SMALL} as="h3">
+          {search?.keyword
+            ? sprintf(
+                /* translators: %s: search term. */
+                __(
+                  'Sorry, we couldn\'t find any results matching "%s"',
                   'web-stories'
-                )}
-          </Headline>
-        </EmptyContentMessage>
-      ),
-    [showLoadingAnimation, search?.keyword]
-  );
+                ),
+                search.keyword
+              )
+            : __('Bookmark a story or template to get started!', 'web-stories')}
+        </Headline>
+      </EmptyContentMessage>
+    );
+  }, [
+    actions,
+    allPagesFetched,
+    initialFocusId,
+    isLoading,
+    page.requestNextPage,
+    search?.keyword,
+    templateMenu,
+    templates,
+    view.pageSize,
+  ]);
 
   return (
     <Layout.Scrollable>
@@ -155,38 +183,7 @@ function Content({
               height: view.pageSize.height,
             }}
           >
-            <StandardViewContentGutter>
-              {templates.length > 0 ? (
-                <>
-                  <SavedTemplateGridView
-                    bottomActionLabel={__('Use template', 'web-stories')}
-                    centerActionLabelByStatus={
-                      TEMPLATES_GALLERY_ITEM_CENTER_ACTION_LABELS
-                    }
-                    actions={actions}
-                    pageSize={view.pageSize}
-                    templateMenu={templateMenu}
-                    templates={templates}
-                    returnFocusId={initialFocusId}
-                  />
-                  <InfiniteScroller
-                    allDataLoadedAriaMessage={__(
-                      'All templates are loaded',
-                      'web-stories'
-                    )}
-                    loadingAriaMessage={__(
-                      'Loading more templates',
-                      'web-stories'
-                    )}
-                    isLoading={isLoading}
-                    canLoadMore={!allPagesFetched}
-                    onLoadMore={page.requestNextPage}
-                  />
-                </>
-              ) : (
-                emptyContent
-              )}
-            </StandardViewContentGutter>
+            <StandardViewContentGutter>{pageContent}</StandardViewContentGutter>
           </UnitsProvider>
         </TransformProvider>
       </FontProvider>
