8324cef8200e90dfab9b3107a0b27ff95229ecf4
samwhale <samuelestrella226@gmail.com>
2021-03-09 09:52:13 -0700

    Checkpoint

---
+++ assets/src/design-system/components/loadingSpinner/index.js
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * External dependencies
+ */
+import styled, { keyframes } from 'styled-components';
+
+/**
+ * Internal dependencies
+ */
+import { BEZIER } from '../../../animation/constants';
+
+const CIRCLE_DIAMETER = 95;
+const NUM_CIRCLES = 4;
+const ANIMATION_DURATION = 4;
+
+const TAU = Math.PI * 2;
+
+const getAngleOfCircle = (index) => {
+  return (TAU * index) / NUM_CIRCLES;
+};
+
+const getCirclePosition = (angle) => {
+  const radius = CIRCLE_DIAMETER / 2;
+  const x = Math.sin(angle) * radius;
+  const y = -Math.cos(angle) * radius;
+
+  return { x: x.toFixed(1), y: y.toFixed(1) };
+};
+
+const Container = styled.div`
+  position: relative;
+  height: ${CIRCLE_DIAMETER}px;
+  width: ${CIRCLE_DIAMETER}px;
+  color: ${({ theme }) => theme.colors.interactiveBg.brandNormal};
+`;
+
+const Circle = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%)
+    ${({ $position: { x, y } }) => `translate(${x}px, ${y}px)`};
+
+  height: 12px;
+  width: 12px;
+  background-color: ${({ theme }) => theme.colors.interactiveBg.brandNormal};
+  border-radius: ${({ theme }) => theme.borders.radius.round};
+
+  animation-name: ${({ circleIndex }) => keyframes`
+    // first circle (animating over 1 sec)
+    // 0% - opacity 1
+    // 25% - opacity 0
+    // 26% -> 100% - opacity 1
+
+    0% {
+      /* opacity: ${1 - circleIndex / NUM_CIRCLES}; */
+      opacity: ${
+        Math.sin(getAngleOfCircle(circleIndex + NUM_CIRCLES / 4) / 2) / 2 + 0.5
+      };
+    }
+    100% {
+      opacity: ${
+        Math.sin(getAngleOfCircle(circleIndex + NUM_CIRCLES / 4) / 2) / 2 + 0.5
+      };
+    }
+  `};
+  animation-fill-mode: both;
+  animation-duration: ${ANIMATION_DURATION}s;
+  animation-iteration-count: infinite;
+  /* animation-delay: ${({ circleIndex }) =>
+    ((ANIMATION_DURATION * circleIndex) / NUM_CIRCLES).toFixed(1)}s; */
+  /* animation-timing-function: ${BEZIER.easeInOutQuad}; */
+`;
+
+const numCircles = new Array(NUM_CIRCLES).fill(1);
+
+export function LoadingSpinner(props) {
+  return (
+    <Container {...props}>
+      {numCircles.map((_, index) => {
+        return (
+          <Circle
+            key={index}
+            circleIndex={index}
+            $position={getCirclePosition(getAngleOfCircle(index))}
+          />
+        );
+      })}
+    </Container>
+  );
+}
