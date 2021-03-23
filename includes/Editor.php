dc18e7d5b3f7a4d91adb1a3d53ef99f2c6d1d062
Jonny Harris <jon@spacedmonkey.co.uk>
2021-03-23 11:58:54 +0000

    Filter get_avatar, style_loader_tag and script_loader_tag

--- includes/Editor.php
+++ includes/Editor.php
@@ -47,6 +47,9 @@ class Editor {
 		add_action( 'admin_footer-post-new.php', [ $this, 'admin_footer' ] );
 		add_action( 'load-post.php', [ $this, 'admin_header' ] );
 		add_action( 'load-post-new.php', [ $this, 'admin_header' ] );
+		add_filter( 'style_loader_tag', [ $this, 'style_loader_tag' ], 10, 3 );
+		add_filter( 'script_loader_tag', [ $this, 'script_loader_tag' ], 10, 3 );
+		add_filter( 'get_avatar', [ $this, 'get_avatar' ], 10, 6 );
 	}
 
 	/**
@@ -89,7 +92,7 @@ class Editor {
 
 		$document = Document::fromHtml( $html );
 
-		if( ! $document ){
+		if ( ! $document ) {
 			return;
 		}
 
@@ -110,6 +113,7 @@ class Editor {
 				}
 
 				$cross_origin = $node->getAttribute( 'crossorigin' );
+
 				if ( $cross_origin ) {
 					continue;
 				}
@@ -123,7 +127,7 @@ class Editor {
 				}
 
 				$url  = esc_url( $value );
-				$html = str_replace( "{$attribute}='{$url}'", "crossorigin {$attribute}='{$url}'", $html );
+				$html = str_replace( "{$attribute}='{$url}'", "crossorigin='anonymous' {$attribute}='{$url}'", $html );
 			}
 		}
 
@@ -131,9 +135,119 @@ class Editor {
 	}
 
 	/**
+	 * Filters the HTML link tag of an enqueued style.
+	 *
+	 * @param string $tag    The link tag for the enqueued style.
+	 * @param string $handle The style's registered handle.
+	 * @param string $href   The stylesheet's source URL.
+	 *
+	 * @return string
+	 */
+	public function style_loader_tag( $tag, $handle, $href ) {
+		$screen = $this->get_current_screen();
+		if ( ! $screen ) {
+			return $tag;
+		}
+
+		if ( Story_Post_Type::POST_TYPE_SLUG !== $screen->post_type ) {
+			return $tag;
+		}
+
+		$site_url = site_url();
+
+		if ( $this->starts_with( $href, $site_url ) ) {
+			return $tag;
+		}
+
+		if ( $this->starts_with( $href, '/' ) ) {
+			return $tag;
+		}
+
+		$tag = str_replace( "href='{$href}'", "crossorigin='anonymous' href='{$href}'", $tag );
+
+		return $tag;
+	}
+
+	/**
+	 * Filters the HTML script tag of an enqueued script.
+	 *
+	 * @param string $tag    The `<script>` tag for the enqueued script.
+	 * @param string $handle The script's registered handle.
+	 * @param string $src    The script's source URL.
+	 *
+	 * @return string
+	 */
+	public function script_loader_tag( $tag, $handle, $src ) {
+		$screen = $this->get_current_screen();
+		if ( ! $screen ) {
+			return $tag;
+		}
+
+		if ( Story_Post_Type::POST_TYPE_SLUG !== $screen->post_type ) {
+			return $tag;
+		}
+
+		$site_url = site_url();
+
+		if ( $this->starts_with( $src, $site_url ) ) {
+			return $tag;
+		}
+
+		if ( $this->starts_with( $src, '/' ) ) {
+			return $tag;
+		}
+
+		$tag = str_replace( "src='{$src}'", "crossorigin='anonymous' src='{$src}'", $tag );
+
+		return $tag;
+	}
+
+	/**
+	 * Filter the avatar tag.
+	 *
+	 * @param string $avatar      HTML for the user's avatar.
+	 * @param mixed  $id_or_email The avatar to retrieve. Accepts a user_id, Gravatar MD5 hash,
+	 *                            user email, WP_User object, WP_Post object, or WP_Comment object.
+	 * @param int    $size        Square avatar width and height in pixels to retrieve.
+	 * @param string $default     URL for the default image or a default type. Accepts '404', 'retro', 'monsterid',
+	 *                            'wavatar', 'indenticon', 'mystery', 'mm', 'mysteryman', 'blank', or
+	 *                            'gravatar_default'. Default is the value of the 'avatar_default' option, with a
+	 *                            fallback of 'mystery'.
+	 * @param string $alt         Alternative text to use in the avatar image tag. Default empty.
+	 * @param array  $args        Arguments passed to get_avatar_data(), after processing.
+	 *
+	 * @return string
+	 */
+	public function get_avatar( $avatar, $id_or_email, $size, $default, $alt, $args ) {
+		$screen = $this->get_current_screen();
+		if ( ! $screen ) {
+			return $avatar;
+		}
+
+		if ( Story_Post_Type::POST_TYPE_SLUG !== $screen->post_type ) {
+			return $avatar;
+		}
+		
+		$site_url = site_url();
+		$src      = esc_url( $args['url'] );
+
+		if ( $this->starts_with( $src, $site_url ) ) {
+			return $avatar;
+		}
+
+		if ( $this->starts_with( $src, '/' ) ) {
+			return $avatar;
+		}
+
+		$avatar = str_replace( "src='{$src}'", "crossorigin='anonymous' src='{$src}'", $avatar );
+
+		return $avatar;
+	}
+
+	/**
 	 * Does string start with.
 	 *
-	 * @param string $string String to search.
+	 * @param string $string       String to search.
 	 * @param string $start_string String to search with.
 	 *
 	 * @return bool
