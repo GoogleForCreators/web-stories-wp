aedffc8b07d9e6e902c5745ec90ffa47f066f016
Samuel Estrella <samuelestrella226@gmail.com>
2021-03-25 06:44:23 -0600

    Document Panel: Update Page Advancement Panel (#6824)
    * Update switch
    * remove default label props, make required
    * add disabled styling
    * reduce font size of label
    * add hover styling
    
    * Update the switch in the editor
    
    * Update page advancement panel
    
    * Fix bug - ref not being added
    
    * Run on all keys
    
    * Update disabled colors
    
    * Fix disabled background color
    
    * Fix disabled clor of label
    
    * Revert disabled state update
    
    * PR Fixes
    * inline default props
    * fix label height
    * make radio input opaque since it can be seen in weird cases

--- assets/src/edit-story/components/form/stories/switch.js
+++ assets/src/edit-story/components/form/stories/switch.js
@@ -18,6 +18,8 @@
  * External dependencies
  */
 import { useState } from 'react';
+import styled from 'styled-components';
+import { action } from '@storybook/addon-actions';
 import { boolean, text } from '@storybook/addon-knobs';
 
 /**
@@ -30,19 +32,34 @@ export default {
   component: Switch,
 };
 
+const Container = styled.div`
+  padding: 20px 50px;
+  background-color: ${({ theme }) => theme.colors.bg.primary};
+  border: 1px solid ${({ theme }) => theme.colors.standard.black};
+`;
+
 export const _default = () => {
   const onLabel = text('OnLabel', 'Fit to Device');
   const offLabel = text('OffLabel', 'Do not format');
   const disabled = boolean('Disabled', false);
   const [value, setValue] = useState(true);
 
+  const handleChange = (evt, newValue) => {
+    action(`Switching to: '${newValue ? 'On' : 'Off'}'`)(evt);
+    setValue(newValue);
+  };
+
   return (
-    <Switch
-      value={value}
-      onLabel={onLabel}
-      offLabel={offLabel}
-      onChange={setValue}
-      disabled={disabled}
-    />
+    <Container>
+      <Switch
+        groupLabel="Switch"
+        name="test-switch"
+        value={value}
+        onLabel={onLabel}
+        offLabel={offLabel}
+        onChange={handleChange}
+        disabled={disabled}
+      />
+    </Container>
   );
 };
