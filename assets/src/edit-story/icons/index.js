45932d2deb15f9fe127d9ecd63ca646f90bc79ef
Miina Sikk <miina.sikk@gmail.com>
2021-03-31 13:50:58 +0300

    Remove more icons.

--- assets/src/edit-story/icons/index.js
+++ assets/src/edit-story/icons/index.js
@@ -14,32 +14,12 @@
  * limitations under the License.
  */
 
-export { ReactComponent as Arrow } from './arrow.svg';
 export { ReactComponent as ArrowDown } from './arrow_down.svg';
 export { ReactComponent as Close } from './close.svg';
-export { ReactComponent as Curve } from './curve.svg';
-export { ReactComponent as DefaultImage } from './default_image.svg';
-export { ReactComponent as Delete } from './delete_icon.svg';
-export { ReactComponent as DistributeHorizontal } from './horizontal_distribute.svg';
-export { ReactComponent as DistributeVertical } from './vertical_distribute.svg';
 export { ReactComponent as Dropdown } from './dropdown.svg';
-export { ReactComponent as Duplicate } from './duplicate_icon.svg';
-export { ReactComponent as EditPencil } from './edit_pencil.svg';
-export { ReactComponent as External } from './external.svg';
-export { ReactComponent as GridView } from './grid_view.svg';
-export { ReactComponent as Info } from './info.svg';
 export { ReactComponent as KeyArrowUp } from './key_arrow_up.svg';
-export { ReactComponent as LayoutHelper } from './layout_helper.svg';
-export { ReactComponent as Link } from './link.svg';
 export { ReactComponent as Media } from './media.svg';
 export { ReactComponent as More } from './more_horiz.svg';
-export { ReactComponent as OffsetHorizontal } from './offset_horizontal.svg';
-export { ReactComponent as OffsetVertical } from './offset_vertical.svg';
-export { ReactComponent as PlayCircular } from './play_circular.svg';
-export { ReactComponent as Rectangle } from './rectangle.svg';
 export { ReactComponent as Remove } from './remove.svg';
-export { ReactComponent as Shapes } from './shapes.svg';
-export { ReactComponent as StopCircular } from './stop_circular.svg';
 export { ReactComponent as TurningLine } from './turning_line.svg';
 export { ReactComponent as Union } from './union.svg';
-export { ReactComponent as Video } from './video.svg';
