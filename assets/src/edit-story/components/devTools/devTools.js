240a1e060014a7448039a5598a9ec0a6c93a627b
Miina Sikk <miina.sikk@gmail.com>
2021-03-31 14:46:48 +0300

    More replacements.

--- assets/src/edit-story/components/devTools/devTools.js
+++ assets/src/edit-story/components/devTools/devTools.js
@@ -39,14 +39,14 @@ const Container = styled.div`
   height: 50%;
   border: 6px solid ${({ theme }) => theme.colors.bg.secondary};
   background: ${({ theme }) => theme.colors.bg.primary};
-  color: ${({ theme }) => theme.DEPRECATED_THEME.colors.fg.v2};
+  color: ${({ theme }) => theme.colors.fg.secondary};
 `;
 
 const Textarea = styled.textarea`
   flex: 1;
   border: 0;
   background: ${({ theme }) => theme.colors.bg.primary};
-  color: ${({ theme }) => theme.DEPRECATED_THEME.colors.fg.v2};
+  color: ${({ theme }) => theme.colors.fg.secondary};
   white-space: nowrap;
   overflow: auto;
 `;
