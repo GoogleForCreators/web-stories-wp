2f84d96fdc4fe73acfea9436f149c4ede99049e6
Emily Bartman <emily.bartman@formidable.com>
2021-04-01 15:05:28 -0700

    delete dashboard button

--- assets/src/dashboard/components/index.js
+++ assets/src/dashboard/components/index.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { default as Button } from './button';
 export { default as CardGallery } from './cardGallery';
 export { default as CardGrid } from './cardGrid';
 export {
