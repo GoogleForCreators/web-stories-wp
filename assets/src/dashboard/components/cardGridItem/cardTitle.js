5eed3373c56f43ce4aff6bea1b13e1f2f06fe487
Emily Bartman <emily.bartman@formidable.com>
2021-04-01 17:25:25 -0700

    use brand colors for lock icon

--- assets/src/dashboard/components/cardGridItem/cardTitle.js
+++ assets/src/dashboard/components/cardGridItem/cardTitle.js
@@ -83,7 +83,7 @@ const DateHelperText = styled(Text).attrs(() => ({
 `;
 // TODO: Fix dirty workaround.
 const ListIcon = styled(LockSVG)`
-  color: ${({ theme }) => theme.DEPRECATED_THEME.colors.gray900};
+  color: ${({ theme }) => theme.colors.gray[90]};
   display: inline-block;
   height: 36px;
   width: 36px;
