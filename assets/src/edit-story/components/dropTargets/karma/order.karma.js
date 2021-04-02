14675c65b5f3332a3bb019104659292494cee24e
Miina Sikk <miina.sikk@gmail.com>
2021-04-02 17:24:51 +0300

    Fix

--- assets/src/edit-story/components/dropTargets/karma/order.karma.js
+++ assets/src/edit-story/components/dropTargets/karma/order.karma.js
@@ -42,7 +42,7 @@ describe('Drop-Target order', () => {
     const canvas = fixture.editor.canvas.fullbleed.container;
 
     await fixture.events.mouse.seq(({ down, moveRel, up }) => [
-      moveRel(bgMedia, 5, 5),
+      moveRel(bgMedia, 20, 20),
       down(),
       moveRel(canvas, 10, 10),
       up(),
