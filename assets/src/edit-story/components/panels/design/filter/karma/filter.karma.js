8206b0b942c4f7d7bf195e80c144d8354fdbbcca
Miina Sikk <miina.sikk@gmail.com>
2021-04-02 15:58:59 +0300

    Change.

--- assets/src/edit-story/components/panels/design/filter/karma/filter.karma.js
+++ assets/src/edit-story/components/panels/design/filter/karma/filter.karma.js
@@ -38,7 +38,7 @@ fdescribe('Filter Panel', () => {
     // Drag first media element straight to canvas edge to set as background
     const media = fixture.editor.library.media.item(0);
     const canvas = fixture.editor.canvas.fullbleed.container;
-    await fixture.events.mouse.moveRel(media, 25, 25);
+    await fixture.events.mouse.moveRel(media, 10, 10);
     await fixture.events.mouse.seq(({ down, moveRel, up }) => [
       moveRel(media, 25, 25),
       down(),
