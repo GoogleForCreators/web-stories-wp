3974644814cf2e9c77d4b2faf493a562533779d4
Jonny Harris <spacedmonkey@users.noreply.github.com>
2021-04-14 13:06:43 +0100

    Change comment
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- assets/src/edit-story/app/media/utils/createResource.js
+++ assets/src/edit-story/app/media/utils/createResource.js
@@ -58,7 +58,7 @@ import getTypeFromMime from './getTypeFromMime';
  * resource.
  * @property {boolean} local Whether the resource has been already uploaded to
  * the server.
- * @property {boolean} isOptimized Whether the resource has been already transcoded.
+ * @property {boolean} isOptimized Whether the resource has already been optimized.
  * @property {Object} sizes Object of image sizes.
  * @property {?Object} output An optional object of video sizes for rendering gifs as videos
  */
