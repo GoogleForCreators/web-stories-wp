b2a90a403f610b112e76815d7820300f41084603
Pascal Birchler <pascalb@google.com>
2021-04-15 15:56:18 +0200

    Add trailing period

--- assets/src/dashboard/app/views/myStories/content/storiesView/index.js
+++ assets/src/dashboard/app/views/myStories/content/storiesView/index.js
@@ -290,7 +290,7 @@ function StoriesView({
           secondaryText={__('Cancel', 'web-stories')}
           secondaryRest={{
             ['aria-label']: sprintf(
-              /* translators: %s: story title */
+              /* translators: %s: story title. */
               __('Cancel deleting story "%s"', 'web-stories'),
               titleFormatted(activeStory.title)
             ),
@@ -299,7 +299,7 @@ function StoriesView({
           onPrimary={handleOnDeleteStory}
           primaryRest={{
             ['aria-label']: sprintf(
-              /* translators: %s: story title */
+              /* translators: %s: story title. */
               __('Confirm deleting story "%s"', 'web-stories'),
               titleFormatted(activeStory.title)
             ),
