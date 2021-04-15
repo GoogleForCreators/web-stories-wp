59fa2738c7046f28485dc884cf2746351ef27190
Samuel Estrella <samuelestrella226@gmail.com>
2021-04-15 14:44:22 -0600

    Help Center: Add 'Embed Web Story' quick tip (#7203)
    Co-authored-by: Brittany Feenstra <brittany.feenstra@formidable.com>

--- assets/src/edit-story/components/helpCenter/quickTip/index.js
+++ assets/src/edit-story/components/helpCenter/quickTip/index.js
@@ -24,6 +24,7 @@ import { TranslateWithMarkup } from '@web-stories-wp/i18n';
  * Internal dependencies
  */
 import {
+  Link,
   themeHelpers,
   Text,
   THEME_CONSTANTS,
@@ -54,6 +55,7 @@ const Overflow = styled.div`
 const Video = styled.video`
   height: 180px;
   margin-bottom: ${GUTTER_WIDTH}px;
+  max-width: 100%;
 `;
 
 const Title = styled.h1`
@@ -89,6 +91,9 @@ export function QuickTip({
   description,
   isLeftToRightTransition = true,
   figureSrc,
+  figureSrcImg,
+  figureAlt,
+  href,
   isDone = false,
   ...transitionProps
 }) {
@@ -114,6 +119,16 @@ export function QuickTip({
               <source src={`${cdnURL}${figureSrc}.mp4`} type="video/mp4" />
             </Video>
           )}
+          {/* `figureSrcImg` is temporary until we get an animation for the embed quick tip.
+          Once we have the animation then the .png image won't be necessary */}
+          {figureSrcImg && (
+            <Video
+              as="img"
+              alt={figureAlt}
+              src={`${cdnURL}${figureSrcImg}.png`}
+              crossOrigin="anonymous"
+            />
+          )}
           {isDone && (
             <DoneContainer>
               <DoneCheckmark height={135} width={135} />
@@ -128,6 +143,14 @@ export function QuickTip({
             >
               <TranslateWithMarkup
                 mapping={{
+                  a: (
+                    <Link
+                      size={THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.SMALL}
+                      rel="noreferrer noopener"
+                      target="_blank"
+                      href={href}
+                    />
+                  ),
                   screenreader: <VisuallyHidden />,
                 }}
               >
@@ -142,7 +165,12 @@ export function QuickTip({
 }
 
 QuickTip.propTypes = {
+  /* Temporary props: `figureAlt`, `figureSrc`.
+  See assets/src/edit-story/components/helpCenter/constants.js. */
+  figureAlt: PropTypes.string,
   figureSrc: PropTypes.string,
+  figureSrcImg: PropTypes.string,
+  href: PropTypes.string,
   isDone: PropTypes.bool,
   title: PropTypes.string.isRequired,
   description: PropTypes.arrayOf(PropTypes.string).isRequired,
