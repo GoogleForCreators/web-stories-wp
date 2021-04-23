7090fb1ddefe22ac97f233866801fbc11817d404
Jonny Harris <jon@spacedmonkey.co.uk>
2021-04-23 16:05:41 +0100

    Add some comments.

--- tests/phpunit/includes/Test_Case.php
+++ tests/phpunit/includes/Test_Case.php
@@ -2,14 +2,16 @@
 /**
  * Test_Class class.
  *
+ * Basic test case for use with a WP Integration test test suite.
+ *
  * @package   Google\Web_Stories
- * @copyright 2020 Google LLC
+ * @copyright 2021 Google LLC
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  * @link      https://github.com/google/web-stories-wp
  */
 
 /**
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
