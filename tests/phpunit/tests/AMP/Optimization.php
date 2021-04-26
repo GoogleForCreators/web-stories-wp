c0678532c6f95316c720a1823917e468f2460e33
Jonny Harris <jon@spacedmonkey.co.uk>
2021-04-26 21:29:24 +0100

    Fix test.

--- tests/phpunit/tests/AMP/Optimization.php
+++ tests/phpunit/tests/AMP/Optimization.php
@@ -20,6 +20,7 @@ namespace Google\Web_Stories\Tests\AMP;
 use Google\Web_Stories_Dependencies\AmpProject\Dom\Document;
 use Google\Web_Stories_Dependencies\AmpProject\Optimizer\Configuration;
 use Google\Web_Stories_Dependencies\AmpProject\Optimizer\Transformer\AmpBoilerplate;
+use Google\Web_Stories_Dependencies\AmpProject\Optimizer\Transformer\AmpBoilerplateErrorHandler;
 use Google\Web_Stories_Dependencies\AmpProject\Optimizer\Transformer\ReorderHead;
 use Google\Web_Stories_Dependencies\AmpProject\Optimizer\Transformer\PreloadHeroImage;
 use Google\Web_Stories\Tests\Private_Access;
@@ -99,6 +100,7 @@ class Optimization extends \WP_UnitTestCase {
 
 		$transformers = [
 			AmpBoilerplate::class,
+			AmpBoilerplateErrorHandler::class,
 			ReorderHead::class,
 		];
 
