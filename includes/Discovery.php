6907f81ad989e0af6c82e55779a9b4b204d1b419
Jonny Harris <jon@spacedmonkey.co.uk>
2021-04-27 13:01:23 +0100

    Update to 2.3.1

--- includes/Discovery.php
+++ includes/Discovery.php
@@ -223,7 +223,7 @@ class Discovery extends Service_Base {
 		$metadata = $this->get_open_graph_metadata();
 
 		foreach ( $metadata as $name => $value ) {
-			printf( '<meta property="%s" content="%s" />', esc_attr( $name ), esc_attr( $value ) ); // phpcs:ignore WordPressVIPMinimum.Security.ProperEscapingFunction.notAttrEscAttr
+			printf( '<meta property="%s" content="%s" />', esc_attr( $name ), esc_attr( $value ) );
 		}
 	}
 
@@ -297,7 +297,7 @@ class Discovery extends Service_Base {
 		$metadata = $this->get_twitter_metadata();
 
 		foreach ( $metadata as $name => $value ) {
-			printf( '<meta name="%s" content="%s" />', esc_attr( $name ), esc_attr( $value ) ); // phpcs:ignore WordPressVIPMinimum.Security.ProperEscapingFunction.notAttrEscAttr
+			printf( '<meta name="%s" content="%s" />', esc_attr( $name ), esc_attr( $value ) );
 		}
 	}
 
@@ -371,7 +371,7 @@ class Discovery extends Service_Base {
 		$title = sprintf( $post_type_title, get_bloginfo( 'name' ), $separator, $name );
 
 		printf(
-			'<link rel="alternate" type="%s" title="%s" href="%s">', // phpcs:ignore WordPressVIPMinimum.Security.ProperEscapingFunction.notAttrEscAttr
+			'<link rel="alternate" type="%s" title="%s" href="%s">',
 			esc_attr( feed_content_type() ),
 			esc_attr( $title ),
 			esc_url( $feed_url )
