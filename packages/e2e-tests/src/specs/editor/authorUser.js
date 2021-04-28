056740835ba965286819340470d55a5c33c15b69
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-04-28 16:26:23 -0700

    two more instances of "dismiss" button needing to become "continue to publish"

--- packages/e2e-tests/src/specs/editor/authorUser.js
+++ packages/e2e-tests/src/specs/editor/authorUser.js
@@ -31,6 +31,7 @@ import {
 
 async function publishStory() {
   await expect(page).toClick('button', { text: 'Publish' });
+  await expect(page).toClick('button', { text: 'Continue to publish' });
   await expect(page).toMatch('Story published!');
   await expect(page).toClick('button', { text: 'Dismiss' });
   await expect(page).toMatchElement('button', {
