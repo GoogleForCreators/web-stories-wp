35f17038ce596da147368332f4627d31c25b9dbb
Miina Sikk <miina.sikk@gmail.com>
2021-04-30 09:35:11 +0300

    PR feedback.

--- assets/src/edit-story/elements/utils/duplicatePage.js
+++ assets/src/edit-story/elements/utils/duplicatePage.js
@@ -22,17 +22,14 @@ import { v4 as uuidv4 } from 'uuid';
 /**
  * Internal dependencies
  */
+import objectWithout from '../../utils/objectWithout';
 import createNewElement from './createNewElement';
 
 const duplicatePage = (oldPage) => {
+  const { elements: oldElements, animations: oldAnimations, ...rest } = oldPage;
+
   // Remove title and templateId for inserting the page.
-  const {
-    elements: oldElements,
-    animations: oldAnimations,
-    templateId,
-    title,
-    ...rest
-  } = oldPage;
+  const cleanPage = objectWithout(rest, ['postId', 'title']);
 
   // Ensure all existing elements get new ids
   const elementIdTransferMap = {};
@@ -55,7 +52,7 @@ const duplicatePage = (oldPage) => {
   const newAttributes = {
     elements,
     animations,
-    ...rest,
+    ...cleanPage,
   };
 
   return createNewElement('page', newAttributes);
