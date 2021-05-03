0349fda1313b884eaad0a838ece0778a938ffe3b
Emily Bartman <emily.bartman@formidable.com>
2021-05-03 08:15:47 -0700

    cleanup

--- assets/src/edit-story/components/panels/design/animation/test/effectInput.js
+++ assets/src/edit-story/components/panels/design/animation/test/effectInput.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,9 +19,6 @@
  */
 import { screen } from '@testing-library/react';
 
-/**
- * Internal dependencies
- */
 /**
  * Internal dependencies
  */
