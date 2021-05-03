GoogleForCreators/web-stories-wp

commit 4532d6f3e590d4a4b4bb51a7985814dec9a43f6e

ESLint: Enable import/no-useless-path-segments rule (#7320)
* Implement rule.

* Run lint:js:fix.

.eslintrc
@@ -79,6 +79,9 @@
     "import/dynamic-import-chunkname": [ "error", {
       "webpackChunknameFormat": "[0-9a-zA-Z-_/.[\\]]+"
     } ],
+    "import/no-useless-path-segments": ["error", {
+      "noUselessIndex": true
+    }],
     "no-restricted-properties": "error",
     "no-return-assign": "error",
     "no-return-await": "error",

packages/commander/src/index.js
@@ -35,7 +35,7 @@ import {
   getCurrentVersionNumber,
   updateVersionNumbers,
   updateCdnUrl,
-} from './utils/index.js';
+} from './utils';
 
 const PLUGIN_DIR = process.cwd();
 const PLUGIN_FILE = 'web-stories.php';

assets/src/animation/parts/pulse/test/index.js
@@ -17,7 +17,7 @@
 /**
  * Internal dependencies
  */
-import { generatePulseKeyframes } from '../index';
+import { generatePulseKeyframes } from '..';
 
 describe('Pulse Effect', () => {
   describe('generatePulseKeyframes', () => {

assets/src/dashboard/app/views/editorSettings/adManagement/googleAdSense/stories/index.js
@@ -22,7 +22,7 @@ import { text } from '@storybook/addon-knobs';
 /**
  * Internal dependencies
  */
-import GoogleAdSense from '../';
+import GoogleAdSense from '..';
 
 export default {
   title: 'Dashboard/Views/EditorSettings/AdManagement/GoogleAdSense',

assets/src/dashboard/app/views/editorSettings/googleAnalytics/stories/index.js
@@ -23,7 +23,7 @@ import { boolean, text } from '@storybook/addon-knobs';
 /**
  * Internal dependencies
  */
-import GoogleAnalyticsSettings from '../';
+import GoogleAnalyticsSettings from '..';
 
 export default {
   title: 'Dashboard/Views/EditorSettings/GoogleAnalytics',

assets/src/dashboard/app/views/editorSettings/mediaOptimization/stories/index.js
@@ -22,7 +22,7 @@ import { boolean } from '@storybook/addon-knobs';
 /**
  * Internal dependencies
  */
-import MediaOptimizationSettings from '../';
+import MediaOptimizationSettings from '..';
 
 export default {
   title: 'Dashboard/Views/EditorSettings/MediaOptimization',

assets/src/dashboard/app/views/editorSettings/publisherLogo/stories/index.js
@@ -26,7 +26,7 @@ import { action } from '@storybook/addon-actions';
  */
 import formattedPublisherLogos from '../../../../../dataUtils/formattedPublisherLogos';
 import { getResourceFromLocalFile } from '../../../../../utils';
-import PublisherLogoSettings from '../';
+import PublisherLogoSettings from '..';
 
 export default {
   title: 'Dashboard/Views/EditorSettings/PublisherLogo',

assets/src/dashboard/app/views/editorSettings/telemetry/stories/index.js
@@ -22,7 +22,7 @@ import { boolean } from '@storybook/addon-knobs';
 /**
  * Internal dependencies
  */
-import TelemetrySettings from '../';
+import TelemetrySettings from '..';
 
 export default {
   title: 'Dashboard/Views/EditorSettings/Telemetry',

commit c2d994a5d86b3b47c1e48f5f66bbcaeea2fcfb8b

Use full paths for native ESM imports
Fixes regression introduced in #7320 that broke the repo’s commander CLI utility (e.g. `npm run workflow:version`).

Native ESM imports in Node need full paths, including file name. Just specifying a directory is not enough.

.eslintrc
@@ -369,6 +369,18 @@
     },
     {
       "files": [
+        "packages/commander/**/*.js",
+        "packages/fonts/src/cli.js",
+        "packages/migration/src/cli.js"
+      ],
+      "rules": {
+        "import/no-useless-path-segments": ["error", {
+          "noUselessIndex": false
+        }]
+      }
+    },
+    {
+      "files": [
         "__mocks__/**/*.js",
         "bin/**/*.js",
         "babel.config.cjs",

packages/commander/src/index.js
@@ -35,7 +35,7 @@ import {
   getCurrentVersionNumber,
   updateVersionNumbers,
   updateCdnUrl,
-} from './utils';
+} from './utils/index.js';
 
 const PLUGIN_DIR = process.cwd();
 const PLUGIN_FILE = 'web-stories.php';