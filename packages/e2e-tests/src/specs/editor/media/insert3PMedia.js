22a43d0fd91e54f8c8ad2942d277a4b7789fcede
Jonny Harris <jon@spacedmonkey.co.uk>
2021-05-06 16:19:04 +0100

    Add check for window.crossOriginIsolated

--- packages/e2e-tests/src/specs/editor/media/insert3PMedia.js
+++ packages/e2e-tests/src/specs/editor/media/insert3PMedia.js
@@ -22,6 +22,11 @@ describe('Inserting 3P Media', () => {
   it('should dismiss message', async () => {
     await createNewStory();
 
+    const crossOriginIsolated = await page.evaluate(
+      () => window.crossOriginIsolated
+    );
+    expect(crossOriginIsolated).toBeTrue();
+
     await expect(page).not.toMatchElement('[data-testid="FrameElement"]');
     await expect(page).toClick('#library-tab-media3p');
     await expect(page).toClick('button', { text: 'Dismiss' });
