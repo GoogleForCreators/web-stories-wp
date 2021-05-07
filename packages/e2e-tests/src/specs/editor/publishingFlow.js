eb7c83888b6e1efab9a80353a3b631b17b0d41c3
Jonny Harris <spacedmonkey@users.noreply.github.com>
2021-05-07 13:56:29 +0100

    E2E Tests: Temporarily disable flaky tests, pending further investigation (#7414)

--- packages/e2e-tests/src/specs/editor/publishingFlow.js
+++ packages/e2e-tests/src/specs/editor/publishingFlow.js
@@ -33,7 +33,9 @@ import {
   insertStoryTitle,
 } from '@web-stories-wp/e2e-test-utils';
 
-describe('Publishing Flow', () => {
+// Disable for https://github.com/google/web-stories-wp/issues/6238
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip('Publishing Flow', () => {
   let stopRequestInterception;
 
   beforeAll(async () => {
