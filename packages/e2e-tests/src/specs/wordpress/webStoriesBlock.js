8aaff5a84cc5e30e7bed936f8bf9ffede13d0bfa
Jonny Harris <spacedmonkey@users.noreply.github.com>
2021-05-07 13:30:59 +0100

    Add comment

--- packages/e2e-tests/src/specs/wordpress/webStoriesBlock.js
+++ packages/e2e-tests/src/specs/wordpress/webStoriesBlock.js
@@ -110,6 +110,7 @@ describe('Web Stories Block', () => {
     await expect(page).toMatchElement('amp-story-player');
     await expect(page).toMatch('Embed Settings');
   });
+  // Disable for https://github.com/google/web-stories-wp/issues/6237
   // eslint-disable-next-line jest/no-disabled-tests
   describe.skip('AMP validation', () => {
     withDisabledToolbarOnFrontend();
