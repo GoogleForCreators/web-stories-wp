cf8042768922331f16b797787c87f9471100244e
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-05-11 10:18:19 -0700

    restore padding that i didnt think did anything in storybook but in dashboard is needed

--- assets/src/design-system/components/contextMenu/menu.js
+++ assets/src/design-system/components/contextMenu/menu.js
@@ -35,7 +35,7 @@ const SEPARATOR_BOTTOM_CLASS = 'separatorBottom';
 
 const MenuWrapper = styled.div(
   ({ theme }) => css`
-    padding: 0;
+    padding: ${({ isIconMenu }) => (isIconMenu ? 0 : '5px 0 0')};
     background-color: ${theme.colors.bg.primary};
     pointer-events: none;
     border-radius: ${theme.borders.radius.small};
