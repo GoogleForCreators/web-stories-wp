58f3d9d26bfe293adc282f6acc56161c59f5bcd8
Maxwell Yinger <max.yinger@formidable.com>
2021-05-19 15:29:08 -0600

    reverse directions

--- assets/src/animation/effects/zoom/index.js
+++ assets/src/animation/effects/zoom/index.js
@@ -27,7 +27,7 @@ export function EffectZoom({
   easing,
 }) {
   return AnimationZoom({
-    zoomFrom: scaleDirection === SCALE_DIRECTION.SCALE_OUT ? 1 / 3 : 3,
+    zoomFrom: scaleDirection === SCALE_DIRECTION.SCALE_OUT ? 3 : 1 / 3,
     zoomTo: 1,
     duration,
     delay,
