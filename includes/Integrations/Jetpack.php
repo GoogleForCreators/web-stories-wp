b6640130f238ca998caf061cbfc53c2091a97650
Jonny Harris <jon@spacedmonkey.co.uk>
2021-05-25 12:23:07 +0100

    Add comment.

--- includes/Integrations/Jetpack.php
+++ includes/Integrations/Jetpack.php
@@ -131,6 +131,7 @@ class Jetpack extends Service_Base {
 		$allowed_mime_types = $this->get_allowed_mime_types();
 		$allowed_mime_types = array_merge( ...array_values( $allowed_mime_types ) );
 		if ( in_array( self::VIDEOPRESS_MIME_TYPE, $args['post_mime_type'], true ) && ! array_diff( $allowed_mime_types, $args['post_mime_type'] ) ) {
+			// Load filter at 15, so it load after Media\Media\wp_prepare_attachment_for_js which is loaded at 10.
 			add_filter( 'wp_prepare_attachment_for_js', [ $this, 'filter_admin_ajax_response' ], 15, 2 );
 		}
 
