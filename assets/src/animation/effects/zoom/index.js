c2afd0da6c9e330d0e148e3ab92ade671a0ac30c
Max Yinger <35983235+littlemilkstudio@users.noreply.github.com>
2021-05-31 05:30:48 -0600

    Animations: Switch zoom-in/out behavior (#7612)

--- assets/src/animation/effects/zoom/index.js
+++ assets/src/animation/effects/zoom/index.js
@@ -27,7 +27,7 @@ export function EffectZoom({
   easing = 'cubic-bezier(.3,0,.55,1)',
 }) {
   return AnimationZoom({
-    zoomFrom: scaleDirection === SCALE_DIRECTION.SCALE_OUT ? 1 / 3 : 3,
+    zoomFrom: scaleDirection === SCALE_DIRECTION.SCALE_OUT ? 3 : 1 / 3,
     zoomTo: 1,
     duration,
     delay,
