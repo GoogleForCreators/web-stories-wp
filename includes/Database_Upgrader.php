11d92ef9cf32adf21167a1c7a9db112a874a1566
Pascal Birchler <pascalb@google.com>
2021-06-09 20:59:18 +0200

    Add missing comma

--- includes/Database_Upgrader.php
+++ includes/Database_Upgrader.php
@@ -70,7 +70,7 @@ class Database_Upgrader extends Service_Base {
 		'3.0.5'  => Migrations\Remove_Unneeded_Attachment_Meta::class,
 		'3.0.6'  => Migrations\Add_Media_Source_Video_Optimization::class,
 		'3.0.7'  => Migrations\Add_Media_Source_Source_Video::class,
-		'3.0.8'  => Migrations\Rewrite_Flush::class
+		'3.0.8'  => Migrations\Rewrite_Flush::class,
 		'3.0.9'  => Migrations\Add_VideoPress_Poster_Generation_Media_Source::class,
 		'3.0.10' => Migrations\Add_Media_Source_Gif_Conversion::class,
 	];
