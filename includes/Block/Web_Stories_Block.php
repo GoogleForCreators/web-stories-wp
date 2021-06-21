e335270c562c09fcfd7d239887adfe5bbf2355e3
Jonny Harris <jon@spacedmonkey.co.uk>
2021-06-21 15:30:56 +0100

    Make it so javascript doesn't have to be built to run PHPunit tests.

--- includes/Block/Web_Stories_Block.php
+++ includes/Block/Web_Stories_Block.php
@@ -87,7 +87,11 @@ class Web_Stories_Block extends Embed_Base {
 			$this->get_script_settings()
 		);
 
-		$base_path = $this->assets->get_base_path( 'assets/js/block.json' );
+		$base_path = $this->get_base_path( 'assets/js/block.json' );
+		if ( ! $base_path ) {
+			$base_path = $this->get_base_path( 'assets/src/web-stories-block/block/block.json' );
+		}
+
 		// Note: does not use 'script' and 'style' args, and instead uses 'render_callback'
 		// to enqueue these assets only when needed.
 		register_block_type_from_metadata(
@@ -139,6 +143,24 @@ class Web_Stories_Block extends Embed_Base {
 		 * @param array $settings Array of settings passed to web stories block.
 		 */
 		return apply_filters( 'web_stories_block_settings', $settings );
+	}
+
+	/**
+	 * Get path to json file and check if file exists.
+	 *
+	 * @since 1.9.0
+	 *
+	 * @param string $path Path to file.
+	 *
+	 * @return false|string
+	 */
+	protected function get_base_path( $path ) {
+		$base_path = $this->assets->get_base_path( $path );
+		if ( ! is_readable( $base_path ) ) {
+			return false;
+		}
+
+		return $base_path;
 	}
 
 	/**
