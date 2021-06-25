7562873328ec223b8d7be92261e9693d3202a09d
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-06-25 14:13:02 -0700

    adjusting the 2 popup toggle buttons for help center and checklist to be less wide to fix the immediate overlap issue. TODO #8086 to address truly responsive grid of the entire carousel. New component in edit-story called `ToggleButton`, since it is specific to the editor i didnt put it in the design system.  Now these buttons have tooltips and no caret on toggle.

---
+++ assets/src/edit-story/components/toggleButton/stories/index.js
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { action } from '@storybook/addon-actions';
+import { boolean, number, text } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+/**
+ * Internal dependencies
+ */
+import { Icons } from '../../../../design-system';
+import { ToggleButton } from '..';
+
+export default {
+  title: 'Stories Editor/Components/ToggleButton',
+};
+
+const Container = styled.div`
+  margin: 20px;
+  display: grid;
+  grid-gap: 8px;
+  padding: 8px;
+  grid-template-columns: 200px 200px;
+  background-color: ${({ theme }) => theme.colors.bg.primary};
+`;
+const HelpCenterIcon = styled(Icons.QuestionMarkOutline)`
+  height: 32px;
+  width: auto;
+  display: block;
+`;
+
+const ChecklistIcon = styled(Icons.Checkbox)`
+  height: 32px;
+  width: auto;
+  display: block;
+`;
+// Override light theme because this component is only set up for dark theme right now given fg and bg coloring
+export const _default = () => (
+  <Container>
+    <ToggleButton
+      label="Help"
+      MainIcon={HelpCenterIcon}
+      onClick={() => action('help center clicked')()}
+      aria-owns="my popupId"
+      aria-label={text('help center aria-label', 'my help center label')}
+      isOpen={boolean('help center is open', false)}
+      notificationCount={number('help center notification count', 0)}
+    />
+    <ToggleButton
+      label="Checklist"
+      MainIcon={ChecklistIcon}
+      onClick={() => action('checklist clicked')()}
+      aria-owns="my popupId"
+      aria-label={text('checklist aria-label', 'my checklist label')}
+      isOpen={boolean('checklist is open', false)}
+      notificationCount={number('checklist notification count', 0)}
+    />
+  </Container>
+);
