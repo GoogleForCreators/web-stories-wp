f5c431b00ea156da5ec2b4083006db03007c96a1
samwhale <samuelestrella226@gmail.com>
2021-06-30 15:55:20 -0600

    Add video optimization checkbox

---
+++ assets/src/edit-story/components/checklist/videoOptimizationCheckbox/index.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * External dependencies
+ */
+import { TranslateWithMarkup, __ } from '@web-stories-wp/i18n';
+import styled from 'styled-components';
+/**
+ * Internal dependencies
+ */
+import { trackClick } from '../../../../../../packages/tracking/src';
+import {
+  Checkbox,
+  Link,
+  Text,
+  THEME_CONSTANTS,
+} from '../../../../design-system';
+import { useConfig, useCurrentUser } from '../../../app';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 16px 0;
+`;
+
+const CheckboxContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DescriptionText = styled(Text).attrs({
+  size: THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.SMALL,
+})`
+  color: ${({ theme }) => theme.colors.fg.secondary};
+  margin-bottom: 16px;
+`;
+
+const CheckboxLabel = styled(DescriptionText)`
+  margin-left: 8px;
+  margin-bottom: 0;
+`;
+
+function VideoOptimizationToggle() {
+  const { dashboardSettingsLink } = useConfig();
+  const { currentUser, toggleWebStoriesMediaOptimization } = useCurrentUser(
+    ({ state, actions }) => ({
+      currentUser: state.currentUser,
+      toggleWebStoriesMediaOptimization:
+        actions.toggleWebStoriesMediaOptimization,
+    })
+  );
+
+  return (
+    <Container>
+      <DescriptionText>
+        {__(
+          'Optimize all videos in the Story to ensure smooth playback.',
+          'web-stories'
+        )}
+      </DescriptionText>
+      <CheckboxContainer>
+        <Checkbox
+          id="automatic-video-optimization-toggle"
+          checked={currentUser?.meta?.web_stories_media_optimization}
+          onChange={toggleWebStoriesMediaOptimization}
+        />
+        <CheckboxLabel
+          forwardedAs="label"
+          htmlFor="automatic-video-optimization-toggle"
+        >
+          <TranslateWithMarkup
+            mapping={{
+              a: (
+                <Link
+                  size={THEME_CONSTANTS.TYPOGRAPHY.PRESET_SIZES.SMALL}
+                  onClick={(evt) =>
+                    trackClick(evt, 'click_video_optimization_settings')
+                  }
+                  href={dashboardSettingsLink}
+                  isBold
+                />
+              ),
+            }}
+          >
+            {__(
+              'Enable automatic optimization. Change this any time in <a>Settings</a>.',
+              'web-stories'
+            )}
+          </TranslateWithMarkup>
+        </CheckboxLabel>
+      </CheckboxContainer>
+    </Container>
+  );
+}
+
+export default VideoOptimizationToggle;
