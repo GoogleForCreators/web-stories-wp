54bfad177d19cfb6fb7de4471205d5ce0a962190
Sreelaya <63154588+Sreelayavuyyuru@users.noreply.github.com>
2021-06-30 18:04:49 +0530

    Update assets/src/edit-story/components/library/test/mediaElement.js
    Co-authored-by: Pascal Birchler <pascal.birchler@gmail.com>

--- assets/src/edit-story/components/library/test/mediaElement.js
+++ assets/src/edit-story/components/library/test/mediaElement.js
@@ -102,7 +102,7 @@ describe('MediaElement', () => {
     };
 
     renderMediaElement(resource, 'local');
-    expect(queryByAriaLabel('More')).not.toBeInTheDocument();
+    expect(screen.queryByAltText('More')).not.toBeInTheDocument();
 
     const element = screen.getByAltText('video :)');
     Simulate.focus(element);
