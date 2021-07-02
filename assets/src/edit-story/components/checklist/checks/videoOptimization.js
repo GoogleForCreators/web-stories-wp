26267bd05d83cb38deb3571c58aede0f68ec29e5
Emily Bartman <emily.bartman@formidable.com>
2021-07-02 14:10:54 -0700

    remove feature flag: bulk video optimization

--- assets/src/edit-story/components/checklist/checks/videoOptimization.js
+++ assets/src/edit-story/components/checklist/checks/videoOptimization.js
@@ -24,7 +24,6 @@ import styled from 'styled-components';
  * Internal dependencies
  */
 import { useCallback, useMemo, useReducer } from 'react';
-import { useFeature } from 'flagged';
 import {
   Button,
   BUTTON_SIZES,
@@ -100,9 +99,6 @@ function reducer(state, action) {
 
 export const BulkVideoOptimization = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const isBulkVideoOptimizationEnabled = useFeature(
-    'enableBulkVideoOptimization'
-  );
   const story = useStory(({ state: storyState }) => storyState);
   const unoptimizedVideos = filterStoryElements(
     story,
@@ -193,16 +189,14 @@ export const BulkVideoOptimization = () => {
     isRendered && (
       <ChecklistCard
         cta={
-          isBulkVideoOptimizationEnabled && (
-            <OptimizeButton
-              type={BUTTON_TYPES.SECONDARY}
-              size={BUTTON_SIZES.SMALL}
-              onClick={handleUpdateVideos}
-              disabled={isTranscoding}
-            >
-              {optimizeButtonCopy}
-            </OptimizeButton>
-          )
+          <OptimizeButton
+            type={BUTTON_TYPES.SECONDARY}
+            size={BUTTON_SIZES.SMALL}
+            onClick={handleUpdateVideos}
+            disabled={isTranscoding}
+          >
+            {optimizeButtonCopy}
+          </OptimizeButton>
         }
         title={title}
         cardType={
