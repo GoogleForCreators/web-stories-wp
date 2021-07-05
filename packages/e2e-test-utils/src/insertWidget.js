26e295acf018dac7ed1e06b33ac443f99908e9e1
Jonny Harris <jon@spacedmonkey.co.uk>
2021-07-05 16:09:46 +0100

    Remove widget workaround for WP 5.3

--- packages/e2e-test-utils/src/insertWidget.js
+++ packages/e2e-test-utils/src/insertWidget.js
@@ -14,17 +14,6 @@
  * limitations under the License.
  */
 async function insertWidget(name) {
-  // Hotfix for WordPress 5.3 where `wpWidgets.l10n.widgetAdded`
-  // is referenced by wp-admin/js/widgets.js, but doesn't actually exist.
-  // See:
-  // https://github.com/WordPress/wordpress-develop/blob/8eb0eb36e026ca5c7f7c8f84b28390709c98089b/src/wp-includes/script-loader.php#L1735-L1747
-  // https://github.com/WordPress/wordpress-develop/blob/8eb0eb36e026ca5c7f7c8f84b28390709c98089b/src/js/_enqueues/admin/widgets.js#L721
-  await page.evaluate(() => {
-    if (window.wpWidgets?.l10n) {
-      window.wpWidgets.l10n.widgetAdded = 'Widget added';
-    }
-  });
-
   await expect(page).toMatch(name);
   await expect(page).toClick('button', { text: 'Add widget: ' + name });
   await expect(page).toClick('button', { text: 'Add Widget' });
