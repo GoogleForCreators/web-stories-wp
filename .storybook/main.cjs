73381306c172ad1c524b4b95ed9e0855f513068f
Pascal Birchler <pascalb@google.com>
2021-07-08 13:54:29 +0200

    Fix storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -19,7 +19,6 @@ module.exports = {
     './stories/**/*.js',
     '../assets/src/dashboard/**/stories/*.@(js|mdx)',
     '../assets/src/edit-story/**/stories/*.@(js|mdx)',
-    '../assets/src/animation/**/stories/*.@(js|mdx)',
     '../packages/activation-notice/src/**/stories/*.@(js|mdx)',
     '../packages/design-system/src/**/stories/*.@(js|mdx)',
   ],
