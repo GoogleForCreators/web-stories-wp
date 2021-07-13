880d27ccd1199734fe36b511769e7ec24e4fa57c
swissspidy <swissspidy@users.noreply.github.com>
2021-07-13 17:00:06 +0000

    Update assets version to 10

--- web-stories.php
+++ web-stories.php
@@ -49,7 +49,7 @@ define( 'WEBSTORIES_PLUGIN_DIR_URL', plugin_dir_url( WEBSTORIES_PLUGIN_FILE ) );
 define( 'WEBSTORIES_ASSETS_URL', WEBSTORIES_PLUGIN_DIR_URL . 'assets' );
 define( 'WEBSTORIES_MINIMUM_PHP_VERSION', '7.0' );
 define( 'WEBSTORIES_MINIMUM_WP_VERSION', '5.5' );
-define( 'WEBSTORIES_CDN_URL', 'https://wp.stories.google/static/main' );
+define( 'WEBSTORIES_CDN_URL', 'https://wp.stories.google/static/10' );
 
 if ( ! defined( 'WEBSTORIES_DEV_MODE' ) ) {
 	define( 'WEBSTORIES_DEV_MODE', false );
