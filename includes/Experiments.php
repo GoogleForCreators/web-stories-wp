30f6dc7efe872b8ffd2f2937a2f4f1ff7316dd03
merapi <marcin@webskill.pl>
2021-07-14 15:08:38 +0200

    Enable eyedropper by default

--- includes/Experiments.php
+++ includes/Experiments.php
@@ -420,6 +420,7 @@ class Experiments extends Service_Base {
 				'label'       => __( 'Eyedropper', 'web-stories' ),
 				'description' => __( 'Enable choosing color using an eyedropper', 'web-stories' ),
 				'group'       => 'editor',
+				'default'     => true,
 			],
 		];
 	}
