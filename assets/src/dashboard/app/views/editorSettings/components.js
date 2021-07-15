001d40d63a57c5d4a6227925960e79485bce0593
Max Yinger <35983235+littlemilkstudio@users.noreply.github.com>
2021-07-15 12:20:25 -0500

    accommodate more borders
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- assets/src/dashboard/app/views/editorSettings/components.js
+++ assets/src/dashboard/app/views/editorSettings/components.js
@@ -109,8 +109,8 @@ export const Error = styled(CenterMutedText)`
 
 export const UploadedContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 100px);
-  grid-auto-rows: 100px;
+  grid-template-columns: repeat(auto-fill, 102px);
+  grid-auto-rows: 102px;
   grid-column-gap: 12px;
   grid-row-gap: 20px;
   padding-bottom: 20px;
