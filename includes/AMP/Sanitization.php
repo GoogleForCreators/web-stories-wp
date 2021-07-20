209cc9d42fde12ed235664fab8de57dea2123b51
Pascal Birchler <pascalb@google.com>
2021-07-20 13:28:21 +0200

    Fix `AMP_Dev_Mode_Sanitizer` usage
    It was missing before

--- includes/AMP/Sanitization.php
+++ includes/AMP/Sanitization.php
@@ -33,6 +33,7 @@ use Google\Web_Stories\Story_Post_Type;
 use Google\Web_Stories\Traits\Publisher;
 use Google\Web_Stories_Dependencies\AMP_Allowed_Tags_Generated;
 use Google\Web_Stories_Dependencies\AMP_Content_Sanitizer;
+use Google\Web_Stories_Dependencies\AMP_Dev_Mode_Sanitizer;
 use Google\Web_Stories_Dependencies\AMP_DOM_Utils;
 use Google\Web_Stories_Dependencies\AMP_Layout_Sanitizer;
 use Google\Web_Stories_Dependencies\AMP_Script_Sanitizer;
@@ -468,7 +469,7 @@ class Sanitization {
 
 			$sanitizers = array_merge(
 				[
-					\AMP_Dev_Mode_Sanitizer::class => [
+					AMP_Dev_Mode_Sanitizer::class => [
 						'element_xpaths' => $dev_mode_xpaths,
 					],
 				],
