3e869f0f3738c0421c77f6eb0faf2e4310c7dec6
Jonny Harris <jon@spacedmonkey.co.uk>
2021-07-21 15:41:29 +0100

    Unskip test.

--- packages/e2e-tests/src/specs/wordpress/blockWidget.js
+++ packages/e2e-tests/src/specs/wordpress/blockWidget.js
@@ -36,9 +36,7 @@ describe('Web Stories Widget Block', () => {
     await deleteWidgets();
   });
 
-  // Disable for https://github.com/google/web-stories-wp/issues/8402
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('should insert a new web stories block', async () => {
+  it('should insert a new web stories block', async () => {
     await visitBlockWidgetScreen();
     await expect(page).toClick('button[aria-label="Add block"]');
     await page.type('.block-editor-inserter__search-input', 'Web Stories');
