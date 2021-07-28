02832ca8e2c1843ad7449df330ca5ced0893c96e
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-07-28 09:14:20 -0700

    use percentage and transform to center svg to make it easier to read.

--- assets/src/dashboard/app/views/myStories/content/storyListItem/components.js
+++ assets/src/dashboard/app/views/myStories/content/storyListItem/components.js
@@ -42,10 +42,9 @@ export const PreviewWrapper = styled.div`
 
 export const LockIcon = styled(Icons.LockClosed)`
   position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   height: 24px;
   display: block;
   margin: auto;
