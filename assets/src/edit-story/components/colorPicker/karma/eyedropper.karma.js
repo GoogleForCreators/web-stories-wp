b34ba74fc0df710a7ed3b71298cfd35e7f51fd94
merapi <marcin@webskill.pl>
2021-08-04 16:24:40 +0200

    Should work without steps options (Moveable is fixed)

--- assets/src/edit-story/components/colorPicker/karma/eyedropper.karma.js
+++ assets/src/edit-story/components/colorPicker/karma/eyedropper.karma.js
@@ -54,7 +54,7 @@ describe('Eyedropper', () => {
     const image = fixture.editor.library.media.item(1);
     const canvas = fixture.editor.canvas.framesLayer.fullbleed;
     await fixture.events.mouse.seq(({ down, moveRel, up }) => [
-      moveRel(image, 1, 1, { steps: 4 }),
+      moveRel(image, 1, 1),
       down(),
       moveRel(canvas, 30, 30),
       up(),
