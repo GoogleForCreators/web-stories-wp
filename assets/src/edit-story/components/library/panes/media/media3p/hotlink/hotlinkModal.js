7acf5e230fb908cb056088c7b020750a846fd243
Miina Sikk <miina.sikk@gmail.com>
2021-08-09 11:11:33 +0300

    Add extension check

--- assets/src/edit-story/components/library/panes/media/media3p/hotlink/hotlinkModal.js
+++ assets/src/edit-story/components/library/panes/media/media3p/hotlink/hotlinkModal.js
@@ -18,7 +18,7 @@
  */
 import { __, sprintf, translateToExclusiveList } from '@web-stories-wp/i18n';
 import { Input } from '@web-stories-wp/design-system';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import styled from 'styled-components';
 
 /**
@@ -26,13 +26,20 @@ import styled from 'styled-components';
  */
 import { useConfig } from '../../../../../../app';
 import Dialog from '../../../../../dialog';
+import useLibrary from '../../../../useLibrary';
 
 const InputWrapper = styled.div`
   margin: 16px 4px;
+  width: 470px;
+  height: 100px;
 `;
 
 function HotlinkModal({ isOpen, onClose }) {
   const { allowedFileTypes } = useConfig();
+  const { insertElement } = useLibrary((state) => ({
+    insertElement: state.actions.insertElement,
+  }));
+
   const [hasError, setHasError] = useState(false);
 
   // @todo We're not really uploading anything here, so should we have a fixed list instead?
@@ -49,19 +56,58 @@ function HotlinkModal({ isOpen, onClose }) {
     __('Invalid link. %s', 'web-stories'),
     description
   );
-  const [link, setLink] = useState('https://');
+  const [link, setLink] = useState(
+    'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/31120612/French-Bulldog-laying-down-in-the-grass.jpg'
+  );
+
+  const getFileInfo = useCallback(
+    (value = link) => {
+      const ext = value.split(/[#?]/)[0].split('.').pop().trim();
+      if (!allowedFileTypes.includes(ext)) {
+        setHasError(true);
+      } else {
+        setHasError(false);
+      }
+      return {
+        ext,
+        type: ['m4v', 'mp4'].includes(ext) ? 'video' : 'image',
+      };
+    },
+    [link, allowedFileTypes]
+  );
+
+  const onInsert = useCallback(() => {
+    /*const img = document.createElement('img');
+    img.crossOrigin = 'anonymous';
+    img.addEventListener('load', function () {
+      console.log(img);
+      console.log(img.naturalHeight, img.naturalWidth);
+    });
+    img.src = link + '?cancel-cache=1';*/
+    if (hasError) {
+      return;
+    }
+    const { type } = getFileInfo();
+    insertElement(type, {
+      resource: { alt: '', id: 'test', width: 300, height: 300 },
+    });
+  }, [insertElement, link, hasError, getFileInfo]);
+
   return (
     <Dialog
       onClose={onClose}
       isOpen={isOpen}
       title={__('Insert link', 'web-stories')}
-      onPrimary={() => console.log('heo')}
+      onPrimary={() => onInsert()}
       primaryText={__('Insert', 'web-stories')}
       secondaryText={__('Cancel', 'web-stories')}
     >
       <InputWrapper>
         <Input
-          onChange={({ target: { value } }) => setLink(value)}
+          onChange={({ target: { value } }) => {
+            setLink(value);
+            getFileInfo(value);
+          }}
           value={link}
           hint={hasError ? error : description}
           hasError={hasError}
