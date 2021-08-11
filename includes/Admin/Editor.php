337f0d66c476db7990dff00008b51f5a825fa41b
Jonny Harris <spacedmonkey@users.noreply.github.com>
2021-08-11 12:24:48 +0100

    Allow hiding 3rd party media tab (#8642)

--- includes/Admin/Editor.php
+++ includes/Admin/Editor.php
@@ -336,6 +336,7 @@ class Editor extends Service_Base {
 				],
 				'version'                      => WEBSTORIES_VERSION,
 				'nonce'                        => $nonce,
+				'showMedia3p'                  => true,
 				'encodeMarkup'                 => $this->decoder->supports_decoding(),
 				'metaBoxes'                    => $this->meta_boxes->get_meta_boxes_per_location(),
 				'ffmpegCoreUrl'                => trailingslashit( WEBSTORIES_CDN_URL ) . 'js/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',
