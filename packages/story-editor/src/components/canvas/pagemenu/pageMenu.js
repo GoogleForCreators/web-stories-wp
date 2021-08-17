9f4f35cf874b5671ac595f386eb3b8d21355f613
John Qiao <sqiao@google.com>
2021-08-17 01:48:58 -0700

    Canvas: disable delete button when there is only one page (#8688)
    * disable delete button when there is only one page
    
    * made the context selector more specific

--- packages/story-editor/src/components/canvas/pagemenu/pageMenu.js
+++ packages/story-editor/src/components/canvas/pagemenu/pageMenu.js
@@ -63,17 +63,19 @@ function PageMenu() {
   const {
     currentPageNumber,
     currentPage,
+    numberOfPages,
     deleteCurrentPage,
     addPage,
     hasAnimations,
   } = useStory(
     ({
-      state: { currentPageNumber, currentPage },
+      state: { currentPageNumber, currentPage, pages },
       actions: { deleteCurrentPage, addPage },
     }) => {
       return {
         currentPageNumber,
         currentPage,
+        numberOfPages: pages.length,
         deleteCurrentPage,
         addPage,
         hasAnimations: currentPage?.animations?.length > 0,
@@ -101,6 +103,7 @@ function PageMenu() {
   );
 
   const isWidePage = pageWidth > 280;
+  const disableDeleteButton = numberOfPages <= 1;
 
   const handleUndo = useCallback(() => undo(), [undo]);
 
@@ -122,6 +125,7 @@ function PageMenu() {
       <CountSpace />
       <PageMenuButton
         title={__('Delete Page', 'web-stories')}
+        disabled={disableDeleteButton}
         onClick={handleDeletePage}
         aria-label={__('Delete Page', 'web-stories')}
       >
