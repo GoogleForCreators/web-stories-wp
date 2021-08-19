1ce0a284dae000b78e997c1bbc59f5feb421abe7
Pascal Birchler <pascalb@google.com>
2021-08-19 16:49:07 +0200

    Display Saved Colors panel for the background

--- packages/story-editor/src/components/inspector/design/getDesignPanelsForSelection.js
+++ packages/story-editor/src/components/inspector/design/getDesignPanelsForSelection.js
@@ -53,17 +53,26 @@ function getDesignPanelsForSelection(elements) {
   }
 
   const isBackground = elements.length === 1 && elements[0].isBackground;
+  const isBackgroundMedia = !elements[0].isDefaultBackground;
 
   // Only display background panel in case of background element.
   if (isBackground) {
     const panels = [];
 
-    const isBackgroundMedia = !elements[0].isDefaultBackground;
-    if (isBackgroundMedia) {
+    if (!isBackgroundMedia) {
       panels.push({
-        type: PanelTypes.PAGE_BACKGROUND,
-        Panel: PageBackgroundPanel,
+        type: PanelTypes.COLOR_PRESETS,
+        Panel: ColorPresetPanel,
       });
+    }
+
+    // The Page Background panel is shown for all types of backgrounds.
+    panels.push({
+      type: PanelTypes.PAGE_BACKGROUND,
+      Panel: PageBackgroundPanel,
+    });
+
+    if (isBackgroundMedia) {
       panels.push({
         type: PanelTypes.FILTER,
         Panel: FilterPanel,
@@ -86,12 +95,6 @@ function getDesignPanelsForSelection(elements) {
       panels.push({
         type: PanelTypes.IMAGE_ACCESSIBILITY,
         Panel: ImageAccessibilityPanel,
-      });
-      // In case of default background without media:
-    } else {
-      panels.unshift({
-        type: PanelTypes.PAGE_BACKGROUND,
-        Panel: PageBackgroundPanel,
       });
     }
 
