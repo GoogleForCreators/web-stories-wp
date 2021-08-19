5e21d7d7867aa904a9351eeca47c39f59d1b6e8a
William Chou <willchou@google.com>
2021-08-19 19:35:19 -0400

    Skip flaky text pane tests.

--- packages/story-editor/src/components/library/panes/text/karma/textPane.karma.js
+++ packages/story-editor/src/components/library/panes/text/karma/textPane.karma.js
@@ -66,7 +66,8 @@ describe('CUJ: Creator can Add and Write Text: Consecutive text presets', () => 
       await waitFor(() => fixture.editor.canvas.framesLayer.frames[0].node);
     });
 
-    it('should add text presets below each other if added consecutively', async () => {
+    // TODO(#8740): Fix flaky test.
+    it.skip('should add text presets below each other if added consecutively', async () => {
       await fixture.events.mouse.moveRel(
         fixture.editor.library.text.preset('Title 1'),
         10,
@@ -79,6 +80,7 @@ describe('CUJ: Creator can Add and Write Text: Consecutive text presets', () => 
       );
 
       await addPreset('Title 1');
+      // TODO(#8740): Seems like this line is flaky.
       await waitFor(() => fixture.editor.canvas.framesLayer.frames[1].node);
       await addPreset('Title 3');
       await waitFor(() => fixture.editor.canvas.framesLayer.frames[2].node);
@@ -88,7 +90,8 @@ describe('CUJ: Creator can Add and Write Text: Consecutive text presets', () => 
       await fixture.snapshot('consecutively added different text presets');
     });
 
-    it('should ensure staggered presets fit on the page', async () => {
+    // TODO(#8740): Fix flaky test.
+    it.skip('should ensure staggered presets fit on the page', async () => {
       const POSITION_MARGIN = dataFontEm(1);
       const PARAGRAPH_TEXT =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
@@ -100,6 +103,7 @@ describe('CUJ: Creator can Add and Write Text: Consecutive text presets', () => 
       let nodeIndex = 1;
 
       const verifyDefaultPosition = async (name, content) => {
+        // TODO(#8740): Seems like this line is flaky.
         await waitFor(
           () => fixture.editor.canvas.framesLayer.frames[nodeIndex].node
         );
