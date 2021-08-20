b565d57a670639accdfda58adfc99aec3ba11756
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-08-20 09:04:44 -0700

    rebase catch up

--- packages/story-editor/src/components/keyboardShortcutsMenu/shortcutMenuSection.js
+++ packages/story-editor/src/components/keyboardShortcutsMenu/shortcutMenuSection.js
@@ -17,7 +17,6 @@
 /**
  * External dependencies
  */
-import { Fragment } from '@web-stories-wp/react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
