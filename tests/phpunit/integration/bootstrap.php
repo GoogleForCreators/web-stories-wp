9efda04ab3dd6b764da695bee4048404c5bf32ba
Pascal Birchler <pascalb@google.com>
2021-09-06 15:20:24 +0200

    Use bootstrap utils from `yoast/wp-test-utils`

--- tests/phpunit/integration/bootstrap.php
+++ tests/phpunit/integration/bootstrap.php
@@ -25,22 +25,14 @@
  */
 
 use DG\BypassFinals;
-
 BypassFinals::enable();
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+require_once dirname( __DIR__, 3 ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
+$_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once $_tests_dir . 'includes/functions.php';
 
 /**
  * Manually load the plugin being tested.
@@ -51,8 +43,7 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+Yoast\WPTestUtils\WPIntegration\bootstrap_it();
 
 define( 'WEB_STORIES_TEST_DATA_DIR', __DIR__ . '/data' );
 
