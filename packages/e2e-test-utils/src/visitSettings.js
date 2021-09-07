8cbe1a64d4a3563c78d1cbac7af3587786a3a8d1
Pascal Birchler <pascalb@google.com>
2021-09-07 13:12:53 +0200

    Avoid using `page.url()`

--- packages/e2e-test-utils/src/visitSettings.js
+++ packages/e2e-test-utils/src/visitSettings.js
@@ -33,7 +33,11 @@ async function visitSettings() {
     text: 'Settings',
   });
 
-  expect(page.url()).toContain('#/editor-settings');
+  // Not using page.url() as it is buggy on Firefox.
+  // See https://github.com/puppeteer/puppeteer/issues/6787.
+  expect(await page.evaluate(() => location.href)).toContain(
+    '#/editor-settings'
+  );
 
   await expect(page).toMatchElement('h2', { text: 'Settings' });
   await expect(page).toMatch('Data Sharing Opt-in');
