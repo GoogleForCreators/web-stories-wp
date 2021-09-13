bdd23b33958fb5ae5850c518fd37d91f156adff9
samwhale <samuelestrella226@gmail.com>
2021-09-13 11:54:34 -0600

    No need for optional chaining

--- packages/story-editor/src/components/form/hierarchical/utils/index.js
+++ packages/story-editor/src/components/form/hierarchical/utils/index.js
@@ -112,7 +112,7 @@ export const filterOption = (option, labelText = '') => {
   // Find if current option matches
   const matchesCurrentOption = option.label
     .toLowerCase()
-    .includes(labelText?.trim().toLowerCase());
+    .includes(labelText.trim().toLowerCase());
 
   // create new option with new children
   const newOption = { ...rest, options: newChildren };
