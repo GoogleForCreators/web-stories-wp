dfe923314d482565616802612de7f3486588fcd7
Pascal Birchler <pascalb@google.com>
2021-09-15 10:25:50 +0200

    Lint fix after merge

--- includes/templates/admin/edit-story.php
+++ includes/templates/admin/edit-story.php
@@ -111,7 +111,7 @@ $preload_paths = [
 	'/web-stories/v1/users/me/',
 	'/web-stories/v1/taxonomies/?' . build_query(
 		[
-			'type' => $post_type_object->name
+			'type' => $post_type_object->name,
 		]
 	),
 ];
