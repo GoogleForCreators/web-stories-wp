a66f569299fbbd5039a3aaa95f80acda637c1a85
Miina Sikk <miina.sikk@gmail.com>
2021-09-17 15:03:15 +0300

    Start adding Moveable for Presets

--- packages/story-editor/src/components/library/panes/text/fontPreview.js
+++ packages/story-editor/src/components/library/panes/text/fontPreview.js
@@ -28,23 +28,26 @@ import {
 import { Text } from '@web-stories-wp/design-system';
 import { useFeature } from 'flagged';
 import { trackEvent } from '@web-stories-wp/tracking';
+import { useUnits } from '@web-stories-wp/units';
 
 /**
  * Internal dependencies
  */
-import { useFont, useHistory } from '../../../app';
-import StoryPropTypes from '../../../types';
-import stripHTML from '../../../utils/stripHTML';
-import { focusStyle } from '../../panels/shared';
-import usePageAsCanvas from '../../../utils/usePageAsCanvas';
-import useLibrary from '../useLibrary';
+import { useFont, useHistory } from '../../../../app';
+import StoryPropTypes from '../../../../types';
+import stripHTML from '../../../../utils/stripHTML';
+import { focusStyle } from '../../../panels/shared';
+import usePageAsCanvas from '../../../../utils/usePageAsCanvas';
+import useLibrary from '../../useLibrary';
+import LibraryMoveable from '../shared/libraryMoveable';
 
 const Preview = styled.button`
+  position: relative;
   display: flex;
   align-items: center;
   background-color: ${({ theme }) =>
     theme.colors.interactiveBg.secondaryNormal};
-  padding: 8px 16px;
+  padding: 8px 0px;
   border-radius: ${({ theme }) => theme.borders.radius.small};
   width: 100%;
   border: none;
@@ -65,6 +68,15 @@ const PreviewText = styled(Text).attrs({ forwardedAs: 'span' })`
   font-weight: ${({ fontWeight }) => fontWeight};
   font-family: ${({ fontFamily }) => fontFamily};
   line-height: normal;
+  margin: 0px 16px;
+`;
+
+const DragContainer = styled.div`
+  position: absolute;
+  opacity: 0;
+  background-color: ${({ theme }) => theme.colors.opacity.white24};
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
 `;
 
 function FontPreview({ title, element, insertPreset, getPosition }) {
@@ -77,6 +89,11 @@ function FontPreview({ title, element, insertPreset, getPosition }) {
     state: { versionNumber },
   } = useHistory();
 
+  const { dataToEditorX, dataToEditorY } = useUnits((state) => ({
+    dataToEditorX: state.actions.dataToEditorX,
+    dataToEditorY: state.actions.dataToEditorY,
+  }));
+
   const { pageCanvasData } = useLibrary((state) => ({
     pageCanvasData: state.state.pageCanvasData,
   }));
@@ -85,6 +102,7 @@ function FontPreview({ title, element, insertPreset, getPosition }) {
   const enableSmartTextColor = useFeature('enableSmartTextColor');
 
   const presetDataRef = useRef({});
+  const previewRef = useRef(null);
 
   useEffect(() => {
     maybeEnqueueFontStyle([
@@ -150,11 +168,39 @@ function FontPreview({ title, element, insertPreset, getPosition }) {
     trackEvent('insert_text_preset', { name: title });
   }, [insertPreset, element, title]);
 
-  return (
-    <Preview onClick={onClick}>
-      <PreviewText font={font} fontSize={fontSize} fontWeight={fontWeight}>
+  const getTextDisplay = (props = {}) => {
+    return (
+      <PreviewText
+        font={font}
+        fontSize={fontSize}
+        fontWeight={fontWeight}
+        {...props}
+      >
         {title}
       </PreviewText>
+    );
+  };
+
+  const sizeProps = {};
+  if (previewRef.current) {
+    const { width, height } = previewRef.current.getBoundingClientRect();
+    sizeProps.width = dataToEditorX(width);
+    sizeProps.height = dataToEditorY(height);
+  }
+  return (
+    <Preview ref={previewRef} onClick={onClick}>
+      {getTextDisplay()}
+      {previewRef.current && (
+        <LibraryMoveable
+          cloneElement={DragContainer}
+          cloneProps={{
+            children: getTextDisplay({ fontSize: dataToEditorY(fontSize) }),
+            ...sizeProps,
+          }}
+          elementProps={element}
+          type={'text'}
+        />
+      )}
     </Preview>
   );
 }
