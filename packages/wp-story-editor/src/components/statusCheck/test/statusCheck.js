5993a37c3b36a5e5b47ddc45f2c7fe8e1691baae
Sayed Taqui <sayedwp@gmail.com>
2021-09-17 22:16:34 +0530

    Update StatusCheck unit test according to the new changes

--- packages/wp-story-editor/src/components/statusCheck/test/statusCheck.js
+++ packages/wp-story-editor/src/components/statusCheck/test/statusCheck.js
@@ -22,28 +22,22 @@ import {
   waitForElementToBeRemoved,
 } from '@testing-library/react';
 import { setAppElement } from '@web-stories-wp/design-system';
-import {
-  renderWithTheme,
-  ConfigContext,
-  APIContext,
-} from '@web-stories-wp/story-editor';
+import { renderWithTheme, ConfigContext } from '@web-stories-wp/story-editor';
 
 /**
  * Internal dependencies
  */
 import StatusCheck from '../statusCheck';
 
-function setup(response) {
-  const configValue = { api: { statusCheck: '' }, metadata: { publisher: '' } };
+jest.mock('../../../api/statusCheck');
+import { getStatusCheck } from '../../../api/statusCheck';
 
-  const getStatusCheck = () => response;
-  const apiValue = { actions: { getStatusCheck } };
+function render() {
+  const configValue = { api: { statusCheck: '' }, metadata: { publisher: '' } };
 
   return renderWithTheme(
     <ConfigContext.Provider value={configValue}>
-      <APIContext.Provider value={apiValue}>
-        <StatusCheck />
-      </APIContext.Provider>
+      <StatusCheck />
     </ConfigContext.Provider>
   );
 }
@@ -61,8 +55,10 @@ describe('statusCheck', () => {
     document.documentElement.removeChild(modalWrapper);
   });
 
-  it('should do nothing if successfull', async () => {
-    setup(Promise.resolve({ success: true }));
+  it('should do nothing if successful', async () => {
+    getStatusCheck.mockReturnValue(Promise.resolve({ success: true }));
+
+    render();
 
     // This seems to be the best way to validate, that a certain
     // element does *not* appear. Not very elegant, though.
@@ -72,7 +68,9 @@ describe('statusCheck', () => {
   });
 
   it('should display dismissible dialog if failed', async () => {
-    setup(Promise.reject(new Error('api failed')));
+    getStatusCheck.mockReturnValue(Promise.reject(new Error('api failed')));
+
+    render();
 
     const dialog = await screen.findByRole('dialog');
     expect(dialog).toBeInTheDocument();
