2f367cfb312d7425166ef187299b6cb17a108a0b
merapi <marcin@webskill.pl>
2021-09-20 22:00:52 +0200

    Test fail reason

--- packages/e2e-tests/src/specs/editor/inspector.js
+++ packages/e2e-tests/src/specs/editor/inspector.js
@@ -20,8 +20,7 @@ import { createNewStory, withUser } from '@web-stories-wp/e2e-test-utils';
 
 describe('Inspector', () => {
   describe('Document', () => {
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip('should display publisher logo upload', async () => {
+    it('should display publisher logo upload', async () => {
       await createNewStory();
 
       await expect(page).toClick('li[role="tab"]', { text: 'Document' });
