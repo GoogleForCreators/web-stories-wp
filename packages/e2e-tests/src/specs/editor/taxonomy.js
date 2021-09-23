bec48d880be3ea302f2bfd4f1a556fabc2fff85f
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-09-23 14:37:53 -0700

    try to be more specific on id?

--- packages/e2e-tests/src/specs/editor/taxonomy.js
+++ packages/e2e-tests/src/specs/editor/taxonomy.js
@@ -42,9 +42,9 @@ describe('Taxonomy', () => {
     await expect(page).toClick('li[role="tab"]', { text: 'Document' });
     await expect(page).toMatch('Categories and Tags');
     // Toggle the panel which is collapsed by default.
-    // await expect(page).toClick('[aria-label="Categories and Tags"]');
-    await page.waitForSelector('#expand_add_new_hierarchical_term');
-    await expect(page).toClick('#expand_add_new_hierarchical_term');
+    await expect(page).toClick('[aria-label="Categories and Tags"]');
+    await page.waitForSelector('button#expand_add_new_hierarchical_term');
+    await expect(page).toClick('button#expand_add_new_hierarchical_term');
     await page.waitForSelector('input[name="New Category Name"]');
     // add a parent category
     await page.type('input[name="New Category Name"]', 'music genres');
