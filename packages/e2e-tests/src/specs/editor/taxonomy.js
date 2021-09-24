fca50683af7aecc780953b083bda49d9c0fde9b2
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-09-24 13:03:17 -0700

    withExperiment hook keeps timing out so trying without

--- packages/e2e-tests/src/specs/editor/taxonomy.js
+++ packages/e2e-tests/src/specs/editor/taxonomy.js
@@ -16,16 +16,10 @@
 /**
  * External dependencies
  */
-import {
-  createNewStory,
-  withExperimentalFeatures,
-  withUser,
-} from '@web-stories-wp/e2e-test-utils';
+import { createNewStory, withUser } from '@web-stories-wp/e2e-test-utils';
 import percySnapshot from '@percy/puppeteer';
 
 describe('Taxonomy', () => {
-  withExperimentalFeatures(['enableTaxonomiesSupport']);
-
   const goToAndExpandTaxonomyPanel = async () => {
     await createNewStory();
     await expect(page).toClick('li[role="tab"]', { text: 'Document' });
