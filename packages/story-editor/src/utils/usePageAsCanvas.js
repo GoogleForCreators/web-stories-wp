cdddd1066152b306a5d6af7cf78a6121e87bd74e
Miina Sikk <miina.sikk@gmail.com>
2021-10-18 11:46:20 +0300

    Add catch.

--- packages/story-editor/src/utils/usePageAsCanvas.js
+++ packages/story-editor/src/utils/usePageAsCanvas.js
@@ -93,16 +93,16 @@ function usePageAsCanvas() {
         };
         // If we haven't started the generation yet, create a promise.
         if (!pageCanvasPromise) {
-          import(
-            /* webpackChunkName: "chunk-html-to-image" */ 'html-to-image'
-          ).then((htmlToImage) => {
-            const promise = htmlToImage.toCanvas(fullbleedContainer, {
-              fontEmbedCss: '',
-              pixelRatio: 1,
-            });
-            setPageCanvasPromise(promise);
-            promise.then(onCompletion).catch(onFail);
-          });
+          import(/* webpackChunkName: "chunk-html-to-image" */ 'html-to-image')
+            .then((htmlToImage) => {
+              const promise = htmlToImage.toCanvas(fullbleedContainer, {
+                fontEmbedCss: '',
+                pixelRatio: 1,
+              });
+              setPageCanvasPromise(promise);
+              promise.then(onCompletion).catch(onFail);
+            })
+            .catch(() => {});
         } else if (resetZoom) {
           // If we already have a promise and we're resetting zoom, we're inserting an element.
           // This means that the callback might have changed and we need to handle the promise again.
