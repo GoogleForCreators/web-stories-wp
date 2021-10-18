caa57fcf9564256f1f0be97621e9e30667da7d95
Sayed Taqui <sayedwp@gmail.com>
2021-10-18 06:55:01 +0530

    Create a separate file for EditorSettings component

--- packages/dashboard/src/app/views/index.js
+++ packages/dashboard/src/app/views/index.js
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 export { default as MyStoriesView } from './myStories';
-export { default as EditorSettingsView } from './editorSettings';
+export { EditorSettings as EditorSettingsView } from './editorSettings';
 export { default as ExploreTemplatesView } from './exploreTemplates';
 export { default as TemplateDetailsView } from './templateDetails';
