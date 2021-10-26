5fffd2a4455f2002421679c0295ae3b1b960ff78
Sayed Taqui <sayedwp@gmail.com>
2021-10-26 21:13:25 +0530

    Revert previous commit

--- packages/e2e-tests/src/specs/dashboard/telemetryBanner.js
+++ packages/e2e-tests/src/specs/dashboard/telemetryBanner.js
@@ -68,9 +68,9 @@ describe('Telemetry Banner', () => {
   });
 
   it('should keep focus on the checkbox when checking/unchecking via keyboard', async () => {
-    const checkbox = await expect(page).toMatchElement(
-      '#telemetry-banner-opt-in'
-    );
+    await page.waitForTimeout(300); // Because dashboard takes some time before banner appears.
+    const checkbox = await page.$('#telemetry-banner-opt-in');
+
     await checkbox.focus();
     await page.keyboard.press('Space');
     const optedIn = await page.evaluate(() => {
