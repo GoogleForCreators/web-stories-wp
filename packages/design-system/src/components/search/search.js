848364487d27e4ad32e0c0b957cd2ce86c358177
Pascal Birchler <pascalb@google.com>
2021-10-27 00:27:27 +0200

    Fix incorrect comparison

--- packages/design-system/src/components/search/search.js
+++ packages/design-system/src/components/search/search.js
@@ -108,7 +108,7 @@ export const Search = ({
   });
 
   const isMenuHidden = useMemo(
-    () => Boolean(disabled || !inputState?.value?.length === 0),
+    () => Boolean(disabled || inputState?.value?.length === 0),
     [disabled, inputState]
   );
 
