e05f7e32edf6f437c140816b260adb976884cfcb
Morten Barklund <morten.barklund@xwp.co>
2021-11-03 01:01:14 -0400

    Run all tests again

--- packages/story-editor/src/components/library/panes/text/karma/textPane.karma.js
+++ packages/story-editor/src/components/library/panes/text/karma/textPane.karma.js
@@ -204,7 +204,7 @@ describe('CUJ: Creator can Add and Write Text: Consecutive text presets', () => 
   };
 
   describe('Easier/smarter text color', () => {
-    fit('should add text color based on background', async () => {
+    it('should add text color based on background', async () => {
       // Enable the smart colors first.
       await fixture.editor.library.textTab.click();
       fixture.editor.library.text.smartColorToggle.click();
