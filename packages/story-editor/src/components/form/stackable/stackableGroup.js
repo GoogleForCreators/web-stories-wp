351eedec6668c39a2be74f93ae4c611deb097ce3
Jasper <jasper.kinoti@gmail.com>
2021-11-09 13:03:20 +0300

    fix(web-stories-wp): fix border radius for stackable group when there is only one input
    Closes #webstories-9461

--- packages/story-editor/src/components/form/stackable/stackableGroup.js
+++ packages/story-editor/src/components/form/stackable/stackableGroup.js
@@ -35,12 +35,14 @@ const StackableContainer = styled.div`
     border-radius: 0;
 
     &:first-of-type > div {
-      border-radius: 4px 0 0 4px;
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
       margin-left: 0;
     }
 
     &:last-of-type > div {
-      border-radius: 0 4px 4px 0;
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
     }
   }
 `;
