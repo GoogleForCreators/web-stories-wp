e738d66199749b86d30df86344722f6d4d06b18b
Miina Sikk <miina.sikk@gmail.com>
2021-11-11 17:59:26 +0200

    Fix typo causing test failure.

--- packages/story-editor/src/components/checklist/karma/checklist.karma.js
+++ packages/story-editor/src/components/checklist/karma/checklist.karma.js
@@ -47,7 +47,7 @@ describe('Checklist integration', () => {
     let clickCount = 1;
     while (clickCount <= count) {
       // eslint-disable-next-line no-await-in-loop
-      await fixture.events.click(fixture.editor.canvas.pageActiona.addPage);
+      await fixture.events.click(fixture.editor.canvas.pageActions.addPage);
       // eslint-disable-next-line no-await-in-loop, no-loop-func
       await waitFor(() => {
         expect(fixture.editor.carousel.pages.length).toBe(clickCount + 1);
