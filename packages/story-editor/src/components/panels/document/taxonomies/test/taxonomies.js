50eb7f9e9c871c0ed007324364e17665a02ad5f7
Sayed Taqui <sayedwp@gmail.com>
2021-11-12 17:14:41 +0530

    Update taxonomy labels

--- packages/story-editor/src/components/panels/document/taxonomies/test/taxonomies.js
+++ packages/story-editor/src/components/panels/document/taxonomies/test/taxonomies.js
@@ -98,7 +98,7 @@ describe('TaxonomiesPanel', () => {
           slug: 'web_story_category',
           restBase: 'web_story_category',
           name: 'Categories',
-          labels: { not_found: '' },
+          labels: { notFound: '' },
           hierarchical: false,
         },
       ],
@@ -128,9 +128,9 @@ describe('TaxonomiesPanel', () => {
           restBase: 'web_story_category',
           name: 'Categories',
           labels: {
-            search_items: 'Story Categories',
-            add_new_item: 'Add New',
-            not_found: '',
+            searchItems: 'Story Categories',
+            addNewItem: 'Add New',
+            notFound: '',
           },
           hierarchical: true,
           visibility: {
