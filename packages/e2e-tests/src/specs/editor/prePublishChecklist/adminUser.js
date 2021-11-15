03a51f7df190a40c217d02ffec8b5d2863262ef7
Ayush Nirwal <53055971+codingcarrots20@users.noreply.github.com>
2021-11-15 19:34:15 +0530

    E2E Tests: Add tests for publish panel (#9695)

---
+++ packages/e2e-tests/src/specs/editor/prePublishChecklist/adminUser.js
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  createNewStory,
+  publishStory,
+  triggerHighPriorityChecklistSection,
+  uploadPublisherLogoEditor,
+} from '@web-stories-wp/e2e-test-utils';
+import percySnapshot from '@percy/puppeteer';
+
+describe('Pre-Publish Checklist : Admin User', () => {
+  const addNewTextElement = async () => {
+    await expect(page).toClick('button[aria-label="Add new text element"]');
+  };
+  const addNewPage = async () => {
+    await expect(page).toClick('button[aria-label="Add New Page"]');
+    await addNewTextElement();
+  };
+  const addPages = async (number) => {
+    for (let i = 0; i < number; i++) {
+      // eslint-disable-next-line no-await-in-loop
+      await addNewPage();
+    }
+  };
+
+  beforeEach(async () => {
+    await createNewStory();
+  });
+
+  it('should show the checklist', async () => {
+    await expect(page).toClick('button[aria-label="Checklist"]');
+    await triggerHighPriorityChecklistSection();
+    await expect(page).toMatchElement(
+      '#pre-publish-checklist[data-isexpanded="true"]'
+    );
+  });
+
+  it('should show that there is no poster attached to the story', async () => {
+    await expect(page).toClick('[data-testid^="mediaElement"]');
+    await expect(page).toMatchElement('[data-testid="imageElement"]');
+
+    await publishStory();
+
+    await page.reload();
+    await expect(page).toMatchElement('input[placeholder="Add title"]');
+
+    await expect(page).toClick('button[aria-label^="Checklist: "]');
+    await expect(page).toMatchElement(
+      '#pre-publish-checklist[data-isexpanded="true"]'
+    );
+    await expect(page).toMatch('Add poster image');
+    await percySnapshot(page, 'Prepublish checklist');
+  });
+
+  it('should show cards related to poster image issues', async () => {
+    await addNewTextElement();
+    await addPages(3);
+
+    await expect(page).toClick('button', { text: 'Publish' });
+    await expect(page).toClick('button', { text: 'Review Checklist' });
+    await expect(page).toMatch('Add poster image');
+
+    await expect(page).toClick('p', { text: 'Document' });
+
+    //find publish panel button
+    const publishPanelButton = await expect(page).toMatchElement(
+      '#inspector-tab-document button',
+      { text: 'Publishing' }
+    );
+
+    const isPublishPanelExpanded = await publishPanelButton.evaluate(
+      (node) => node.getAttribute('aria-expanded') === 'true'
+    );
+
+    //open publish panel if not open
+    if (!isPublishPanelExpanded) {
+      await publishPanelButton.click();
+    }
+    await uploadPublisherLogoEditor('example-1.jpg', false);
+
+    await expect(page).not.toMatch('Add poster image');
+  });
+
+  it('should focus on media button when poster image issue card is clicked', async () => {
+    await addNewTextElement();
+    await addPages(3);
+
+    await expect(page).toClick('button', { text: 'Publish' });
+    await expect(page).toClick('button', { text: 'Review Checklist' });
+    const title = await expect(page).toMatchElement('h2', {
+      text: 'Add poster image',
+    });
+    const button = await title.evaluateHandle((node) => node.parentNode);
+
+    expect(button).toBeDefined();
+
+    await button.click();
+
+    const isMediaPickerInFocus = await page.evaluate(
+      () => document.activeElement.getAttribute('aria-label') === 'Poster image'
+    );
+
+    expect(isMediaPickerInFocus).toBeTrue();
+  });
+});
---
+++ packages/e2e-tests/src/specs/editor/publishPanel/adminUser.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { createNewStory } from '@web-stories-wp/e2e-test-utils';
+
+describe('Publish panel in document tab', () => {
+  const openPublishPanel = async () => {
+    await expect(page).toClick('p', { text: 'Document' });
+
+    //find publish panel button
+    const publishPanelButton = await expect(page).toMatchElement(
+      '#inspector-tab-document button',
+      { text: 'Publishing' }
+    );
+
+    const isPublishPanelExpanded = await publishPanelButton.evaluate(
+      (node) => node.getAttribute('aria-expanded') === 'true'
+    );
+
+    //open publish panel if not open
+    if (!isPublishPanelExpanded) {
+      await publishPanelButton.click();
+    }
+  };
+  beforeEach(async () => {
+    await createNewStory();
+    await openPublishPanel();
+  });
+
+  it('should allow changing author', async () => {
+    const authorDropDownButton = await expect(page).toMatchElement(
+      'button[aria-label="Author"]'
+    );
+    await expect(authorDropDownButton).toMatch('admin');
+
+    await authorDropDownButton.click();
+
+    const authorDropDownOptions = await expect(page).toMatchElement(
+      '[aria-label="Option List Selector"]'
+    );
+
+    await expect(authorDropDownOptions).toClick('li', { text: 'author' });
+
+    await expect(authorDropDownButton).toMatch('author');
+  });
+
+  it('should allow searching author', async () => {
+    const authorDropDownButton = await expect(page).toMatchElement(
+      'button[aria-label="Author"]'
+    );
+    await expect(authorDropDownButton).toMatch('admin');
+
+    await authorDropDownButton.click();
+
+    const authorDropDownOptions = await expect(page).toMatchElement(
+      '[aria-label="Option List Selector"]'
+    );
+
+    const optionListBeforeSearch = await authorDropDownOptions.$$eval(
+      'li[role="option"]',
+      (nodeList) => nodeList.map((node) => node.innerText)
+    );
+    expect(optionListBeforeSearch).toBeDefined();
+    expect(optionListBeforeSearch.length).toBeGreaterThan(1);
+
+    await page.keyboard.type('auth');
+
+    // wait for search results
+    await page.waitForTimeout(200);
+
+    const optionListAfterSearch = await authorDropDownOptions.$$eval(
+      'li[role="option"]',
+      (nodeList) => nodeList.map((node) => node.innerText)
+    );
+
+    expect(optionListAfterSearch).toHaveLength(1);
+
+    await expect(authorDropDownOptions).toClick('li', { text: 'author' });
+    await expect(authorDropDownButton).toMatch('author');
+  });
+});
