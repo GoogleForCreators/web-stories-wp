846a334b53817d95d922b42b4ee356444ee6b738
Morten Barklund <morten.barklund@xwp.co>
2021-11-29 14:30:24 -0500

    Fixed position and layering of page attachment in display layer

--- packages/story-editor/src/components/canvas/layout.js
+++ packages/story-editor/src/components/canvas/layout.js
@@ -136,6 +136,15 @@ const PageAreaContainer = styled(Area).attrs({
         100% - ${hasHorizontalOverflow ? themeHelpers.SCROLLBAR_WIDTH : 0}px
       );
     `}
+
+      overflow: ${({ showOverflow }) => (showOverflow ? 'visible' : 'hidden')};
+      width: calc(
+        100% - ${hasVerticalOverflow ? themeHelpers.SCROLLBAR_WIDTH : 0}px
+      );
+      height: calc(
+        100% - ${hasHorizontalOverflow ? themeHelpers.SCROLLBAR_WIDTH : 0}px
+      );
+    `}
 `;
 
 const Layer = forwardRef(function Layer({ children, ...rest }, ref) {
@@ -177,6 +186,23 @@ const PageClip = styled.div`
       justify-content: center;
       align-items: center;
     `}
+
+      overflow: hidden;
+      width: ${hasHorizontalOverflow
+        ? 'calc(var(--page-width-px) + var(--page-padding-px))'
+        : `calc(var(--viewport-width-px) - ${themeHelpers.SCROLLBAR_WIDTH}px)`};
+      flex-basis: ${hasHorizontalOverflow
+        ? 'calc(var(--page-width-px) + var(--page-padding-px))'
+        : `calc(var(--viewport-width-px) - ${themeHelpers.SCROLLBAR_WIDTH}px)`};
+      height: ${hasVerticalOverflow
+        ? 'calc(var(--fullbleed-height-px) + var(--page-padding-px))'
+        : `calc(var(--viewport-height-px) - ${themeHelpers.SCROLLBAR_WIDTH}px)`};
+      flex-shrink: 0;
+      flex-grow: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
 `;
 
 const FullbleedContainer = styled.div`
@@ -193,6 +219,25 @@ const FullbleedContainer = styled.div`
   ${({ isControlled }) =>
     isControlled &&
     css`
+      left: var(--scroll-left-px);
+      top: var(--scroll-top-px);
+    `};
+
+  ${({ isBackgroundSelected, theme }) =>
+    isBackgroundSelected &&
+    css`
+      &:before {
+        content: '';
+        position: absolute;
+        top: -4px;
+        left: -4px;
+        right: -4px;
+        bottom: -4px;
+        border: ${theme.colors.border.selection} 1px solid;
+        border-radius: ${theme.borders.radius.medium};
+      }
+    `}
+
       left: var(--scroll-left-px);
       top: var(--scroll-top-px);
     `};
@@ -439,11 +484,11 @@ const PageArea = forwardRef(function PageArea(
               ) : (
                 children
               )}
+              {overlay}
             </PageAreaWithoutOverflow>
           </FullbleedContainer>
         </PaddedPage>
       </PageClip>
-      {overlay}
     </PageAreaContainer>
   );
 });
