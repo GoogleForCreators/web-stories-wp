4003b80e60fe423c2c37a312bf55b5ea96efb5f8
Jonny Harris <jon@spacedmonkey.co.uk>
2021-12-06 14:23:07 +0000

    Move function.

--- packages/story-editor/src/app/media/utils/useMediaUploadQueue/index.js
+++ packages/story-editor/src/app/media/utils/useMediaUploadQueue/index.js
@@ -230,6 +230,14 @@ function useMediaUploadQueue() {
           let newFile = file;
           let newPosterFile = posterFile;
 
+          if (
+            resource.type === 'video' &&
+            resource.isMuted !== null &&
+            additionalData?.web_stories_is_muted === undefined
+          ) {
+            additionalData.web_stories_is_muted = resource.isMuted;
+          }
+
           // Convert animated GIFs to videos if possible.
           if (
             isTranscodingEnabled &&
@@ -319,14 +327,6 @@ function useMediaUploadQueue() {
           });
 
           const trackTiming = getTimeTracker('load_upload_media');
-
-          if (
-            resource.type === 'video' &&
-            resource.isMuted !== null &&
-            additionalData?.web_stories_is_muted === undefined
-          ) {
-            additionalData.web_stories_is_muted = resource.isMuted;
-          }
 
           try {
             // The newly uploaded file won't have a poster yet.
