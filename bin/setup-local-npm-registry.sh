df5e1b4c122a34156e5d2c0a6995e6d0e6689937
Pascal Birchler <pascalb@google.com>
2021-12-07 19:48:52 +0100

    Set up bash scripts for Verdaccio

---
+++ bin/setup-local-npm-registry.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_REGISTRY_URL=http://localhost:4873
+
+echo "Starting up local npm registry..."
+
+# Start local registry.
+tmp_registry_log=`mktemp`
+mkdir -p $HOME/.config/verdaccio
+nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_registry_log &
+
+# Wait for `verdaccio` to boot
+grep -q 'http address' <(tail -f $tmp_registry_log)
+
+echo "Local registry up and running! ${LOCAL_REGISTRY_URL}"
+
+echo "Logging in..."
+
+# Log in to Verdaccio so we can publish packages
+npx npm-cli-login -u admin -p password -e test@example.com -r $LOCAL_REGISTRY_URL
