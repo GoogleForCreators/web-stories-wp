76d1b1f3ba0ae56f654c94988a6a30e40d980e60
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2021-12-09 18:10:19 +0530

    fix eslint issues

--- packages/story-editor/src/elements/video/playPauseButton.js
+++ packages/story-editor/src/elements/video/playPauseButton.js
@@ -84,34 +84,6 @@ const ButtonWrapper = styled.div.attrs({ role: 'button', tabIndex: -1 })`
     opacity: 0;
     transition: opacity 100ms;
   }
-
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    `}
-  cursor: pointer;
-  pointer-events: initial;
-  width: ${PLAY_BUTTON_SIZE}px;
-  height: ${PLAY_BUTTON_SIZE}px;
-  overflow: hidden;
-  opacity: ${({ isAbove }) => (isAbove ? 1 : 0)};
-  &.button-enter {
-    opacity: 0;
-  }
-  &.button-enter-active,
-  &.button-enter-done {
-    opacity: 1;
-    transition: opacity 100ms;
-  }
-  &.button-exit {
-    opacity: 1;
-  }
-  &.button-exit-active,
-  &.button-exit-done {
-    opacity: 0;
-    transition: opacity 100ms;
-  }
 `;
 
 const iconCss = css`
