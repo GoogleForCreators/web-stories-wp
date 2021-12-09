d4ca5578b3b7be4484e7a60dc1af43dcdf28179d
Pascal Birchler <pascalb@google.com>
2021-12-09 15:53:57 +0100

    Remove `FileProvider`

--- packages/story-editor/src/storyEditor.js
+++ packages/story-editor/src/storyEditor.js
@@ -35,7 +35,6 @@ import theme, { GlobalStyle } from './theme';
 import ErrorBoundary from './components/errorBoundary';
 import { ConfigProvider } from './app/config';
 import { APIProvider } from './app/api';
-import { FileProvider } from './app/file';
 import { Media3pApiProvider } from './app/media/media3p/api';
 import { HistoryProvider } from './app/history';
 import { StoryProvider } from './app/story';
@@ -65,42 +64,40 @@ function StoryEditor({ config, initialEdits, children }) {
           <ErrorBoundary>
             <ConfigProvider config={_config}>
               <APIProvider>
-                <FileProvider>
-                  <Media3pApiProvider>
-                    <HistoryProvider size={50}>
-                      <SnackbarProvider>
-                        <StoryProvider
-                          storyId={storyId}
-                          initialEdits={initialEdits}
-                        >
-                          <TaxonomyProvider>
-                            <CurrentUserProvider>
-                              <FontProvider>
-                                <MediaProvider>
-                                  <AutoSaveHandler />
-                                  <TransformProvider>
-                                    <DropTargetsProvider>
-                                      <HelpCenterProvider>
-                                        <GlobalStyle />
-                                        <DevTools />
-                                        <DefaultMoveableGlobalStyle />
-                                        <CropMoveableGlobalStyle />
-                                        <ModalGlobalStyle />
-                                        <CalendarStyle />
-                                        <KeyboardOnlyOutlines />
-                                        {children}
-                                      </HelpCenterProvider>
-                                    </DropTargetsProvider>
-                                  </TransformProvider>
-                                </MediaProvider>
-                              </FontProvider>
-                            </CurrentUserProvider>
-                          </TaxonomyProvider>
-                        </StoryProvider>
-                      </SnackbarProvider>
-                    </HistoryProvider>
-                  </Media3pApiProvider>
-                </FileProvider>
+                <Media3pApiProvider>
+                  <HistoryProvider size={50}>
+                    <SnackbarProvider>
+                      <StoryProvider
+                        storyId={storyId}
+                        initialEdits={initialEdits}
+                      >
+                        <TaxonomyProvider>
+                          <CurrentUserProvider>
+                            <FontProvider>
+                              <MediaProvider>
+                                <AutoSaveHandler />
+                                <TransformProvider>
+                                  <DropTargetsProvider>
+                                    <HelpCenterProvider>
+                                      <GlobalStyle />
+                                      <DevTools />
+                                      <DefaultMoveableGlobalStyle />
+                                      <CropMoveableGlobalStyle />
+                                      <ModalGlobalStyle />
+                                      <CalendarStyle />
+                                      <KeyboardOnlyOutlines />
+                                      {children}
+                                    </HelpCenterProvider>
+                                  </DropTargetsProvider>
+                                </TransformProvider>
+                              </MediaProvider>
+                            </FontProvider>
+                          </CurrentUserProvider>
+                        </TaxonomyProvider>
+                      </StoryProvider>
+                    </SnackbarProvider>
+                  </HistoryProvider>
+                </Media3pApiProvider>
               </APIProvider>
             </ConfigProvider>
           </ErrorBoundary>
