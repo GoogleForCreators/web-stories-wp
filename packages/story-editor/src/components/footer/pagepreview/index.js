e370be29c26dce0e0f1333cbf32029f3f9713556
Jasper <jasper.kinoti@gmail.com>
2021-12-09 20:10:20 +0300

    cleanup

--- packages/story-editor/src/components/footer/pagepreview/index.js
+++ packages/story-editor/src/components/footer/pagepreview/index.js
@@ -17,33 +17,33 @@
 /**
  * External dependencies
  */
- import styled, { css } from 'styled-components';
- import { rgba } from 'polished';
- import PropTypes from 'prop-types';
- import { generatePatternStyles } from '@web-stories-wp/patterns';
- import { UnitsProvider } from '@web-stories-wp/units';
- import {
-   useState,
-   useRef,
-   useCallback,
-   useEffect,
- } from '@web-stories-wp/react';
- import { useFeature } from 'flagged';
+import styled, { css } from 'styled-components';
+import { rgba } from 'polished';
+import PropTypes from 'prop-types';
+import { generatePatternStyles } from '@web-stories-wp/patterns';
+import { UnitsProvider } from '@web-stories-wp/units';
+import {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+} from '@web-stories-wp/react';
+import { useFeature } from 'flagged';
 
- /**
-  * Internal dependencies
-  */
- import StoryPropTypes from '../../../types';
- import {
-   requestIdleCallback,
-   cancelIdleCallback,
- } from '../../../utils/idleCallback';
- import { TransformProvider } from '../../transform';
- import DisplayElement from '../../canvas/displayElement';
- import usePerformanceTracking from '../../../utils/usePerformanceTracking';
- import { TRACKING_EVENTS } from '../../../constants/performanceTrackingEvents';
+/**
+ * Internal dependencies
+ */
+import StoryPropTypes from '../../../types';
+import {
+  requestIdleCallback,
+  cancelIdleCallback,
+} from '../../../utils/idleCallback';
+import { TransformProvider } from '../../transform';
+import DisplayElement from '../../canvas/displayElement';
+import usePerformanceTracking from '../../../utils/usePerformanceTracking';
+import { TRACKING_EVENTS } from '../../../constants/performanceTrackingEvents';
 
- const Page = styled.button`
+const Page = styled.button`
    display: block;
    position: relative;
    cursor: ${({ isInteractive }) => (isInteractive ? 'pointer' : 'default')};
@@ -67,21 +67,21 @@
      border-width: 1px;
      border-radius: 8px;
      border-color: ${({ isActive, theme }) =>
-       isActive ? theme.colors.border.defaultActive : 'transparent'};
+    isActive ? theme.colors.border.defaultActive : 'transparent'};
    }
    ${({ isInteractive, isActive, theme }) =>
-     isInteractive &&
-     css`
+    isInteractive &&
+    css`
        &:focus::after {
          border-color: ${rgba(
-           theme.colors.border.selection,
-           isActive ? 1 : 0.7
-         )};
+      theme.colors.border.selection,
+      isActive ? 1 : 0.7
+    )};
        }
      `}
  `;
 
- const PreviewWrapper = styled.div`
+const PreviewWrapper = styled.div`
    height: 100%;
    position: relative;
    overflow: hidden;
@@ -90,122 +90,122 @@
    ${({ background }) => generatePatternStyles(background)}
  `;
 
- const Image = styled.img`
+const Image = styled.img`
    width: 100%;
  `;
 
- // PagePreview is used in the editor's Carousel as well as in the Checklist and GridView
- function PagePreview({
-   page,
-   label,
-   isCacheable = false,
-   cachedImage = null,
-   setCachedImage = null,
-   ...props
- }) {
-   const { backgroundColor } = page;
-   const { width, height, isActive } = props;
+// PagePreview is used in the editor's Carousel as well as in the Checklist and GridView
+function PagePreview({
+  page,
+  label,
+  isCacheable = false,
+  cachedImage = null,
+  setCachedImage = null,
+  ...props
+}) {
+  const { backgroundColor } = page;
+  const { width, height, isActive } = props;
 
-   const [pageNode, setPageNode] = useState();
-   const setPageRef = useCallback((node) => node && setPageNode(node), []);
-   const pageAtGenerationTime = useRef();
-   const enableThumbnailCaching =
-     useFeature('enableThumbnailCaching') && isCacheable;
+  const [pageNode, setPageNode] = useState();
+  const setPageRef = useCallback((node) => node && setPageNode(node), []);
+  const pageAtGenerationTime = useRef();
+  const enableThumbnailCaching =
+    useFeature('enableThumbnailCaching') && isCacheable;
 
-   // Whenever the page is re-generated
-   // remove the old (and now stale) image blob
-   useEffect(() => {
-     if (
-       enableThumbnailCaching &&
-       isActive &&
-       pageAtGenerationTime.current !== page
-     ) {
-       setCachedImage({ pageId: page.id, cachedImage: null });
-       pageAtGenerationTime.current = null;
-     }
-   }, [page, setCachedImage, isActive, enableThumbnailCaching]);
+  // Whenever the page is re-generated
+  // remove the old (and now stale) image blob
+  useEffect(() => {
+    if (
+      enableThumbnailCaching &&
+      isActive &&
+      pageAtGenerationTime.current !== page
+    ) {
+      setCachedImage({ pageId: page.id, cachedImage: null });
+      pageAtGenerationTime.current = null;
+    }
+  }, [page, setCachedImage, isActive, enableThumbnailCaching]);
 
-   useEffect(() => {
-     // If this is not the active page, there is a page node, we
-     // don't already have a snapshot and thumbnail caching is active
-     if (enableThumbnailCaching && !isActive && pageNode && !cachedImage) {
-       // Schedule an idle callback to actually generate the image
-       const id = requestIdleCallback(
-         () => {
-           import(
+  useEffect(() => {
+    // If this is not the active page, there is a page node, we
+    // don't already have a snapshot and thumbnail caching is active
+    if (enableThumbnailCaching && !isActive && pageNode && !cachedImage) {
+      // Schedule an idle callback to actually generate the image
+      const id = requestIdleCallback(
+        () => {
+          import(
              /* webpackChunkName: "chunk-html-to-image" */ 'html-to-image'
-           ).then((htmlToImage) => {
-             htmlToImage
-               .toJpeg(pageNode, { quality: 1 })
-               .then((image) =>
-                 setCachedImage({ pageId: page.id, cachedImage: image })
-               );
-             pageAtGenerationTime.current = page;
-           });
-         },
-         { timeout: 5000 }
-       );
-       // If the page somehow regenerates before the snapshot is taken,
-       // make sure to cancel the old request
-       return () => cancelIdleCallback(id);
-     }
-     // Required because of eslint: consistent-return
-     return undefined;
-   }, [
-     enableThumbnailCaching,
-     isActive,
-     pageNode,
-     cachedImage,
-     setCachedImage,
-     page,
-   ]);
+          ).then((htmlToImage) => {
+            htmlToImage
+              .toJpeg(pageNode, { quality: 1 })
+              .then((image) =>
+                setCachedImage({ pageId: page.id, cachedImage: image })
+              );
+            pageAtGenerationTime.current = page;
+          });
+        },
+        { timeout: 5000 }
+      );
+      // If the page somehow regenerates before the snapshot is taken,
+      // make sure to cancel the old request
+      return () => cancelIdleCallback(id);
+    }
+    // Required because of eslint: consistent-return
+    return undefined;
+  }, [
+    enableThumbnailCaching,
+    isActive,
+    pageNode,
+    cachedImage,
+    setCachedImage,
+    page,
+  ]);
 
-   usePerformanceTracking({
-     node: pageNode,
-     eventData: TRACKING_EVENTS.PAGE_PREVIEW_CLICK,
-   });
+  usePerformanceTracking({
+    node: pageNode,
+    eventData: TRACKING_EVENTS.PAGE_PREVIEW_CLICK,
+  });
 
-   return (
-     <UnitsProvider pageSize={{ width, height }}>
-       <TransformProvider>
-         <Page ref={setPageRef} aria-label={label} {...props}>
-           <PreviewWrapper background={backgroundColor}>
-             {cachedImage ? (
-               <Image
-                 src={cachedImage}
-                 width={width}
-                 height={height}
-                 alt={label}
-               />
-             ) : (
-               page.elements.map((element) => (
-                 <DisplayElement
-                   key={element.id}
-                   previewMode
-                   element={element}
-                   page={page}
-                 />
-               ))
-             )}
-           </PreviewWrapper>
-         </Page>
-       </TransformProvider>
-     </UnitsProvider>
-   );
- }
+  return (
+    <UnitsProvider pageSize={{ width, height }}>
+      <TransformProvider>
+        <Page ref={setPageRef} aria-label={label} {...props}>
+          <PreviewWrapper background={backgroundColor}>
+            {cachedImage ? (
+              <Image
+                src={cachedImage}
+                width={width}
+                height={height}
+                alt={label}
+              />
+            ) : (
+              page.elements.map((element) => (
+                <DisplayElement
+                  key={element.id}
+                  previewMode
+                  element={element}
+                  page={page}
+                />
+              ))
+            )}
+          </PreviewWrapper>
+        </Page>
+      </TransformProvider>
+    </UnitsProvider>
+  );
+}
 
- PagePreview.propTypes = {
-   page: StoryPropTypes.page.isRequired,
-   label: PropTypes.string,
-   isCacheable: PropTypes.bool,
-   cachedImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-   setCachedImage: PropTypes.func,
-   pageImageData: PropTypes.string,
-   width: PropTypes.number.isRequired,
-   height: PropTypes.number.isRequired,
-   isInteractive: PropTypes.bool,
-   isActive: PropTypes.bool,
-   tabIndex: PropTypes.number,
- };
+PagePreview.propTypes = {
+  page: StoryPropTypes.page.isRequired,
+  label: PropTypes.string,
+  isCacheable: PropTypes.bool,
+  cachedImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  setCachedImage: PropTypes.func,
+  pageImageData: PropTypes.string,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  isInteractive: PropTypes.bool,
+  isActive: PropTypes.bool,
+  tabIndex: PropTypes.number,
+};
 
- export default PagePreview;
+export default PagePreview;
