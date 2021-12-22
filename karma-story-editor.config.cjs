11d8d57443ec1902d232468941eb2caad06f06ca
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-12-22 09:24:04 -0700

    add google font src as pattern to include for roboto in karma config

--- karma-story-editor.config.cjs
+++ karma-story-editor.config.cjs
@@ -68,6 +68,15 @@ module.exports = function (config) {
         served: true,
         nocache: false,
       },
+      {
+        pattern:
+          'https://fonts.googleapis.com/css2?display=auto&family=Roboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C100%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900',
+        type: 'css',
+        watched: false,
+        included: true,
+        served: true,
+        nocache: false,
+      },
       'node_modules/axe-core/axe.js',
     ],
 
