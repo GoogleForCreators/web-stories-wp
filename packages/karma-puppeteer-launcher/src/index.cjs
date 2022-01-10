2d488150ce661100f98608ed0719787140fb580c
merapi <marcin@webskill.pl>
2022-01-10 23:56:12 +0100

    Test small MWMPC

--- packages/karma-puppeteer-launcher/src/index.cjs
+++ packages/karma-puppeteer-launcher/src/index.cjs
@@ -51,7 +51,7 @@ function puppeteerBrowser(baseBrowserDecorator, config) {
         // We use this flag to work-around this issue.
         '--disable-dev-shm-usage',
         // Remove: Blocked attempt to create a WebMediaPlayer as there are too many WebMediaPlayers already in existence. See crbug.com/1144736#c27
-        '--max-web-media-player-count=250',
+        '--max-web-media-player-count=20',
         '--devtools-flags=disable',
       ],
     };
