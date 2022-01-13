da8aee9daf25c8554146d1b948b11754da65e4b6
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2022-01-13 13:46:55 +0530

    Change popup constant reference to design-system

--- packages/story-editor/src/components/library/panes/media/local/mediaPane.js
+++ packages/story-editor/src/components/library/panes/media/local/mediaPane.js
@@ -37,6 +37,7 @@ import {
   THEME_CONSTANTS,
   useSnackbar,
   Icons,
+  PLACEMENT,
 } from '@web-stories-wp/design-system';
 
 /**
@@ -56,7 +57,6 @@ import {
 } from '../common/styles';
 import PaginatedMediaGallery from '../common/paginatedMediaGallery';
 import Flags from '../../../../../flags';
-import { Placement } from '../../../../popup/constants';
 import { PANE_PADDING } from '../../shared';
 import { LOCAL_MEDIA_TYPE_ALL } from '../../../../../app/media/local/types';
 import { focusStyle } from '../../../../panels/shared';
@@ -343,7 +343,7 @@ function MediaPane(props) {
               selectedValue={mediaType?.toString() || FILTER_NONE}
               onMenuItemClick={onFilter}
               options={FILTERS}
-              placement={Placement.BOTTOM_START}
+              placement={PLACEMENT.BOTTOM_START}
             />
             {isSearching && media.length > 0 && (
               <SearchCount>
