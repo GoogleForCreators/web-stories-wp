45f931a68575fffc37c7732bc90fbd3094bf0aea
Pascal Birchler <pascalb@google.com>
2022-01-20 13:22:16 +0100

    Move `getFontDataFromUrl` to `wp-dashboard`

--- packages/fonts/src/utils/index.js
+++ packages/fonts/src/utils/index.js
@@ -16,4 +16,3 @@
 
 export { default as getFontCSS } from './getFontCSS';
 export { default as getGoogleFontURL } from './getGoogleFontURL';
-export { default as getFontDataFromUrl } from './getFontDataFromUrl';
