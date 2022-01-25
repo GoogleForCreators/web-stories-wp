6b38d9d37e96b8df13fac04bb072d7c37963d63c
Pascal Birchler <pascalb@google.com>
2022-01-25 22:44:30 +0100

    Remove web worker code for now, pending todo

--- packages/story-editor/src/utils/getBlurHashFromImage.js
+++ packages/story-editor/src/utils/getBlurHashFromImage.js
@@ -13,65 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * External dependencies
- */
-import { preloadImage } from '@googleforcreators/media';
-import { getTimeTracker, trackError } from '@googleforcreators/tracking';
 
-/**
- * Internal dependencies
- */
-// eslint-disable-next-line import/default
-import Worker from './generateBlurhash.worker';
-
-const getImageData = (image) => {
-  const { width, height } = image;
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(image, 0, 0);
-  return ctx.getImageData(0, 0, width, height);
-};
-
-const getBlurHashFromImage = async (src) => {
-  let image;
-  try {
-    image = await preloadImage({ src });
-  } catch {
-    return Promise.reject(new Error('Failed to get blurhash from image'));
-  }
-  const imageData = getImageData(image);
-  const { data, width, height } = imageData;
-
-  const trackTiming = getTimeTracker('load_get_blurhash');
-  return new Promise((resolve, reject) => {
-    const worker = new Worker(); // lgtm[js/call-to-non-callable]
-    worker.postMessage({
-      image: data,
-      width,
-      height,
-      componentX: 4,
-      componentY: 4,
-    });
-    worker.addEventListener('message', function (event) {
-      worker.terminate(); // lgtm [js/property-access-on-non-object]
-      trackTiming();
-      if (event.data.type === 'success') {
-        resolve(event.data.blurHash);
-      } else {
-        trackError('blurhash_generation', event.data.error?.message);
-        reject(event.data.error);
-      }
-    });
-    worker.addEventListener('error', (e) => {
-      worker.terminate(); // lgtm [js/property-access-on-non-object]
-      trackTiming();
-      trackError('blurhash_generation', e?.message);
-      reject(e);
-    });
+//eslint-disable-next-line require-await
+const getBlurHashFromImage = async () => {
+  return new Promise((resolve) => {
+    // TODO: Find solution for inline workers with Rollup.
+    return resolve();
   });
 };
 export default getBlurHashFromImage;
