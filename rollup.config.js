f462acdd6ec07e745879490e38fde7c7ec37afce
Pascal Birchler <pascalb@google.com>
2022-01-31 19:57:15 +0100

    Tweak Terser options

--- rollup.config.js
+++ rollup.config.js
@@ -58,7 +58,18 @@ const plugins = [
   }),
   css(),
   dynamicImportVars(),
-  terser(),
+  terser({
+    // We preserve function names that start with capital letters as
+    // they're _likely_ component names, and these are useful to have
+    // in tracebacks and error messages.
+    keep_fnames: /__|_x|_n|_nx|sprintf|^[A-Z].+$/,
+    output: {
+      comments: /translators:/i,
+    },
+    mangle: {
+      reserved: ['__', '_x', '_n', '_nx', 'sprintf'],
+    },
+  }),
   license({
     sourcemap: true,
     banner: `Copyright <%= moment().format('YYYY') %> Google LLC
