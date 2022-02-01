5548418aca5587ff8e339f1485ddc197eb9dd605
BGraham <brooke.graham@formidable.com>
2022-02-01 10:33:17 -0700

    clean up

--- packages/story-editor/src/components/panels/design/link/karma/link.karma.js
+++ packages/story-editor/src/components/panels/design/link/karma/link.karma.js
@@ -198,7 +198,7 @@ describe('Link Panel', () => {
       // Verify that the description is not displayed when hovering without url.
       await fixture.events.mouse.click(left - 5, top - 5);
       await fixture.events.mouse.moveRel(frame, 50, 10);
-      const removedDescription = fixture.screen.queryByRole(tooltip);
+      const removedDescription = fixture.screen.queryByText(linkDescription);
       expect(removedDescription).toBeNull();
       await fixture.snapshot(
         'Element is hovered on. The link tooltip is not visible'
