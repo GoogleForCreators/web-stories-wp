GoogleForCreators/web-stories-wp

commit 1843b8c9638fb63dfa8710363ba21da9704ed553

Context Menu: Refactor right click menu provider (#10361)

packages/story-editor/src/app/rightClickMenu/menus/foregroundMediaMenu.js
@@ -1,0 +1,221 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  ContextMenu,
+  ContextMenuComponents,
+} from '@googleforcreators/design-system';
+import { useRef } from '@googleforcreators/react';
+/**
+ * Internal dependencies
+ */
+import {
+  RIGHT_CLICK_MENU_LABELS,
+  RIGHT_CLICK_MENU_SHORTCUTS,
+} from '../constants';
+import {
+  useCopyPasteActions,
+  useElementActions,
+  useLayerActions,
+} from '../hooks';
+import { useStory } from '../../..';
+import { useLocalMedia } from '../..';
+import useVideoTrim from '../../../components/videoTrim/useVideoTrim';
+import useRightClickMenu from '../useRightClickMenu';
+import useLayerSelect from '../useLayerSelect';
+import {
+  DEFAULT_DISPLACEMENT,
+  MenuPropType,
+  SubMenuContainer,
+  SUB_MENU_ARIA_LABEL,
+} from './shared';
+
+function ForegroundMediaMenu({ parentMenuRef }) {
+  const { copiedElementType, selectedElement } = useStory(({ state }) => ({
+    copiedElementType: state.copiedElementState.type,
+    selectedElement: state.selectedElements?.[0],
+  }));
+  const { handleCopyStyles, handlePasteStyles } = useCopyPasteActions();
+  const {
+    handleDuplicateSelectedElements,
+    handleOpenScaleAndCrop,
+    handleSetPageBackground,
+  } = useElementActions();
+  const {
+    canElementMoveBackwards,
+    canElementMoveForwards,
+    handleSendBackward,
+    handleSendToBack,
+    handleBringForward,
+    handleBringToFront,
+  } = useLayerActions();
+
+  const canTranscodeResource = useLocalMedia(
+    (value) => value.state.canTranscodeResource
+  );
+  const { hasTrimMode, toggleTrimMode } = useVideoTrim(
+    ({ state, actions }) => ({
+      hasTrimMode: state.hasTrimMode,
+      toggleTrimMode: actions.toggleTrimMode,
+    })
+  );
+
+  const subMenuRef = useRef();
+  const { menuPosition, onCloseMenu } = useRightClickMenu();
+  const layerSelectProps = useLayerSelect({
+    menuPosition,
+    isMenuOpen: true,
+  });
+
+  const { closeSubMenu, isSubMenuOpen, subMenuItems, ...subMenuTriggerProps } =
+    layerSelectProps || {};
+
+  const isVideo = selectedElement?.type === 'video';
+  const scaleLabel = isVideo
+    ? RIGHT_CLICK_MENU_LABELS.SCALE_AND_CROP_VIDEO
+    : RIGHT_CLICK_MENU_LABELS.SCALE_AND_CROP_IMAGE;
+  const copyLabel = isVideo
+    ? RIGHT_CLICK_MENU_LABELS.COPY_VIDEO_STYLES
+    : RIGHT_CLICK_MENU_LABELS.COPY_IMAGE_STYLES;
+  const pasteLabel = isVideo
+    ? RIGHT_CLICK_MENU_LABELS.PASTE_VIDEO_STYLES
+    : RIGHT_CLICK_MENU_LABELS.PASTE_IMAGE_STYLES;
+
+  const showToggleTrimMode = isVideo && hasTrimMode;
+
+  return (
+    <>
+      {layerSelectProps && (
+        <>
+          <ContextMenuComponents.SubMenuTrigger
+            closeSubMenu={closeSubMenu}
+            parentMenuRef={parentMenuRef}
+            subMenuRef={subMenuRef}
+            isSubMenuOpen={isSubMenuOpen}
+            {...subMenuTriggerProps}
+          />
+          <SubMenuContainer
+            ref={subMenuRef}
+            position={{
+              x:
+                (parentMenuRef.current.firstChild?.offsetWidth ||
+                  DEFAULT_DISPLACEMENT) + 2,
+              y: 0,
+            }}
+          >
+            <ContextMenu
+              onDismiss={onCloseMenu}
+              isOpen={isSubMenuOpen}
+              onCloseSubMenu={closeSubMenu}
+              aria-label={SUB_MENU_ARIA_LABEL}
+              isSubMenu
+              parentMenuRef={parentMenuRef}
+            >
+              {subMenuItems.map(({ key, ...menuItemProps }) => (
+                <ContextMenuComponents.MenuItem key={key} {...menuItemProps} />
+              ))}
+            </ContextMenu>
+          </SubMenuContainer>
+          <ContextMenuComponents.MenuSeparator />
+        </>
+      )}
+
+      <ContextMenuComponents.MenuButton
+        onClick={handleDuplicateSelectedElements}
+      >
+        {RIGHT_CLICK_MENU_LABELS.DUPLICATE_ELEMENTS(1)}
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendBackward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_BACKWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_BACKWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendToBack}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_TO_BACK}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_TO_BACK.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringForward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_FORWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_FORWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringToFront}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_TO_FRONT}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_TO_FRONT.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton onClick={handleSetPageBackground}>
+        {RIGHT_CLICK_MENU_LABELS.SET_AS_PAGE_BACKGROUND}
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton onClick={handleOpenScaleAndCrop}>
+        {scaleLabel}
+      </ContextMenuComponents.MenuButton>
+      {showToggleTrimMode && (
+        <ContextMenuComponents.MenuButton
+          disabled={!canTranscodeResource(selectedElement?.resource)}
+          onClick={toggleTrimMode}
+        >
+          {RIGHT_CLICK_MENU_LABELS.TRIM_VIDEO}
+        </ContextMenuComponents.MenuButton>
+      )}
+
+      <ContextMenuComponents.MenuButton onClick={handleCopyStyles}>
+        {copyLabel}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.COPY_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={copiedElementType !== selectedElement?.type}
+        onClick={handlePasteStyles}
+      >
+        {pasteLabel}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.PASTE_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+    </>
+  );
+}
+ForegroundMediaMenu.propTypes = MenuPropType;
+
+export default ForegroundMediaMenu;

packages/story-editor/src/app/rightClickMenu/menus/pageMenu.js
@@ -1,0 +1,175 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  ContextMenu,
+  ContextMenuComponents,
+} from '@googleforcreators/design-system';
+import { useRef } from '@googleforcreators/react';
+/**
+ * Internal dependencies
+ */
+import useVideoTrim from '../../../components/videoTrim/useVideoTrim';
+import { RIGHT_CLICK_MENU_LABELS } from '../constants';
+import { useElementActions, usePageActions } from '../hooks';
+import { useStory } from '../../..';
+import { useLocalMedia } from '../..';
+import useRightClickMenu from '../useRightClickMenu';
+import useLayerSelect from '../useLayerSelect';
+import {
+  DEFAULT_DISPLACEMENT,
+  MenuPropType,
+  SubMenuContainer,
+  SUB_MENU_ARIA_LABEL,
+} from './shared';
+
+function PageMenu({ parentMenuRef }) {
+  const { currentPageIndex, canDeletePage, selectedElement } = useStory(
+    ({ state }) => ({
+      currentPageIndex: state.currentPageIndex,
+      canDeletePage: state.pages.length > 1,
+      selectedElement: state.selectedElements?.[0],
+    })
+  );
+  const { handleOpenScaleAndCrop, handleRemovePageBackground } =
+    useElementActions();
+  const { handleAddPageAtPosition, handleDeletePage, handleDuplicatePage } =
+    usePageActions();
+
+  const canTranscodeResource = useLocalMedia(
+    (value) => value.state.canTranscodeResource
+  );
+  const { hasTrimMode, toggleTrimMode } = useVideoTrim(
+    ({ state, actions }) => ({
+      hasTrimMode: state.hasTrimMode,
+      toggleTrimMode: actions.toggleTrimMode,
+    })
+  );
+
+  const subMenuRef = useRef();
+  const { menuPosition, onCloseMenu } = useRightClickMenu();
+  const layerSelectProps = useLayerSelect({
+    menuPosition,
+    isMenuOpen: true,
+  });
+
+  const { closeSubMenu, isSubMenuOpen, subMenuItems, ...subMenuTriggerProps } =
+    layerSelectProps || {};
+
+  const disableBackgroundMediaActions = selectedElement?.isDefaultBackground;
+  const isVideo = selectedElement?.type === 'video';
+  const detachLabel = isVideo
+    ? RIGHT_CLICK_MENU_LABELS.DETACH_VIDEO_FROM_BACKGROUND
+    : RIGHT_CLICK_MENU_LABELS.DETACH_IMAGE_FROM_BACKGROUND;
+  const scaleLabel = isVideo
+    ? RIGHT_CLICK_MENU_LABELS.SCALE_AND_CROP_BACKGROUND_VIDEO
+    : RIGHT_CLICK_MENU_LABELS.SCALE_AND_CROP_BACKGROUND_IMAGE;
+
+  const showTrimModeAction = isVideo && hasTrimMode;
+
+  return (
+    <>
+      {layerSelectProps && (
+        <>
+          <ContextMenuComponents.SubMenuTrigger
+            closeSubMenu={closeSubMenu}
+            parentMenuRef={parentMenuRef}
+            subMenuRef={subMenuRef}
+            isSubMenuOpen={isSubMenuOpen}
+            {...subMenuTriggerProps}
+          />
+          <SubMenuContainer
+            ref={subMenuRef}
+            position={{
+              x:
+                (parentMenuRef.current.firstChild?.offsetWidth ||
+                  DEFAULT_DISPLACEMENT) + 2,
+              y: 0,
+            }}
+          >
+            <ContextMenu
+              onDismiss={onCloseMenu}
+              isOpen={isSubMenuOpen}
+              onCloseSubMenu={closeSubMenu}
+              aria-label={SUB_MENU_ARIA_LABEL}
+              isSubMenu
+              parentMenuRef={parentMenuRef}
+            >
+              {subMenuItems.map(({ key, ...menuItemProps }) => (
+                <ContextMenuComponents.MenuItem key={key} {...menuItemProps} />
+              ))}
+            </ContextMenu>
+          </SubMenuContainer>
+          <ContextMenuComponents.MenuSeparator />
+        </>
+      )}
+
+      <ContextMenuComponents.MenuButton
+        disabled={disableBackgroundMediaActions}
+        onClick={handleRemovePageBackground}
+      >
+        {detachLabel}
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuButton
+        disabled={disableBackgroundMediaActions}
+        onClick={handleOpenScaleAndCrop}
+      >
+        {scaleLabel}
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      {showTrimModeAction && (
+        <>
+          <ContextMenuComponents.MenuButton
+            disabled={!canTranscodeResource(selectedElement?.resource)}
+            onClick={toggleTrimMode}
+          >
+            {detachLabel}
+          </ContextMenuComponents.MenuButton>
+          <ContextMenuComponents.MenuSeparator />
+        </>
+      )}
+
+      <ContextMenuComponents.MenuButton
+        onClick={() => handleAddPageAtPosition(currentPageIndex + 1)}
+      >
+        {RIGHT_CLICK_MENU_LABELS.ADD_NEW_PAGE_AFTER}
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        onClick={() => handleAddPageAtPosition(currentPageIndex)}
+      >
+        {RIGHT_CLICK_MENU_LABELS.ADD_NEW_PAGE_BEFORE}
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton onClick={handleDuplicatePage}>
+        {RIGHT_CLICK_MENU_LABELS.DUPLICATE_PAGE}
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canDeletePage}
+        onClick={handleDeletePage}
+      >
+        {RIGHT_CLICK_MENU_LABELS.DELETE_PAGE}
+      </ContextMenuComponents.MenuButton>
+    </>
+  );
+}
+PageMenu.propTypes = MenuPropType;
+
+export default PageMenu;

packages/story-editor/src/app/rightClickMenu/menus/shapeMenu.js
@@ -1,0 +1,182 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  ContextMenu,
+  ContextMenuComponents,
+} from '@googleforcreators/design-system';
+import { useRef } from '@googleforcreators/react';
+/**
+ * Internal dependencies
+ */
+import {
+  RIGHT_CLICK_MENU_LABELS,
+  RIGHT_CLICK_MENU_SHORTCUTS,
+} from '../constants';
+import {
+  useCopyPasteActions,
+  useElementActions,
+  useLayerActions,
+  usePresetActions,
+} from '../hooks';
+import useLayerSelect from '../useLayerSelect';
+import { useStory } from '../../..';
+import useRightClickMenu from '../useRightClickMenu';
+import {
+  DEFAULT_DISPLACEMENT,
+  MenuPropType,
+  SubMenuContainer,
+  SUB_MENU_ARIA_LABEL,
+} from './shared';
+
+function ShapeMenu({ parentMenuRef }) {
+  const { copiedElementType, selectedElementType } = useStory(({ state }) => ({
+    copiedElementType: state.copiedElementState.type,
+    selectedElementType: state.selectedElements?.[0].type,
+  }));
+  const { handleCopyStyles, handlePasteStyles } = useCopyPasteActions();
+  const { handleDuplicateSelectedElements } = useElementActions();
+  const {
+    canElementMoveBackwards,
+    canElementMoveForwards,
+    handleSendBackward,
+    handleSendToBack,
+    handleBringForward,
+    handleBringToFront,
+  } = useLayerActions();
+  const { handleAddColorPreset } = usePresetActions();
+
+  const subMenuRef = useRef();
+  const { menuPosition, onCloseMenu } = useRightClickMenu();
+  const layerSelectProps = useLayerSelect({
+    menuPosition,
+    isMenuOpen: true,
+  });
+
+  const { closeSubMenu, isSubMenuOpen, subMenuItems, ...subMenuTriggerProps } =
+    layerSelectProps || {};
+
+  return (
+    <>
+      {layerSelectProps && (
+        <>
+          <ContextMenuComponents.SubMenuTrigger
+            closeSubMenu={closeSubMenu}
+            parentMenuRef={parentMenuRef}
+            subMenuRef={subMenuRef}
+            isSubMenuOpen={isSubMenuOpen}
+            {...subMenuTriggerProps}
+          />
+          <SubMenuContainer
+            ref={subMenuRef}
+            position={{
+              x:
+                (parentMenuRef.current.firstChild?.offsetWidth ||
+                  DEFAULT_DISPLACEMENT) + 2,
+              y: 0,
+            }}
+          >
+            <ContextMenu
+              onDismiss={onCloseMenu}
+              isOpen={isSubMenuOpen}
+              onCloseSubMenu={closeSubMenu}
+              aria-label={SUB_MENU_ARIA_LABEL}
+              isSubMenu
+              parentMenuRef={parentMenuRef}
+            >
+              {subMenuItems.map(({ key, ...menuItemProps }) => (
+                <ContextMenuComponents.MenuItem key={key} {...menuItemProps} />
+              ))}
+            </ContextMenu>
+          </SubMenuContainer>
+          <ContextMenuComponents.MenuSeparator />
+        </>
+      )}
+
+      <ContextMenuComponents.MenuButton
+        onClick={handleDuplicateSelectedElements}
+      >
+        {RIGHT_CLICK_MENU_LABELS.DUPLICATE_ELEMENTS(1)}
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendBackward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_BACKWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_BACKWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendToBack}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_TO_BACK}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_TO_BACK.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringForward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_FORWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_FORWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringToFront}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_TO_FRONT}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_TO_FRONT.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton onClick={handleCopyStyles}>
+        {RIGHT_CLICK_MENU_LABELS.COPY_SHAPE_STYLES}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.COPY_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={copiedElementType !== selectedElementType}
+        onClick={handlePasteStyles}
+      >
+        {RIGHT_CLICK_MENU_LABELS.PASTE_SHAPE_STYLES}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.PASTE_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton onClick={handleAddColorPreset}>
+        {RIGHT_CLICK_MENU_LABELS.ADD_TO_COLOR_PRESETS}
+      </ContextMenuComponents.MenuButton>
+    </>
+  );
+}
+ShapeMenu.propTypes = MenuPropType;
+
+export default ShapeMenu;

packages/story-editor/src/app/rightClickMenu/menus/textMenu.js
@@ -1,0 +1,185 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  ContextMenu,
+  ContextMenuComponents,
+} from '@googleforcreators/design-system';
+import { useRef } from '@googleforcreators/react';
+/**
+ * Internal dependencies
+ */
+import {
+  RIGHT_CLICK_MENU_LABELS,
+  RIGHT_CLICK_MENU_SHORTCUTS,
+} from '../constants';
+import {
+  useCopyPasteActions,
+  useElementActions,
+  useLayerActions,
+  usePresetActions,
+} from '../hooks';
+import useLayerSelect from '../useLayerSelect';
+import { useStory } from '../../..';
+import useRightClickMenu from '../useRightClickMenu';
+import {
+  DEFAULT_DISPLACEMENT,
+  MenuPropType,
+  SubMenuContainer,
+  SUB_MENU_ARIA_LABEL,
+} from './shared';
+
+function TextMenu({ parentMenuRef }) {
+  const { copiedElementType, selectedElementType } = useStory(({ state }) => ({
+    copiedElementType: state.copiedElementState.type,
+    selectedElementType: state.selectedElements?.[0].type,
+  }));
+  const { handleCopyStyles, handlePasteStyles } = useCopyPasteActions();
+  const { handleDuplicateSelectedElements } = useElementActions();
+  const {
+    canElementMoveBackwards,
+    canElementMoveForwards,
+    handleSendBackward,
+    handleSendToBack,
+    handleBringForward,
+    handleBringToFront,
+  } = useLayerActions();
+  const { handleAddColorPreset, handleAddTextPreset } = usePresetActions();
+
+  const subMenuRef = useRef();
+  const { menuPosition, onCloseMenu } = useRightClickMenu();
+  const layerSelectProps = useLayerSelect({
+    menuPosition,
+    isMenuOpen: true,
+  });
+
+  const { closeSubMenu, isSubMenuOpen, subMenuItems, ...subMenuTriggerProps } =
+    layerSelectProps || {};
+
+  return (
+    <>
+      {layerSelectProps && (
+        <>
+          <ContextMenuComponents.SubMenuTrigger
+            closeSubMenu={closeSubMenu}
+            parentMenuRef={parentMenuRef}
+            subMenuRef={subMenuRef}
+            isSubMenuOpen={isSubMenuOpen}
+            {...subMenuTriggerProps}
+          />
+          <SubMenuContainer
+            ref={subMenuRef}
+            position={{
+              x:
+                (parentMenuRef.current.firstChild?.offsetWidth ||
+                  DEFAULT_DISPLACEMENT) + 2,
+              y: 0,
+            }}
+          >
+            <ContextMenu
+              onDismiss={onCloseMenu}
+              isOpen={isSubMenuOpen}
+              onCloseSubMenu={closeSubMenu}
+              aria-label={SUB_MENU_ARIA_LABEL}
+              isSubMenu
+              parentMenuRef={parentMenuRef}
+            >
+              {subMenuItems.map(({ key, ...menuItemProps }) => (
+                <ContextMenuComponents.MenuItem key={key} {...menuItemProps} />
+              ))}
+            </ContextMenu>
+          </SubMenuContainer>
+          <ContextMenuComponents.MenuSeparator />
+        </>
+      )}
+
+      <ContextMenuComponents.MenuButton
+        onClick={handleDuplicateSelectedElements}
+      >
+        {RIGHT_CLICK_MENU_LABELS.DUPLICATE_ELEMENTS(1)}
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendBackward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_BACKWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_BACKWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveBackwards}
+        onClick={handleSendToBack}
+      >
+        {RIGHT_CLICK_MENU_LABELS.SEND_TO_BACK}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.SEND_TO_BACK.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringForward}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_FORWARD}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_FORWARD.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={!canElementMoveForwards}
+        onClick={handleBringToFront}
+      >
+        {RIGHT_CLICK_MENU_LABELS.BRING_TO_FRONT}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.BRING_TO_FRONT.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+
+      <ContextMenuComponents.MenuSeparator />
+
+      <ContextMenuComponents.MenuButton onClick={handleCopyStyles}>
+        {RIGHT_CLICK_MENU_LABELS.COPY_STYLES}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.COPY_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton
+        disabled={copiedElementType !== selectedElementType}
+        onClick={handlePasteStyles}
+      >
+        {RIGHT_CLICK_MENU_LABELS.PASTE_STYLES}
+        <ContextMenuComponents.MenuShortcut>
+          {RIGHT_CLICK_MENU_SHORTCUTS.PASTE_STYLES.display}
+        </ContextMenuComponents.MenuShortcut>
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton onClick={handleAddTextPreset}>
+        {RIGHT_CLICK_MENU_LABELS.ADD_TO_TEXT_PRESETS}
+      </ContextMenuComponents.MenuButton>
+      <ContextMenuComponents.MenuButton onClick={handleAddColorPreset}>
+        {RIGHT_CLICK_MENU_LABELS.ADD_TO_COLOR_PRESETS}
+      </ContextMenuComponents.MenuButton>
+    </>
+  );
+}
+TextMenu.propTypes = MenuPropType;
+
+export default TextMenu;

packages/dashboard/src/components/layout/test/useLayoutContext.js
@@ -20,9 +20,6 @@ import { renderHook } from '@testing-library/react-hooks';
 /**
  * Internal dependencies
  */
-/**
- * Internal dependencies
- */
 import LayoutProvider from '../provider';
 import useLayoutContext from '../useLayoutContext';
 

packages/story-editor/src/app/rightClickMenu/hooks/constants.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,12 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const SHARED_DEFAULT_ATTRIBUTES = {
-  opacity: 100,
-  flip: {
-    vertical: false,
-    horizontal: false,
-  },
-  rotationAngle: 0,
-  lockAspectRatio: true,
-};
+
+/**
+ * External dependencies
+ */
+import { prettifyShortcut } from '@googleforcreators/design-system';
+import { __, sprintf } from '@googleforcreators/i18n';
+
+export const UNDO_HELP_TEXT = sprintf(
+  /* translators: %s: Ctrl/Cmd + Z keyboard shortcut */
+  __('Press %s to undo the last change', 'web-stories'),
+  prettifyShortcut('mod+z')
+);

commit 3aaa2a5fd05586200518b43674fdaa20188dc558

Fix broken test caused by circular import
Introduced in #10361

packages/story-editor/src/app/rightClickMenu/menus/foregroundMediaMenu.js
@@ -34,8 +34,7 @@ import {
   useElementActions,
   useLayerActions,
 } from '../hooks';
-import { useStory } from '../../..';
-import { useLocalMedia } from '../..';
+import { useStory, useLocalMedia } from '../..';
 import useVideoTrim from '../../../components/videoTrim/useVideoTrim';
 import useRightClickMenu from '../useRightClickMenu';
 import useLayerSelect from '../useLayerSelect';

packages/story-editor/src/app/rightClickMenu/menus/pageMenu.js
@@ -22,14 +22,14 @@ import {
   ContextMenuComponents,
 } from '@googleforcreators/design-system';
 import { useRef } from '@googleforcreators/react';
+
 /**
  * Internal dependencies
  */
 import useVideoTrim from '../../../components/videoTrim/useVideoTrim';
 import { RIGHT_CLICK_MENU_LABELS } from '../constants';
 import { useElementActions, usePageActions } from '../hooks';
-import { useStory } from '../../..';
-import { useLocalMedia } from '../..';
+import { useStory, useLocalMedia } from '../..';
 import useRightClickMenu from '../useRightClickMenu';
 import useLayerSelect from '../useLayerSelect';
 import {

packages/story-editor/src/app/rightClickMenu/menus/shapeMenu.js
@@ -22,6 +22,7 @@ import {
   ContextMenuComponents,
 } from '@googleforcreators/design-system';
 import { useRef } from '@googleforcreators/react';
+
 /**
  * Internal dependencies
  */
@@ -36,7 +37,7 @@ import {
   usePresetActions,
 } from '../hooks';
 import useLayerSelect from '../useLayerSelect';
-import { useStory } from '../../..';
+import { useStory } from '../..';
 import useRightClickMenu from '../useRightClickMenu';
 import {
   DEFAULT_DISPLACEMENT,

packages/story-editor/src/app/rightClickMenu/menus/textMenu.js
@@ -22,6 +22,7 @@ import {
   ContextMenuComponents,
 } from '@googleforcreators/design-system';
 import { useRef } from '@googleforcreators/react';
+
 /**
  * Internal dependencies
  */
@@ -36,7 +37,7 @@ import {
   usePresetActions,
 } from '../hooks';
 import useLayerSelect from '../useLayerSelect';
-import { useStory } from '../../..';
+import { useStory } from '../..';
 import useRightClickMenu from '../useRightClickMenu';
 import {
   DEFAULT_DISPLACEMENT,