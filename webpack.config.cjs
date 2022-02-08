323fee1de58dda21f33244e7fff61b0e805a2bbb
Pascal Birchler <pascalb@google.com>
2022-02-08 11:17:02 +0100

    Try fixing fonts import

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -56,7 +56,7 @@ const sharedConfig = {
   resolve: {
     // Fixes resolving packages in the monorepo so we use the "src" folder, not "dist".
     // TODO: Revisit after upgrading to webpack v5 or when splitting repository.
-    mainFields: ['browser', 'module', 'main', 'source'],
+    mainFields: ['exports', 'browser', 'module', 'main', 'source'],
   },
   mode,
   devtool: !isProduction ? 'source-map' : undefined,
