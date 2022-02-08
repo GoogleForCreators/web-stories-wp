e5f3bcb895afa7054ffe347d125b3a6639ccd927
Brittany Feenstra <brittany.feenstra@formidable.com>
2022-02-08 16:46:57 -0700

    conditionally render panel so when Doc Panel is missing in unit tests the component doesnt implode.

--- packages/story-editor/src/components/publishModal/test/mainContent.js
+++ packages/story-editor/src/components/publishModal/test/mainContent.js
@@ -22,6 +22,7 @@ import { axe } from 'jest-axe';
  * Internal dependencies
  */
 import renderWithTheme from '../../../testUtils/renderWithTheme';
+import { InspectorContext } from '../../..';
 import { INPUT_KEYS } from '../constants';
 import MainContent from '../mainContent';
 
@@ -38,27 +39,39 @@ describe('publishModal/mainContent', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it('should have no accessibility issues', async () => {
-    const { container } = renderWithTheme(
-      <MainContent
-        handleUpdateStoryInfo={mockHandleUpdateStoryInfo}
-        handleUpdateSlug={mockHandleUpdateSlug}
-        inputValues={mockInputValues}
-      />
+
+  const inspectorContextValue = {
+    actions: { loadUsers: jest.fn() },
+    state: {
+      users: [{ value: 'foo' }, { value: 'bar' }],
+    },
+    data: {
+      modalInspectorTab: {
+        Pane: null,
+      },
+    },
+  };
+
+  const view = () => {
+    return renderWithTheme(
+      <InspectorContext.Provider value={inspectorContextValue}>
+        <MainContent
+          handleUpdateStoryInfo={mockHandleUpdateStoryInfo}
+          handleUpdateSlug={mockHandleUpdateSlug}
+          inputValues={mockInputValues}
+        />
+      </InspectorContext.Provider>
     );
+  };
+  it('should have no accessibility issues', async () => {
+    const { container } = view();
 
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
 
   it('should trigger handleUpdateStoryInfo on title input change', () => {
-    renderWithTheme(
-      <MainContent
-        handleUpdateStoryInfo={mockHandleUpdateStoryInfo}
-        handleUpdateSlug={mockHandleUpdateSlug}
-        inputValues={mockInputValues}
-      />
-    );
+    view();
 
     const titleInput = screen.getByRole('textbox', { name: 'Story Title' });
 
@@ -74,13 +87,7 @@ describe('publishModal/mainContent', () => {
   });
 
   it('should trigger handleUpdateStoryInfo on excerpt input change', () => {
-    renderWithTheme(
-      <MainContent
-        handleUpdateStoryInfo={mockHandleUpdateStoryInfo}
-        handleUpdateSlug={mockHandleUpdateSlug}
-        inputValues={mockInputValues}
-      />
-    );
+    view();
 
     const descriptionInput = screen.getByRole('textbox', {
       name: 'Story Description',
