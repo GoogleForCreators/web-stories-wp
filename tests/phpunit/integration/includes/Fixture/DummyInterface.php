0e8ae038ffad701e06aedf1db3c8dfc0f5f8c794
Jonny Harris <jon@spacedmonkey.co.uk>
2022-02-09 16:31:11 +0000

    Ignore even more type hint.

--- tests/phpunit/integration/includes/Fixture/DummyInterface.php
+++ tests/phpunit/integration/includes/Fixture/DummyInterface.php
@@ -3,6 +3,5 @@
 namespace Google\Web_Stories\Tests\Integration\Fixture;
 
 interface DummyInterface {
-
-	public function get_dummy(): void;
+	public function get_dummy(); // phpcs:ignore SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingNativeTypeHint
 }
