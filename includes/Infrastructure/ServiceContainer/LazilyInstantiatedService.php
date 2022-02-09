1f836513b2544a8f3cf3d00feb90c2fa773e1de9
Jonny Harris <jon@spacedmonkey.co.uk>
2022-02-09 15:29:15 +0000

    Fix lints.

--- includes/Infrastructure/ServiceContainer/LazilyInstantiatedService.php
+++ includes/Infrastructure/ServiceContainer/LazilyInstantiatedService.php
@@ -56,8 +56,7 @@ final class LazilyInstantiatedService implements Service {
 	 * @return Service Properly instantiated service.
 	 */
 	public function instantiate(): Service {
-		$instantiation = $this->instantiation; // Because uniform variable syntax not supported in PHP 5.6.
-		$service       = $instantiation();
+		$service = $this->instantiation();
 
 		if ( ! $service instanceof Service ) {
 			throw InvalidService::from_service( $service );
