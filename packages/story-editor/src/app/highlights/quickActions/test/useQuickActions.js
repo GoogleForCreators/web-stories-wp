60e387c85849c2598108c95de14478a6bb51bdcd
Tim Arney <tim@line37.com>
2022-02-21 17:54:21 -0500

    add useLocalMedia

--- packages/story-editor/src/app/highlights/quickActions/test/useQuickActions.js
+++ packages/story-editor/src/app/highlights/quickActions/test/useQuickActions.js
@@ -95,6 +95,7 @@ jest.mock('@googleforcreators/media', () => ({
 
 jest.mock('../../../media', () => ({
   ...jest.requireActual('../../../media'),
+  useLocalMedia: jest.fn(),
   TRANSCODABLE_MIME_TYPES: ['video/mp4'],
 }));
 
