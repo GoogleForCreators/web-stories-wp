8ec40e8c44cf5f3f96b4da70f4c83e619d654b95
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-03-03 14:55:45 -0500

    Bump react-photo-album from 1.7.0 to 1.9.0

--- packages/story-editor/src/components/library/panes/media/common/mediaGallery.js
+++ packages/story-editor/src/components/library/panes/media/common/mediaGallery.js
@@ -17,7 +17,7 @@
 /**
  * External dependencies
  */
-import { useCallback, forwardRef } from '@googleforcreators/react';
+import { useCallback } from '@googleforcreators/react';
 import PropTypes from 'prop-types';
 import Gallery from 'react-photo-album';
 
@@ -30,9 +30,11 @@ import { GalleryContainer } from './styles';
 const PHOTO_MARGIN = 8;
 const TARGET_ROW_HEIGHT = 110;
 
-const ContainerRenderer = forwardRef(({ children }, ref) => {
-  return <GalleryContainer ref={ref}>{children}</GalleryContainer>;
-});
+// eslint-disable-next-line react/prop-types -- disable react/prop-types eslint rule for the local component
+const ContainerRenderer = ({ children, containerRef }) => {
+  return <GalleryContainer ref={containerRef}>{children}</GalleryContainer>;
+};
+
 const RowRenderer = ({ children }) => {
   return children;
 };
