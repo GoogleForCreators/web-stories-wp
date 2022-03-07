7068edc97d641896b8175e3da69d7dba3690ba11
codingcarrots20 <coding.carrots@gmail.com>
2022-03-07 18:36:49 +0530

    add test for updateMedia

--- packages/wp-story-editor/src/api/test/media.js
+++ packages/wp-story-editor/src/api/test/media.js
@@ -34,18 +34,18 @@ jest.mock('@wordpress/api-fetch');
 describe('Media API Callbacks', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+  const MEDIA_PATH = `/web-stories/v1/media/`;
 
+  it('getMedia with cacheBust:true should call api with &cache_bust=true', () => {
     apiFetch.mockReturnValue(
       Promise.resolve({
         body: GET_MEDIA_RESPONSE_BODY,
         headers: GET_MEDIA_RESPONSE_HEADER,
       })
     );
-  });
-
-  it('getMedia with cacheBust:true should call api with &cache_bust=true', () => {
     const { getMedia } = bindToCallbacks(apiCallbacks, {
-      api: { media: '/web-stories/v1/media/' },
+      api: { media: MEDIA_PATH },
     });
     getMedia({
       mediaType: '',
@@ -57,6 +57,49 @@ describe('Media API Callbacks', () => {
     expect(apiFetch).toHaveBeenCalledWith(
       expect.objectContaining({
         path: expect.stringMatching('&cache_bust=true'),
+      })
+    );
+  });
+
+  it('updateMedia converts arguments mapping', () => {
+    apiFetch.mockReturnValue(Promise.resolve(GET_MEDIA_RESPONSE_BODY[0]));
+    const { updateMedia } = bindToCallbacks(apiCallbacks, {
+      api: { media: MEDIA_PATH },
+    });
+
+    const mediaId = 1;
+    const mockData = {
+      baseColor: '#123456',
+      blurHash: 'asdafd-dsfgh',
+      isMuted: false,
+      mediaSource: 'source-video',
+      optimizedId: 12,
+      mutedId: 13,
+      featuredMedia: 14,
+      altText: 'New Alt Text',
+      storyId: 11,
+    };
+    const expectedWpKeysMapping = {
+      meta: {
+        web_stories_base_color: mockData.baseColor,
+        web_stories_blurhash: mockData.blurHash,
+        web_stories_optimized_id: mockData.optimizedId,
+        web_stories_muted_id: mockData.mutedId,
+      },
+      web_stories_is_muted: mockData.isMuted,
+      web_stories_media_source: mockData.mediaSource,
+      post: mockData.storyId,
+      featured_media: mockData.featuredMedia,
+      alt_text: mockData.altText,
+    };
+
+    updateMedia(mediaId, mockData);
+
+    expect(apiFetch).toHaveBeenCalledWith(
+      expect.objectContaining({
+        path: MEDIA_PATH + `${mediaId}/`,
+        method: 'POST',
+        data: expectedWpKeysMapping,
       })
     );
   });
