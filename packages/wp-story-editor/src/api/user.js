382fead2d160d6bb1ee9f9e5ff9f12cf685f88e5
codingcarrots20 <coding.carrots@gmail.com>
2022-03-07 14:14:48 +0530

    update getCurrentUser callback

--- packages/wp-story-editor/src/api/user.js
+++ packages/wp-story-editor/src/api/user.js
@@ -26,13 +26,11 @@ import { snakeToCamelCaseObjectKeys } from '@web-stories-wp/wp-utils';
 export function getCurrentUser(config) {
   return apiFetch({
     path: config.api.currentUser,
-  }).then((resp) => {
-    return {
-      trackingOptin: resp.meta.web_stories_tracking_optin,
-      onboarding: resp.meta.web_stories_onboarding,
-      mediaOptimization: resp.meta.web_stories_media_optimization,
-    };
-  });
+  }).then((resp) => ({
+    trackingOptin: resp.meta.web_stories_tracking_optin,
+    onboarding: resp.meta.web_stories_onboarding,
+    mediaOptimization: resp.meta.web_stories_media_optimization,
+  }));
 }
 export function updateCurrentUser(config, data) {
   const { trackingOptin, onboarding, mediaOptimization } = data;
