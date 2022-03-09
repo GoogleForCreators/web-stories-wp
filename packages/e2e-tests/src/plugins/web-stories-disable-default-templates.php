d70a0a2fc8da7aac9fd09dacb57aa5d5453cac9e
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-09 14:34:49 +0000

    Remove comment.

--- packages/e2e-tests/src/plugins/web-stories-disable-default-templates.php
+++ packages/e2e-tests/src/plugins/web-stories-disable-default-templates.php
@@ -5,11 +5,6 @@
  * Description: Disable default templates.
  * Author:      Google
  * Author URI:  https://opensource.google.com
- *
- * @package   Google\Site_Kit
- * @copyright 2022 Google LLC
- * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
- * @link      https://sitekit.withgoogle.com
  */
 
 namespace Google\Web_Stories\E2E\DefaultTemplates;
