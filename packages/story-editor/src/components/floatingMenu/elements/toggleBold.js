8a9bf77e218185a7eab81ccce0d18fec853425e0
Miina Sikk <miina.sikk@gmail.com>
2022-03-09 15:44:57 +0200

    Make bold toggle functional

--- packages/story-editor/src/components/floatingMenu/elements/toggleBold.js
+++ packages/story-editor/src/components/floatingMenu/elements/toggleBold.js
@@ -19,18 +19,46 @@
  */
 import { Icons } from '@googleforcreators/design-system';
 import { __ } from '@googleforcreators/i18n';
+import { trackEvent } from '@googleforcreators/tracking';
+import { useCallback } from '@googleforcreators/react';
 
 /**
  * Internal dependencies
  */
-import { IconButton } from './shared';
+import useRichTextFormatting from '../../panels/design/textStyle/useRichTextFormatting';
+import updateProperties from '../../inspector/design/updateProperties';
+import { useStory } from '../../../app';
+import { IconButton, useProperties } from './shared';
 
 function ToggleBold() {
+  const { content } = useProperties(['content']);
+  const updateSelectedElements = useStory(
+    (state) => state.actions.updateSelectedElements
+  );
+
+  const pushUpdate = useCallback(
+    (update) => {
+      trackEvent('floating_menu', {
+        name: 'set_bold',
+        element: 'text',
+      });
+      updateSelectedElements({
+        properties: (element) => updateProperties(element, update, true),
+      });
+    },
+    [updateSelectedElements]
+  );
+  const {
+    textInfo: { isBold },
+    handlers: { handleClickBold },
+  } = useRichTextFormatting([{ content, type: 'text' }], pushUpdate);
+
   return (
     <IconButton
+      isToggled={isBold}
       Icon={Icons.LetterBBold}
       title={__('Toggle bold text', 'web-stories')}
-      onClick={() => {}}
+      onClick={() => handleClickBold(!isBold)}
     />
   );
 }
