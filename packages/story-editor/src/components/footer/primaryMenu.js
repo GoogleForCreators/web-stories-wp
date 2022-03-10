6ac4d6330419dfe5369b0b20b4908e4537faacce
Brittany Feenstra <brittany.feenstra@formidable.com>
2022-03-09 17:25:11 -0700

    zoom isnt getting picked up in karma anymore? adding the zindex fix to it that is on the other bits of the footer to see if that works.

--- packages/story-editor/src/components/footer/primaryMenu.js
+++ packages/story-editor/src/components/footer/primaryMenu.js
@@ -22,6 +22,7 @@ import styled from 'styled-components';
 /**
  * Internal dependencies
  */
+import { Z_INDEX_FOOTER_POPUP } from '../../constants/zIndex';
 import ZoomSelector from './zoomSelector';
 import { GridViewButton } from './gridview';
 import { FOOTER_MENU_GAP, FOOTER_MARGIN } from './constants';
@@ -32,6 +33,7 @@ const Wrapper = styled.div`
   justify-content: flex-end;
   width: 100%;
   height: 100%;
+  z-index: ${Z_INDEX_FOOTER_POPUP};
 `;
 
 const MenuItems = styled.div`
