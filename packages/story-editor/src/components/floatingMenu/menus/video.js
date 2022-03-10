e58c814733dfa4c910ba7d9b65d8b308315abe09
Brooke Graham <brooke.graham@formidable.com>
2022-03-10 10:17:57 -0700

    Floating Menus: Deduplicate Replace Media  (#10874)
    * remove swap media from floating menus
    
    * remove separator

--- packages/story-editor/src/components/floatingMenu/menus/video.js
+++ packages/story-editor/src/components/floatingMenu/menus/video.js
@@ -23,7 +23,6 @@ import { memo } from '@googleforcreators/react';
  * Internal dependencies
  */
 import {
-  SwapMedia,
   LayerOpacity,
   FlipHorizontal,
   FlipVertical,
@@ -39,10 +38,6 @@ import {
 const FloatingVideoMenu = memo(function FloatingVideoMenu() {
   return (
     <>
-      <SwapMedia />
-
-      <Separator />
-
       <LayerOpacity />
 
       <Separator />
