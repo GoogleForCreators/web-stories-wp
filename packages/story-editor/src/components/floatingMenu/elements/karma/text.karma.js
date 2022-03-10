20488bcd5e82e29cfbf71cac192206a1d98bfa6b
Brittany Feenstra <brittany.feenstra@formidable.com>
2022-03-10 11:57:37 -0700

    add the disable test comment i forgot on eslint, confirmed that this is the issue and not anything to do with zindex

--- packages/story-editor/src/components/floatingMenu/elements/karma/text.karma.js
+++ packages/story-editor/src/components/floatingMenu/elements/karma/text.karma.js
@@ -98,6 +98,7 @@ describe('Design Menu: Text Styles', () => {
 
     // This is failing due to keyboard problems
     // TODO #10872 https://github.com/GoogleForCreators/web-stories-wp/issues/10872
+    // eslint-disable-next-line jasmine/no-disabled-tests
     xit('should allow changing text color for a selection from the design menu', async () => {
       // Enter edit-mode
       await fixture.events.keyboard.press('Enter');
