23f6fda9fff7140c5aeab633f611dbdc772b78cd
Maxwell Yinger <max.yinger@formidable.com>
2022-03-10 10:39:17 -0600

    just use actual context selector for tests vs mocking it

--- packages/story-editor/src/app/pageCanvas/test/usePageCanvas.js
+++ packages/story-editor/src/app/pageCanvas/test/usePageCanvas.js
@@ -17,18 +17,17 @@
  * External dependencies
  */
 import { act, renderHook } from '@testing-library/react-hooks';
-import { v4 as uuidv4 } from 'uuid';
-
 /**
  * Internal dependencies
  */
-jest.mock('../../story/useStory');
-jest.mock('../../../utils/storyPageToCanvas');
 import { PageCanvasProvider, usePageCanvas } from '..';
 import storyPageToCanvas from '../../../utils/storyPageToCanvas';
 import useStory from '../../story/useStory';
 import createMockPage from '../testUtils/createMockPage';
-import createMockUseContextSelector from '../testUtils/createMockUseContextSelector';
+import { MockStoryProvider, useStoryMock } from '../testUtils/useStoryMock';
+
+jest.mock('../../story/useStory');
+jest.mock('../../../utils/storyPageToCanvas');
 
 jest.mock('../../../utils/contrastUtils', () => {
   return {
@@ -43,24 +42,27 @@ jest.mock('../getPixelDataFromCanvas', () => {
   };
 });
 
-function renderUsePageCanvas() {
-  return renderHook(() => usePageCanvas(), {
-    wrapper: PageCanvasProvider,
+function renderUsePageCanvas(_mockStoryContext) {
+  return renderHook(() => [usePageCanvas(), useStoryMock()], {
+    wrapper: ({ children }) => (
+      <MockStoryProvider mockContextValue={_mockStoryContext}>
+        <PageCanvasProvider>{children}</PageCanvasProvider>
+      </MockStoryProvider>
+    ),
   });
 }
 
 describe('usePageCanvas', () => {
   let mockStoryContext;
   let currentPage;
-  let forceUseStoryRender;
 
   beforeEach(() => {
-    currentPage = createMockPage();
     storyPageToCanvas.mockImplementation(
       () =>
         'lets pretend this is a canvas. I wont tell the computer if you dont 🤫'
     );
 
+    currentPage = createMockPage();
     mockStoryContext = {
       actions: {},
       state: {
@@ -70,12 +72,7 @@ describe('usePageCanvas', () => {
       },
     };
 
-    // kind of a jank ass mock implementation of useContextSelector but it works
-    const getContextValue = () => mockStoryContext;
-    const { mockUseContextSelector, forceRender } =
-      createMockUseContextSelector(getContextValue);
-    forceUseStoryRender = forceRender;
-    useStory.mockImplementation(mockUseContextSelector);
+    useStory.mockImplementation(useStoryMock);
   });
 
   afterEach(() => {
@@ -85,57 +82,70 @@ describe('usePageCanvas', () => {
   describe('calculateAccessibleTextColors', () => {
     describe('page canvas cache', () => {
       it('generates a page canvas when called and canvas cache is not populated', async () => {
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
         const element = { id: 'a' };
 
         // cache populates with generated canvas for currentPage
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(element);
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
+            element
+          );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
       });
 
       it('does not generate a page canvas when called and canvas cache is populated', async () => {
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
         const element1 = { id: 'a' };
         const element2 = { id: 'b' };
 
         // cache populates with generated canvas for currentPage
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(element1);
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
+            element1
+          );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
 
         // cache already populated with generated canvas for currentPage
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(element2);
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
+            element2
+          );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
       });
 
       it('generates a page canvas when called and canvas cache is stale', async () => {
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
+        const [, useStoryMockCurrent] = result.current;
+        const { actions: storyActions } = useStoryMockCurrent;
+
         const element1 = { id: 'a' };
         const element2 = { id: 'b' };
 
         // cache populates with generated canvas for currentPage
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(element1);
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
+            element1
+          );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
 
-        // make currentPage stale mocking an update to it
-        const updatedCurrentPage = {
-          ...currentPage,
-          elements: [...currentPage.elements],
-        };
-        mockStoryContext.state.pages = [updatedCurrentPage];
-        mockStoryContext.state.currentPage = updatedCurrentPage;
-        act(() => forceUseStoryRender());
+        // make currentPage stale by making an update to it
+        act(() => storyActions.duplicateLastElement());
+        expect(result.current[0].state.pageCanvasMap).toStrictEqual({});
 
         // cache populates with generated canvas for updated currentPage
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(element2);
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
+            element2
+          );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(2);
       });
@@ -149,14 +159,15 @@ describe('usePageCanvas', () => {
         mockStoryContext.state.selectedElementIds = [selectedElement.id];
 
         // render hook
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
 
         // since we're testing an existing element against the currentPage
         // calculateAccessibleTextColors knows to exclude the element we're testing
         // from the generated canvas. It holds this canvas generated from a partial
         // of the page in the snapshot cache.
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
             selectedElement
           );
         });
@@ -170,34 +181,37 @@ describe('usePageCanvas', () => {
         mockStoryContext.state.selectedElementIds = [selectedElement.id];
 
         // render hook
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
+        const [, useStoryMockCurrent] = result.current;
+        const { actions: storyActions } = useStoryMockCurrent;
 
         // since we're testing an existing element against the currentPage
         // calculateAccessibleTextColors knows to exclude the element we're testing
         // from the generated canvas. It holds this canvas generated from a partial
         // of the page in the snapshot cache.
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
             selectedElement
           );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
 
         // update the selected element
-        currentPage.elements = [
-          ...currentPage.elements.filter(({ id }) => id !== selectedElement.id),
-          { ...selectedElement, opacity: 50 },
-        ];
-        act(() => forceUseStoryRender());
+        act(() =>
+          storyActions.updateSelectedElement({ properties: { opacity: 50 } })
+        );
 
         // change selection
-        const anotherElement = currentPage.elements[0];
-        mockStoryContext.state.selectedElementIds = [anotherElement.id];
-        act(() => forceUseStoryRender());
+        const anotherElementId = currentPage.elements[0].id;
+        act(() =>
+          storyActions.setSelectedElement({ elementId: anotherElementId })
+        );
 
         // revert selection back to originally selected element
-        mockStoryContext.state.selectedElementIds = [selectedElement.id];
-        act(() => forceUseStoryRender());
+        act(() =>
+          storyActions.setSelectedElement({ elementId: selectedElement.id })
+        );
 
         // since we haven't called calculateAccessibleTextColors since the
         // last story partial page changed, and with the current selection
@@ -205,7 +219,8 @@ describe('usePageCanvas', () => {
         // last time we called calculateAccessibleTextColors, the snapshot
         // cache should still be valid and shouldn't have been updated.
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
             selectedElement
           );
         });
@@ -219,30 +234,32 @@ describe('usePageCanvas', () => {
         mockStoryContext.state.selectedElementIds = [selectedElement.id];
 
         // render hook
-        const { result } = renderUsePageCanvas();
+        const { result } = renderUsePageCanvas(mockStoryContext);
+        const [, useStoryMockCurrent] = result.current;
+        const { actions: storyActions } = useStoryMockCurrent;
 
         // since we're testing an existing element against the currentPage
         // calculateAccessibleTextColors knows to exclude the element we're testing
         // from the generated canvas. It holds this canvas generated from a partial
         // of the page in the snapshot cache.
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
             selectedElement
           );
         });
         expect(storyPageToCanvas).toHaveBeenCalledTimes(1);
 
         // add a new element
-        const newElement = { ...selectedElement, id: uuidv4() };
-        currentPage.elements = [...currentPage.elements, newElement];
-        act(() => forceUseStoryRender());
+        act(() => storyActions.duplicateLastElement());
 
         // in this scenario, the snapshot cache is no longer valid
         // because we've added a new element to the currentPage.
         // We should be generating a new canvas based on the updated
         // page partial in this scenario.
         await act(async () => {
-          await result.current.actions.calculateAccessibleTextColors(
+          const [usePageCanvasCurrent] = result.current;
+          await usePageCanvasCurrent.actions.calculateAccessibleTextColors(
             selectedElement
           );
         });
