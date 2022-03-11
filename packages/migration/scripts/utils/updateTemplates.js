6bb9a6b44b2618c8c8636da2ed0bb57fe2673796
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-11 11:52:31 +0000

    Fix lint.

--- packages/migration/scripts/utils/updateTemplates.js
+++ packages/migration/scripts/utils/updateTemplates.js
@@ -23,7 +23,6 @@ import { join, resolve } from 'path';
 /**
  * Internal dependencies
  */
-// eslint-disable-next-line import/no-unresolved
 import { migrate, DATA_VERSION } from '../module.js';
 
 function updateTemplates(templatesDir) {
