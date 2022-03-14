bb50c5d9ae3613280aab31580821ad518458663a
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-14 18:21:14 +0000

    Revert changes.

--- packages/e2e-tests/src/specs/wordpress/postLocking.js
+++ packages/e2e-tests/src/specs/wordpress/postLocking.js
@@ -24,8 +24,9 @@ import {
   insertStoryTitle,
   publishStory,
   visitAdminPage,
+  activatePlugin,
+  deactivatePlugin,
   takeSnapshot,
-  withPlugin,
 } from '@web-stories-wp/e2e-test-utils';
 
 const percyCSS = `.dashboard-grid-item-date { display: none; }`;
@@ -34,7 +35,6 @@ const storyTitle = 'Test post lock';
 
 describe('Post Locking', () => {
   withExperimentalFeatures(['enablePostLocking']);
-  withPlugin('e2e-tests-post-lock-mock');
 
   beforeAll(async () => {
     await createNewStory();
@@ -42,6 +42,12 @@ describe('Post Locking', () => {
     await insertStoryTitle(storyTitle);
 
     await publishStory();
+
+    await activatePlugin('e2e-tests-post-lock-mock');
+  });
+
+  afterAll(async () => {
+    await deactivatePlugin('e2e-tests-post-lock-mock');
   });
 
   it('should be able to open the dashboard with locked story', async () => {
