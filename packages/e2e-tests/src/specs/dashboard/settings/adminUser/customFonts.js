19d1a75843870e514ec5ea73470168fc9660e707
Jonny Harris <jon@spacedmonkey.co.uk>
2022-03-21 12:16:35 +0000

    Handle errors when adding fonts.

---
+++ packages/e2e-tests/src/specs/dashboard/settings/adminUser/customFonts.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import {
+  withExperimentalFeatures,
+  addCustomFont,
+  removeCustomFont,
+} from '@web-stories-wp/e2e-test-utils';
+
+const OPEN_SANS_CONDENSED_URL = `${process.env.WP_BASE_URL}/wp-content/e2e-assets/OpenSansCondensed-Light.ttf`;
+
+describe('Custom Fonts', () => {
+  withExperimentalFeatures(['customFonts']);
+
+  afterEach(async () => {
+    await removeCustomFont();
+  });
+
+  it('should show error on trying add font twice', async () => {
+    await addCustomFont(OPEN_SANS_CONDENSED_URL);
+    await addCustomFont(OPEN_SANS_CONDENSED_URL);
+
+    await expect(page).toMatch(
+      'A font with this name Open Sans Condensed Light already exists.'
+    );
+  });
+});
--- packages/wp-dashboard/src/components/editorSettings/customFonts/index.js
+++ packages/wp-dashboard/src/components/editorSettings/customFonts/index.js
@@ -187,12 +187,10 @@ function CustomFontsSettings({
   const handleOnSave = useCallback(async () => {
     if (canSave) {
       const urlWithProtocol = withProtocol(fontUrl);
-      let canFetch = false;
       try {
         await fetch(urlWithProtocol, {
           method: 'HEAD',
         });
-        canFetch = true;
       } catch (err) {
         setInputError(
           __(
@@ -200,34 +198,46 @@ function CustomFontsSettings({
             'web-stories'
           )
         );
+        return;
       }
-      if (canFetch) {
-        try {
-          trackEvent('add_custom_font', {
-            url: urlWithProtocol,
-          });
-          const fontData = await getFontDataFromUrl(urlWithProtocol);
-          if (!fontData.family) {
-            setInputError(
-              __('Something went wrong, please try again.', 'web-stories')
-            );
-          } else {
-            await addCustomFont({ ...fontData, url: urlWithProtocol });
-            await fetchCustomFonts();
-            setFontUrl('');
-          }
-        } catch (err) {
+      let fontData;
+      try {
+        trackEvent('add_custom_font', {
+          url: urlWithProtocol,
+        });
+        fontData = await getFontDataFromUrl(urlWithProtocol);
+        if (!fontData.family) {
           setInputError(
-            sprintf(
-              /* translators: %s: list of allowed font types. */
-              __(
-                'Getting font data failed, please ensure the URL points directly to a %s file.',
-                'web-stories'
-              ),
-              translateToExclusiveList(ALLOWED_FONT_TYPES)
-            )
+            __('Something went wrong, please try again.', 'web-stories')
           );
+          return;
         }
+      } catch (err) {
+        setInputError(
+          sprintf(
+            /* translators: %s: list of allowed font types. */
+            __(
+              'Getting font data failed, please ensure the URL points directly to a %s file.',
+              'web-stories'
+            ),
+            translateToExclusiveList(ALLOWED_FONT_TYPES)
+          )
+        );
+        return;
+      }
+
+      try {
+        await addCustomFont({ ...fontData, url: urlWithProtocol });
+        await fetchCustomFonts();
+        setFontUrl('');
+      } catch (err) {
+        setInputError(
+          sprintf(
+            /* translators: %s: font name. */
+            __('A font with this name %s already exists.', 'web-stories'),
+            fontData.family
+          )
+        );
       }
     }
   }, [addCustomFont, fetchCustomFonts, canSave, fontUrl]);
