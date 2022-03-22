3f1900173f8475697aafd6f11168ce269544e7ce
BGraham <brooke.graham@formidable.com>
2022-03-22 14:12:32 -0600

    add radius

--- packages/story-editor/src/components/header/title.js
+++ packages/story-editor/src/components/header/title.js
@@ -43,6 +43,8 @@ const Input = styled.input`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  border-radius: ${({ theme }) => theme.borders.radius.small};
+
   ${themeHelpers.focusableOutlineCSS};
 
   ${({ isHighlighted }) =>
