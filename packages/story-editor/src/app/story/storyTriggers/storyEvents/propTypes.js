d7d1376569b306617e73f4a99ad0b43ddba508b7
Sayed Taqui <sayedtaqui@users.noreply.github.com>
2022-03-22 13:09:16 +0530

    Update todo

--- packages/story-editor/src/app/story/storyTriggers/storyEvents/propTypes.js
+++ packages/story-editor/src/app/story/storyTriggers/storyEvents/propTypes.js
@@ -17,7 +17,7 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import { OutputStory as storyPropType } from '@googleforcreators/output'; // @todo Doesn't look correct.
+import { OutputStory as storyPropType } from '@googleforcreators/output'; // @todo See issue #11015
 
 export const registerPropTypes = {
   currentStory: storyPropType,
