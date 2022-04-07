ab2293651684f015a9a0404671941a3198d41fce
Sayed Taqui <sayedtaqui@users.noreply.github.com>
2022-04-07 17:34:43 +0530

    Run eslint

--- packages/create-web-stories/bin/common.js
+++ packages/create-web-stories/bin/common.js
@@ -23,16 +23,16 @@ import fse from 'fs-extra';
 import chalk from 'chalk';
 
 /**
- * Internal dependencies.
+ * Internal dependencies
  */
 import { PRIVATE_REGISTRY_URL, REACT_SUPPORTED_VERSION } from './constants.js';
 
-const __filename = fileURLToPath( import.meta.url );
-const __dirname = dirname( __filename );
-const wavingHand = String.fromCodePoint( 0x1f44b );
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const wavingHand = String.fromCodePoint(0x1f44b);
 
-const LOGO = fse.readFileSync( path.join( __dirname, 'text-art.txt' ) );
-const WELCOME_MESSAGE = `Hi! ${ wavingHand } Welcome to Web stories. Let us help you try our story creation suite`;
+const LOGO = fse.readFileSync(path.join(__dirname, 'text-art.txt'));
+const WELCOME_MESSAGE = `Hi! ${wavingHand} Welcome to Web stories. Let us help you try our story creation suite`;
 const BOILERPLATE_DIR_PATH = `../boilerplate`;
 const SHARED_DIR_PATH = '../shared';
 const CONFIG_FILE = 'config.json';
@@ -41,161 +41,191 @@ const CONFIG_FILE = 'config.json';
  * Get boilerplate config.json data list.
  */
 const getBoilerplateConfigList = () => {
-  const boilerplateDir = path.join( __dirname, BOILERPLATE_DIR_PATH );
-  const boilerplateDirNames = fse.readdirSync( boilerplateDir );
+  const boilerplateDir = path.join(__dirname, BOILERPLATE_DIR_PATH);
+  const boilerplateDirNames = fse.readdirSync(boilerplateDir);
 
-  return boilerplateDirNames.map( ( name ) => {
+  return boilerplateDirNames.map((name) => {
     const configPath = path.join(
       __dirname,
       BOILERPLATE_DIR_PATH,
       name,
-      CONFIG_FILE,
+      CONFIG_FILE
     );
 
-    return JSON.parse( fse.readFileSync( configPath ) );
-  } );
+    return JSON.parse(fse.readFileSync(configPath));
+  });
 };
 
 /**
  * Get boilerplate data.
+ *
+ * @param {string} boilerplateName Boilerplate name.
  */
-const getBoilerplateConfig = ( boilerplateName ) => {
-  return getBoilerplateConfigList().find( ( { name } ) => name === boilerplateName );
+const getBoilerplateConfig = (boilerplateName) => {
+  return getBoilerplateConfigList().find(
+    ({ name }) => name === boilerplateName
+  );
 };
 
 /**
  * Install dependencies from configuration.
+ *
+ * @param {string} boilerplateName Boilerplate name.
+ * @param {string} projectPath Project path.
+ * @param {boolean} isPrivate Is private
  */
-function installDependenciesFromConfig( boilerplateName, projectPath, isPrivate ) {
-  const config = getBoilerplateConfig( boilerplateName );
+function installDependenciesFromConfig(
+  boilerplateName,
+  projectPath,
+  isPrivate
+) {
+  const config = getBoilerplateConfig(boilerplateName);
   const { dependencies, devDependencies } = config;
 
-  if ( isPrivate ) {
-    log( `\nUsing registry at url \`${ PRIVATE_REGISTRY_URL }\`\n` );
+  if (isPrivate) {
+    log(`\nUsing registry at url \`${PRIVATE_REGISTRY_URL}\`\n`);
   }
 
-  const registry = isPrivate ? `--registry ${ PRIVATE_REGISTRY_URL } ` : '';
+  const registry = isPrivate ? `--registry ${PRIVATE_REGISTRY_URL} ` : '';
 
-  if ( dependencies && dependencies.length ) {
-    const command = `npm ${ registry }i ` + dependencies.join( ' ' );
-    execSync( command, {
+  if (dependencies && dependencies.length) {
+    const command = `npm ${registry}i ` + dependencies.join(' ');
+    execSync(command, {
       cwd: projectPath,
-    } );
+    });
   }
 
-  if ( devDependencies && devDependencies.length ) {
-    const command = `npm ${ registry }i --save-dev` + devDependencies.join( ' ' );
-    execSync( command, {
+  if (devDependencies && devDependencies.length) {
+    const command = `npm ${registry}i --save-dev` + devDependencies.join(' ');
+    execSync(command, {
       cwd: projectPath,
-    } );
+    });
   }
 }
 
 /**
  * Get boilerplate path.
+ *
+ * @param {string} boilerplateName Boilerplate name.
  */
-function getBoilerplatePath( boilerplateName ) {
-  const boilerplateDir = path.join( __dirname, BOILERPLATE_DIR_PATH );
-  const boilerplateDirNames = fse.readdirSync( boilerplateDir );
+function getBoilerplatePath(boilerplateName) {
+  const boilerplateDir = path.join(__dirname, BOILERPLATE_DIR_PATH);
+  const boilerplateDirNames = fse.readdirSync(boilerplateDir);
 
-  const boilerplateDirName = boilerplateDirNames.find( ( dirName ) => {
-    const configPath = path.join( boilerplateDir, dirName, CONFIG_FILE );
-    const config = JSON.parse( fse.readFileSync( configPath ) );
+  const boilerplateDirName = boilerplateDirNames.find((dirName) => {
+    const configPath = path.join(boilerplateDir, dirName, CONFIG_FILE);
+    const config = JSON.parse(fse.readFileSync(configPath));
     return config.name === boilerplateName;
-  } );
+  });
 
-  return path.join( boilerplateDir, boilerplateDirName );
+  return path.join(boilerplateDir, boilerplateDirName);
 }
 
 /**
  * Scaffold boilerplate with CRA.
+ *
+ * @param {string} boilerplateName Boilerplate name.
+ * @param {string} projectName Project name.
+ * @param {boolean} isPrivate Is private.
  */
-function scaffoldBoilerplateWithCRA( boilerplateName, projectName, isPrivate ) {
-  const boilerplatePath = getBoilerplatePath( boilerplateName );
-  const sharedPath = path.join( __dirname, SHARED_DIR_PATH );
-  const projectPath = path.join( process.cwd(), projectName );
+function scaffoldBoilerplateWithCRA(boilerplateName, projectName, isPrivate) {
+  const boilerplatePath = getBoilerplatePath(boilerplateName);
+  const sharedPath = path.join(__dirname, SHARED_DIR_PATH);
+  const projectPath = path.join(process.cwd(), projectName);
 
-  execSync( `npx create-react-app@latest ${ projectName }`, {
+  execSync(`npx create-react-app@latest ${projectName}`, {
     cwd: process.cwd(),
-  } );
+  });
 
   replaceDir(
-    path.resolve( boilerplatePath, 'src' ),
-    path.resolve( projectPath, 'src' ),
+    path.resolve(boilerplatePath, 'src'),
+    path.resolve(projectPath, 'src')
   );
 
   replaceDir(
-    path.resolve( boilerplatePath, 'public' ),
-    path.resolve( projectPath, 'public' ),
+    path.resolve(boilerplatePath, 'public'),
+    path.resolve(projectPath, 'public')
   );
 
-  const { replacements } = getBoilerplateConfig( boilerplateName );
+  const { replacements } = getBoilerplateConfig(boilerplateName);
 
-  replacements.forEach( ( { src, dest } ) => {
-    fse.copySync( path.join( sharedPath, src ), path.join( projectPath, dest ) );
-  } );
+  replacements.forEach(({ src, dest }) => {
+    fse.copySync(path.join(sharedPath, src), path.join(projectPath, dest));
+  });
 
   // Install boilerplate dependencies listed in meta
-  installDependenciesFromConfig( boilerplateName, projectPath, isPrivate );
+  installDependenciesFromConfig(boilerplateName, projectPath, isPrivate);
 
   // Downgrade react and react-dom to the supported version.
-  execSync( `npm i react@${ REACT_SUPPORTED_VERSION } react-dom@${ REACT_SUPPORTED_VERSION }`, {
-    cwd: projectPath,
-  } );
+  execSync(
+    `npm i react@${REACT_SUPPORTED_VERSION} react-dom@${REACT_SUPPORTED_VERSION}`,
+    {
+      cwd: projectPath,
+    }
+  );
 }
 
 /**
  * Scaffold custom boilerplate.
+ *
+ * @param {string} boilerplateName Boilerplate name.
+ * @param {string} projectName Project name.
+ * @param {boolean} isPrivate Is private.
  */
-function scaffoldBoilerplateCustom( boilerplateName, projectName, isPrivate ) {
-  const projectPath = path.join( process.cwd(), projectName );
-  const sharedPath = path.join( __dirname, SHARED_DIR_PATH );
+function scaffoldBoilerplateCustom(boilerplateName, projectName, isPrivate) {
+  const projectPath = path.join(process.cwd(), projectName);
+  const sharedPath = path.join(__dirname, SHARED_DIR_PATH);
 
-  const boilerplatePath = getBoilerplatePath( boilerplateName );
+  const boilerplatePath = getBoilerplatePath(boilerplateName);
 
-  fse.copySync( boilerplatePath, projectPath );
-  fse.removeSync( path.join( projectPath, CONFIG_FILE ) );
+  fse.copySync(boilerplatePath, projectPath);
+  fse.removeSync(path.join(projectPath, CONFIG_FILE));
 
   // Get files from shared folder as per the mapping.
-  const { replacements } = getBoilerplateConfig( boilerplateName );
+  const { replacements } = getBoilerplateConfig(boilerplateName);
 
-  replacements.forEach( ( { src, dest } ) => {
-    fse.copySync( path.join( sharedPath, src ), path.join( projectPath, dest ) );
-  } );
+  replacements.forEach(({ src, dest }) => {
+    fse.copySync(path.join(sharedPath, src), path.join(projectPath, dest));
+  });
 
   // Install main dependencies.
-  execSync( 'npm i', {
-    cwd: path.join( process.cwd(), projectName ),
-  } );
+  execSync('npm i', {
+    cwd: path.join(process.cwd(), projectName),
+  });
 
   // Install boilerplate dependencies listed in meta.
-  installDependenciesFromConfig(
-    boilerplateName,
-    projectPath,
-    isPrivate,
-  );
+  installDependenciesFromConfig(boilerplateName, projectPath, isPrivate);
 }
 
 /**
  * Replace directory.
+ *
+ * @param {string} sourcePath Source path.
+ * @param {string} destinationPath Destination path.
  */
-function replaceDir( sourcePath, destinationPath ) {
-  fse.removeSync( destinationPath );
-  fse.copySync( path.join( sourcePath ), path.join( destinationPath ) );
+function replaceDir(sourcePath, destinationPath) {
+  fse.removeSync(destinationPath);
+  fse.copySync(path.join(sourcePath), path.join(destinationPath));
 }
 
 /**
  * Utility function for logging.
+ *
+ * @param {string} text Text to log.
+ * @param {string} color Color supported by chalk.
  */
-function log( text, color ) {
-  console.log( color ? chalk[ color ]( text ) : text );
+function log(text, color = '') {
+  console.log(color ? chalk[color](text) : text);
 }
 
 /**
  * Get boilerplate directory name.
+ *
+ * @param {string} boilerplate Boilerplate.
+ * @param {string} setupType Setup type
  */
-function getBoilerplateName( boilerplate, setupType ) {
-  return ( boilerplate && setupType ) ? `${ boilerplate }-${ setupType }` : 'none';
+function getBoilerplateName(boilerplate, setupType) {
+  return boilerplate && setupType ? `${boilerplate}-${setupType}` : 'none';
 }
 
 export {
@@ -206,5 +236,5 @@ export {
   scaffoldBoilerplateCustom,
   scaffoldBoilerplateWithCRA,
   WELCOME_MESSAGE,
-  LOGO
+  LOGO,
 };
--- packages/create-web-stories/bin/index.js
+++ packages/create-web-stories/bin/index.js
@@ -15,9 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * External dependencies
- */
 import { program } from 'commander';
 import prompts from 'prompts';
 import fse from 'fs-extra';
@@ -37,62 +34,65 @@ import {
 import {
   DEFAULT_PROJECT_NAME,
   BOILERPLATE_NAMES,
-  SETUP_TYPES
+  SETUP_TYPES,
 } from './constants.js';
 
 program
-  .option( '-p, --private', 'install from local registry' )
-  .option( '-n, --name <name>', 'project folder name', 'none' )
+  .option('-p, --private', 'install from local registry')
+  .option('-n, --name <name>', 'project folder name', 'none')
   .option(
     '-t, --setupType <setup-type>',
     'type of setup to use for scaffolding the project, pass 0 for CRA or 1 for custom setup',
-    'none',
+    'none'
   )
   .option(
     '-b, --boilerplate <boilerplate>',
     'which boilerplate to use',
-    'none',
+    'none'
   );
 
-program.parse( process.argv );
+program.parse(process.argv);
 
 const options = program.opts();
-const isPrivate = Boolean( options.private );
+const isPrivate = Boolean(options.private);
 const boilerplate = options.boilerplate;
 let projectName = options.name;
 let setupType = options.setupType;
-let boilerplateName = getBoilerplateName( options.boilerplate, setupType );
+let boilerplateName = getBoilerplateName(options.boilerplate, setupType);
 
 const userPrompts = [];
 const boilerplateDataList = getBoilerplateConfigList();
-const foldersInCwd = fse.readdirSync( process.cwd() );
+const foldersInCwd = fse.readdirSync(process.cwd());
 
 // Prompt for project name if option does not have default value
-if ( projectName === 'none' ) {
-  userPrompts.push( {
+if (projectName === 'none') {
+  userPrompts.push({
     type: 'text',
     name: 'projectName',
-    message: `Project Name (default : ${ DEFAULT_PROJECT_NAME })`,
-    validate: ( value ) => {
-      if ( value.match( /[^a-z-]/g ) ) {
+    message: `Project Name (default : ${DEFAULT_PROJECT_NAME})`,
+    validate: (value) => {
+      if (value.match(/[^a-z-]/g)) {
         return `Project name can only have small case characters and hyphen`;
-      } else if ( foldersInCwd.includes( value ) ) {
+      } else if (foldersInCwd.includes(value)) {
         return 'OOPS! Looks like you already have a directory with that name';
       }
 
       return true;
     },
-  } );
-} else if ( projectName.match( /[^a-z-]/g ) || foldersInCwd.includes( projectName ) ) {
+  });
+} else if (
+  projectName.match(/[^a-z-]/g) ||
+  foldersInCwd.includes(projectName)
+) {
   log(
     'Invalid project name: Project name can only have small case characters and hyphen',
-    'red',
+    'red'
   );
-  process.exit( 1 );
+  process.exit(1);
 }
 
 // Prompt for setup type if option has default value.
-if ( setupType === 'none' ) {
+if (setupType === 'none') {
   const choices = [
     {
       title: 'Create React App',
@@ -104,104 +104,105 @@ if ( setupType === 'none' ) {
     },
   ];
 
-  userPrompts.push( {
+  userPrompts.push({
     type: 'select',
     name: 'setupType',
     message: 'Please select what kind for setup would you prefer:',
     choices,
-  } );
-} else if ( ! Object.values( SETUP_TYPES ).includes( setupType ) ) { // Validate, if value was passed as an argument.
+  });
+} else if (!Object.values(SETUP_TYPES).includes(setupType)) {
+  // Validate, if value was passed as an argument.
   log(
-    `Invalid setup type passed. Argument value should be either ${ SETUP_TYPES.CRA } or ${ SETUP_TYPES.CUSTOM }`,
-    'red',
+    `Invalid setup type passed. Argument value should be either ${SETUP_TYPES.CRA} or ${SETUP_TYPES.CUSTOM}`,
+    'red'
   );
 
-  process.exit( 1 );
+  process.exit(1);
 }
 
 // Add prompt for what boilerplate to use if option has default value.
-if ( boilerplate === 'none' ) {
+if (boilerplate === 'none') {
   const craChoices = boilerplateDataList
-    .filter( ( { type } ) => type === SETUP_TYPES.CRA )
-    .map( ( { displayName, description, name } ) => {
+    .filter(({ type }) => type === SETUP_TYPES.CRA)
+    .map(({ displayName, description, name }) => {
       return {
         title: displayName,
         description,
         value: name,
       };
-    } );
+    });
 
   const customChoices = boilerplateDataList
-    .filter( ( { type } ) => type === SETUP_TYPES.CUSTOM )
-    .map( ( { displayName, description, name } ) => {
+    .filter(({ type }) => type === SETUP_TYPES.CUSTOM)
+    .map(({ displayName, description, name }) => {
       return {
         title: displayName,
         description,
-        value: name
+        value: name,
       };
-    } );
+    });
 
-  userPrompts.push( {
+  userPrompts.push({
     type: 'select',
     name: 'boilerplate',
     message: 'Boilerplate Type:',
-    choices: ( previousResponse ) => {
-      switch ( previousResponse ) {
+    choices: (previousResponse) => {
+      switch (previousResponse) {
         case SETUP_TYPES.CRA:
           return craChoices;
         case SETUP_TYPES.CUSTOM:
-          return customChoices
+          return customChoices;
       }
     },
-  } );
-} else if ( ! Object.values( BOILERPLATE_NAMES ).includes( boilerplate ) ) {
-  const validBoilerplateNames = Object.values( BOILERPLATE_NAMES ).join( ', ' );
+  });
+} else if (!Object.values(BOILERPLATE_NAMES).includes(boilerplate)) {
+  const validBoilerplateNames = Object.values(BOILERPLATE_NAMES).join(', ');
 
   log(
-    `Invalid boilerplate: boilerplate names can only be one of these - ${ validBoilerplateNames }`,
-    'red',
+    `Invalid boilerplate: boilerplate names can only be one of these - ${validBoilerplateNames}`,
+    'red'
   );
 
-  process.exit( 1 );
+  process.exit(1);
 }
 
-( async () => {
-  log( '\n\n' );
-  log( LOGO, 'cyan' );
-  log( '\n\n' );
-  log( WELCOME_MESSAGE, 'cyan' );
+(async () => {
+  log('\n\n');
+  log(LOGO, 'cyan');
+  log('\n\n');
+  log(WELCOME_MESSAGE, 'cyan');
 
-  if ( userPrompts.length > 0 ) {
-    const response = await prompts( userPrompts, {
+  if (userPrompts.length > 0) {
+    const response = await prompts(userPrompts, {
       onCancel: () => {
-        process.exit( 1 );
+        process.exit(1);
       },
-    } );
+    });
 
     projectName = response.projectName || DEFAULT_PROJECT_NAME;
     setupType = response.setupType;
     boilerplateName = response.boilerplate;
   }
 
-  log( '\n\n' );
-  log( 'Please wait, while we set things up....', 'green' );
+  log('\n\n');
+  log('Please wait, while we set things up....', 'green');
 
-  switch ( setupType ) {
+  switch (setupType) {
     case SETUP_TYPES.CRA:
-      scaffoldBoilerplateWithCRA( boilerplateName, projectName, isPrivate );
+      scaffoldBoilerplateWithCRA(boilerplateName, projectName, isPrivate);
       break;
     case SETUP_TYPES.CUSTOM:
-      scaffoldBoilerplateCustom( boilerplateName, projectName, isPrivate );
+      scaffoldBoilerplateCustom(boilerplateName, projectName, isPrivate);
       break;
     default:
       break;
   }
 
-  log( '\n\n' );
-  log( 'Project created successfully!!', 'green' );
-  log( '\n\n' );
-  log( 'Go into the project directory' );
-  log( `cd ${ projectName }`, 'cyan' );
-  log( 'Run project in development mode.' );
-  log( 'npm start', 'cyan' );
-} )();
+  log('\n\n');
+  log('Project created successfully!!', 'green');
+  log('\n\n');
+  log('Go into the project directory');
+  log(`cd ${projectName}`, 'cyan');
+  log('Run project in development mode.');
+  log('npm start', 'cyan');
+})();
