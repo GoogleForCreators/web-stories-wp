182c2ad1955ed09b8fddb887b247eca6779205a9
Pascal Birchler <pascalb@google.com>
2022-04-11 15:35:51 +0200

    Remove nested describe

--- packages/e2e-tests/src/specs/editor/saveStory.js
+++ packages/e2e-tests/src/specs/editor/saveStory.js
@@ -29,7 +29,7 @@ import {
  */
 import { addAllowedErrorMessage } from '../../config/bootstrap';
 
-describe('Save story snackbar', () => {
+describe('Saving Story', () => {
   let removeErrorMessage;
   let stopRequestInterception;
   let mockResponse;
@@ -63,38 +63,36 @@ describe('Save story snackbar', () => {
     mockResponse = undefined;
   });
 
-  describe('Story saved', () => {
-    it('should display published dialog', async () => {
-      await createNewStory();
-      await insertStoryTitle('Test story');
-      await publishStory();
-      await expect(page).toMatch('Story published.');
+  it('should display published dialog', async () => {
+    await createNewStory();
+    await insertStoryTitle('Test story');
+    await publishStory();
+    await expect(page).toMatch('Story published.');
+  });
+
+  it('should display detailed error snackbar message', async () => {
+    mockResponse = {
+      status: 500,
+      body: JSON.stringify({
+        code: 'internal_server_error',
+        message: '<p>There has been a critical error on this website</p>',
+        data: {
+          status: 500,
+        },
+      }),
+    };
+    await createNewStory();
+    await insertStoryTitle('Test story 500');
+    await expect(page).toClick('button', { text: /^Publish$/ });
+    await expect(page).toMatchElement('div[aria-label="Story details"]');
+    await expect(page).toMatch('Story Details');
+    await expect(page).toClick('div[aria-label="Story details"] button', {
+      text: /^Publish$/,
     });
 
-    it('should display error 500 snackbar message', async () => {
-      mockResponse = {
-        status: 500,
-        body: JSON.stringify({
-          code: 'internal_server_error',
-          message: '<p>There has been a critical error on this website</p>',
-          data: {
-            status: 500,
-          },
-        }),
-      };
-      await createNewStory();
-      await insertStoryTitle('Test story 500');
-      await expect(page).toClick('button', { text: /^Publish$/ });
-      await expect(page).toMatchElement('div[aria-label="Story details"]');
-      await expect(page).toMatch('Story Details');
-      await expect(page).toClick('div[aria-label="Story details"] button', {
-        text: /^Publish$/,
-      });
-
-      await page.waitForSelector('[role="alert"]');
-      await expect(page).toMatchElement('[role="alert"]', {
-        text: 'Failed to save the story: There has been a critical error on this website (Internal Server Error)',
-      });
+    await page.waitForSelector('[role="alert"]');
+    await expect(page).toMatchElement('[role="alert"]', {
+      text: 'Failed to save the story: There has been a critical error on this website (Internal Server Error)',
     });
   });
 });
