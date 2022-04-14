39a3ebce535969e4ad2c7fa3d139e3ba9b5c1448
Sayed Taqui <sayedtaqui@users.noreply.github.com>
2022-04-14 13:57:25 +0530

    Fix navigator error for node env

--- packages/design-system/src/components/keyboard/index.js
+++ packages/design-system/src/components/keyboard/index.js
@@ -387,7 +387,7 @@ function crossesDialogBoundary(target, keyTarget) {
  * @return {boolean} True if platform is a Mac.
  */
 export function isPlatformMacOS() {
-  const { platform } = window.navigator;
+  const { platform = '' } = globalThis.navigator || {};
   return platform.includes('Mac') || ['iPad', 'iPhone'].includes(platform);
 }
 
