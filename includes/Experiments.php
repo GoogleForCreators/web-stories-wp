bad2b9010da9192dbadb626f402c6373ffbc361d
Pascal Birchler <pascalb@google.com>
2022-04-20 17:00:20 +0200

    Also remove `enhancedPageBackgroundAudio` again

--- includes/Experiments.php
+++ includes/Experiments.php
@@ -300,18 +300,6 @@ class Experiments extends Service_Base implements HasRequirements {
 				'default'     => true,
 			],
 			/**
-			 * Author: @spacedmonkey
-			 * Issue: #8821
-			 * Creation date: 2022-01-19
-			 */
-			[
-				'name'        => 'enhancedPageBackgroundAudio',
-				'label'       => __( 'Page Background Audio', 'web-stories' ),
-				'description' => __( 'Enable adding captions to background audio', 'web-stories' ),
-				'group'       => 'editor',
-				'default'     => true,
-			],
-			/**
 			 * Author: @barklund
 			 * Issue: #10112
 			 * Creation date: 2022-01-27
