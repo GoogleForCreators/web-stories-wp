fefffff69c434598dcaf2cc978db4ec97d58f1f0
Tim Arney <tim@line37.com>
2022-04-22 07:10:53 -0400

    add floating menu

---
+++ packages/e2e-tests/src/specs/editor/shopping/productMenu.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * External dependencies
+ */
+import {
+  createNewStory,
+  withExperimentalFeatures,
+  minWPVersionRequired,
+  withPlugin,
+} from '@web-stories-wp/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import { insertProduct, setShoppingProvider } from './testUtils';
+
+describe('Shopping product', () => {
+  minWPVersionRequired('5.8');
+  describe('Product menu', () => {
+    withExperimentalFeatures(['shoppingIntegration']);
+    withPlugin('woocommerce');
+    beforeAll(async () => {
+      await setShoppingProvider('WooCommerce');
+    });
+
+    function isStoryEmpty() {
+      return page.evaluate(() => {
+        if (document.getElementById('emptystate-message')) {
+          return true;
+        }
+        return false;
+      });
+    }
+
+    it('should show a floating menu with product dropdown', async () => {
+      const productText = 'Hoodie with Zipper';
+      await createNewStory();
+      await expect(isStoryEmpty()).resolves.toBe(true);
+      await insertProduct(productText);
+      await expect(isStoryEmpty()).resolves.toBe(false);
+      await page.waitForSelector(
+        '[aria-label="Design menu"] [aria-label="Product"]'
+      );
+      await expect(page).toMatchElement(
+        '[aria-label="Design menu"] [aria-label="Product"]',
+        { text: productText }
+      );
+      await expect(page).toClick('button', { text: 'Remove product' });
+      await expect(isStoryEmpty()).resolves.toBe(true);
+    });
+  });
+});
---
+++ packages/e2e-tests/src/specs/editor/shopping/testUtils.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * External dependencies
+ */
+import { visitSettings } from '@web-stories-wp/e2e-test-utils';
+
+export const insertProduct = async (product) => {
+  // Switch to the Products tab and wait for initial list of products to be fetched.
+  await Promise.all([
+    page.waitForResponse(
+      (response) =>
+        response.url().includes('web-stories/v1/products') &&
+        response.status() === 200
+    ),
+    expect(page).toClick('[aria-controls="library-pane-shopping"]'),
+  ]);
+
+  // This clicks on the Product dropdown and waits for the dropdown menu to open.
+  await expect(page).toClick('[aria-label="Product"]');
+  await page.waitForSelector('[aria-label="Search"]');
+
+  await page.type('[aria-label="Search"]', product);
+  await page.keyboard.press('ArrowDown');
+  await page.keyboard.press('Enter');
+  await expect(page).toClick('button', { text: 'Insert product' });
+};
+
+export const setShoppingProvider = async (provider) => {
+  await visitSettings();
+  // Small trick to ensure we scroll to this input.
+  const shoppingProviderDropdown = await page.$(
+    'button[aria-label="Shopping provider"]'
+  );
+  await shoppingProviderDropdown.focus();
+
+  await expect(page).toClick('button[aria-label="Shopping provider"]');
+
+  await expect(page).toClick('[role="listbox"] li', {
+    text: provider,
+  });
+
+  await expect(page).toMatch('Setting saved.');
+};
