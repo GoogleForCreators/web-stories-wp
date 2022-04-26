b9d88d89bd6b84e071c0be42e19f927e30941b3b
Matthew Ritter <matthewwilliamritter@gmail.com>
2022-04-26 16:19:12 -0400

    Set 'MAX' pageRange to 1 if totalPages is falsy to avoid the page being set to 0

--- packages/dashboard/src/utils/useStoryView.js
+++ packages/dashboard/src/utils/useStoryView.js
@@ -60,7 +60,7 @@ export default function useStoryView({
 
   const setPageClamped = useCallback(
     (newPage) => {
-      const pageRange = { MIN: 1, MAX: totalPages };
+      const pageRange = { MIN: 1, MAX: totalPages || 1 };
       setPage(clamp(newPage, pageRange));
     },
     [totalPages]
