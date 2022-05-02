058b58024d309f7f62ff1c88e2c694fbf762ebf6
Jonny Harris <spacedmonkey@users.noreply.github.com>
2022-05-02 20:53:40 +0100

    Media: Add support for hotlinking audio files (#11221)
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- includes/Experiments.php
+++ includes/Experiments.php
@@ -344,6 +344,17 @@ class Experiments extends Service_Base implements HasRequirements {
 				'group'       => 'editor',
 			],
 			/**
+			 * Author: @spacedmonkey
+			 * Issue: #10888
+			 * Creation date: 2022-04-08
+			 */
+			[
+				'name'        => 'audioHotlinking',
+				'label'       => __( 'Audio hotlinking', 'web-stories' ),
+				'description' => __( 'Enable hotlinking background audio', 'web-stories' ),
+				'group'       => 'editor',
+			],
+			/**
 			 * Author: @barklund
 			 * Issue: #7332
 			 * Creation date: 2022-04-19
