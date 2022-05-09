f0cfa5c8c1997a860bb14080fdd2aba8a4bf02f8
Pascal Birchler <pascalb@google.com>
2022-05-09 16:16:58 +0200

    Enable audio & caption hotlinking by default

--- includes/Experiments.php
+++ includes/Experiments.php
@@ -330,6 +330,7 @@ class Experiments extends Service_Base implements HasRequirements {
 				'label'       => __( 'Caption hotlinking', 'web-stories' ),
 				'description' => __( 'Enable hotlinking of captions', 'web-stories' ),
 				'group'       => 'editor',
+				'default'     => true,
 			],
 			/**
 			 * Author: @spacedmonkey
@@ -341,6 +342,7 @@ class Experiments extends Service_Base implements HasRequirements {
 				'label'       => __( 'Audio hotlinking', 'web-stories' ),
 				'description' => __( 'Enable hotlinking background audio', 'web-stories' ),
 				'group'       => 'editor',
+				'default'     => true,
 			],
 			/**
 			 * Author: @barklund
