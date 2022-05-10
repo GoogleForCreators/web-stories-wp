5e44f065402dcb10749d773116ab87cb2cacfe29
Pascal Birchler <pascalb@google.com>
2022-05-10 16:13:56 +0200

    Add workaround for uuid jest compat

--- packages/jest-resolver/src/index.cjs
+++ packages/jest-resolver/src/index.cjs
@@ -19,6 +19,33 @@ const isLocalRepo = (name) =>
 
 // See https://jestjs.io/docs/configuration#resolver-string
 module.exports = (request, options) => {
+  // Workaround for https://github.com/uuidjs/uuid/pull/616.
+  //
+  // jest-environment-jsdom 28+ tries to use browser exports instead of default exports,
+  // but uuid only offers an ESM browser export and not a CommonJS one. Jest does not yet
+  // support ESM modules natively, so this causes a Jest error related to trying to parse
+  // "export" syntax.
+  //
+  // This workaround prevents Jest from considering uuid's module-based exports at all;
+  // it falls back to uuid's CommonJS+node "main" property.
+  //
+  // Once we're able to migrate our Jest config to ESM and a browser crypto
+  // implementation is available for the browser+ESM version of uuid to use (eg, via
+  // https://github.com/jsdom/jsdom/pull/3352 or a similar polyfill), this can go away.
+  if ('uuid' === request) {
+    return options.defaultResolver(request, {
+      ...options,
+      packageFilter: (pkg) => {
+        if ('uuid' === pkg.name) {
+          delete pkg['exports'];
+          delete pkg['module'];
+        }
+
+        return pkg;
+      },
+    });
+  }
+
   if (!isLocalRepo(request)) {
     return options.defaultResolver(request, options);
   }
