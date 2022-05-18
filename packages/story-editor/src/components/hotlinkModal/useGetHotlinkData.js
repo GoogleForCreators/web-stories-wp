e33470f788a88bb08faf5fd8481fec4be60ac9cf
Jonny Harris <jon@spacedmonkey.co.uk>
2022-05-18 15:13:36 +0100

    Fix package.

--- packages/story-editor/src/components/hotlinkModal/useGetHotlinkData.js
+++ packages/story-editor/src/components/hotlinkModal/useGetHotlinkData.js
@@ -17,7 +17,7 @@
 /**
  * External dependencies
  */
-import { useCallback } from '@web-stories-wp/react';
+import { useCallback } from '@googleforcreators/react';
 /**
  * Internal dependencies
  */
