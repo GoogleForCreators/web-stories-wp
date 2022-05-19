3e3b3e8cce5ae1183a2b790ee7b66140716b7302
Morten Barklund <morten.barklund@xwp.co>
2022-05-19 12:45:24 -0400

    Added a migration to remove irrelevant tag name properties from story data

---
+++ packages/migration/src/migrations/test/v0042_removeTagNames.js
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Internal dependencies
+ */
+import removeTagNames from '../v0042_removeTagNames';
+
+describe('removeTagNames', () => {
+  it('should remove tag names', () => {
+    expect(
+      removeTagNames({
+        pages: [
+          {
+            elements: [
+              {
+                id: '1',
+                type: 'text',
+                tagName: 'auto',
+              },
+              {
+                id: '2',
+                type: 'text',
+              },
+              {
+                id: '3',
+                type: 'text',
+                tagName: 'h1',
+              },
+            ],
+          },
+          {
+            elements: [
+              {
+                id: '4',
+                type: 'text',
+                tagName: 'auto',
+              },
+            ],
+          },
+        ],
+      })
+    ).toStrictEqual({
+      pages: [
+        {
+          elements: [
+            {
+              id: '1',
+              type: 'text',
+            },
+            {
+              id: '2',
+              type: 'text',
+            },
+            {
+              id: '3',
+              type: 'text',
+            },
+          ],
+        },
+        {
+          elements: [
+            {
+              id: '4',
+              type: 'text',
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
