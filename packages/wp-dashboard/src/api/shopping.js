f33402dd4f787be1c1307f28cb4fb20618278d5f
Tim Arney <tim@line37.com>
2022-05-27 06:22:30 -0400

    add test connection button

--- packages/wp-dashboard/src/api/shopping.js
+++ packages/wp-dashboard/src/api/shopping.js
@@ -13,6 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * External dependencies
@@ -20,16 +39,11 @@
 import { addQueryArgs } from '@googleforcreators/url';
 
 /**
- * WordPress dependencies
- */
-import apiFetch from '@wordpress/api-fetch';
-
-/**
- * Get products
+ * Get products.
  *
- * @param {Object} config Configuration object.
+ * @param {Object} config Configuation object.
  * @param {string} search Search term.
- * @return {Promise} The response from the API.
+ * @return {Promise} Request promise.
  */
 export function getProducts(config, search) {
   return apiFetch({
