4747d13429963facedf8b6935a909318619d629e
Tim Arney <tim@line37.com>
2022-05-28 10:38:41 -0400

    lint

--- includes/REST_API/Products_Controller.php
+++ includes/REST_API/Products_Controller.php
@@ -155,7 +155,7 @@ class Products_Controller extends REST_Controller implements HasRequirements {
 		$query             = $this->shopping_vendors->get_vendor_class( $shopping_provider );
 
 		if ( ! $query ) {
-			$shopping_provider = $this->settings->get_setting( Settings::SETTING_NAME_SHOPPING_PROVIDER );
+			$shopping_provider = \strval( $this->settings->get_setting( Settings::SETTING_NAME_SHOPPING_PROVIDER ) );
 			if ( 'none' === $shopping_provider ) {
 				return new WP_Error( 'rest_shopping_provider', __( 'Please setup shopping provider', 'web-stories' ), [ 'status' => 400 ] );
 			}
