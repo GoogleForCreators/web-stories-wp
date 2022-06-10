c77193a33c18aa43bfe614fa364df17a60911243
Tim Arney <tim@line37.com>
2022-06-10 07:14:51 -0400

    handle trim entries

--- packages/story-editor/src/app/story/effects/useHistoryEntry.js
+++ packages/story-editor/src/app/story/effects/useHistoryEntry.js
@@ -37,12 +37,18 @@ const ELEMENT_PROPS_TO_IGNORE = [
   'resource.isMuted',
   'resource.posterId',
   'resource.poster',
-  'resource.isOptimized',
   'resource.font.metrics',
   'resource.font.weights',
   'resource.font.variants',
   'resource.font.fallbacks',
   'resource.font.styles',
+  'resource.isOptimized',
+  'resource.length',
+  'resource.lengthFormatted',
+  'resource.trimData.original',
+  'resource.trimData.start',
+  'resource.trimData.end',
+  'resource.creationDate',
 ];
 
 // Record any change to core variables in history (history will know if it's a replay)
@@ -118,6 +124,15 @@ function useHistoryEntry({ story, current, pages, selection, capabilities }) {
     }
 
     if (!skipAddingEntry) {
+
+      console.log({
+        story,
+        current: currentPageIndexRef.current,
+        selection: selectedElementIdsRef.current,
+        pages,
+        capabilities,
+      });
+
       stateToHistory({
         story,
         current: currentPageIndexRef.current,
