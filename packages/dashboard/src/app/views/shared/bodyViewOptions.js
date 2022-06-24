d48ba8aea8f84c295e1d91321441b19f23e67091
Matthew Ritter <matthewwilliamritter@gmail.com>
2022-06-24 07:06:11 -0400

    Dashboard: Allow dashboard filters on both grid and list view (#11769)

--- packages/dashboard/src/app/views/shared/bodyViewOptions.js
+++ packages/dashboard/src/app/views/shared/bodyViewOptions.js
@@ -33,7 +33,7 @@ import {
  */
 import { AuthorPropTypes } from '../../../utils/useStoryView.js';
 import { StandardViewContentGutter, ViewStyleBar } from '../../../components';
-import { DROPDOWN_TYPES, VIEW_STYLE } from '../../../constants';
+import { DROPDOWN_TYPES } from '../../../constants';
 import useFilters from '../myStories/filters/useFilters';
 
 const DisplayFormatContainer = styled.div`
@@ -96,7 +96,7 @@ export default function BodyViewOptions({
           <TranslateWithMarkup>{resultsLabel}</TranslateWithMarkup>
         </Text>
         <ControlsContainer>
-          {layoutStyle === VIEW_STYLE.GRID && filters?.length
+          {filters?.length
             ? filters.map((filter) => (
                 <StorySortDropdownContainer key={filter.key}>
                   <StyledDatalist
@@ -123,7 +123,7 @@ export default function BodyViewOptions({
                 </StorySortDropdownContainer>
               ))
             : null}
-          {layoutStyle === VIEW_STYLE.GRID && showAuthorDropdown && (
+          {showAuthorDropdown && (
             <StorySortDropdownContainer>
               <StyledDatalist
                 hasSearch
@@ -142,7 +142,7 @@ export default function BodyViewOptions({
               />
             </StorySortDropdownContainer>
           )}
-          {layoutStyle === VIEW_STYLE.GRID && showSortDropdown && (
+          {showSortDropdown && (
             <StorySortDropdownContainer>
               <StyledDropDown
                 ariaLabel={sortDropdownAriaLabel}
