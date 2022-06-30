9fcc6ffe0b8c6ba21e27d2f95d4a579a489a3efa
Pascal Birchler <pascalb@google.com>
2022-06-30 19:29:46 +0200

    Prepare 1.22.1 release (#11850)

--- includes/namespace.php
+++ includes/namespace.php
@@ -198,6 +198,11 @@ function rest_preload_api_request( $memo, $path ): array {
 		}
 	}
 
+	/**
+	 * URL parts.
+	 *
+	 * @var array{path:string, query?: string} $path_parts
+	 */
 	$path_parts = wp_parse_url( (string) $path );
 	if ( ! \is_array( $path_parts ) ) {
 		return $memo;
