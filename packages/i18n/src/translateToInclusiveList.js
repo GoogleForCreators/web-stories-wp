145312439600bd357dc4eccc000cff56abea4c6e
Jonny Harris <jon@spacedmonkey.co.uk>
2022-07-04 17:42:25 +0100

    Feedback.

--- packages/i18n/src/translateToInclusiveList.js
+++ packages/i18n/src/translateToInclusiveList.js
@@ -45,9 +45,8 @@ function translateToInclusiveList(options) {
         /* translators: %1$s: is a comma separated list. %2$s: last entry in list  */
         __('%1$s, and %2$s', 'web-stories'),
         options.slice(0, options.length - 1).join(
-          /* eslint-disable-next-line @wordpress/i18n-no-flanking-whitespace -- Expected behaviour.
-           * translators: delimiter used in a list */
-          __(', ', 'web-stories')
+          /* translators: delimiter used in a list */
+          __(', ', 'web-stories') // eslint-disable-line @wordpress/i18n-no-flanking-whitespace -- Expected behaviour.
         ),
         options[options.length - 1]
       );
