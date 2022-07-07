be352cd693f5ae584dfb8ba04c6fbbfb275b4179
mwritter <matthewwilliamritter@gmail.com>
2022-07-07 16:11:43 -0400

    Fixing tests

--- packages/e2e-tests/src/specs/editor/publishPanel/adminUser.js
+++ packages/e2e-tests/src/specs/editor/publishPanel/adminUser.js
@@ -82,7 +82,7 @@ describe('Publish panel in document tab', () => {
     await page.keyboard.type('auth');
 
     // wait for search results
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(500);
 
     const optionListAfterSearch = await authorDropDownOptions.$$eval(
       'li[role="option"]',
