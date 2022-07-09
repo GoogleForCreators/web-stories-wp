1337130dd74b5cceefed0d90f56cdba06782c164
Tim Arney <tim@line37.com>
2022-07-08 20:19:30 -0400

    try without delete media

--- packages/e2e-tests/src/config/bootstrap.js
+++ packages/e2e-tests/src/config/bootstrap.js
@@ -238,7 +238,7 @@ beforeAll(async () => {
   await trashAllPosts('web-story');
   await trashAllTerms('web_story_category');
   await trashAllTerms('web_story_tag');
-  await deleteAllMedia();
+  // await deleteAllMedia(); try without delete media
 
   await clearLocalStorage();
 });
