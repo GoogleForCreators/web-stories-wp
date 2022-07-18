0dd371957975027828c94b82a19ebdaeff0b3f1a
Tim Arney <tim@line37.com>
2022-07-18 07:38:14 -0400

    fix comment

--- packages/e2e-tests/src/specs/wordpress/blockWidget.js
+++ packages/e2e-tests/src/specs/wordpress/blockWidget.js
@@ -86,7 +86,7 @@ describe('Web Stories Widget Block', () => {
     await expect(page).toMatch('Embed Settings');
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests -- -- TODO(#11931): Fix flakey test.
+  // eslint-disable-next-line jest/no-disabled-tests -- TODO(#11931): Fix flakey test.
   it.skip('should insert a legacy web stories widget', async () => {
     await activatePlugin('classic-widgets');
 
