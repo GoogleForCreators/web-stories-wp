03918b2fbe584bad974bba68c246833540ee5f37
Tim Arney <tim@line37.com>
2022-07-20 07:33:35 -0400

    move skip to describe

--- packages/e2e-tests/src/specs/editor/publishingFlow.js
+++ packages/e2e-tests/src/specs/editor/publishingFlow.js
@@ -33,7 +33,8 @@ import {
  */
 import { addAllowedErrorMessage } from '../../config/bootstrap.js';
 
-describe('Publishing Flow', () => {
+//eslint-disable-next-line jest/no-disabled-tests -- TODO(#11970): Fix flakey test.
+describe.skip('Publishing Flow', () => {
   let uploadedFiles;
   let removeCORSErrorMessage;
   let removeResourceErrorMessage;
@@ -91,8 +92,7 @@ describe('Publishing Flow', () => {
     await expect(page).toMatchElement('[alt="Preview image"]');
   }
 
-  //eslint-disable-next-line jest/no-disabled-tests -- TODO(#11970): Fix flakey test.
-  it.skip('should guide me towards creating a new post to embed my story with poster', async () => {
+  it('should guide me towards creating a new post to embed my story with poster', async () => {
     await createNewStory();
 
     await insertStoryTitle('Publishing Flow Test');
