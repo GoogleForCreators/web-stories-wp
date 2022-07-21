b3b639478e5fa24d7f39bd8be066472c458dbe8e
Miina Sikk <miina.sikk@gmail.com>
2022-07-21 18:56:23 +0300

    Revert debugging data

--- packages/story-editor/src/components/canvas/karma/quickActions.karma.js
+++ packages/story-editor/src/components/canvas/karma/quickActions.karma.js
@@ -1116,7 +1116,7 @@ describe('Quick Actions integration', () => {
       const insertElement = await fixture.renderHook(() => useInsertElement());
       const sticker = await fixture.act(() =>
         insertElement('sticker', {
-          border: {},
+          border: null,
           borderRadius: null,
           flip: { vertical: false, horizontal: false },
           focalX: 50,
