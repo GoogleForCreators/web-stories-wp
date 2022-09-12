a18878ff11ca8bf59329a61d44ec120aed099fdf
Jonny Harris <jon@spacedmonkey.co.uk>
2022-09-12 17:13:24 +0100

    First type at converting to typescript for text sets.

---
+++ packages/text-sets/src/loadTextSets.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * External dependencies
+ */
+import { migrate } from '@googleforcreators/migration';
+/**
+ * Internal dependencies
+ */
+import type { Data, Element, MinMax, TextSet } from './types';
+
+function updateMinMax(minMax: MinMax, element: Element): MinMax {
+  // Purposely mutating object so passed
+  // in minMax is modified
+  minMax.minX = Math.min(minMax.minX, element.x);
+  minMax.maxX = Math.max(minMax.maxX, element.x + element.width);
+
+  minMax.minY = Math.min(minMax.minY, element.y);
+  minMax.maxY = Math.max(minMax.maxY, element.y + element.height);
+
+  return minMax;
+}
+
+async function loadTextSet(name: string): Promise<Array<object>> {
+  const data: TextSet = (await import(
+    /* webpackChunkName: "chunk-web-stories-textset-[index]" */ `./raw/${name}.json`
+  )) as TextSet;
+  const migrated: Data = migrate(data.default, data.default.version);
+  return migrated.pages.reduce(
+    (
+      sets,
+      {
+        elements,
+        fonts,
+        id,
+      }: { elements: Array<Element>; fonts: string; id: number }
+    ) => {
+      const minMax = {
+        minX: Infinity,
+        maxX: 0,
+        minY: Infinity,
+        maxY: 0,
+      };
+
+      const textElements = elements.filter((element: Element) => {
+        return !element.isBackground && Boolean(updateMinMax(minMax, element));
+      });
+
+      return [
+        ...sets,
+        {
+          textSetFonts: fonts,
+          id,
+          textSetCategory: name,
+          elements: textElements.map((e: Element) => ({
+            ...e,
+            // Offset elements so the text set's
+            // default position is (0,0)
+            normalizedOffsetX: e.x - minMax.minX,
+            normalizedOffsetY: e.y - minMax.minY,
+            // The overall text set width & height
+            // is the delta between the max/mins
+            textSetWidth: minMax.maxX - minMax.minX,
+            textSetHeight: minMax.maxY - minMax.minY,
+          })),
+        },
+      ];
+    },
+    []
+  );
+}
+
+export default async function loadTextSets() {
+  const textSets = [
+    'cover',
+    'step',
+    'section_header',
+    'editorial',
+    'contact',
+    'table',
+    'list',
+    'quote',
+  ];
+
+  const results = await Promise.all(
+    textSets.map(async (name: string): Promise<Array<object>> => {
+      return [name, await loadTextSet(name)];
+    })
+  );
+
+  return Object.fromEntries(results);
+}
