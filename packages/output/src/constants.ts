adc1da98b0c502848c3951f49b38de127ade27eb
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2022-09-23 15:09:50 +0530

    Convert constants add type package as dependency

--- packages/output/src/constants.ts
+++ packages/output/src/constants.ts
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export const DEFAULT_PAGE_DURATION = 7;
-export const DEFAULT_AUTO_ADVANCE = true;
+export const DEFAULT_PAGE_DURATION: number = 7;
+export const DEFAULT_AUTO_ADVANCE: boolean = true;
