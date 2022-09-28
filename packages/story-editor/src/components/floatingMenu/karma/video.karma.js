dc78a75e417d19ab159badc70b9f0e292ef2f522
Marcin Pietruszka <marcin@webskill.pl>
2022-09-28 21:37:52 +0200

    Lint

--- packages/story-editor/src/components/floatingMenu/karma/video.karma.js
+++ packages/story-editor/src/components/floatingMenu/karma/video.karma.js
@@ -37,7 +37,7 @@ describe('Video Design Menu: Keyboard Navigation', () => {
   afterEach(() => {
     fixture.restore();
   });
-  
+
   it('should navigate into and out of floating menu by keyboard', async () => {
     const elementUpdates = {
       opacity: {
