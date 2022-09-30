e2308a7aa93067df12bf5406f36a617ee25e58b6
Pascal Birchler <pascalb@google.com>
2022-09-30 10:44:38 +0200

    Update Jest and snapshots

--- packages/story-editor/src/components/library/test/termsDialog.js
+++ packages/story-editor/src/components/library/test/termsDialog.js
@@ -13,21 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 /**
  * External dependencies
  */
@@ -53,16 +39,6 @@ jest.mock('@googleforcreators/design-system', () => ({
 }));
 
 describe('TermsDialog', () => {
-  it('should render', () => {
-    renderWithTheme(<TermsDialog />);
-    const link = screen.queryByLabelText(
-      /^Learn more by visiting Web Stories for WordPress/
-    );
-    expect(link).toMatchSnapshot();
-    expect(localStore.getItemByKey).toHaveBeenCalledWith(
-      LOCAL_STORAGE_PREFIX.TERMS_MEDIA3P
-    );
-  });
   it('should store the acknowledgement in the localStore when the window is dismissed', async () => {
     renderWithTheme(<TermsDialog />);
     const dismiss = screen.queryByText(/^Dismiss/);
