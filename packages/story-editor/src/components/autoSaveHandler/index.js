8407b2ce93636e8bd71b0a7d2299afac62a0b889
Tim Arney <tim@line37.com>
2022-09-30 07:03:26 -0400

    add function check

--- packages/story-editor/src/components/autoSaveHandler/index.js
+++ packages/story-editor/src/components/autoSaveHandler/index.js
@@ -49,7 +49,9 @@ function AutoSaveHandler() {
     // back false after the save.
     // This timeout will thus be re-started when some new change occurs after an autosave.
     const timeout = setTimeout(
-      () => cachedSaveStory?.current(),
+      () =>
+        cachedSaveStory?.current instanceof Function &&
+        cachedSaveStory.current(),
       autoSaveInterval * 1000
     );
 
