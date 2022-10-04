a01867b972184c42142357958c1f4d1cad42592a
Pascal Birchler <pascalb@google.com>
2022-10-04 11:11:45 +0200

    Deactivate importer again after install

--- bin/local-env/install-wordpress.sh
+++ bin/local-env/install-wordpress.sh
@@ -142,11 +142,11 @@ fi
 echo -e $(status_message "Installing and activating RTL Tester plugin...")
 wp plugin install rtl-tester --activate --force --quiet
 
-echo -e $(status_message "Installing WordPress importer...")
-wp plugin install wordpress-importer --activate --force --quiet
-
-# Only install woocommerce on latest version of WordPress.
+# Only install WooCommerce on latest version of WordPress.
 if [ "$WP_VERSION" == "latest" ]; then
+  echo -e $(status_message "Installing WordPress importer...")
+  wp plugin install wordpress-importer --activate --force --quiet
+
 	echo -e $(status_message "Installing WooCommerce plugin...")
 	wp plugin install woocommerce --activate --force --quiet
 fi
@@ -234,8 +234,12 @@ wp plugin list --format=yaml
 
 # Only install woocommerce on latest version of WordPress.
 if [ "$WP_VERSION" == "latest" ]; then
-	echo -e $(status_message "Import sample woocommerce products...")
+	echo -e $(status_message "Import sample WooCommerce products...")
 	wp import /var/www/html/wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip --quiet
-	# deactivate test etc... can activate as needed
+
+  echo -e $(status_message "Deactivating WooCommerce again...")
 	wp plugin deactivate woocommerce
+
+  echo -e $(status_message "Deactivating WordPress importer again...")
+  wp plugin deactivate wordpress-importer --quiet
 fi
