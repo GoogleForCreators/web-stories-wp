ed88e9388d71a20fa581b3a1b9cd5d8278bcca7f
Pascal Birchler <pascalb@google.com>
2022-10-05 10:42:34 +0200

    Re-enable story saving failing test
    See #11992

--- packages/e2e-tests/src/specs/editor/saveStory.js
+++ packages/e2e-tests/src/specs/editor/saveStory.js
@@ -21,7 +21,6 @@ import {
   addRequestInterception,
   createNewStory,
   insertStoryTitle,
-  publishStory,
 } from '@web-stories-wp/e2e-test-utils';
 
 /**
@@ -50,6 +49,7 @@ describe('Saving Story', () => {
         request.respond(mockResponse);
         return;
       }
+
       request.continue();
     });
   });
@@ -65,16 +65,7 @@ describe('Saving Story', () => {
     stopRequestInterception();
   });
 
-  //eslint-disable-next-line jest/no-disabled-tests -- TODO(#11992): Fix flakey test.
-  it.skip('should display published dialog', async () => {
-    await createNewStory();
-    await insertStoryTitle('Test story');
-    await publishStory();
-    await expect(page).toMatch('Story published.');
-  });
-
-  // eslint-disable-next-line jest/no-disabled-tests -- TODO(#11992): Fix flakey test.
-  it.skip('should display detailed error snackbar message', async () => {
+  it('should display detailed error snackbar message', async () => {
     mockResponse = {
       status: 500,
       body: JSON.stringify({
