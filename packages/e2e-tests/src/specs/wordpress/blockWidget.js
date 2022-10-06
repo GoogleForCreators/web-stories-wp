54188c7a10e51b373c629558b630449ea9a91145
Pascal Birchler <pascalb@google.com>
2022-10-06 22:22:36 +0200

    Re-enable block widget test
    See #11931

--- packages/e2e-tests/src/specs/wordpress/blockWidget.js
+++ packages/e2e-tests/src/specs/wordpress/blockWidget.js
@@ -56,7 +56,7 @@ describe('Web Stories Widget Block', () => {
     removeErrorMessage();
   });
 
-  it('should insert a new web stories block', async () => {
+  it('should insert a new Web Stories block', async () => {
     await visitBlockWidgetScreen();
     await expect(page).toClick('button[aria-label="Add block"]');
     await page.type('input[placeholder="Search"]', 'Web Stories');
@@ -88,8 +88,7 @@ describe('Web Stories Widget Block', () => {
     await expect(page).toMatch('Embed Settings');
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests -- TODO(#11931): Fix flakey test.
-  it.skip('should insert a legacy web stories widget', async () => {
+  it('should insert and transform a legacy Web Stories widget', async () => {
     await activatePlugin('classic-widgets');
 
     await visitAdminPage('widgets.php');
@@ -111,23 +110,31 @@ describe('Web Stories Widget Block', () => {
       'Test widget'
     );
 
-    await expect(page).toMatchElement(
-      '.widget-liquid-right .widget-control-save'
-    );
-
-    await page.keyboard.press('Enter');
+    await Promise.all([
+      expect(page).toClick(
+        '.widget-liquid-right .widget-control-save:not(:disabled)'
+      ),
+      page.waitForResponse(
+        (response) =>
+          // eslint-disable-next-line jest/no-conditional-in-test
+          response.url().includes('/wp-admin/admin-ajax.php') &&
+          response.status() === 200
+      ),
+    ]);
 
     await expect(page).toMatchElement(
       '.widget-liquid-right .widget-control-save:disabled'
     );
 
     await deactivatePlugin('classic-widgets');
-    await visitBlockWidgetScreen();
-    const selector = '.wp-block-legacy-widget';
 
-    await page.waitForSelector(selector);
-    await expect(page).toMatchElement(selector);
-    await expect(page).toClick(selector);
+    await visitBlockWidgetScreen();
+
+    // Wait for widget to render.
+    await page.waitForSelector('.wp-block-legacy-widget__edit-preview');
+    await expect(page).toClick('.wp-block-legacy-widget');
+
+    await expect(page).toMatchElement('.web-stories-field-wrapper');
 
     await page.evaluate(() => {
       const input = document.querySelector('.web-stories-field-wrapper input');
@@ -135,13 +142,39 @@ describe('Web Stories Widget Block', () => {
     });
 
     await page.type('.web-stories-field-wrapper input', 'Test Block Widget');
-    await page.keyboard.press('Escape');
-    await expect(page).toMatchElement(selector);
-    await expect(page).toClick(selector);
-    await expect(page).toClick(
-      '.components-dropdown button[aria-label="Legacy Widget"]'
+
+    await page.waitForResponse(
+      (response) =>
+        // eslint-disable-next-line jest/no-conditional-in-test
+        response
+          .url()
+          .includes('wp/v2/widget-types/web_stories_widget/render') &&
+        response.status() === 200
     );
-    await expect(page).toClick('button', { text: 'Web Stories' });
-    await expect(page).toMatch('Test Block Widget');
+
+    await expect(page).toClick('.components-button.is-primary', {
+      text: 'Update',
+    });
+
+    await expect(page).toClick('.wp-block-legacy-widget');
+
+    // More reliable here than toClick(), page.click(), or transformBlockTo()
+    await page.evaluate(() =>
+      document.querySelector('button[aria-label="Legacy Widget"]').click()
+    );
+
+    await expect(page).toClick('button[role="menuitem"]', {
+      text: 'Web Stories',
+    });
+
+    // The core/legacy-widget block should have been replaced by core/heading + web-stories/embed.
+    const widgetBlocks = await page.evaluate(() =>
+      wp.data
+        .select('core/block-editor')
+        .getBlocks(wp.data.select('core/block-editor').getBlockOrder()[0])
+        .map(({ name }) => name)
+        .join(',')
+    );
+    await expect(widgetBlocks).toBe('core/heading,web-stories/embed');
   });
 });
