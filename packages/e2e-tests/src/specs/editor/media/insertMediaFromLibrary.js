4fb30a8bdc84cbe3c320a39c4e0f46972cbfb618
Pascal Birchler <pascalb@google.com>
2022-10-07 13:55:49 +0200

    Re-enable media library test
    See #7107

--- packages/e2e-tests/src/specs/editor/media/insertMediaFromLibrary.js
+++ packages/e2e-tests/src/specs/editor/media/insertMediaFromLibrary.js
@@ -26,28 +26,32 @@ import {
 jest.retryTimes(2, { logErrorsBeforeRetry: true });
 
 describe('Inserting Media from Media Library', () => {
-  // Uses the existence of the element's frame element as an indicator for successful insertion.
-  // TODO https://github.com/googleforcreators/web-stories-wp/issues/7107
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('should insert an image by clicking on it', async () => {
+  let uploadedFiles;
+
+  beforeEach(() => (uploadedFiles = []));
+
+  afterEach(async () => {
+    for (const file of uploadedFiles) {
+      // eslint-disable-next-line no-await-in-loop
+      await deleteMedia(file);
+    }
+  });
+
+  it('should insert an image by clicking on it', async () => {
     await createNewStory();
 
     const filename = await uploadMedia('example-1.jpg', true);
+    uploadedFiles.push(filename);
 
     await page.waitForSelector('[data-testid="mediaElement-image"]');
-    // Clicking will only act on the first element.
+
+    // This will click in the center of the element, opening the "+" insertion menu.
     await expect(page).toClick('[data-testid="mediaElement-image"]');
-    await expect(page).toClick('menuitem', { text: 'Insert image' });
+    await expect(page).toClick('[role="menu"] [role="menuitem"]', {
+      text: 'Insert image',
+    });
 
-    await page.waitForSelector('[data-testid="frameElement"]:nth-of-type(2)');
-
-    // First match is for the background element, second for the image.
-    await expect(page).toMatchElement(
-      '[data-testid="frameElement"]:nth-of-type(2)'
-    );
-
+    await page.waitForSelector('[data-testid="imageElement"]');
     await expect(page).toMatchElement('[data-testid="imageElement"]');
-
-    await deleteMedia(filename);
   });
 });
