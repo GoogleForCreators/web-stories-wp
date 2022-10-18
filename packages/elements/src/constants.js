b3fd7060015a9fb28685f302f329cadaaa439bcb
Tim Arney <tim@line37.com>
2022-10-18 06:09:46 -0400

    reset file

--- packages/elements/src/constants.js
+++ packages/elements/src/constants.js
@@ -35,12 +35,41 @@ export const MULTIPLE_VALUE = '((MULTIPLE))';
 
 export const TEXT_ELEMENT_DEFAULT_FONT = {
   family: 'Roboto',
+  weights: [100, 300, 400, 500, 700, 900],
+  styles: ['italic', 'regular'],
+  variants: [
+    [0, 100],
+    [1, 100],
+    [0, 300],
+    [1, 300],
+    [0, 400],
+    [1, 400],
+    [0, 500],
+    [1, 500],
+    [0, 700],
+    [1, 700],
+    [0, 900],
+    [1, 900],
+  ],
+  fallbacks: ['Helvetica Neue', 'Helvetica', 'sans-serif'],
+  service: 'fonts.google.com',
   metrics: {
     upm: 2048,
     asc: 1900,
     des: -500,
+    tAsc: 1536,
+    tDes: -512,
+    tLGap: 102,
+    wAsc: 1946,
+    wDes: 512,
+    xH: 1082,
+    capH: 1456,
+    yMin: -555,
+    yMax: 2163,
+    hAsc: 1900,
+    hDes: -500,
+    lGap: 0,
   },
-  weights: [100, 300, 400, 500, 700, 900],
 };
 
 export const BACKGROUND_TEXT_MODE = {
