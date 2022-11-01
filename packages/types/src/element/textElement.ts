199199712a34f1c2cbe6d24264a6dc9977ab65eb
Miina Sikk <miina.sikk@gmail.com>
2022-11-01 17:33:26 -0300

    Add migration to remove redundant props.

--- packages/types/src/element/textElement.ts
+++ packages/types/src/element/textElement.ts
@@ -81,6 +81,4 @@ export interface TextElement extends Element {
   tagName?: 'h1' | 'h2' | 'h3' | 'p';
   padding?: Padding;
   marginOffset?: number;
-  // TODO(#12258): Remove this.
-  fontWeight?: number;
 }
