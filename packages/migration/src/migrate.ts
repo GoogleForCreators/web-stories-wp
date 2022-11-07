438554ad4a701668e427f4452ecd73c8a181e959
Miina <miina.sikk@gmail.com>
2022-11-07 13:22:48 +0100

    Remove scaling properties from non-media elements (#12605)
    * Stop adding scaling props to all elements.
    
    * Remove scaling props from types, too
    
    * Add migration to remove redundant props.
    
    * Add migration fully
    
    * Lint

--- packages/migration/src/migrate.ts
+++ packages/migration/src/migrate.ts
@@ -68,6 +68,7 @@ import removeTrackName from './migrations/v0042_removeTrackName';
 import removeTagNames from './migrations/v0043_removeTagNames';
 import unusedProperties from './migrations/v0044_unusedProperties';
 import globalPageAdvancement from './migrations/v0045_globalPageAdvancement';
+import removeRedundantScalingProperties from './migrations/v0046_removeRedundantScalingProperties';
 
 type MigrationFn<T, S> = (storyData: T) => S;
 
@@ -119,6 +120,7 @@ const MIGRATIONS: Record<number, MigrationFn<any, any>[]> = { // eslint-disable-
   43: [removeTagNames],
   44: [unusedProperties],
   45: [globalPageAdvancement],
+  46: [removeRedundantScalingProperties],
 };
 
 export const DATA_VERSION = Math.max.apply(
