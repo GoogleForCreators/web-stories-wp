e5c2f8d0c972b9e53e2623b6abbf687a39a20fda
Miina Sikk <miina.sikk@gmail.com>
2022-11-21 11:19:58 -0300

    Move types.

--- packages/story-editor/src/types/apiProvider.ts
+++ packages/story-editor/src/types/apiProvider.ts
@@ -13,10 +13,162 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * External dependencies
+ */
+import type {
+  FontData,
+  MediaElement,
+  ProductData,
+} from '@googleforcreators/elements';
+import type {
+  Resource,
+  ResourceId,
+  TrimData,
+  VideoResource,
+} from '@googleforcreators/media';
+import type { Template } from '@googleforcreators/templates';
+
 /**
  * Internal dependencies
  */
-import type { APICallbacks } from './configProvider';
+import type { RawStory, StoryReturnData, StorySaveData } from './story';
+import type { PageTemplate, Taxonomy } from './configProvider';
+
+interface TemplateData {
+  story_data: PageTemplate;
+  featured_media: number;
+  title?: string;
+}
+
+type Term = {
+  id: number;
+  name: string;
+  slug: string;
+  taxonomy: string;
+};
+
+type Author = {
+  id: number;
+  name: string;
+  url?: string;
+  description?: string;
+  slug: string;
+  link: string;
+  avatarUrls?: Record<number, string>;
+};
+
+type User = {
+  id: number;
+  mediaOptimization: boolean;
+  onboarding: Record<string, boolean>;
+  trackingOptin: boolean;
+};
+
+type GetFontProps = {
+  service?: string;
+  include?: string;
+  search?: string;
+};
+
+type HotlinkInfo = {
+  ext?: string;
+  fileName?: string;
+  fileSize?: number;
+  mimeType?: string;
+  type?: string;
+  width?: number;
+  height?: number;
+};
+
+type LinkMetaData = {
+  title: string;
+  image: string;
+};
+
+type UploadMediaProps = {
+  onUploadSuccess?: (media: MediaElement) => void;
+  onUploadProgress?: (media: MediaElement) => void;
+  onUploadError?: (media: MediaElement) => void;
+  cropVideo?: boolean;
+  additionalData: {
+    originalId?: number;
+    mediaId?: number;
+    storyId?: number;
+    templateId?: number;
+    optimizedId?: number;
+    cropOriginId?: number;
+    mutedId?: number;
+    posterId?: number;
+    isMuted?: boolean;
+    mediaSource?: string;
+    trimData?: TrimData;
+    baseColor?: string;
+    blurHash?: string;
+    isGif?: boolean;
+    altText?: string;
+  };
+  originalResourceId: ResourceId;
+  resource: Resource;
+};
+export interface APICallbacks {
+  addPageTemplate?: (data: TemplateData) => Promise<PageTemplate>;
+  autoSaveById?: (story: StorySaveData) => Promise<StoryReturnData>;
+  createTaxonomyTerm?: (
+    endpoint: string,
+    term: {
+      name: string;
+      parent?: number;
+      slug?: string;
+    }
+  ) => Promise<Term>;
+  deleteMedia?: (id: number) => Promise<boolean>;
+  deletePageTemplate?: (id: number) => Promise<boolean>;
+  getAuthors?: () => Promise<Author[]>;
+  getCurrentUser?: () => Promise<User>;
+  getCustomPageTemplates?: (page: number | boolean) => Promise<{
+    hasMore: boolean;
+    templates: PageTemplate[];
+  }>;
+  getFonts?: (props: GetFontProps) => Promise<FontData[]>;
+  getHotlinkInfo?: (link: string) => Promise<HotlinkInfo>;
+  getLinkMetadata?: (link: string) => Promise<LinkMetaData>;
+  getMedia?: (props: {
+    mediaType: string;
+    searchTerm: string;
+    pagingNum: number;
+  }) => Promise<{
+    data: Resource[];
+    headers: Record<string, string>;
+  }>;
+  getMediaById?: (id: number) => Promise<Resource>;
+  getMediaForCorsCheck?: () => Promise<Resource[]>;
+  getMutedMediaById?: (id: number) => Promise<VideoResource>;
+  getOptimizedMediaById?: (id: number) => Promise<Resource>;
+  getPageTemplates?: () => Promise<Template[]>;
+  getPosterMediaById?: (id: number) => Promise<Resource>;
+  getProducts?: () => Promise<ProductData[]>;
+  getProxyUrl?: (src: string) => string;
+  getStoryById?: (id: number) => Promise<RawStory>;
+  getTaxonomies?: () => Promise<Taxonomy[]>;
+  getTaxonomyTerm?: (props: {
+    search?: string;
+    per_page?: number;
+  }) => Promise<Term>;
+  saveStoryById?: (data: StorySaveData) => Promise<StoryReturnData>;
+  updateCurrentUser?: (data: {
+    mediaOptimization?: boolean;
+    onboarding?: Record<string, boolean>;
+    trackingOptin?: boolean;
+  }) => Promise<User>;
+  updateMedia?: (id: number, data: Partial<Resource>) => Promise<Resource>;
+  updatePageTemplate?: (
+    id: number,
+    data: Partial<PageTemplate>
+  ) => Promise<PageTemplate>;
+  uploadMedia?: (files: string[], props: UploadMediaProps) => Promise<Resource>;
+}
 
 export interface APIState {
   actions: APICallbacks;
--- packages/story-editor/src/types/configProvider.ts
+++ packages/story-editor/src/types/configProvider.ts
@@ -17,24 +17,12 @@
 /**
  * External dependencies
  */
-import type {
-  MediaElement,
-  Page,
-  FontData,
-  ProductData,
-} from '@googleforcreators/elements';
-import type {
-  Resource,
-  ResourceId,
-  TrimData,
-  VideoResource,
-} from '@googleforcreators/media';
-import type { Template } from '@googleforcreators/templates';
+import type { Page } from '@googleforcreators/elements';
 
 /**
  * Internal dependencies
  */
-import type {RawStory, Story, StoryReturnData, StorySaveData} from './story';
+import type { APICallbacks } from './apiProvider';
 
 export interface Capabilities {
   /** If the user has permissions to upload files. */
@@ -81,82 +69,6 @@ export interface Tip {
 export interface PageTemplate extends Page {
   version: string;
 }
-interface TemplateData {
-  story_data: PageTemplate;
-  featured_media: number;
-  title?: string;
-}
-
-type Term = {
-  id: number;
-  name: string;
-  slug: string;
-  taxonomy: string;
-};
-
-type Author = {
-  id: number;
-  name: string;
-  url?: string;
-  description?: string;
-  slug: string;
-  link: string;
-  avatarUrls?: Record<number, string>;
-};
-
-type User = {
-  id: number;
-  mediaOptimization: boolean;
-  onboarding: Record<string, boolean>;
-  trackingOptin: boolean;
-};
-
-type GetFontProps = {
-  service?: string;
-  include?: string;
-  search?: string;
-};
-
-type HotlinkInfo = {
-  ext?: string;
-  fileName?: string;
-  fileSize?: number;
-  mimeType?: string;
-  type?: string;
-  width?: number;
-  height?: number;
-};
-
-type LinkMetaData = {
-  title: string;
-  image: string;
-};
-
-type UploadMediaProps = {
-  onUploadSuccess?: (media: MediaElement) => void;
-  onUploadProgress?: (media: MediaElement) => void;
-  onUploadError?: (media: MediaElement) => void;
-  cropVideo?: boolean;
-  additionalData: {
-    originalId?: number;
-    mediaId?: number;
-    storyId?: number;
-    templateId?: number;
-    optimizedId?: number;
-    cropOriginId?: number;
-    mutedId?: number;
-    posterId?: number;
-    isMuted?: boolean;
-    mediaSource?: string;
-    trimData?: TrimData;
-    baseColor?: string;
-    blurHash?: string;
-    isGif?: boolean;
-    altText?: string;
-  };
-  originalResourceId: ResourceId;
-  resource: Resource;
-};
 
 export type Taxonomy = {
   name: string;
@@ -172,64 +84,6 @@ export type Taxonomy = {
   visibility: Record<string, boolean>;
   restPath: string;
 };
-
-export interface APICallbacks {
-  addPageTemplate?: (data: TemplateData) => Promise<PageTemplate>;
-  autoSaveById?: (story: StorySaveData) => Promise<StoryReturnData>;
-  createTaxonomyTerm?: (
-    endpoint: string,
-    term: {
-      name: string;
-      parent?: number;
-      slug?: string;
-    }
-  ) => Promise<Term>;
-  deleteMedia?: (id: number) => Promise<boolean>;
-  deletePageTemplate?: (id: number) => Promise<boolean>;
-  getAuthors?: () => Promise<Author[]>;
-  getCurrentUser?: () => Promise<User>;
-  getCustomPageTemplates?: (page: number | boolean) => Promise<{
-    hasMore: boolean;
-    templates: PageTemplate[];
-  }>;
-  getFonts?: (props: GetFontProps) => Promise<FontData[]>;
-  getHotlinkInfo?: (link: string) => Promise<HotlinkInfo>;
-  getLinkMetadata?: (link: string) => Promise<LinkMetaData>;
-  getMedia?: (props: {
-    mediaType: string;
-    searchTerm: string;
-    pagingNum: number;
-  }) => Promise<{
-    data: Resource[];
-    headers: Record<string, string>;
-  }>;
-  getMediaById?: (id: number) => Promise<Resource>;
-  getMediaForCorsCheck?: () => Promise<Resource[]>;
-  getMutedMediaById?: (id: number) => Promise<VideoResource>;
-  getOptimizedMediaById?: (id: number) => Promise<Resource>;
-  getPageTemplates?: () => Promise<Template[]>;
-  getPosterMediaById?: (id: number) => Promise<Resource>;
-  getProducts?: () => Promise<ProductData[]>;
-  getProxyUrl?: (src: string) => string;
-  getStoryById?: (id: number) => Promise<RawStory>;
-  getTaxonomies?: () => Promise<Taxonomy[]>;
-  getTaxonomyTerm?: (props: {
-    search?: string;
-    per_page?: number;
-  }) => Promise<Term>;
-  saveStoryById?: (data: StorySaveData) => Promise<StoryReturnData>;
-  updateCurrentUser?: (data: {
-    mediaOptimization?: boolean;
-    onboarding?: Record<string, boolean>;
-    trackingOptin?: boolean;
-  }) => Promise<User>;
-  updateMedia?: (id: number, data: Partial<Resource>) => Promise<Resource>;
-  updatePageTemplate?: (
-    id: number,
-    data: Partial<PageTemplate>
-  ) => Promise<PageTemplate>;
-  uploadMedia?: (files: string[], props: UploadMediaProps) => Promise<Resource>;
-}
 
 export interface ConfigState {
   /** Interval in seconds. */
