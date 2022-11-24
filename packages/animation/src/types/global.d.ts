40a9134b4fb00d99af43d6bdde7e77f586a3dac0
Morten Barklund <morten.barklund@xwp.co>
2022-11-24 15:19:00 +0100

    Trying to fix errors in d.ts. files
    Dunno why eslint complains, but maybe it is the missing header?

--- packages/animation/src/types/global.d.ts
+++ packages/animation/src/types/global.d.ts
@@ -1,14 +1,33 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import type { PropsWithChildren } from 'react';
+
 declare namespace JSX {
   interface IntrinsicElements {
-    'amp-story-animation': {
+    'amp-story-animation': PropsWithChildren<{
       layout: string;
       trigger: string;
-      children: React.ReactNode;
-    };
-    'amp-animation': {
+    }>;
+    'amp-animation': PropsWithChildren<{
       id: string;
       layout: string;
-      children: React.ReactNode;
-    };
+    }>;
   }
 }
