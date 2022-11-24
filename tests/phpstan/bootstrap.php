10d346efa4ed7cdad65cd160d6fcddfe86de7f0d
Jonny Harris <jon@spacedmonkey.co.uk>
2022-11-24 11:39:34 +0000

    Fix bootstrap.

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -2,12 +2,21 @@
 /*
  * Global constants used in the analysed code need to be defined in bootstrap files.
  */
-
-define( 'WEBSTORIES_VERSION', '0.0.0' );
-define( 'WEBSTORIES_DB_VERSION', '0.0.0' );
-define( 'WEBSTORIES_AMP_VERSION', '0.0.0' );
-define( 'WEBSTORIES_PLUGIN_FILE', dirname( __DIR__, 2 ) . '/web-stories.php' ); // phpcs:ignore PHPCompatibility.FunctionUse.NewFunctionParameters.dirname_levelsFound
-define( 'WEBSTORIES_PLUGIN_DIR_PATH', dirname( WEBSTORIES_PLUGIN_FILE ) );
+if ( ! defined( 'WEBSTORIES_VERSION' ) ) {
+	define( 'WEBSTORIES_VERSION', '0.0.0' );
+}
+if ( ! defined( 'WEBSTORIES_DB_VERSION' ) ) {
+	define( 'WEBSTORIES_DB_VERSION', '0.0.0' );
+}
+if ( ! defined( 'WEBSTORIES_AMP_VERSION' ) ) {
+	define( 'WEBSTORIES_AMP_VERSION', '0.0.0' );
+}
+if ( ! defined( 'WEBSTORIES_PLUGIN_FILE' ) ) {
+	define( 'WEBSTORIES_PLUGIN_FILE', dirname( __DIR__, 2 ) . '/web-stories.php' ); // phpcs:ignore PHPCompatibility.FunctionUse.NewFunctionParameters.dirname_levelsFound
+}
+if ( ! defined( 'WEBSTORIES_PLUGIN_DIR_PATH' ) ) {
+	define( 'WEBSTORIES_PLUGIN_DIR_PATH', dirname( WEBSTORIES_PLUGIN_FILE ) );
+}
 define( 'WEBSTORIES_PLUGIN_DIR_URL', 'https://example.com/wp-content/plugins/web-stories/' );
 define( 'WEBSTORIES_CDN_URL', 'https://wp.stories.google/static/main/' );
 define( 'WEBSTORIES_MINIMUM_PHP_VERSION', '5.6' );
--- tests/phpunit/unit/bootstrap.php
+++ tests/phpunit/unit/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types = 1 );
 
 /**
  * PHPUnit bootstrap file.
@@ -11,13 +11,21 @@ declare(strict_types = 1);
  * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
  */
 
-require_once __DIR__ . '/../../../vendor/yoast/wp-test-utils/src/BrainMonkey/bootstrap.php';
+if ( ! defined( 'ABSPATH' ) ) {
+	require_once __DIR__ . '/../../../vendor/yoast/wp-test-utils/src/BrainMonkey/bootstrap.php';
+}
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../../third-party/vendor/scoper-autoload.php';
 
-define( 'WEBSTORIES_VERSION', '1.0.0' );
-define( 'WEBSTORIES_DB_VERSION', '1.0.0' );
-define( 'WEBSTORIES_AMP_VERSION', '1.0.0' );
+if ( ! defined( 'WEBSTORIES_VERSION' ) ) {
+	define( 'WEBSTORIES_VERSION', '1.0.0' );
+}
+if ( ! defined( 'WEBSTORIES_DB_VERSION' ) ) {
+	define( 'WEBSTORIES_DB_VERSION', '1.0.0' );
+}
+if ( ! defined( 'WEBSTORIES_AMP_VERSION' ) ) {
+	define( 'WEBSTORIES_AMP_VERSION', '1.0.0' );
+}
 
 if ( ! defined( 'WEBSTORIES_PLUGIN_DIR_PATH' ) ) {
 	define( 'WEBSTORIES_PLUGIN_DIR_PATH', dirname( __DIR__, 3 ) );
