2498304ec03dac1a1aecced84806aab58cf587ba
Pascal Birchler <pascalb@google.com>
2022-11-25 15:25:30 +0100

    Try to adjust web-animations-js types

--- packages/animation/src/typings/web-animations-js.d.ts
+++ packages/animation/src/typings/web-animations-js.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2022 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,3 +15,5 @@
  */
 
 declare module 'web-animations-js/web-animations-next-lite.min.js';
+
+export {};
