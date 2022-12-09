e5c166c95b9cab23f88e2c1cd812ef3d8bc83baf
Jonny Harris <jon@spacedmonkey.co.uk>
2022-12-09 16:10:28 +0000

    Feedback 5.

--- packages/story-editor/src/utils/idleCallback.ts
+++ packages/story-editor/src/utils/idleCallback.ts
@@ -17,15 +17,14 @@
 /* Source: https://github.com/behnammodi/polyfill/blob/master/window.polyfill.js */
 const requestIdleCallback =
   window.requestIdleCallback ||
-  function (callback: IdleRequestCallback, options?: IdleRequestOptions) {
-    options = options || {};
+  function (callback: IdleRequestCallback, options: IdleRequestOptions = {}) {
     const relaxation = 1;
     const timeout = options.timeout || relaxation;
     const start = performance.now();
     return setTimeout(() => {
       callback({
         get didTimeout() {
-          return options?.timeout
+          return options.timeout
             ? false
             : performance.now() - start - relaxation > timeout;
         },
