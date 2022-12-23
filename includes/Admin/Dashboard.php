236709ecb35e5bc0e0088ebbfa1a4033d08f4d72
Miina Sikk <miina.sikk@gmail.com>
2022-12-23 10:34:19 -0300

    Lint

--- includes/Admin/Dashboard.php
+++ includes/Admin/Dashboard.php
@@ -365,7 +365,7 @@ class Dashboard extends Service_Base {
 								'link',
 								'preview_link',
 								'edit_link',
-								'_links', // Needed for WP 6.1+
+								'_links', // Needed for WP 6.1+.
 								'_embedded',
 								// _web_stories_envelope will add these fields, we need them too.
 								'body',
