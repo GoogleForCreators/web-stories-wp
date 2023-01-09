922d29ddffedf7d3d083eb631f013d69c054cd66
Miina Sikk <miina.sikk@gmail.com>
2023-01-09 16:39:30 -0300

    Fix

--- packages/story-editor/src/app/story/useStoryReducer/reducers/arrangeElement.ts
+++ packages/story-editor/src/app/story/useStoryReducer/reducers/arrangeElement.ts
@@ -63,11 +63,11 @@ export const arrangeElement = (
   draft: ReducerStateDraft,
   { elementId, position, groupId = false }: ArrangeElementProps
 ) => {
-  if (!elementId && draft.selection.length !== 1) {
+  if (draft.selection.length !== 1) {
     return;
   }
 
-  const idToArrange = elementId !== null ? elementId : draft.selection[0];
+  const idToArrange = elementId ? elementId : draft.selection[0];
 
   const page = draft.pages.find(({ id }) => id === draft.current);
 
