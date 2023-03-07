c6dcb19d8482ca568ab7987aeea0f3af129e87a9
Pascal Birchler <pascalb@google.com>
2023-03-07 12:26:47 +0100

    Update config

--- packages/e2e-tests/src/jest.config.js
+++ packages/e2e-tests/src/jest.config.js
@@ -31,7 +31,7 @@ process.env.WP_BASE_URL = WP_BASE_URL;
 export default {
   rootDir: '../../../',
   resolver: '@web-stories-wp/jest-resolver',
-  preset: '<rootDir>/packages/e2e-tests/node_modules/jest-puppeteer',
+  preset: '<rootDir>/node_modules/jest-puppeteer',
   testEnvironment: '<rootDir>/packages/e2e-tests/src/puppeteerEnvironment.js',
   testMatch: ['**/specs/**/*.[jt]s'],
   testPathIgnorePatterns: [
@@ -48,7 +48,7 @@ export default {
     '<rootDir>/packages/e2e-tests/src/config/bootstrap.js',
     '<rootDir>/packages/e2e-tests/node_modules/@wordpress/jest-puppeteer-axe',
     '<rootDir>/packages/e2e-tests/node_modules/@wordpress/jest-console',
-    '<rootDir>/packages/e2e-tests/node_modules/expect-puppeteer',
+    '<rootDir>/node_modules/expect-puppeteer',
   ],
   modulePathIgnorePatterns: ['<rootDir>/build', '<rootDir>/vendor'],
 };
