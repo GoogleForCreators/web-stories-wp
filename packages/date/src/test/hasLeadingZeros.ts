52a52512c10943c61c84e267124b7f1de46aacc1
Anurag Vasanwala <75766877+AnuragVasanwala@users.noreply.github.com>
2023-08-09 17:16:07 +0530

    🧪 Add unit test for `hasLeadingZeros`

---
+++ packages/date/src/test/hasLeadingZeros.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Internal dependencies
+ */
+import hasLeadingZeros from '../hasLeadingZeros';
+import { updateSettings } from '../settings';
+
+describe('date/hasLeadingZeros', () => {
+  it('should default to true', () => {
+    expect(hasLeadingZeros()).toBeTrue();
+  });
+
+  it('should detect time format correctly', () => {
+    updateSettings({
+      timeFormat: 'h:i A',
+    });
+
+    const hasLeadingZeros1 = hasLeadingZeros();
+    expect(hasLeadingZeros1).toBeTrue();
+
+    updateSettings({
+      timeFormat: 'H:i A',
+    });
+
+    const hasLeadingZeros2 = hasLeadingZeros();
+    expect(hasLeadingZeros2).toBeTrue();
+
+    updateSettings({
+      timeFormat: 'g:i',
+    });
+
+    const hasLeadingZeros3 = hasLeadingZeros();
+    expect(hasLeadingZeros3).toBeFalse();
+
+    updateSettings({
+      timeFormat: 'G:i',
+    });
+
+    const hasLeadingZeros4 = hasLeadingZeros();
+    expect(hasLeadingZeros4).toBeFalse();
+  });
+});
