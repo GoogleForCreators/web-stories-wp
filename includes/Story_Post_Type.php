1a9733b9854a9f56cc0eea4dc728f00500cb5120
Pascal Birchler <pascalb@google.com>
2024-05-23 11:27:26 +0200

    Delay getting `web_stories_archive_page_id` option

--- includes/Story_Post_Type.php
+++ includes/Story_Post_Type.php
@@ -288,19 +288,17 @@ class Story_Post_Type extends Post_Type_Base implements HasRequirements, HasMeta
 	 */
 	public function get_has_archive() {
 		$archive_page_option    = $this->settings->get_setting( $this->settings::SETTING_NAME_ARCHIVE );
-		$custom_archive_page_id = (int) $this->settings->get_setting( $this->settings::SETTING_NAME_ARCHIVE_PAGE_ID );
 		$has_archive            = true;
 
 		if ( 'disabled' === $archive_page_option ) {
 			$has_archive = false;
-		} elseif (
-			'custom' === $archive_page_option &&
-			$custom_archive_page_id &&
-			'publish' === get_post_status( $custom_archive_page_id )
-		) {
-			$uri = get_page_uri( $custom_archive_page_id );
-			if ( $uri ) {
-				$has_archive = urldecode( $uri );
+		} elseif ( 'custom' === $archive_page_option ) {
+			$custom_archive_page_id = (int) $this->settings->get_setting( $this->settings::SETTING_NAME_ARCHIVE_PAGE_ID );
+			if ( $custom_archive_page_id && 'publish' === get_post_status( $custom_archive_page_id ) ) {
+				$uri = get_page_uri( $custom_archive_page_id );
+				if ( $uri ) {
+					$has_archive = urldecode( $uri );
+				}
 			}
 		}
 
