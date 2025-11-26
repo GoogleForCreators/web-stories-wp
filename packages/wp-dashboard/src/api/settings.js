760bd57f6be185f387c486a7a6edc7578c478c90
Pascal Birchler <pascalb@google.com>
2025-11-26 11:42:50 +0100

    Lint fix

--- packages/wp-dashboard/src/api/settings.js
+++ packages/wp-dashboard/src/api/settings.js
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * External dependencies
- */
-import { addsettingsArgs } from '@googleforcreators/url';
 
 /**
  * WordPress dependencies
